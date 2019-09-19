dac170a375e788969a1b466379c571abbf9d6ed3
tunisiano187 <fabian@bowlman.org>
2019-09-19 17:56:47 +0000

    First commit

---
+++ WMESTSData.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         WMESTSData
+// @namespace    https://gitlab.com/WMEScripts
+// @version      0.1
+// @description  Datas for the script WME-send-to-slack
+// @author       Tunisiano18
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+var _0xedc1=["\x33\x30\x2D\x64\x65\x6D\x61\x6E\x64\x65\x73\x5F\x77\x6D\x65\x5F\x61\x75\x74\x6F"];var channel=_0xedc1[0]
