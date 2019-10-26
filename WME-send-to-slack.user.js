f7ff79bd3f3b581c2392b5b165f2ed9e7c553d79
tunisiano187 <fabian@bowlman.org>
2019-10-26 15:48:34 +0000

    Update WME-send-to-slack.user.js

--- WME-send-to-slack.user.js
+++ WME-send-to-slack.user.js
@@ -5,7 +5,7 @@
 // @namespace       https://en.tipeee.com/Tunisiano18
 // @description     Script to send unlock/closures/Validations requests to slack
 // @description:fr  Ce script vous permettant d'envoyer vos demandes de délock/fermeture et de validation directement sur slack
-// @version         2019.10.26.01
+// @version         2019.10.26.02
 // @include 	    /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
 // @exclude         https://www.waze.com/user/*editor/*
 // @exclude         https://www.waze.com/*/user/*editor/*
