0b78de1b3ac274b3fbfcea3971cf167430b240e4
tunisiano187 <fabian@bowlman.org>
2020-05-22 21:10:19 +0200

    AU

--- automatic/fah/update.ps1
+++ automatic/fah/update.ps1
@@ -1,30 +1,42 @@
-$installer_url = 'https://download.foldingathome.org/releases/public/release/fah-installer/windows-10-32bit/'
-Write-host 'Check Folder'
-$version = ((Invoke-WebRequest -Uri $installer_url -UseBasicParsing).Links | Where-Object  {$_.href -match '^v\d+([.]\d+)?'} | ForEach-Object {($_.href -replace '[^.\d]', '')} | Measure-Object -Max).Maximum
-$installer = "$($installer_url)v$($version)/latest.exe"
-$working_dir = "."
-$install_fname = 'folding_installer.exe'
-Write-host 'Download'
-Invoke-WebRequest -Uri $installer -OutFile "$working_dir\$install_fname"
-Write-host 'Install'
-. $working_dir/$install_fname /S
-$version_file=$(tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex 'folding').DisplayVersion
-while($version_file.count -eq 0)
-{
-	$version_file=$(tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex 'folding').DisplayVersion
-	Write-host "Loop"
-	Start-Sleep -Seconds 5
+import-module au
+
+$releases = 'https://download.foldingathome.org/releases/public/release/fah-installer/windows-10-32bit/'
+
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
 }
-Write-host "Version : $version_file"
-$installer_url = "$($installer_url)v$($version)/fah-installer_$($version_file)_x86.exe"
-Write-host 'Check SHA1'
-$sha1=$(checksum "$working_dir\$install_fname" -t sha1)
-$sha1
 
-Write-host 'Updating files'
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
 
-tools/Replace-FileString.ps1 "~url~" $installer_url fah/tools/chocolateyInstall.ps1 -Overwrite
-tools/Replace-FileString.ps1 "~sha1~" $sha1 fah/tools/chocolateyInstall.ps1 -Overwrite
-tools/Replace-FileString.ps1 "7.6.9" $version_file fah/fah.nuspec -Overwrite
-tools/Replace-FileString.ps1 "7.6.9" $version_file .gitlab-ci.yml -Overwrite
-tools/Replace-FileString.ps1 "7.6.9" $version_file upload.ps1 -Overwrite
+function global:au_GetLatest {
+	Write-host 'Check Folder'
+	$version_folder = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '^v\d+([.]\d+)?'} | ForEach-Object {($_.href -replace '[^.\d]', '')} | Measure-Object -Max).Maximum
+	$installer = "$($releases)v$($version_folder)/latest.exe"
+	$working_dir = "."
+	$install_fname = 'folding_installer.exe'
+	Write-host 'Download'
+	Invoke-WebRequest -Uri $installer -OutFile "$working_dir\$install_fname"
+	Write-host 'Install'
+	. $working_dir/$install_fname /S
+	$version=Get-Version('folding')
+	Write-host "Version : $version"
+	$url32 = "$($releases)v$($version_folder)/fah-installer_$($version)_x86.exe"
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
