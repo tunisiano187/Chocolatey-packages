c45798f89c2dc697b5d4a9308453763f201ce0e8
tunisiano187 <fabian@bowlman.org>
2020-05-23 19:30:48 +0200

    var

--- automatic/ultracopier/tools/chocolateyInstall.ps1
+++ automatic/ultracopier/tools/chocolateyInstall.ps1
@@ -4,7 +4,8 @@ $packageName = 'ultracopier'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.1/ultracopier-windows-x86-2.2.4.1-setup.exe'
 $url64='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.1/ultracopier-windows-x86_64-2.2.4.1-setup.exe'
-
+$checksum32=''
+$checksum64=''
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,9 +15,9 @@ $packageArgs = @{
 
   softwareName  = 'ultracopier*'
 
-  checksum      = '58436D58224F2EC4F6A2DFF12F8771894000F9FAAAEBC6143755F426CB94B55B'
+  checksum      = $checksum32
   checksumType  = 'sha256'
-  checksum64      = 'D3A50BFD0FF22A3A4A4BA319C44E4958B9C50CA3B49FF2093B58C60A4C4B7E66'
+  checksum64      = $checksum64
   checksumType64  = 'sha256'
 
   
