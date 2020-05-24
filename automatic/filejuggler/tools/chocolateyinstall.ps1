099f155ba795ea2001c891ae11c9d99fd7c3974e
tunisiano187 <fabian@bowlman.org>
2020-05-24 15:49:00 +0200

    checksumtype

--- automatic/filejuggler/tools/chocolateyinstall.ps1
+++ automatic/filejuggler/tools/chocolateyinstall.ps1
@@ -5,4 +5,4 @@ $url        = 'https://www.filejuggler.com/download/filejuggler.exe'
 $checksum   = 'C0ACFE3DCB51ABD982A4AA4CF5369AB6'
 $checksumtype	= 'sha256'
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -checksum $checksum -ValidExitCodes @(0,1223)
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -checksum $checksum -ValidExitCodes @(0,1223) -ChecksumType $checksumtype
