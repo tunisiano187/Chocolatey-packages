9eab07b81efc49984669eb3e4c94f043c2e4390e
tunisiano187 <fabian@bowlman.org>
2020-05-25 19:35:04 +0200

    boinc

--- automatic/boinc/tools/chocolateyInstall.ps1
+++ automatic/boinc/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $installerType = 'exe'
 $silentArgs = '/S /v/qn'
 $url = '~url~'
 $checksum = '~sha1~'
-$checksumType = 'sha1'
+$checksumType = 'sha256'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
