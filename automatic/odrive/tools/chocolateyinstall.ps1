46f618bfbeb3dd56b4da7fb5779161afa6b7a9d9
tunisiano187 <fabian@bowlman.org>
2020-05-28 14:01:03 +0200

    odrive

---
+++ automatic/odrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageArgs = @{
+  packageName   = 'odrive'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = 'https://www.odrive.com/downloaddesktop?platform=win'
+  silentArgs    = '/quiet'
+  validExitCodes= @(0,3010)
+  softwareName  = 'odrive*'
+  checksum      = '080B6EBD59F2ECE205BE811AF6BD0A74634BFE03'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
