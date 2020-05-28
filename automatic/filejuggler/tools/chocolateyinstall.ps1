422fd4cab12d68ee7c875a2cede2d97029980298
tunisiano187 <fabian@bowlman.org>
2020-05-28 17:58:20 +0200

    /s

--- automatic/filejuggler/tools/chocolateyinstall.ps1
+++ automatic/filejuggler/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $packageName= 'FileJuggler'
 $installerType      = 'msi'
-$silentArgs = '/qb'
+$silentArgs = '/s'
 $url        = 'https://www.filejuggler.com/download/filejuggler.exe'
 $checksum   = 'C0ACFE3DCB51ABD982A4AA4CF5369AB6'
 $checksumtype	= 'sha256'
--- automatic/filejuggler/update.ps1
+++ automatic/filejuggler/update.ps1
@@ -28,7 +28,7 @@ function global:au_GetLatest {
 	Write-host 'Download'
 	Invoke-WebRequest -Uri $installer -OutFile "$working_dir\$install_fname"
 	Write-host 'Install'
-	. $working_dir/$install_fname '/qb'
+	. $working_dir/$install_fname /s
 	$version=Get-Version('Juggler')
 	Write-host "Version : $version"
 	$url32 = $release
