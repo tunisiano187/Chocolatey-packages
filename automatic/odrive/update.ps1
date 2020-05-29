d8d3389520c901b09d18c9b75f16fd2cf5a2ee68
tunisiano187 <fabian@bowlman.org>
2020-05-29 09:07:21 +0200

    odrive

--- automatic/odrive/update.ps1
+++ automatic/odrive/update.ps1
@@ -1,3 +1,38 @@
 import-module au
 
-update
+$url32 = 'https://www.odrive.com/downloaddesktop?platform=win'
+
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion | select-object -first 1
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion | select-object -first 1
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
+}
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$working_dir = "../../"
+	$install_fname = 'odrive.exe'
+	Write-host 'Download'
+	Invoke-WebRequest -Uri $url32 -OutFile "$working_dir\$install_fname"
+	Write-host 'Install'
+	. $working_dir/$install_fname /quiet
+	$version=Get-Version('^odrive$')
+	Write-host "Version : $version"
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
