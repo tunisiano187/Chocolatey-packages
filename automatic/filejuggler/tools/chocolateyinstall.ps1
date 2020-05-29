1e80aac52e12d7bc5c026354abf6de13e6bc92a4
tunisiano187 <fabian@bowlman.org>
2020-05-29 08:04:12 +0200

    No File Juggler as it don't work

--- automatic/filejuggler/tools/chocolateyinstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageName= 'FileJuggler'
-$installerType      = 'msi'
-$silentArgs = '/s'
-$url        = 'https://www.filejuggler.com/download/filejuggler.exe'
-$checksum   = 'C0ACFE3DCB51ABD982A4AA4CF5369AB6'
-$checksumtype	= 'sha256'
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -checksum $checksum -ValidExitCodes @(0,1223) -ChecksumType $checksumtype
