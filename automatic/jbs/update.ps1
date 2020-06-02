2a39c20da982c0df8c166d0a3f1b63e99b02f90e
tunisiano187 <fabian@bowlman.org>
2020-06-02 21:07:27 +0200

    (

--- automatic/jbs/update.ps1
+++ automatic/jbs/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	$jbs = (Invoke-WebRequest -Uri $releases)
-	$url32 = (($jbs.Links | Where-Object {$_ -match 'exe'})[0].href
+	$url32 = ($jbs.Links | Where-Object {$_ -match 'exe'})[0].href
 	$version = ($jbs.Links | Where-Object {$_.innerHTML -match 'Version'})[0].innerHTML.split(' ')[1]
 	
 	$Latest = @{ URL32 = $url32; Version = $version }
