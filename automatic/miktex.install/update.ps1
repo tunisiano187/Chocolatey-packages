62f410afa0a57827f5fcc8ed24f23270174d0b72
tunisiano187 <fabian@bowlman.org>
2020-06-03 19:56:36 +0200

    64 bit link

--- automatic/miktex.install/update.ps1
+++ automatic/miktex.install/update.ps1
@@ -1,6 +1,7 @@
 import-module au
 
-$releases = 'http://mirrors.ctan.org/systems/win32/miktex/setup/windows-x86/'
+$release32 = 'http://mirrors.ctan.org/systems/win32/miktex/setup/windows-x86/'
+$release64 = 'http://mirrors.ctan.org/systems/win32/miktex/setup/windows-x64/'
 
 function global:au_SearchReplace {
 	@{
@@ -14,9 +15,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$filename32 = ((Invoke-WebRequest -Uri $releases).Links | Where-Object {$_ -match 'basic-'}  | Where-Object {$_ -match 'exe'}).href
+	$filename32 = ((Invoke-WebRequest -Uri $release32).Links | Where-Object {$_ -match 'basic-'}  | Where-Object {$_ -match 'exe'}).href
 	$url32 = Join-Path $releases $filename32
-	$url64 = $url32.replace('x86','X64');
+	$filename64 = ((Invoke-WebRequest -Uri $release64).Links | Where-Object {$_ -match 'basic-'}  | Where-Object {$_ -match 'exe'}).href
+	$url64 = Join-Path $releases $filename64[-1]
 	$version=$filename32.split('-')[2].replace('.exe','')
 	
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
