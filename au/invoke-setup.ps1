a0bfea9d272705c7e59219d0c64b79af441fbb59
tunisiano187 <fabian@bowlman.org>
2020-06-03 06:44:05 +0200

    jbs version by installation

--- au/invoke-setup.ps1
+++ au/invoke-setup.ps1
@@ -1,20 +1,8 @@
-Add-WindowsCapability -Online -Name ServerCore.AppCompatibility~~~~0.0.1.0;
-Enable-WindowsOptionalFeature –FeatureName "Internet-Explorer-Optional-amd64" -All -Online;
-$ok='no';
-while ($ok -eq 'no')
+try
 {
-	$ok='yes';
-	$tests=0;
-	try
-	{
-		$download_page = Invoke-WebRequest -Uri "https://github.com/htacg/tidy-html5/releases" -ErrorAction SilentlyContinue;
-	}
-	catch
-	{
-		Write-Host -fore Red $_;
-		$ok='no';
-		start-sleep -Seconds 1;
-		$test++;
-		if($test -eq 5){exit;}
-	}
+    $download_page = Invoke-WebRequest -Uri "https://github.com/htacg/tidy-html5/releases" -ErrorAction SilentlyContinue
+}
+catch
+{
+	 Write-Host -fore Red $_
 }
--- automatic/jbs/update.ps1
+++ automatic/jbs/update.ps1
@@ -2,6 +2,16 @@ import-module au
 
 $releases = 'https://johnsad.ventures/software/backgroundswitcher/windows/'
 
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
+}
+
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
@@ -14,7 +24,14 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$jbs = (Invoke-WebRequest -Uri $releases)
 	$url32 = ($jbs.Links | Where-Object {$_ -match 'exe'})[0].href
-	$version = ($jbs.Links | Where-Object {$_.innerHTML -match 'Version'})[0].innerHTML.split(' ')[1]
+	
+	$working_dir = "."
+	$install_fname = 'jbs.exe'
+	Write-host 'Download'
+	Invoke-WebRequest -Uri $url32 -OutFile "$working_dir\$install_fname"
+	Write-host 'Install'
+	. $working_dir/$install_fname /S
+	$version=Get-Version('switcher')
 	
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
