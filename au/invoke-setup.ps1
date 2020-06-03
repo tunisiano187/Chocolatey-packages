e343a7be236f8bb8b887310d8b55e3559c0e8db9
tunisiano187 <fabian@bowlman.org>
2020-06-03 06:32:29 +0200

    ;

--- au/invoke-setup.ps1
+++ au/invoke-setup.ps1
@@ -1,19 +1,19 @@
-Add-WindowsCapability -Online -Name ServerCore.AppCompatibility~~~~0.0.1.0
-Enable-WindowsOptionalFeature –FeatureName "Internet-Explorer-Optional-amd64" -All -Online
+Add-WindowsCapability -Online -Name ServerCore.AppCompatibility~~~~0.0.1.0;
+Enable-WindowsOptionalFeature –FeatureName "Internet-Explorer-Optional-amd64" -All -Online;
 $ok='no';
 while ($ok -eq 'no')
 {
-	$ok='yes'
+	$ok='yes';
 	$tests=0;
 	try
 	{
-		$download_page = Invoke-WebRequest -Uri "https://github.com/htacg/tidy-html5/releases" -ErrorAction SilentlyContinue
+		$download_page = Invoke-WebRequest -Uri "https://github.com/htacg/tidy-html5/releases" -ErrorAction SilentlyContinue;
 	}
 	catch
 	{
-		Write-Host -fore Red $_
-		$ok='no'
-		start-sleep -Seconds 1
+		Write-Host -fore Red $_;
+		$ok='no';
+		start-sleep -Seconds 1;
 		$test++;
 		if($test -eq 5){exit;}
 	}
