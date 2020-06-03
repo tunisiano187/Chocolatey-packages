b0758100d90dcd857a75aed62a0a61825a2c4715
tunisiano187 <fabian@bowlman.org>
2020-06-03 20:19:24 +0200

    vars

--- automatic/miktex.install/update.ps1
+++ automatic/miktex.install/update.ps1
@@ -16,9 +16,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	$filename32 = ((Invoke-WebRequest -Uri $release32 -UseBasicParsing).Links | Where-Object {$_ -match 'basic-'}  | Where-Object {$_ -match 'exe'}).href
-	$url32 = Join-Path $releases $filename32
+	$url32 = Join-Path $release32 $filename32
 	$filename64 = ((Invoke-WebRequest -Uri $release64 -UseBasicParsing).Links | Where-Object {$_ -match 'basic-'}  | Where-Object {$_ -match 'exe'}).href
-	$url64 = Join-Path $releases $filename64[-1]
+	$url64 = Join-Path $release64 $filename64[-1]
 	$version=$filename32.split('-')[2].replace('.exe','')
 	
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
