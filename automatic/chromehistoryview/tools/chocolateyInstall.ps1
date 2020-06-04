6df93045a2811ab107d18209876b5142f5f5b7b3
tunisiano187 <fabian@bowlman.org>
2020-06-04 18:38:40 +0200

    chromehistoryview

---
+++ automatic/chromehistoryview/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'chromehistoryview'
+$url = 'http://www.nirsoft.net/utils/chromehistoryview.zip'
+$checksum = 'a2161a23cdcc92293301402b267e60ff638f83e129a2ad3c1684609c0323d2e6'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
---
+++ automatic/chromehistoryview/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$url32 = 'http://www.nirsoft.net/utils/chromehistoryview.zip'
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$File = "./chromehistoryview.zip"
+	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
+	Expand-Archive $File -DestinationPath .\chv
+	
+	$version=$(Get-Content .\chv\readme.txt | Where-Object {$_ -match ' Version'})[0].split(' ')[2]
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
