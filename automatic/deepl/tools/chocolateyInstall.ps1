0d343df0eafa60c0ae463f1fd68fe6a384418d43
tunisiano187 <fabian@bowlman.org>
2020-06-05 15:40:07 +0200

    DeepL

---
+++ automatic/deepl/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$packageName = 'deepl'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url64 = 'https://appdownload.deepl.com/windows/full/DeepLSetup.exe'
+$checksum64 = '1498BFCA4361B90D9F7F0345A5E0981B257270CC2D52761B4D3464FE22F8C6F4'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Invoke-WebRequest -Uri $url64 -OutFile ".\DeepLSetup.exe"
+
+$url64 = ".\DeepLSetup.exe"
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/deepl/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+
+$release = 'https://appdownload.deepl.com/windows/full/DeepLSetup.exe'
+
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
+}
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$working_dir = "."
+	$install_fname = 'deepl.exe'
+	Write-host 'Download'
+	Invoke-WebRequest -Uri $release -OutFile "$working_dir\$install_fname"
+	. $working_dir/$install_fname /S
+	$version=Get-Version('DeepL')
+	Write-host "Version : $version"
+	
+	$Latest = @{ URL32 = $release; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
