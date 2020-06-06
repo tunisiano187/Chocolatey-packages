494b466f1cd5c41709ec8e6662c8ed065af24dc2
tunisiano187 <fabian@bowlman.org>
2020-06-06 08:58:14 +0200

    Set-DefaultBrowser

--- automatic/deepl/update.ps1
+++ automatic/deepl/update.ps1
@@ -2,6 +2,20 @@ import-module au
 
 $release = 'https://appdownload.deepl.com/windows/full/DeepLSetup.exe'
 
+function Set-DefaultBrowser
+{
+    cup -y firefox
+	
+	$regKey      = "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\{0}\UserChoice"
+    $regKeyFtp   = $regKey -f 'ftp'
+    $regKeyHttp  = $regKey -f 'http'
+    $regKeyHttps = $regKey -f 'https'
+
+	Set-ItemProperty $regKeyFtp   -name ProgId FirefoxURL
+	Set-ItemProperty $regKeyHttp  -name ProgId FirefoxURL
+	Set-ItemProperty $regKeyHttps -name ProgId FirefoxURL
+} 
+
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
@@ -12,6 +26,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
+	Set-DefaultBrowser
 	cinst -y deepl --ignore-checksums
 	$version=$(ls "$($env:LOCALAPPDATA)\deepl\app*" -Directory).Name.split("-")[1]
 	
