813abcd867ca2ff9eae9b1f6370db67d0a49534b
tunisiano187 <fabian@bowlman.org>
2020-06-06 22:37:29 +0200

    FileJuggler

---
+++ automatic/filejuggler/tools/chocolateyInstall.ahk
@@ -0,0 +1,8 @@
+WinWait, File Juggler Setup, 
+
+IfWinNotActive, File Juggler Setup, , WinActivate, File Juggler Setup, 
+WinWaitActive, File Juggler Setup, 
+Send, {ALTDOWN}n{ALTUP}{ALTDOWN}a{ALTUP}{ALTDOWN}n{ALTUP}{ALTDOWN}n{ALTUP}{ALTDOWN}i{ALTUP}
+Sleep,20000
+Send, {TAB}
+Send, {ALTDOWN}f{ALTUP}
---
+++ automatic/filejuggler/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+
+$release = 'https://www.filejuggler.com/download/filejuggler.exe'
+
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
+}
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	Write-host 'Check Folder'
+	$working_dir = "."
+	$install_fname = 'Filejuggler.exe'
+	Write-host 'Download'
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "$install_fname"
+	Invoke-WebRequest -Uri $release -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+	Write-host "Version : $version"
+	$url32 = $release
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
