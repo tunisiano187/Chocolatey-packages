db56468f79d62fd1125e3849374d9167a21ab3bf
tunisiano187 <fabian@bowlman.org>
2020-06-06 09:44:49 +0200

    other way

--- au/invoke-setup.ps1
+++ au/invoke-setup.ps1
@@ -1,14 +1,6 @@
-function Set-DefaultBrowser
-{
-	$regKey      = "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\{0}\UserChoice"
-    $regKeyFtp   = $regKey -f 'ftp'
-    $regKeyHttp  = $regKey -f 'http'
-    $regKeyHttps = $regKey -f 'https'
-
-	Set-ItemProperty $regKeyFtp   -name ProgId ChromeHTML
-	Set-ItemProperty $regKeyHttp  -name ProgId ChromeHTML
-	Set-ItemProperty $regKeyHttps -name ProgId ChromeHTML
-}
-
 cup -y googlechrome
-Set-DefaultBrowser
+
+$chromePath = "${Env:ProgramFiles(x86)}\Google\Chrome\Application\" 
+$chromeApp = "chrome.exe"
+$chromeCommandArgs = "--make-default-browser"
+& "$chromePath$chromeApp" $chromeCommandArgs
