3b2b4628ce1a5aac863a74524ee90e6e282b8e72
tunisiano187 <fabian@bowlman.org>
2020-06-06 10:23:47 +0200

    no Chrome needed

--- au/invoke-setup.ps1
+++
@@ -1,6 +0,0 @@
-cup -y googlechrome
-
-$chromePath = "${Env:ProgramFiles}\Google\Chrome\Application\" 
-$chromeApp = "chrome.exe"
-$chromeCommandArgs = "--make-default-browser"
-& "$chromePath$chromeApp" $chromeCommandArgs
