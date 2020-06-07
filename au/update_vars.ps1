661131f8360bcd04831b35185aa788ee3fea1aef
tunisiano187 <fabian@bowlman.org>
2020-06-07 15:57:45 +0200

    upload to Github

--- au/update_vars.ps1
+++ au/update_vars.ps1
@@ -9,11 +9,11 @@ $Env:mail_enablessl   = 'true'
 
 $Env:api_key          = $choco_api          #Chocolatey api key
 $Env:gist_id          = $Gist_id          #Specify your gist id or leave empty for anonymous gist
-#$Env:github_user_repo = $Github_user_repo          #{github_user>/{repo}
+$Env:github_user_repo = $Github_user_repo          #{github_user>/{repo}
 $Env:github_api_key   = $Github_personal_token          #Github personal access token
 $Env:au_Push          = 'true'     #Push to chocolatey
 
-$Env:gitlab_user            = $Gitlab_user   # GitLab username to use for the push
-$Env:gitlab_api_key         = $Gitlab_api   # GitLab API key associated with gitlab_user
-$Env:gitlab_push_url        = $Gitlab_PushURL   # GitLab URL to push to. Must be HTTP or HTTPS. e.g. https://jekotia:MyPassword@git.example.org/jekotia/au.git
-$Env:gitlab_commit_strategy = 'atomic'   # Same values as the Git plugin; single, atomic, or atomictag
+#$Env:gitlab_user            = $Gitlab_user   # GitLab username to use for the push
+#$Env:gitlab_api_key         = $Gitlab_api   # GitLab API key associated with gitlab_user
+#$Env:gitlab_push_url        = $Gitlab_PushURL   # GitLab URL to push to. Must be HTTP or HTTPS. e.g. https://jekotia:MyPassword@git.example.org/jekotia/au.git
+#$Env:gitlab_commit_strategy = 'atomic'   # Same values as the Git plugin; single, atomic, or atomictag
