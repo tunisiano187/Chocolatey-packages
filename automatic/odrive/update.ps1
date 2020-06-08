79df46746385c5937d826cc095b43447f91752a2
tunisiano187 <fabian@bowlman.org>
2020-06-08 19:32:26 +0200

    limit to 1.x

--- automatic/odrive/update.ps1
+++ automatic/odrive/update.ps1
@@ -27,9 +27,12 @@ function global:au_GetLatest {
 	Invoke-WebRequest -Uri $url32 -OutFile $File
 	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
 	$version=$version.replace('.00.','.0.')
-	if($version.Split('.')[1] -lt $compare)
+	if($version.Split('.')[0] -eq "1")
 	{
-		$version=$version.replace(".$($version.Split('.')[1]).",".$($compare).$($version.Split('.')[1].")
+		if($version.Split('.')[1] -lt $compare)
+		{
+			$version=$version.replace(".$($version.Split('.')[1]).",".$($compare).$($version.Split('.')[1].")
+		}
 	}
 	
 	$Latest = @{ URL32 = $url32; Version = $version }
