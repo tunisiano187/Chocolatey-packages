72c1bb73690e2c0b97b834a139bf7caa6a98f1f7
tunisiano187 <fabian@bowlman.org>
2020-06-08 09:07:07 +0200

    _

--- automatic/boinc/update.ps1
+++ automatic/boinc/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	$installer = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'windows'} | Where-Object {$_ -match 'windows_x86_64.exe'} | Where-Object {$_ -match 'boinc_'}).href)[-1]
-	$version=$installer.split('-')[1]
+	$version=$installer.split('_')[1]
 	Write-host "Version : $version"
 	$url32 = "$($releases)$($installer)"
 	
