f8c37fe220564e5864560969f053b84cc21bed27
tunisiano187 <fabian@bowlman.org>
2020-06-10 21:41:33 +0200

    Remove success and fail

--- automatic/dexpot/tools/chocolateyInstall.ps1
+++ automatic/dexpot/tools/chocolateyInstall.ps1
@@ -8,8 +8,6 @@ $validExitCodes = @(0)
 
 try {
 	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
-	Write-ChocolateySuccess $packageName
 } catch {
-	Write-ChocolateyFailure $packageName $($_.Exception.Message)
 	throw	
 }
