12d236878796b3fa29ba5e88b4327b9b62680687
Tunisiano187 <fabian@bowlman.org>
2020-06-17 19:08:11 +0200

    xolidosigndesktop

---
+++ automatic/xolidosigndesktop/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'xolidosigndesktop'
+$installerType = 'EXE'
+$url32 = ''
+$checksumtype = 'sha256'
+$checksum32 = ''
+
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url32
+  silentArgs    = $silentArgs
+  validExitCodes= @(0,3010)
+  softwareName  = 'xolido*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/xolidosigndesktop/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+$url32 = "http://www.xolido.com/instaladores/SetupXolidoSign.exe"
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "SetupXolidoSign.exe"
+	Invoke-WebRequest -Uri $url32 -OutFile $File
+	$version=$([System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion).trim()
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
