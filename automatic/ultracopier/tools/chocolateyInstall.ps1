17141d4cf99492d43a310289223b919714505801
tunisiano187 <fabian@bowlman.org>
2020-06-18 17:46:52 +0000

    AU: ultracopier upgraded from 2.2.4.1 to 2.2.4.3 [skip ci] https://gist.github.com/5c6fb24629843bbb549c5f495dd7f7b8/74de1b16f4f480ad55fa16c4f0dad56998437ba8

--- automatic/ultracopier/tools/chocolateyInstall.ps1
+++ automatic/ultracopier/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'ultracopier'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.1/ultracopier-windows-x86-2.2.4.1-setup.exe'
-$url64='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.1/ultracopier-windows-x86_64-2.2.4.1-setup.exe'
-$checksum32=''
-$checksum64=''
+$url32='http://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.3/ultracopier-windows-x86-2.2.4.3-setup.exe'
+$url64='http://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.3/ultracopier-windows-x86_64-2.2.4.3-setup.exe'
+$checksum32='e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+$checksum64='c34f04a8f44be05d39ebb0aad096500f5a8730695add604e6bf9421ae1529319'
 
 $packageArgs = @{
   packageName   = $packageName
