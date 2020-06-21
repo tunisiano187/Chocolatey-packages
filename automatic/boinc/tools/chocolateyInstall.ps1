5a2bef30a12ea342143a4cb69de3fed981a33b70
Tunisiano187 <fabian@bowlman.org>
2020-06-21 22:19:35 +0200

    Latest version

--- automatic/boinc/tools/chocolateyInstall.ps1
+++ automatic/boinc/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-$packageName = 'boinc'
+﻿$packageName = 'boinc'
 $installerType = 'exe'
 $silentArgs = '/S /v/qn'
-$url = '~url~'
-$checksum = '~sha1~'
+$url = 'https://boinc.berkeley.edu/dl/boinc_7.16.7_windows_x86_64.exe'
+$checksum = '6780ff6b7bf4003f4257b8e02690ac7e1cd0c0f11d6aa4a80823affc4453d658'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
