678124b8d127137d1f2b0ace5bb2816218ef8ce1
Tunisiano187 <fabian@bowlman.org>
2020-06-22 16:58:45 +0200

    Sunet

---
+++ automatic/sunet/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'sunett'
+$installerType = 'exe'
+$silentArgs = '/S /v/qn'
+$url = 'https://boinc.berkeley.edu/dl/boinc_7.16.7_windows_x86_64.exe'
+$checksum = '6780ff6b7bf4003f4257b8e02690ac7e1cd0c0f11d6aa4a80823affc4453d658'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ automatic/sunet/update.ps1
@@ -0,0 +1,41 @@
+import-module au
+
+$release = 'https://boinc-client.oss-accelerate.aliyuncs.com/SUNET%20Setup.exe'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$url32 = $release
+	
+	$File = './sunet.exe'
+
+	Write-host "Downloading"
+	Invoke-WebRequest -Uri $url32 -OutFile $File
+	
+	Write-host "Installation"
+	. $File /S
+	
+	$sunet=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex 'sunet')
+    while($sunet.count -eq 0)
+    {
+        $sunet=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex 'sunet')
+    }
+	$version=$sunet.DisplayVersion
+	if($version -eq "0.0.0") {
+		$versiondate = Get-Date $([Net.HttpWebRequest]::Create($url32).GetResponse().LastModified) -Format "yyyMMdd"
+		$versiondate = Get-Date $versiondate -Format "yyyMMdd"
+		$version = "0.0.0.$versiondate"
+	}
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
