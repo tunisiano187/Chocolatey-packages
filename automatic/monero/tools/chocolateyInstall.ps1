7e6169f94c9076423b72e8f172e3e7fb1e4f1fb6
tunisiano187 <fabian@bowlman.org>
2020-06-23 12:51:06 +0000

    [skip ci] commit from CI runner

--- automatic/monero/tools/chocolateyInstall.ps1
+++ automatic/monero/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$packageName = 'monero'
-$url = ''
-$checksum = ''
-$checksumType = ''
-$url64 = '{{DownloadUrlx64}}'
-$checksum64 = '{{Checksum}}'
-$checksumType64 = 'sha1'
+﻿$packageName = 'monero'
+$url = 'https://downloads.getmonero.org/cli/monero-win-x86-v0.16.0.0.zip'
+$checksum = 'b1fc293dd9c12f90c7c343777298e86a22f25788561629b92ec0b23eb6cfb42c'
+$checksumType = 'sha256'
+$url64 = 'https://downloads.getmonero.org/cli/monero-win-x64-v0.16.0.0.zip'
+$checksum64 = 'f2750efb65b70ae9cad64d977cb0bfbe4dc6d014fe0b7fd565002282d67d469d'
+$checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
