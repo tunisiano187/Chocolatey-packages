1d6c4c7eb8e986c95bbeb6146cd66f557cbe3d74
tunisiano187 <fabian@bowlman.org>
2020-06-23 12:50:29 +0000

    AU: foldit upgraded from 0.0.20160904 to 0.0.20190903 [skip ci] https://gist.github.com/5c6fb24629843bbb549c5f495dd7f7b8/fbbc319d1f16b07bc6ec64b0748602f4e3c561dc

--- automatic/foldit/tools/chocolateyInstall.ps1
+++ automatic/foldit/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'foldit'
 $installerType = 'exe'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$url = 'http://fold.it/portal/files/app/Foldit-win_x86.exe'
+$checksum = 'f92e30c8dc0c1c7502940206d095172fba9676104a16693595266835cd798456'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
