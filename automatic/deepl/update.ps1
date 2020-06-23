0bf4ae7db081cdd89fdb7644e837ce05ad9dc6c4
Tunisiano187 <fabian@bowlman.org>
2020-06-23 02:52:02 +0200

    [skip ci] No force

--- automatic/deepl/update.ps1
+++ automatic/deepl/update.ps1
@@ -15,11 +15,9 @@ function global:au_GetLatest {
 	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "DeepLSetup.exe"
 	Invoke-WebRequest -Uri $release -OutFile $File
 	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
-	if($version -eq '1.11.0') {
-		$version = '1.11.0.1'
-	}
+	
 	$Latest = @{ URL32 = $release; Version = $version }
 	return $Latest
 }
 
-update -ChecksumFor 32 -NoCheckChocoVersion
+update -ChecksumFor 32
