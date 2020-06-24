0caddfd7affdc4c3b73e8aed9c9ecd35ad20c5e1
Tunisiano187 <fabian@bowlman.org>
2020-06-24 22:15:07 +0200

    Test path

--- au/update_vars.ps1
+++ au/update_vars.ps1
@@ -10,7 +10,9 @@ $Env:mail_enablessl   = 'true'
 $Env:api_key          = $choco_api          #Chocolatey api key
 $Env:gist_id          = $Gist_id          #Specify your gist id or leave empty for anonymous gist
 $Env:github_user_repo = $Github_user_repo          #{github_user>/{repo}
-$Env:github_api_key   = $Github_personal_token          #Github personal access token
+if(!(Test-Path Env:github_api_key)) {
+    $Env:github_api_key   = $Github_personal_token          #Github personal access token
+}
 $Env:au_Push          = 'true'     #Push to chocolatey
 
 $Env:gitlab_user            = $GITLAB_USER_LOGIN   # GitLab username to use for the push
