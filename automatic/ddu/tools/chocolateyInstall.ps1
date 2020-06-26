44e25c36424d13ac2031c829298f921e1b0edf1e
tunisiano187 <fabian@bowlman.org>
2020-06-26 19:57:59 +0000

    AU: ddu upgraded from 0.0 to 18.0.2.5 [skip ci] https://gist.github.com/5c6fb24629843bbb549c5f495dd7f7b8/20bf6d3a660c50b5f713dfcca25e877ae2bbd3d9

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'ddu'
 $url = 'https://www.wagnardsoft.com/DDU/download/DDU%20v18.0.2.5.exe'
-$checksum = ''
-$checksumType = ''
+$checksum = 'd99148f66e5ab91c3cabcde6d8468d9a29cd6ef5f66fd5aaa307e036d9a51567'
+$checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
