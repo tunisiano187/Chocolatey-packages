b3d758827a27f1811ef7388f8f35c014e59d34f4
Tunisiano187 <fabian@bowlman.org>
2020-06-27 22:21:28 +0200

    Release notes as thinked in the issue #4

---
+++ scripts/checknuspec.ps1
@@ -0,0 +1,20 @@
+$global:au_Root = "$PSScriptRoot/../automatic"                           #Path to the AU packages
+
+$nuspecs = Get-ChildItem -Path $global:au_Root -Filter *.nuspec -Recurse
+
+foreach ($nuspec in $nuspecs) {
+    [xml]$destspec = Get-Content $nuspec.FullName
+    if(!($destspec.package.metadata.releaseNotes) -or ($destspec.package.metadata.releaseNotes -eq ""))
+    {
+        $FileContent = Get-Content $nuspec.FullName;
+        $NewFileContent = @()
+        for ($i = 0; $i -lt $FileContent.Length; $i++) {
+            if ($FileContent[$i] -like "*\/metadata*") {
+                $NewFileContent += "#### Package
+                * Automatically built and uploaded by [tunisiano](https://chocolatey.org/profiles/tunisiano)"
+            }
+            $NewFileContent += $FileContent[$i].Replace('{{PackageName}}', $($destspec.package.metadata.title).Replace(' ', "-"))
+        }
+        $NewFileContent | Out-File $nuspec.FullName
+    }
+}
