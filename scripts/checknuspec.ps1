4465d12a593271f94f9679cfe7020bfc24fce0b3
Tunisiano187 <fabian@bowlman.org>
2020-06-27 22:41:15 +0200

    Test

--- scripts/checknuspec.ps1
+++ scripts/checknuspec.ps1
@@ -4,17 +4,21 @@ $nuspecs = Get-ChildItem -Path $global:au_Root -Filter *.nuspec -Recurse
 
 foreach ($nuspec in $nuspecs) {
     [xml]$destspec = Get-Content $nuspec.FullName
-    if(!($destspec.package.metadata.releaseNotes) -or ($destspec.package.metadata.releaseNotes -eq ""))
+    if(!($destspec.package.metadata.releaseNotes))
     {
         $FileContent = Get-Content $nuspec.FullName;
         $NewFileContent = @()
         for ($i = 0; $i -lt $FileContent.Length; $i++) {
             if ($FileContent[$i] -like "*\/metadata*") {
-                $NewFileContent += '<releaseNotes>#### Package
-                * Automatically built and uploaded by [tunisiano](https://chocolatey.org/profiles/tunisiano)</releaseNotes>'
+                $NewFileContent += "#### Package
+                * Automatically built and uploaded by [tunisiano](https://chocolatey.org/profiles/tunisiano)"
             }
             $NewFileContent += $FileContent[$i].Replace('{{PackageName}}', $($destspec.package.metadata.title).Replace(' ', "-"))
         }
-        $NewFileContent | Out-File $nuspec.FullName
+        $NewFileContent | Out-File $($nuspec.FullName)
+    } elseif ($destspec.package.metadata.releaseNotes -eq "") {
+        $destspec.package.metadata.releaseNotes = "#### Package
+* Automatically built and uploaded by [tunisiano](https://chocolatey.org/profiles/tunisiano)"
+        $destspec.Save($nuspec.FullName)
     }
 }
