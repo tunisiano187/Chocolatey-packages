6ae2f413bbd8d27ec2ffb8c60da2005ab65c20ad
Tunisiano187 <fabian@bowlman.org>
2020-07-01 21:04:42 +0200

    Spybot

--- automatic/spybot/tools/chocolateyInstall.ps1
+++ automatic/spybot/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'spybot'
 $installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$silentArgs = '/VERYSILENT'
 $url = 'https://www.safer-networking.org/files/spybotsd-2.8.68.0.exe'
 $checksum = '647cfdf796154ba0ce1786fd72f62102653f6fbfcc982e25da26579b844d966a'
 $checksumType = 'sha256'
