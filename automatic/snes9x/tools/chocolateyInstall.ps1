343e5627ec8a707448fdc4d1f59a747e12d5c84a
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-01 14:35:49 +0000

    AU: 1 updated - snes9x [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/d8bea76b579d66606925c4a242d5311b52d54325

--- automatic/snes9x/tools/chocolateyInstall.ps1
+++ automatic/snes9x/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = "snes9x"
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$url = 'http://www.s9x-w32.de/dl/snes9x-1.60-win32-x64.zip'
+$checksum = 'd5914d166e6d9987429566b8ad70f42d8c851c87ababace738918966852a58a3'
 $checksumType = 'sha256'
-$url64 = '{{DownloadUrlx64}}'
-$checksum64 = '{{Checksumx64}}'
+$url64 = 'http://www.s9x-w32.de/dl/snes9x-1.60-win32-x64.zip'
+$checksum64 = 'd5914d166e6d9987429566b8ad70f42d8c851c87ababace738918966852a58a3'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
