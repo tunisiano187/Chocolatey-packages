12e049486e188835090381c007e2dc01863dd9fa
Tunisiano187 <fabian@bowlman.org>
2020-07-05 12:20:20 +0200

    Link + vars

--- automatic/wamp-server/tools/chocolateyInstall.ps1
+++ automatic/wamp-server/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = 'wamp-server'
+﻿$packageName = $env:ChocolateyPackageName
 $installerType = 'EXE'
 $url32 = ''
 $url64 = ''
@@ -7,8 +7,8 @@ $checksum64type = 'sha256'
 $checksum32 = ''
 $checksum64 = ''
 
-$silentArgs = '/verysilent /SUPRRESSMSGBOXES /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS' 
-$validExitCodes = @(0) 
+$silentArgs = '/verysilent /SUPRRESSMSGBOXES /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS'
+$validExitCodes = @(0)
 
 $packageArgs = @{
   packageName   = $packageName
@@ -16,12 +16,12 @@ $packageArgs = @{
   url           = $url32
   url64			= $url64
   silentArgs    = $silentArgs
-  validExitCodes= @(0,3010)
+  validExitCodes= $validExitCodes
   softwareName  = 'wamp-server*'
-  checksum      = $checksum
+  checksum      = $checksum32
   checksum64    = $checksum64
-  checksumType  = 'sha256'
-  checksum64Type= 'sha256'
+  checksumType  = $checksumtype
+  checksum64Type= $checksum64type
 }
 
 $scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
