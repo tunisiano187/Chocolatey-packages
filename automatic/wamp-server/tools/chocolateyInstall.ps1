736177c51bec14024bfd29f0787a1d54c95ada6b
Tunisiano187 <fabian@bowlman.org>
2020-07-05 22:24:32 +0200

    System

--- automatic/wamp-server/tools/chocolateyInstall.ps1
+++ automatic/wamp-server/tools/chocolateyInstall.ps1
@@ -24,7 +24,7 @@ $packageArgs = @{
   checksum64Type= $checksum64type
 }
 
-if(Get-ProcessorBits -compare 64) {
+if([System.Environment]::Is64BitOperatingSystem]) {
   $installahk   = 'chocolateyInstall64.ahk'
 } else {
   $installahk   = 'chocolateyInstall.ahk'
