5068bd309dfe5fc632d5951c45560cb1941e953a
Tunisiano187 <fabian@bowlman.org>
2020-07-05 14:15:09 +0200

    3.2

--- automatic/wamp-server/tools/chocolateyInstall.ps1
+++ automatic/wamp-server/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $installerType = 'EXE'
-$url32 = ''
-$url64 = ''
+$url32 = 'http://wampserver.aviatechno.net/files/install/wampserver3.2.0_x86.exe'
+$url64 = 'http://wampserver.aviatechno.net/files/install/wampserver3.2.0_x64.exe'
 $checksumtype = 'sha256'
 $checksum64type = 'sha256'
-$checksum32 = ''
-$checksum64 = ''
+$checksum32 = '921f2815298ff5b6f965505da9b74f517cddeae6bc3c0508449a6fa62edd952b'
+$checksum64 = '9a89260d4240b938fbcf9d98f29c27e527ac104f8ba67453be94bcd73b54df7a'
 
 $silentArgs = '/verysilent /SUPRRESSMSGBOXES /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS'
 $validExitCodes = @(0)
