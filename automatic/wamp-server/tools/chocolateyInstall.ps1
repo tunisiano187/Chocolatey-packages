f9d7a1c8db10944234baef8f452dfb354d7c6490
tunisiano187 <fabian@bowlman.org>
2020-07-05 20:09:18 +0200

    64 bit and Lang

--- automatic/wamp-server/tools/chocolateyInstall.ps1
+++ automatic/wamp-server/tools/chocolateyInstall.ps1
@@ -6,8 +6,9 @@ $checksumtype = 'sha256'
 $checksum64type = 'sha256'
 $checksum32 = '921f2815298ff5b6f965505da9b74f517cddeae6bc3c0508449a6fa62edd952b'
 $checksum64 = '9a89260d4240b938fbcf9d98f29c27e527ac104f8ba67453be94bcd73b54df7a'
+$arch = ''
 
-$silentArgs = '/verysilent /SUPRRESSMSGBOXES /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS'
+$silentArgs = ''
 $validExitCodes = @(0)
 
 $packageArgs = @{
@@ -24,10 +25,21 @@ $packageArgs = @{
   checksum64Type= $checksum64type
 }
 
+if($arch) {
+  $arch='64'
+}
+
 $scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
+$ahkFile = Join-Path $scriptPath "chocolateyInstall$($arch).ahk"
 $ahkExe = 'AutoHotKey'
-$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+$ahkRun = "$Env:Temp\chocolateyInstall.ahk"
+Copy-Item $ahkFile "$ahkRun" -Force
+Start-Process $ahkExe $ahkRun
+
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkFile = Join-Path $scriptPath "chocolateyLang.ahk"
+$ahkExe = 'AutoHotKey'
+$ahkRun = "$Env:Temp\chocolateyLang.ahk"
 Copy-Item $ahkFile "$ahkRun" -Force
 Start-Process $ahkExe $ahkRun
 
---
+++ automatic/wamp-server/tools/chocolateyLang.ahk
@@ -0,0 +1,4 @@
+WinWait, Setup - WampServer 2, 
+IfWinNotActive, Setup - WampServer 2, , WinActivate, Setup - WampServer 2, 
+WinWaitActive, Setup - WampServer 2, 
+ControlSend,, {Enter}
