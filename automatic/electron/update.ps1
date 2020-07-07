55bd55b614a6fe7abb6547a831725ab7384628e6
tunisiano187 <fabian@bowlman.org>
2020-07-07 20:32:48 +0200

    ChecksumType

--- automatic/electron/update.ps1
+++ automatic/electron/update.ps1
@@ -7,10 +7,10 @@ function global:au_SearchReplace {
 		'tools/chocolateyInstall.ps1' = @{
 			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32Type)'"
+			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
 			"(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64Type)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
 }
@@ -29,4 +29,4 @@ function global:au_GetLatest {
 	return $Latest
 }
 
-update
+update -NoCheckChocoVersion
