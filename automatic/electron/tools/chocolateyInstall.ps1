b2a4111e1becbfad51e8742ab99d8dc1685d89a8
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-07 18:38:21 +0000

    AU: 1 updated - electron [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/2b0f3aa3282d7c6de2d34c6bce1040e115cfc124

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $url = 'https://github.com/electron/electron/releases/download/v9.1.0/electron-v9.1.0-win32-ia32.zip'
 $checksum = 'e2d9d1eff868f2c0f1397c1cf0f05ee01ba8b2154b1f5bb0c9a7358a6465c0db'
-$checksumType = ''
+$checksumType = 'sha256'
 $url64 = 'https://github.com/electron/electron/releases/download/v9.1.0/electron-v9.1.0-win32-x64.zip'
 $checksum64 = '9097ecf63581a7c0e228b3f72a00f0d77990cbcf389e579d43a725bbd6011ca1'
-$checksumType64 = ''
+$checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
