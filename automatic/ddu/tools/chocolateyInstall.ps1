2d6cc3a6cbc6de33155f9e4576a818078825b788
tunisiano187 <fabian@bowlman.org>
2020-07-08 11:05:44 +0200

    Automatic referer

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
-﻿$url = 'https://www.wagnardsoft.com/DDU/download/DDU%20V18.0.2.6.exe'
-$checksum = '4ed5dcc5f32385c3245982b9cde98e1d42651c1e6e7d1f566dc13f5212ab7fb4'
-$checksumType = 'sha256'
+﻿$url            = 'https://www.wagnardsoft.com/DDU/download/DDU%20V18.0.2.6.exe'
+$checksum       = '4ed5dcc5f32385c3245982b9cde98e1d42651c1e6e7d1f566dc13f5212ab7fb4'
+$checksumType   = 'sha256'
+$referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3183'
 
 $packageArgs		        = @{
 	UnzipLocation	          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -15,7 +16,7 @@ $packageArgs		        = @{
             'Accept-Encoding' = 'gzip, deflate, br'
             'Accept-Language' = 'en-US,en;q=0.9'
             'Cookie'          = 'has_js=1; cookie-agreed=2; phpbbwagnardsoft_u=1; phpbbwagnardsoft_k=; phpbbwagnardsoft_sid=dd0e2f5bcec548ecf35a899aafb05f81'
-            referer           = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3183'
+            referer           = $referer
         }
     }
 }
--- automatic/ddu/update.ps1
+++ automatic/ddu/update.ps1
@@ -8,6 +8,7 @@ function global:au_SearchReplace {
 			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]referer\s*=\s*)('.*')" = "`$1'$($Latest.Referer)'"
 		}
 	}
 }
@@ -15,11 +16,13 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	Add-Type -AssemblyName System.Web # To URLDecode
 	$release="https://www.wagnardsoft.com/forums/$([System.Web.HttpUtility]::UrlDecode(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'Released'}).href[0].replace('./','').replace('&amp;','&')))"
+	$splited=$release.split('&')
+	$referer="$($splited[0])&$($splited[1])"
 	$url32=((Invoke-WebRequest -Uri $release -UseBasicParsing).Links | Where-Object {$_ -match '.exe'}).href
 
 	$version=$url32.split('/')[-1].ToLower().split('v')[-1].replace('.exe','')
 
-	$Latest = @{ URL32 = $url32; Version = $version }
+	$Latest = @{ URL32 = $url32; Referer = $referer; Version = $version }
 	return $Latest
 }
 
