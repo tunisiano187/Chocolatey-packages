17c203481c92fcf73fffe8595add55dc0e114a1a
tunisiano187 <fabian@bowlman.org>
2020-07-09 15:37:39 +0200

    Pauby requests

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
 ﻿$url            = 'https://www.wagnardsoft.com/DDU/download/DDU%20V18.0.2.6.exe'
-$checksum       = '4ed5dcc5f32385c3245982b9cde98e1d42651c1e6e7d1f566dc13f5212ab7fb4'
-$checksumType   = 'sha256'
+$checksum       = ''
+$checksumType   = ''
 $referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3183'
 
 $packageArgs		        = @{
+    UnzipLocation	          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 	packageName		          = $env:ChocolateyPackageName
 	url				          = $url
 	checksum		          = $checksum
