6d637c76e9c072cc03592fe8e760b9fa649be6b2
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-09 13:40:31 +0000

    AU: 1 updated - ddu [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/77b5efc6d3ba53a7a4aa2e95fadc8bb306b29a5a

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$url            = 'https://www.wagnardsoft.com/DDU/download/DDU%20V18.0.2.6.exe'
-$checksum       = ''
-$checksumType   = ''
+$checksum       = '4ed5dcc5f32385c3245982b9cde98e1d42651c1e6e7d1f566dc13f5212ab7fb4'
+$checksumType   = 'sha256'
 $referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3183'
 
 $packageArgs		        = @{
