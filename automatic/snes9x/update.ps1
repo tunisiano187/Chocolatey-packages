098f304e729bc92a5be78a9ec736ec97b069e85d
tunisiano187 <fabian@bowlman.org>
2020-07-10 18:45:34 +0200

    Get-Version not correct for thi one

--- automatic/snes9x/update.ps1
+++ automatic/snes9x/update.ps1
@@ -17,8 +17,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	$installer = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'snes9x-'} | Where-Object {$_ -match 'win32'} | Where-Object {$_ -match '.zip'} | Select-Object -First 2 | Sort-Object ).href)
-	$version = Get-Version $installer[0]
-	#$version=$installer[0].split('-')[1]
+	$version=$installer[0].split('-')[1]
 	Write-Output "Version : $version"
 	$url32 = "$("http://www.s9x-w32.de/dl/")$($installer[0])"
 	$url64 = "$("http://www.s9x-w32.de/dl/")$($installer[1])"
