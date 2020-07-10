2609445ea34975cad5ea27b1c60f5b25f3f45e73
tunisiano187 <fabian@bowlman.org>
2020-07-10 18:53:27 +0200

    Comment

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $checksum       = '4ed5dcc5f32385c3245982b9cde98e1d42651c1e6e7d1f566dc13f5212ab7
 $checksumType   = 'sha256'
 $referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3183'
 
-Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/test.html"
+Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/test.html" #  Required to be allowed just after
 
 $packageArgs		        = @{
     UnzipLocation	          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
