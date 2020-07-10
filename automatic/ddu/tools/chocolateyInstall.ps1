43429491058eb9143ca1976eae53272e01abe90f
tunisiano187 <fabian@bowlman.org>
2020-07-10 14:17:44 +0200

    Other solution test

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -3,6 +3,8 @@ $checksum       = '4ed5dcc5f32385c3245982b9cde98e1d42651c1e6e7d1f566dc13f5212ab7
 $checksumType   = 'sha256'
 $referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3183'
 
+Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/test.html"
+
 $packageArgs		        = @{
     UnzipLocation	          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 	packageName		          = $env:ChocolateyPackageName
@@ -11,14 +13,6 @@ $packageArgs		        = @{
 	fileType                  = 'exe'
 	checksumType              = $checksumType
 	silentArgs                = '/s'
-    Options                   = @{
-        Headers                 = @{
-            'Accept-Encoding' = 'gzip, deflate, br'
-            'Accept-Language' = 'en-US,en;q=0.9'
-            'Cookie'          = 'has_js=1; cookie-agreed=2; phpbbwagnardsoft_u=1; phpbbwagnardsoft_k=; phpbbwagnardsoft_sid=dd0e2f5bcec548ecf35a899aafb05f81'
-            referer           = $referer
-        }
-    }
 }
 
 Install-ChocolateyZipPackage @packageArgs
