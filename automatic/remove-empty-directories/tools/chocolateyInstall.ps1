abdb8d62698cf6318b48aef74b3a5134b94c9a28
tunisiano187 <fabian@bowlman.org>
2020-07-11 20:08:47 +0200

    e

--- automatic/remove-empty-directories/tools/chocolateyInstall.ps1
+++ automatic/remove-empty-directories/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $url = 'https://vorboss.dl.sourceforge.net/project/rem-empty-dir/red-v2.2-setup.
 $checksum = '8f163a667304e6d86c306cab637b6f344afdb166fb4ed66190cf10fcbb1b91d3'
 $checksumType = 'sha256'
 
-Install-ChocolateyZipPackag -PackageName "$packageName" `
+Install-ChocolateyZipPackage -PackageName "$packageName" `
                             -Url "$url" `
                             -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
                             -Checksum "$checksum" `
