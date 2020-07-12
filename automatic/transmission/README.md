58a6d860646264781f989d7ec9cda64c6fff0572
tunisiano187 <fabian@bowlman.org>
2020-07-12 20:42:00 +0200

    Transmission

---
+++ automatic/transmission/README.md
@@ -0,0 +1,11 @@
+[![](https://img.shields.io/chocolatey/v/transmission?color=green&label=transmission)](https://chocolatey.org/packages/transmission) [![](https://img.shields.io/chocolatey/dt/transmission)](https://chocolatey.org/packages/transmission)
+
+## Transmission Torrent Client
+Transmission is a cross-platform BitTorrent client that is open source, easy, powerful, and lean.
+
+* Uses fewer resources than other clients
+* Native Mac, GTK+ and Qt GUI clients
+* Daemon ideal for servers, embedded systems, and headless use
+* All these can be remote controlled by Web and Terminal clients
+* Local Peer Discovery
+* Full encryption, DHT, µTP, PEX and Magnet Link support
---
+++ automatic/transmission/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+
+$releases = 'https://github.com/transmission/transmission/releases'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.checksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.checksumType64)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$file = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.msi'}).href | Where-Object {$_ -match '.msi'}) | Select-Object -First 2
+	$url32 = "https://github.com$($file[0])";
+	$url64 = "https://github.com$($file[1])";
+	$version = $file.split('/')[-2].trim()
+
+	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	return $Latest
+}
+
+update
