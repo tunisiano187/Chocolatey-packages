2bfdc2a8aaebe546db2d07d328372ce7f8d18b52
Tunisiano187 <fabian@bowlman.org>
2020-07-15 22:24:14 +0200

    ChecksumType

--- automatic/dexpot/update.ps1
+++ automatic/dexpot/update.ps1
@@ -7,7 +7,7 @@ function global:au_SearchReplace {
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
      }
 }
