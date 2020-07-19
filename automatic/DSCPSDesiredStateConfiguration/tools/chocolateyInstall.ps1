b77479f18c01e48f70af0ed2004aee65dd876e69
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-19 19:23:43 +0000

    AU: 1 updated - DSCPSDesiredStateConfiguration [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/6a6ab9b2510eb6cd7b02cd13c4caf8b70459beb4

--- automatic/DSCPSDesiredStateConfiguration/tools/chocolateyInstall.ps1
+++ automatic/DSCPSDesiredStateConfiguration/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = ''
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://github.com/dsccommunity/xPSDesiredStateConfiguration/archive/v9.1.0.zip'
+$checksum       = '72c9ac7aae5437abf226bb9b58be01dcb89710a575c1830172bab5ab2c47d208'
+$checksumType   = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName $packageName `
                     -url $url `
