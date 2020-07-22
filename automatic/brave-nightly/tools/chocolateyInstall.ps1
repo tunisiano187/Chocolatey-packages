5c08c39fd755714d4a9d7f931e35341a9e467e3d
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-22 15:16:03 +0000

    AU: 2 updated - brave-nightly xHyper-v [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/202d728359bb0cb2d511cc071a3165ea6fbc9864

--- automatic/brave-nightly/tools/chocolateyInstall.ps1
+++ automatic/brave-nightly/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   file        = "$toolsPath\BraveBrowserSilentNightlySetup32.exe"
   file64      = "$toolsPath\BraveBrowserSilentNightlySetup.exe"
 }
-$version = '1.13.35-nightly'
+$version = '1.13.36-nightly'
 $version = $version.Split('-')[0]
 
 [version]$softwareVersion = $version
