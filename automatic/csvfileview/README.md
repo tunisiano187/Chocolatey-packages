427e56b487883626ee31079e72777afc821fb7b9
Tunisiano187 <fabian@bowlman.org>
2020-07-23 21:53:37 +0200

    CSVFileView

---
+++ automatic/csvfileview/README.md
@@ -0,0 +1,12 @@
+# CSVFileView
+[![](https://img.shields.io/chocolatey/v/csvfileview?color=green&label=csvfileview)](https://chocolatey.org/packages/csvfileview) [![](https://img.shields.io/chocolatey/dt/csvfileview)](https://chocolatey.org/packages/csvfileview)
+
+CSVFileView is a simple CSV file viewer/converter utility that allows you to easily view 
+
+
+the content of CSV or tab-delimited file created by NirSoft utilities or by any other 
+software, in a simple table viewer.
+
+You can sort the lines according to one of the fields, remove unwanted fields and change 
+their order, and then save the result back into CSV file, tab-delimited file, XML file, 
+or HTML report.
---
+++ automatic/csvfileview/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+$url32 = 'https://www.nirsoft.net/utils/csvfileview.zip'
+# $url64 = 'https://www.nirsoft.net/utils/csvfileview-x64.zip'
+
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$File = "./csvfileview.zip"
+	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
+	Expand-Archive $File -DestinationPath .\csvfv
+
+	$version=$(Get-Content .\csvfv\readme.txt | Where-Object {$_ -match 'CSVFileView'})[0].split(' ')[-1].Replace('v','')
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
