b5c9799520d19e93be12327151ec358878a68736
tunisiano187 <fabian@bowlman.org>
2020-07-24 16:08:23 +0200

    Verification and License files

--- automatic/rstray/update.ps1
+++ automatic/rstray/update.ps1
@@ -7,6 +7,10 @@ function global:au_SearchReplace {
 		'tools/chocolateyInstall.ps1' = @{
 			"(^[$]version\s*=\s*)('.*')"      = "`$1'$($Latest.VERSION)'"
 		}
+		"tools\VERIFICATION.txt"      = @{
+			"(?i)(x86:).*"        = "`${1} $($Latest.URL32)"
+			"(?i)(checksum:).*" = "`${1} $($Latest.Checksum32)"
+		}
 	}
 }
 
@@ -18,8 +22,8 @@ function global:au_GetLatest {
 	Write-Verbose "Version : $version"
 	$url32 = "https://github.com$($url32)";
 	Invoke-WebRequest -Uri $url32 -OutFile "$(Get-Location)\tools\redshift-tray.zip"
-
-	$Latest = @{ Version = $version }
+	Invoke-WebRequest -Uri "https://github.com/ltGuillaume/Redshift-Tray/raw/master/LICENSE" "$(Get-Location)\tools\license.txt"
+	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
 }
 
