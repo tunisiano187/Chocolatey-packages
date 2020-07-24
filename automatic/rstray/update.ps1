9aaafaee12f49084dfd1b5c789309e23bcf7ae7c
tunisiano187 <fabian@bowlman.org>
2020-07-24 16:11:42 +0200

    link

--- automatic/rstray/update.ps1
+++ automatic/rstray/update.ps1
@@ -22,7 +22,7 @@ function global:au_GetLatest {
 	Write-Verbose "Version : $version"
 	$url32 = "https://github.com$($url32)";
 	Invoke-WebRequest -Uri $url32 -OutFile "$(Get-Location)\tools\redshift-tray.zip"
-	Invoke-WebRequest -Uri "https://github.com/ltGuillaume/Redshift-Tray/raw/master/LICENSE" "$(Get-Location)\tools\license.txt"
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ltGuillaume/Redshift-Tray/master/LICENSE" "$(Get-Location)\tools\license.txt"
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
 }
