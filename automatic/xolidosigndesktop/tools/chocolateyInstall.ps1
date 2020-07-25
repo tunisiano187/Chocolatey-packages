5ac0e997f88b31508eb78c9224b14016d1463a00
Tunisiano187 <fabian@bowlman.org>
2020-07-25 17:09:48 +0200

    XolidoSignDesktop

---
+++ automatic/xolidosigndesktop/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'xolidosigndesktop'
+$installerType = 'EXE'
+$url32 = 'http://www.xolido.com/instaladores/SetupXolidoSign.exe'
+$checksumtype = 'sha256'
+$checksum32 = '6cd3860665666a9efd13fd5ca33ee41807698f9c06788781094152811bf0db25'
+
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url32
+  silentArgs    = $silentArgs
+  validExitCodes= @(0,3010)
+  softwareName  = 'xolido*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
