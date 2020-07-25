7f9d6fc2543ec54a70765e042e3cb05266a6855a
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-25 18:14:14 +0000

    AU: 1 updated - pinginfoview [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/1d4d908ecbb71ed9fe54b38c1cd9a971ddf9b529

--- automatic/pinginfoview/tools/chocolateyInstall.ps1
+++ automatic/pinginfoview/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
+$url = 'https://www.nirsoft.net/utils/pinginfoview.zip'
+$checksum = '9c074b15ff6d0bbf74f668a51907581f85eabec7cbaec42e4a03454719a95f45'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
