780de1d7629a769d2e5698ffecd160b7bf3dc350
Tunisiano187 <fabian@bowlman.org>
2020-07-25 19:48:29 +0200

    Push

--- tosend/try.ps1
+++ tosend/try.ps1
@@ -3,10 +3,11 @@ Write-Verbose "Searching for packages to upload"
 $list = get-childitem ./*.nupkg -Recurse
 foreach ($file in $list) {
     if(choco push $file.FullName --api-key $choco_api) {
+        git checkout -B master
         git rm $file.FullName
         git commit -m "remove $($file.FullName)"
         Write-Verbose "$($file.Name) sent"
-        git push
+        git push --follow-tags origin master
     }
     else {
         Write-Warning "$($file.Name) not sent"
