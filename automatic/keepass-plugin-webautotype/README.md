3616d76b2f9d1821c8315e1c0c20b5abbf0148e4
Tunisiano187 <fabian@bowlman.org>
2020-07-25 21:56:04 +0200

    Keepass-plugin-webautotype

---
+++ automatic/keepass-plugin-webautotype/README.md
@@ -0,0 +1,5 @@
+# keepass-plugin-webautotype
+[![](https://img.shields.io/chocolatey/v/keepass-plugin-webautotype?color=green&label=keepass-plugin-webautotype)](https://chocolatey.org/packages/keepass-plugin-webautotype) [![](https://img.shields.io/chocolatey/dt/keepass-plugin-webautotype)](https://chocolatey.org/packages/keepass-plugin-webautotype)
+
+WebAutoType is a plugin for [KeePass](https://chocolatey.org/packages/keepass) to
+allow the AutoType functionality to work with browser URLs as well as window titles.
---
+++ automatic/keepass-plugin-webautotype/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+$releases = 'https://sourceforge.net/projects/webautotype/files/'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$url32 = "https://sourceforge.net/projects/webautotype/files/latest/download"
+	$version = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match "files\/v"}).href[0].split('/')[-2].replace('v','')
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
