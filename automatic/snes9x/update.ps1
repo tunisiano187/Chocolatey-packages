ee6c5cb0781e131c12043fc0f6a4484407576958
Tunisiano187 <fabian@bowlman.org>
2020-07-26 16:50:01 +0200

    snes9x

--- automatic/snes9x/update.ps1
+++ automatic/snes9x/update.ps1
@@ -16,11 +16,11 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$installer = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'snes9x-'} | Where-Object {$_ -match 'win32'} | Where-Object {$_ -match '.zip'} | Select-Object -First 2 | Sort-Object ).href)
+	$installer = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'snes9x-'} | Where-Object {$_ -match 'win32'} | Where-Object {$_ -match '.zip'} | Group-Object -Property href | select -First 2).Name)
 	$version=$installer[0].split('-')[1]
 	Write-Output "Version : $version"
-	$url32 = "$("http://www.s9x-w32.de/dl/")$($installer[0])"
-	$url64 = "$("http://www.s9x-w32.de/dl/")$($installer[1])"
+	$url32 = "$("http://www.s9x-w32.de/dl/")$($installer[1])"
+	$url64 = "$("http://www.s9x-w32.de/dl/")$($installer[2])"
 
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
