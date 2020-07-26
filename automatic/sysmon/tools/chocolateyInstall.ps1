fde2bd1924e26519cef7c79de6c9e2ef564226f1
Tunisiano187 <fabian@bowlman.org>
2020-07-26 17:21:56 +0200

    sysmon

--- automatic/sysmon/tools/chocolateyInstall.ps1
+++ automatic/sysmon/tools/chocolateyInstall.ps1
@@ -2,16 +2,10 @@
 $url = 'https://download.sysinternals.com/files/Sysmon.zip'
 $checksum = '8d78706b5ed7b7ec2c80bb388e3d361ba2d4b0461cbbd0c787cf523d4cfbfd81'
 $checksumType = 'sha256'
-$url64 = "$url"
-$checksum64 = "$checksum"
-$checksumType64 = "checksumType"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Url64bit "$url64" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
-                             -Checksum64 "$checksum64" `
-                             -ChecksumType64 "$checksumType64"
+                             -Checksum "$checksum"
