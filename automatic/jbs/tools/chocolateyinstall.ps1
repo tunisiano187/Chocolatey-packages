e605bfbd8ba30fa056e1e0704f2700d9cd5e6b69
Tunisiano187 <fabian@bowlman.org>
2020-07-26 17:46:32 +0200

    Type

--- automatic/jbs/tools/chocolateyinstall.ps1
+++ automatic/jbs/tools/chocolateyinstall.ps1
@@ -5,4 +5,4 @@ $url        = 'https://dl.johnsadventures.com/SwitcherSetup52.exe'
 $checksum   = '790BD6562A70F9D9002BBB3F1F11D7D0'
 $checksumtype = 'sha256'
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url -checksum $checksum -ValidExitCodes @(0,1223)
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url -checksum $checksum -ValidExitCodes @(0,1223) -checksumType $checksumtype
