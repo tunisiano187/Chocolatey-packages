3f9efe1eaf9a8314bdbd7531fb6a0aa402aaa619
Tunisiano187 <fabian@bowlman.org>
2020-07-27 18:29:36 +0200

    Enhancement

--- automatic/blink/tools/chocolateyUninstall.ps1
+++ automatic/blink/tools/chocolateyUninstall.ps1
@@ -7,7 +7,7 @@ $osBitness = Get-ProcessorBits
 if ($osBitness -eq 64) {
   $unPath = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
 } else {
-  $unPathx86 = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+  $unPath = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
 }
-$unString = (Get-ItemProperty $unPath\* | select DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageName*"}).UninstallString
+$unString = (Get-ItemProperty $unPath\* | Select-Object DisplayName, UninstallString | Where-Object {$_.DisplayName -like "$packageName*"}).UninstallString
 Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes $validExitCodes
