63dc75262d9e540d0f9668535a0d194e8712b2c3
Tunisiano187 <fabian@bowlman.org>
2020-07-27 10:13:49 +0200

    No error

--- au/update_all.ps1
+++ au/update_all.ps1
@@ -63,6 +63,6 @@ $global:au_Root = "$PSScriptRoot/../automatic"                           #Path t
 $info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
-if ($env:AppVeyor) {
+if ($env:AppVeyor -AND $True -eq $false) {
     if ($info.error_count.total) { throw "$($info.error_count.total) errors during update" }
 }
