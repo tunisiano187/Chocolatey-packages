e72d2a4773a65f4bc9700d8fe8b1bc5f811678f7
Tunisiano187 <fabian@bowlman.org>
2020-07-27 18:51:11 +0200

    trim

--- automatic/blink/update.ps1
+++ automatic/blink/update.ps1
@@ -16,7 +16,7 @@ function global:au_GetLatest {
 	$url32 = Get-RedirectedURL $release
 	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "blink.exe"
 	Invoke-WebRequest -Uri $url32 -OutFile $File
-	$version=$(Get-Command $File).FileVersionInfo.ProductVersion
+	$version=$(Get-Command $File).FileVersionInfo.ProductVersion.trim()
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
