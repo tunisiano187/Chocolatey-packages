df29da31a4d5ab8ff85f3ae22d784ec551e67d27
Tunisiano18 <helpdesk.choc@gmail.com>
2020-07-30 16:15:18 +0000

    AU: 1 updated - blink [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/417e45186002e843f48d28d955ba46a125d2e457

--- automatic/blink/tools/chocolateyInstall.ps1
+++ automatic/blink/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $installerType      = 'exe'
 $silentArgs         = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes     = @(0)
-$url                = '{{DownloadUrl}}'
-$checksum           = '{{Checksum}}'
-$checksumType       = 'md5'
+$url                = 'https://blink.sipthor.net/download.phtml?download&os=nt'
+$checksum           = '88a14a67916a3700d98b6deede1a5f689d6f9674e29da14caf76d7e80f183470'
+$checksumType       = 'sha256'
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
