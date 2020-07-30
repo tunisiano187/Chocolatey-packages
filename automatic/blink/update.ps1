66659145e08c47835bfd25bce5008c8fd950dd69
Tunisiano187 <fabian@bowlman.org>
2020-07-30 16:38:25 +0200

    function

--- automatic/blink/update.ps1
+++ automatic/blink/update.ps1
@@ -2,6 +2,40 @@ import-module au
 
 $release = 'https://blink.sipthor.net/download.phtml?download&os=nt'
 
+function Set-UseUnsafeHeaderParsing
+{
+    param(
+        [Parameter(Mandatory,ParameterSetName='Enable')]
+        [switch]$Enable,
+
+        [Parameter(Mandatory,ParameterSetName='Disable')]
+        [switch]$Disable
+    )
+
+    $ShouldEnable = $PSCmdlet.ParameterSetName -eq 'Enable'
+
+    $netAssembly = [Reflection.Assembly]::GetAssembly([System.Net.Configuration.SettingsSection])
+
+    if($netAssembly)
+    {
+        $bindingFlags = [Reflection.BindingFlags] 'Static,GetProperty,NonPublic'
+        $settingsType = $netAssembly.GetType('System.Net.Configuration.SettingsSectionInternal')
+
+        $instance = $settingsType.InvokeMember('Section', $bindingFlags, $null, $null, @())
+
+        if($instance)
+        {
+            $bindingFlags = 'NonPublic','Instance'
+            $useUnsafeHeaderParsingField = $settingsType.GetField('useUnsafeHeaderParsing', $bindingFlags)
+
+            if($useUnsafeHeaderParsingField)
+            {
+              $useUnsafeHeaderParsingField.SetValue($instance, $ShouldEnable)
+            }
+        }
+    }
+}
+
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
---
+++ scripts/Checks_ps1.ps1
@@ -0,0 +1,2 @@
+Split-Path (Split-Path get-location -Parent ) -Leaf (avant dernier)
+Split-Path (Split-Path (Split-Path (get-location) -Parent ) -Parent) -Leaf (avant-avant-dernoer-)
