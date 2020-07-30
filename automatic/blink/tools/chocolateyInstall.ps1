92c287df6b8ffb8ccc32130e58b0ebbf2cfcb086
Tunisiano187 <fabian@bowlman.org>
2020-07-30 16:27:39 +0200

    Referer

--- automatic/blink/tools/chocolateyInstall.ps1
+++ automatic/blink/tools/chocolateyInstall.ps1
@@ -1,9 +1,23 @@
-﻿$packageName = $env:ChocolateyPackageName
-$installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0)
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'md5'
+﻿$packageName        = $env:ChocolateyPackageName
+$installerType      = 'exe'
+$silentArgs         = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes     = @(0)
+$url                = '{{DownloadUrl}}'
+$checksum           = '{{Checksum}}'
+$checksumType       = 'md5'
+$referer            = 'http://icanblink.com/download/'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
+$options =
+@{
+  Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
+    'Accept-Language' = 'en-GB,en-US;q=0.8,en;q=0.6';
+    Cookie = 'requiredinfo=info';
+    Referer = $referer;
+  }
+}
+
+Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/icanblink.html"  #  Required to be allowed just after
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType" -Options $options
--- automatic/blink/update.ps1
+++ automatic/blink/update.ps1
@@ -8,17 +8,20 @@ function global:au_SearchReplace {
 			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]referer\s*=\s*)('.*')" 		= "`$1'$($Latest.Referer)'"
 		}
 	}
 }
 
 function global:au_GetLatest {
+	$referer        = 'http://icanblink.com/download/'
+	Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/icanblink.html" #  Required to be allowed just after
 	$url32 = $release
 	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "blink.exe"
 	Invoke-WebRequest -Uri $url32 -OutFile $File
 	$version=$(Get-Command $File).FileVersionInfo.ProductVersion.trim()
 
-	$Latest = @{ URL32 = $url32; Version = $version }
+	$Latest = @{ URL32 = $url32; Version = $version; Referer = $referer }
 	return $Latest
 }
 
