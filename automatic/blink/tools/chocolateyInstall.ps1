1bc219165fa0b260a651084dd385f0ad98117416
Tunisiano187 <fabian@bowlman.org>
2020-07-30 18:12:07 +0200

    No referer

--- automatic/blink/tools/chocolateyInstall.ps1
+++ automatic/blink/tools/chocolateyInstall.ps1
@@ -5,17 +5,5 @@ $validExitCodes     = @(0)
 $url                = '{{DownloadUrl}}'
 $checksum           = '{{Checksum}}'
 $checksumType       = 'md5'
-$referer            = 'http://icanblink.com/download/'
 
-$options =
-@{
-  Headers = @{
-    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-    'Accept-Charset' = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3';
-    'Accept-Language' = 'en-GB,en-US;q=0.8,en;q=0.6';
-    Cookie = 'requiredinfo=info';
-    Referer = $referer;
-  }
-}
-Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/icanblink.html" -UseBasicParsing #  Required to be allowed just after
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType" -Options $options
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum "$checksum" -checksumType "$checksumType"
