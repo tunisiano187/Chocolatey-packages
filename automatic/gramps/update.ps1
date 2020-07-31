2b97ab4140999f66313cb15ace988876a1ec7674
Tunisiano187 <fabian@bowlman.org>
2020-07-31 21:22:43 +0200

    Gramps

---
+++ automatic/gramps/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://github.com/gramps-project/gramps/releases/latest'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumtype\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url64\s*=\s*)('.*')"      		= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType64)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	Write-Output 'Check Folder'
+	$installer = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.exe'} | Select-Object -First 2 | Sort-Object ).href
+	Write-Output 'Checking version'
+	$version=$($installer[0]).split('/')[-1].split('-')[1].split('_')[0].trim()
+
+	Write-Output "Version : $version"
+	$url32 = "https://github.com$($installer[1])";
+	$url64 = "https://github.com$($installer[0])";
+
+	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	return $Latest
+}
+
+update
