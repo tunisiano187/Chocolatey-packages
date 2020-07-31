7c040a21dc6263abce6917821c79b8460fda4d90
Tunisiano187 <fabian@bowlman.org>
2020-07-31 11:59:24 +0200

    Blink embed

--- automatic/blink/tools/chocolateyInstall.ps1
+++ automatic/blink/tools/chocolateyInstall.ps1
@@ -2,7 +2,8 @@
 $installerType      = 'exe'
 $silentArgs         = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes     = @(0)
-$url                = 'https://blink.sipthor.net/download.phtml?download&os=nt'
+$toolsPath          = Split-Path $MyInvocation.MyCommand.Definition
+$url                = Join-Path $toolsPath 'blink.exe'
 $checksum           = '88a14a67916a3700d98b6deede1a5f689d6f9674e29da14caf76d7e80f183470'
 $checksumType       = 'sha256'
 
--- automatic/blink/update.ps1
+++ automatic/blink/update.ps1
@@ -6,18 +6,21 @@ $release = 'https://blink.sipthor.net/download.phtml?download&os=nt'
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+		"tools\VERIFICATION.txt"      = @{
+			"(?i)(x86:).*"        				= "`${1} $($Latest.URL32)"
+			"(?i)(checksumtype:).*" 			= "`${1} $($Latest.ChecksumType32)"
+			"(?i)(checksum32:).*" 				= "`${1} $($Latest.Checksum32)"
 		}
 	}
 }
 
 function global:au_GetLatest {
 	$url32 = $release
-	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "blink.exe"
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "tools\blink.exe"
 	(new-object System.Net.WebClient).DownloadFile($url32,$File)
-	#Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
 	$version=$(Get-Command $File).FileVersionInfo.ProductVersion.trim()
 
 	$Latest = @{ URL32 = $url32; Version = $version}
