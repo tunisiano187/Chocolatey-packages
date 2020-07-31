c76e9e5bb48023c10b47b329ce159403406c21b0
Tunisiano187 <fabian@bowlman.org>
2020-07-31 21:00:19 +0200

    jcpicker

---
+++ automatic/jcpicker/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$url32 = 'https://annystudio.com/jcpicker.exe'
+
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "jcpicker.exe"
+	Invoke-WebRequest -Uri $url32 -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+
+	$url32 = 'https://annystudio.com/jcpicker.zip'
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
