60af13fb66b249fff1ffa01b29884f52c4ae92f5
Tunisiano187 <fabian@bowlman.org>
2020-08-01 17:50:08 +0200

    Complete url

--- automatic/geany-plugins/update.ps1
+++ automatic/geany-plugins/update.ps1
@@ -15,6 +15,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'setup.exe'} | Where-Object {$_ -notmatch 'exe.sig'}).href[-1]
 	$version = $url32.split('_')[0].split('-')[-1]
+	$url32 = "https://plugins.geany.org/geany-plugins/$($url32)"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
