adcc9e6bf1689f9e322c0c405448b81da098fa04
Tunisiano18 <helpdesk.choc@gmail.com>
2020-08-01 19:32:28 +0000

    AU: 1 updated - geany-plugins [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/efe5b708cd81572b997ee7ff05f0d5e05f1d658f

--- automatic/geany-plugins/tools/chocolateyinstall.ps1
+++ automatic/geany-plugins/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = ''
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://plugins.geany.org/geany-plugins/geany-plugins-1.36_setup.exe'
+$checksum       = '0115ca49978195094a8b568901f56eb22a3682930116476462462744b974c7fb'
+$checksumType   = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
