ae06496b3393f81ef9a6353ad0d32c0a851f8595
Tunisiano187 <fabian@bowlman.org>
2020-08-02 14:48:20 +0200

    Andyroid x64

--- automatic/andy/tools/chocolateyInstall.ps1
+++ automatic/andy/tools/chocolateyInstall.ps1
@@ -4,12 +4,18 @@ $silentArgs = '/S'
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'md5'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'md5'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
                           -Url "$url" `
+                          -Url64 "$url64" `
                           -ValidExitCodes $validExitCodes `
                           -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
--- automatic/andy/update.ps1
+++ automatic/andy/update.ps1
@@ -1,26 +1,35 @@
 import-module au
 
-$releases 		= 'http://bit.ly/getAndy'
-$checksumType32 = 'sha256'
+$releases 		= 'https://www.andyroid.net/getandy.php'
 
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumtype\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url\s*=\s*)('.*')"      			= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 			= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 		= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      		= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
 }
 
 function global:au_GetLatest {
-	$url32 = $releases
-	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "andy.exe"
-	Invoke-WebRequest -Uri $url32 -OutFile $File
-	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
-	$checksum32 = $(Get-FileHash -Algorithm $checksumType32 $File).Hash
+	$request = Invoke-WebRequest -Method Head -Uri $releases
+	if ($null -ne $request.BaseResponse.ResponseUri) {
+		# This is for Powershell 5
+		$url32 = $request.BaseResponse.ResponseUri.AbsoluteUri
+	}
+	elseif ($null -ne $request.BaseResponse.RequestMessage.RequestUri) {
+		# This is for Powershell core
+		$url32 = $request.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+	}
+	$url64 = $url32.replace('x86.exe', 'x64.exe')
+	$separated = $url32.split('=')[-1].split('_')
+	$version = "$($separated[1]).$($separated[2]).$($separated[3])"
 
-	$Latest = @{ URL32 = $url32; Checksum32 = $checksum32; ChecksumType32 = $checksumType32; Version = $version }
+	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
 }
 
