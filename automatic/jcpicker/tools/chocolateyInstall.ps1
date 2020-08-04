26d78aff3e543e26ef8387932e77ec2232b06f3f
Tunisiano187 <fabian@bowlman.org>
2020-08-04 13:15:36 +0200

    test

--- automatic/jcpicker/tools/chocolateyInstall.ps1
+++ automatic/jcpicker/tools/chocolateyInstall.ps1
@@ -3,7 +3,6 @@ $url            = 'https://annystudio.com/jcpicker.zip'
 $checksum       = 'a8242605238dd16adfdff4fb0ca2b8a79072b147b109ad5dfed7e7ef6f0f503b'
 $checksumType   = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installFile    = Join-Path $toolsDir "$($packageName)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
@@ -12,5 +11,3 @@ Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType"
 
-Set-Content -Path ("$installFile.gui") `
-            -Value $null
