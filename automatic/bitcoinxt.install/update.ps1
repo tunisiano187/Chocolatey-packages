cde3e1cab77a7330578c3eed18a30ffdbe4e3a39
Tunisiano187 <fabian@bowlman.org>
2020-08-04 20:41:01 +0200

    convert letter to number

--- automatic/bitcoinxt.install/update.ps1
+++ automatic/bitcoinxt.install/update.ps1
@@ -19,8 +19,14 @@ function global:au_GetLatest {
 	Write-Verbose 'Get files'
 	$installer = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match "-win"} | Where-Object {$_ -match '.exe'} | Select-Object -First 2 | Sort-Object ).href
 	Write-Verbose 'Checking version'
-	$version=$($installer[0]).split('/')[-2].replace('v','')
-
+	$v=$($installer[0]).split('/')[-2].replace('v','')
+	foreach ($char in [int[]][char[]]$v) {
+		if($char -gt 65){
+			$version = "$version,"
+			$char=$char-64
+		}
+		$version = "$($version)$([char[]]$char)"
+	}
 	Write-Verbose "Version : $version"
 	$url32 = "https://github.com/$($installer[1])";
 	$url64 = "https://github.com/$($installer[0])";
