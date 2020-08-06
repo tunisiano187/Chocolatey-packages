319e5cc07172ad1a3c005d33514359703dc61ad1
Tunisiano187 <fabian@bowlman.org>
2020-08-06 22:44:42 +0200

    Remove unneeded vars

--- automatic/digikam/update.ps1
+++ automatic/digikam/update.ps1
@@ -28,8 +28,6 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
 	$version = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '^\d+([.]\d+)?'} | ForEach-Object {($_.href -replace '[^.\d]', '')} | Measure-Object -Max).Maximum
-	$installer = "$($releases)v$($version)/latest.exe"
-	$working_dir = "."
 	$url32 = "https://download.kde.org/stable/digikam/7.0.0/digiKam-$($version)-Win32.exe"
 	$url64 = "https://download.kde.org/stable/digikam/7.0.0/digiKam-$($version)-Win64.exe"
 
