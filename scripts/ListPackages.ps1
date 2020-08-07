03f4b77813d2a9cf837a2514aa82de9e2b459365
Tunisiano187 <fabian@bowlman.org>
2020-08-07 23:49:44 +0200

    Packages.md

---
+++ scripts/ListPackages.ps1
@@ -0,0 +1,9 @@
+$mdfile = "$PSScriptRoot/../Packages.md"
+Set-Content $mdfile "| Downloads | Package Name |"
+Add-Content $mdfile "|-----------|--------------|"
+$paths = Get-ChildItem -Path "$PSScriptRoot/../automatic/" -Directory | Select-Object FullName
+foreach ($path in $paths) {
+    $package = $path.FullName
+    Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg?color=green&label=$package)](https://chocolatey.org/packages/$package) | [$package]|"
+}
+git commit -m "List Packages"
