a2ff4c2a6325bd7d827978e1842942a0bc259cbc
Tunisiano187 <fabian@bowlman.org>
2020-08-07 23:52:20 +0200

    Add

--- scripts/ListPackages.ps1
+++ scripts/ListPackages.ps1
@@ -6,4 +6,5 @@ foreach ($path in $paths) {
     $package = $path.FullName
     Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg?color=green&label=$package)](https://chocolatey.org/packages/$package) | [$package]|"
 }
+git add $mdfile
 git commit -m "List Packages"
