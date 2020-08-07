1c047dbf08b990727c82804fe6a629f5bf115258
Tunisiano187 <fabian@bowlman.org>
2020-08-08 01:03:24 +0200

    Downloads

--- scripts/ListPackages.ps1
+++ scripts/ListPackages.ps1
@@ -4,7 +4,7 @@ Add-Content $mdfile "|-----------:|--------------|"
 $paths = Get-ChildItem -Path "$PSScriptRoot/../automatic/" -Directory | Select-Object FullName
 foreach ($path in $paths) {
     $package = $path.FullName.split('\')[-1]
-    Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg?color=green&label=$package)](https://chocolatey.org/packages/$package) | [![](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|"
+    Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg)](https://chocolatey.org/packages/$package) | [![](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|"
 }
 git add $mdfile
 git commit -m "[skip ci] List Packages"
