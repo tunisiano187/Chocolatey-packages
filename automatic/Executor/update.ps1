4cea6661edb1b12cb1222a8d2d79fa252060871e
Tunisiano187 <fabian@bowlman.org>
2020-08-08 22:54:39 +0200

    Manual

--- automatic/Executor/update.ps1
+++ automatic/Executor/update.ps1
@@ -13,9 +13,9 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$url32 = 'http://www.1space.dk/executor/ExecutorSetup.exe'
 	$version='1.0.0'
-	$File = Join-Path $env:TEMP "ver.html"
-	Invoke-WebRequest -Uri 'http://www.1space.dk/executor/vhistory.html' -OutFile $File -Proxy "http://115.79.39.96:80"
-	$version = $(Get-Content $File | Where-Object {$_ -match "h3"})[0].split('v')[1].split(' ')[0].trim()
+	# $File = Join-Path $env:TEMP "ver.html"
+	# Invoke-WebRequest -Uri 'http://www.1space.dk/executor/vhistory.html' -OutFile $File -Proxy "http://115.79.39.96:80"
+	# $version = $(Get-Content $File | Where-Object {$_ -match "h3"})[0].split('v')[1].split(' ')[0].trim()
 	Write-Output "Version : $version"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
