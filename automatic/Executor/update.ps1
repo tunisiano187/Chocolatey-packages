25f82c8c06f1854a2595e416bd662322f63e90ab
Tunisiano187 <fabian@bowlman.org>
2020-08-08 22:34:41 +0200

    Version

--- automatic/Executor/update.ps1
+++ automatic/Executor/update.ps1
@@ -13,6 +13,9 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$url32 = 'http://www.1space.dk/executor/ExecutorSetup.exe'
 	$version='1.0.0'
+	$File = Join-Path $env:TEMP "ver.html"
+	Invoke-WebRequest -Uri 'http://www.1space.dk/executor/vhistory.html' -OutFile $File
+	$version = $(Get-Content $File | Where-Object {$_ -match "h3"})[0].split('v')[1].split(' ')[0].trim()
 	Write-Output "Version : $version"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
