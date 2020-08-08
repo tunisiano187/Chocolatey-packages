13074acba93ecc5baa361e0de813463e9ab2cca5
Tunisiano187 <fabian@bowlman.org>
2020-08-08 22:36:36 +0200

    BasicParsing

--- automatic/Executor/update.ps1
+++ automatic/Executor/update.ps1
@@ -14,7 +14,7 @@ function global:au_GetLatest {
 	$url32 = 'http://www.1space.dk/executor/ExecutorSetup.exe'
 	$version='1.0.0'
 	$File = Join-Path $env:TEMP "ver.html"
-	Invoke-WebRequest -Uri 'http://www.1space.dk/executor/vhistory.html' -OutFile $File
+	Invoke-WebRequest -Uri 'http://www.1space.dk/executor/vhistory.html' -OutFile $File -UseBasicParsing
 	$version = $(Get-Content $File | Where-Object {$_ -match "h3"})[0].split('v')[1].split(' ')[0].trim()
 	Write-Output "Version : $version"
 
