71e707da939743818555b57ad9b6a675ecfe291c
Tunisiano187 <fabian@bowlman.org>
2020-08-08 19:02:19 +0200

    1038.0

--- automatic/qttabbar/update.ps1
+++ automatic/qttabbar/update.ps1
@@ -14,9 +14,9 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$url32="$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.zip'}).href)[0]"
+	$url32=$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.zip'}).href)[0]
 
-	$version=$url32.split('Bar')[-1].replace('.zip','')
+	$version="$($url32.split('Bar')[-1].replace('.zip','')).0"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
