29a297a2af7474c9639a63b73c13ad080ba88d17
Tunisiano187 <fabian@bowlman.org>
2020-08-08 20:53:00 +0200

    pre

--- automatic/winmerge/update.ps1
+++ automatic/winmerge/update.ps1
@@ -1,34 +1,38 @@
 import-module au
 
-$releases = 'http://winmerge.org/downloads/'
+$releases = 'https://github.com/WinMerge/winmerge/releases'
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum32Type\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $installer = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.href -match ".exe$"} | Select-Object -First 2 | Sort-Object ).href
+    $version=$($installer[0]).split('/')[-1].split('-')[1]
+    $url32 = "https://github.com/$($installer[1])";
+	$url64 = "https://github.com/$($installer[0])";
 
-    $re32  = "winmerge-.*-Setup.exe"
-    $url32 = $download_page.links | where-object href -match $re32 | select-object -First 1 -expand href
-    $re64  = "winmerge-.*-x64-Setup.exe"
-    $url64 = $download_page.links | where-object href -match $re64 | select-object -First 1 -expand href
-
-    $version = $url32 -split '-|.exe' | select-object -Last 1 -Skip 2
-    if($version -eq '2.16.6') {
-        $version="$version.0"
+    $version = $installer -split '-|.exe' | select-object -Last 1 -Skip 2
+    $tags = Invoke-WebRequest 'https://api.github.com/repos/WinMerge/winmerge/releases' -UseBasicParsing | ConvertFrom-Json
+    foreach ($tag in $tags) {
+        if($tag.tag_name -match $version) {
+            if($tag.prerelease -match "true") {
+                $clnt = new-object System.Net.WebClient;
+                $clnt.OpenRead("https://github.com$($installer[0])").Close();
+                $date = $([datetime]$clnt.ResponseHeaders["Last-Modified"];).ToString("yyyyMMdd")
+                $version = "$version.$($date)-pre"
+            }
+        }
     }
 
     return @{ URL32 = $url32; URL64 = $url64; Version = $version }
 }
 
-update-package
+update-package -ChecksumFor all
