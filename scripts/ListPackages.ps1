f938bd43cedf1d1e7f9bc3de3f0ec6cc97175e9a
Tunisiano187 <fabian@bowlman.org>
2020-08-12 04:15:25 +0200

    Repology to see outdated

--- scripts/ListPackages.ps1
+++ scripts/ListPackages.ps1
@@ -5,7 +5,7 @@ Add-Content $mdfile "|-----------:|--------------|--------------|"
 $paths = Get-ChildItem -Path "$PSScriptRoot/../automatic/" -Directory | Select-Object FullName
 foreach ($path in $paths) {
     $package = $path.FullName.split('\')[-1]
-    Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg)](https://chocolatey.org/packages/$package) | [![](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|[![Packaging status](https://repology.org/badge/vertical-allrepos/$package.svg)](https://repology.org/project/$package/versions)|"
+    Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg)](https://chocolatey.org/packages/$package) | [![](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|[![Chocolatey package](https://repology.org/badge/version-for-repo/chocolatey/$package.svg)](https://repology.org/project/$package/versions))|"
 }
 git add $mdfile
 git commit -m "[skip ci] List Packages"
