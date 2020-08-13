34b363f93756643bc0c90fc83b223a2d6a59dbd2
Tunisiano18 <helpdesk.choc@gmail.com>
2020-08-13 13:50:01 +0000

    AU: 1 updated - minecraft-education [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/15b0063e19713a72c625f0d070b6e6cf44fe43d4

--- automatic/minecraft-education/tools/chocolateyinstall.ps1
+++ automatic/minecraft-education/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 
 
 $packageName      = $env:ChocolateyPackageName
-$url              = 'https://launcher.mojang.com/download/MinecraftInstaller.msi'
-$checksum         = ''
-$checksumType     = ''
+$url              = 'https://aka.ms/downloadmee-desktopApp'
+$checksum         = 'f409330781dc29c0fb3d303e2e9e801afc2190632088edca4fc439701c087375'
+$checksumType     = 'sha256'
 
 $packageArgs = @{
   packageName     = $packageName
