795c8c628dbec3edf5c81831047639e14ef851ba
Tunisiano18 <helpdesk.choc@gmail.com>
2020-08-14 23:28:32 +0200

    Alt text for images

--- scripts/ListPackages.ps1
+++ scripts/ListPackages.ps1
@@ -1,12 +1,13 @@
 $ErrorActionPreference = 'Continue';
 git checkout master
 $mdfile = "$PSScriptRoot/../Packages.md"
-Set-Content $mdfile "| Downloads | Package Name and version | Repology |"
+Set-Content $mdfile "# Automatic Packages currently maintained here"
+Add-Content $mdfile "| Downloads | Package Name and version | Repology |"
 Add-Content $mdfile "|-----------:|--------------|--------------|"
 $paths = Get-ChildItem -Path "$PSScriptRoot/../automatic/" -Directory | Select-Object FullName
 foreach ($path in $paths) {
     $package = $path.FullName.split('\')[-1]
-    Add-Content $mdfile "| [![](http://img.shields.io/chocolatey/dt/$package.svg)](https://chocolatey.org/packages/$package) | [![](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|[![Chocolatey package](https://repology.org/badge/latest-versions/$package.svg)](https://repology.org/project/$package/versions)|"
+    Add-Content $mdfile "| [![$package](http://img.shields.io/chocolatey/dt/$package.svg)](https://chocolatey.org/packages/$package) | [![$package](https://img.shields.io/chocolatey/v/$($package)?color=green&label=$package)](https://github.com/tunisiano187/Chocolatey-packages/tree/master/automatic/$package)|[![Chocolatey package](https://repology.org/badge/latest-versions/$package.svg)](https://repology.org/project/$package/versions)|"
 }
 git add $mdfile
 git commit -m "[skip ci] List Packages"
