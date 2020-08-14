cd1d6bd9d43ea05b6cbcf523f8bb48931457b472
Tunisiano187 <fabian@bowlman.org>
2020-08-14 18:39:59 +0200

    '

--- automatic/dd/tools/chocolateyInstall.ps1
+++ automatic/dd/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = "https://www.chrysocome.net/downloads/dd-0.6beta3.zip"
+$url            = 'https://www.chrysocome.net/downloads/dd-0.6beta3.zip'
 $checksum       = 'C7A6A4CC4A107030B072FB479173D66E6D1D1098CF740C6DA4E9378C16B81C17'
 $checksumType   = 'sha256'
 
