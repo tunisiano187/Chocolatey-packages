3975c53c8353762b0f4b3f0157d058ec1a6094ff
Tunisiano187 <fabian@bowlman.org>
2020-08-15 18:45:56 +0200

    Mobizen

---
+++ automatic/mobizen/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+
+$release = 'https://download.mobizen.com/download/mobizen.exe'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "mobizen.exe"
+	Invoke-WebRequest -Uri $release -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+
+	$Latest = @{ URL32 = $release; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32 -NoCheckChocoVersion
