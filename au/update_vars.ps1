4e69b146998514351203d81fc20e3c685971962a
Tunisiano187 <fabian@bowlman.org>
2020-08-15 23:27:09 +0200

    var

--- au/update_vars.ps1
+++ au/update_vars.ps1
@@ -1,7 +1,7 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 # Copy this file to update_vars.ps1 and set the variables there. Do not include it in the repository.
 
-if(!(Test-Path $Env:mail_user)) {
+if(!(Test-Path Env:mail_user)) {
     $Env:mail_user        = $Mail_user
     $Env:mail_pass        = $Mail_pass
     $Env:mail_server      = 'smtp.gmail.com'
