3f4b261c2ee9a896421f029eb72917ca6fbd3781
Tunisiano187 <fabian@bowlman.org>
2020-08-16 22:01:12 +0200

    copy file

--- automatic/keepass-plugin-quickunlock/tools/chocolateyInstall.ps1
+++ automatic/keepass-plugin-quickunlock/tools/chocolateyInstall.ps1
@@ -29,7 +29,7 @@ if (! $installPath) {
   Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
   $binRoot = Get-BinRoot
   $portPath = Join-Path $binRoot "keepass"
-  $installPath = Get-ChildItemDir $portPath*
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
 }
 if (! $installPath) {
   Write-Verbose "Searching $env:Path for unregistered install..."
@@ -42,6 +42,7 @@ if (! $installPath) {
   Write-Warning "$($packageSearch) not found."
   throw
 }
+Write-Verbose "`t...found."
 
 Write-Verbose "Searching for plugin directory..."
 $pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
