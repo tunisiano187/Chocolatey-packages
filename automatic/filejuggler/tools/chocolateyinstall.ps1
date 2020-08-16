39d1bcb8726b0f75386e86212ac78d11b18221f3
Tunisiano187 <fabian@bowlman.org>
2020-08-16 19:43:05 +0200

    File Juggler

--- automatic/filejuggler/tools/chocolateyinstall.ps1
+++ automatic/filejuggler/tools/chocolateyinstall.ps1
@@ -1,15 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= $env:ChocolateyPackageName
-$installerType      = 'EXE'
-$silentArgs = '/qb'
-$url        = 'https://www.filejuggler.com/download/filejuggler.exe'
-$checksum   = 'd734baf0c5b7fa644303f98ed5d3e7caf4f91f4ffb0915b87d69d015e6fb6f0a'
+$packageName    = $env:ChocolateyPackageName
+$installerType  = 'EXE'
+$silentArgs     = '/qb'
+$url            = 'https://www.filejuggler.com/download/filejuggler.exe'
+$checksum       = 'd734baf0c5b7fa644303f98ed5d3e7caf4f91f4ffb0915b87d69d015e6fb6f0a'
 $checksumtype	= 'sha256'
 
-$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkFile = Join-Path $scriptPath "chocolateyInstall.ahk"
-$ahkExe = 'AutoHotKey'
-$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+$scriptPath     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkFile        = Join-Path $scriptPath "chocolateyInstall.ahk"
+$ahkExe         = 'AutoHotKey'
+$ahkRun         = "$Env:Temp\$(Get-Random).ahk"
+
 Copy-Item $ahkFile "$ahkRun" -Force
 Start-Process $ahkExe $ahkRun
 
--- automatic/filejuggler/update.ps1
+++ automatic/filejuggler/update.ps1
@@ -16,8 +16,9 @@ function Get-Version($name) {
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-			"(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumtype\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
 		}
 	}
 }
