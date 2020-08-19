31456cdc752a7f28b3a7700d69b11c475fe1ff5d
Tunisiano187 <fabian@bowlman.org>
2020-08-19 18:09:56 +0200

    Faceit

---
+++ automatic/faceit/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url            = 'https://cdn.faceit.com/anticheat/FACEITInstaller_64.exe'
+$checksum       = ''
+$checksumType   = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'faceit*' 
+  checksum      = $checksum
+  checksumType  = $checksumType
+
+  # MSI
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
