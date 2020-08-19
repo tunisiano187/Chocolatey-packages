0f4d9556faeb665206ad0712b9a8ea324ce6058a
Tunisiano18 <helpdesk.choc@gmail.com>
2020-08-19 20:20:38 +0000

    AU: 1 updated - faceit [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/32b8c3a95ae3615c0446d18182fa8707c5fd4216

--- automatic/faceit/tools/chocolateyinstall.ps1
+++ automatic/faceit/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url            = 'https://cdn.faceit.com/anticheat/FACEITInstaller_64.exe'
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://faceit-client.faceit-cdn.net/release/FACEIT-setup-latest.exe'
+$checksum       = '133a84e225fb159cea61c41b152193076b4e859380fea22fa905b3ba8c5429b8'
+$checksumType   = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
