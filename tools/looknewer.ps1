7cb838121fa191b52573400020fb5af923049aca
Tunisiano187 <fabian@bowlman.org>
2020-08-22 22:14:17 +0200

    appveyor

--- tools/looknewer.ps1
+++ tools/looknewer.ps1
@@ -1,21 +1,21 @@
 $ErrorActionPreference = 'Continue';
-
-$source = Join-Path $(Get-Location) "Check/list.txt"
+$source = Join-Path $PSScriptRoot "Check/list.txt"
+Write-Host $source
 if((Test-Path $source)) {
     $search = (Get-Content $source | Select-Object -First 1).split(' ')[0]
-    if(!(Test-Path "$(Get-location)/../automatic/$search")) {
+    if(!(Test-Path "$($PSScriptRoot)/../automatic/$search")) {
         $winout = (winget.exe search $search)
         if($($winout[-1] -replace(' {2,}',' ')).split(' ')[-3] -ne "matching") {
             $($winout[-1] -replace(' {2,}',' ')).split(' ')[-3]
         } else {
             Write-host "$search not available on winget"
             Get-Content $source | Add-Content "Check/Todo.txt"
-            get-content $source | Select-Object -Skip 1 | set-content "$source-temp"
+            Get-Content $source | Select-Object -Skip 1 | set-content "$source-temp"
             Move-Item "$source-temp" $source -Force
         }
     } else {
         Write-host "$search already maintained here"
-        get-content $source | Select-Object -Skip 1 | set-content "$source-temp"
+        Get-Content $source | Select-Object -Skip 1 | set-content "$source-temp"
         Move-Item "$source-temp" $source -Force
     }
     git add $source
