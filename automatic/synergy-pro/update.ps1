b4576992cd3342bd5bd1e920bfca79976ce9b6a4
Fabian Clerbois <clerf@technocite.be>
2020-08-24 12:45:03 +0200

    1.11.1

--- automatic/synergy-pro/update.ps1
+++ automatic/synergy-pro/update.ps1
@@ -20,7 +20,7 @@ function global:au_GetLatest {
 	$url32="https://binaries.symless.com/synergy/v1-core-standard/v1.11.1-stable-55ec3105/synergy_v1.11.1-stable_b145-55ec3105_windows_x86.msi"
 	$url64="https://binaries.symless.com/synergy/v1-core-standard/v1.11.1-stable-55ec3105/synergy_v1.11.1-stable_b169-55ec3105_windows_x64.msi"
 
-	$version=""
+	$version="1.11.1"
 	Write-Output "Version : $version"
 
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
