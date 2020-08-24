8f2672ad086937a8d34b6af5dc1694eb022ed487
Tunisiano187 <fabian@bowlman.org>
2020-08-24 17:46:10 +0200

    Synergy-pro

--- automatic/synergy-pro/tools/chocolateyInstall.ps1
+++ automatic/synergy-pro/tools/chocolateyInstall.ps1
@@ -1,21 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32            = ''
+$url64            = ''
+$checksum32       = ''
+$checksum64       = ''
+$checksumType32   = ''
+$checksumType64   = ''
 
 $InstallArgs = @{
    packageName    = $env:ChocolateyPackageName
    fileType       = 'msi'
-   url            = ''
-   url64bit       = ''
-   Checksum       = ''
-   ChecksumType   = ''
-   Checksum64     = ''
-   ChecksumType64 = ''
+   url            = $url32
+   url64bit       = $url64
+   Checksum       = $checksum32
+   ChecksumType   = $checksum64
+   Checksum64     = $checksumType32
+   ChecksumType64 = $checksumType64
    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
    validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @InstallArgs
-
-
-
--- automatic/synergy-pro/update.ps1
+++ automatic/synergy-pro/update.ps1
@@ -1,17 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = "https://members.symless.com/synergy/download/direct?platform=windows&architecture=x"
-
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(Url\s*=\s*)('.*')"        	= "`$1'$($Latest.Url32)'"
-			"(Checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
-			"(ChecksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
-			"(Url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.Url64)'"
-			"(Checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
-			"(ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
 }
@@ -21,11 +19,9 @@ function global:au_GetLatest {
 	$url64="https://binaries.symless.com/synergy/v1-core-standard/v1.11.1-stable-55ec3105/synergy_v1.11.1-stable_b169-55ec3105_windows_x64.msi"
 
 	$version="1.11.1"
-	Write-Output "Version : $version"
 
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
 }
 
 update
-Test-Package
