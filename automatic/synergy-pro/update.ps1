41009964137d9df3d3171236cd6cd76eecddc90e
Tunisiano187 <fabian@bowlman.org>
2020-08-24 17:52:40 +0200

    32

--- automatic/synergy-pro/update.ps1
+++ automatic/synergy-pro/update.ps1
@@ -4,9 +4,9 @@ import-module au
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url32\s*=\s*)('.*')"      	= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
 			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
