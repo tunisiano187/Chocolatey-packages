f62b516ad5b7f5002c3dc46cb29432d9763bbf6d
Tunisiano187 <fabian@bowlman.org>
2020-08-28 18:14:47 +0200

    No Force

--- automatic/sd-card-formatter/update.ps1
+++ automatic/sd-card-formatter/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
 	$url32 = "https://www.sdcard.org/downloads/formatter/eula_windows/$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_ -match 'Accept'} | Where-Object {$_.href -match '.zip'}).href.replace('./',''))"
 	$File = "$env:TEMP/sdc.zip"
 	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
-	Expand-Archive $File -DestinationPath $env:TEMP\sdc
+	Expand-Archive $File -DestinationPath $env:TEMP\sdc -Force
 	$version = $(Get-ChildItem -Path $env:TEMP\sdc -Filter *.exe -Recurse)[0].Name.split(' ')[-2]
 
 	return @{ URL32 = $url32
@@ -25,4 +25,4 @@ function global:au_GetLatest {
 	}
 }
 
-update -ChecksumFor 32 -NoCheckChocoVersion
+update -ChecksumFor 32
