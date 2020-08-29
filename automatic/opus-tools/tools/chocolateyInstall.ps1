07155f8c7d6d57a7642c4c5909a0322790f4d872
Tunisiano187 <fabian@bowlman.org>
2020-08-29 09:28:58 +0200

    32

--- automatic/opus-tools/tools/chocolateyInstall.ps1
+++ automatic/opus-tools/tools/chocolateyInstall.ps1
@@ -3,15 +3,9 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://archive.mozilla.org/pub/opus/win32/opus-tools-0.2-win32.zip'
 $checksum = '99a7c07c46c24727d9798adfc53f5d1d82244e3c249476c22ddf0e1d76a7c86c'
 $checksumType = 'sha256'
-$url64 = 'https://archive.mozilla.org/pub/opus/win64/opus-tools-0.2-win64.zip'
-$checksum64 = '84e643f1cc3fd26fc743ea7830392de765507f72846e24719804d3d07f1ad96f'
-$checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
-                             -url64bit "$url64" `
-                             -checksum64 "$checksum64" ´
-                             -checksumType64 "$checksumType64"
+                             -ChecksumType "$checksumType"
--- automatic/opus-tools/update.ps1
+++ automatic/opus-tools/update.ps1
@@ -9,9 +9,6 @@ function global:au_SearchReplace {
 			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
-			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
-			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
-			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
 }
@@ -19,14 +16,11 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$urls = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match 'opus/win'} | Where-Object {$_.href -match '-win'}).href
 	$url32 = $urls[0]
-	$url64 = $urls[1]
 	$version = $url32.split('-')[-2]
 
 	return @{ 	URL32 = $url32
-				URL64 = $url64
 				Version = $version
 	}
 }
 
-update -NoCheckChocoVersion
-Test-Package
+update -ChecksumFor 32 -NoCheckChocoVersion
