b59d6f3a3051bc26d2f241bb40d6a49a8bf8b8ce
Tunisiano187 <fabian@bowlman.org>
2020-08-29 09:00:46 +0200

    Test

--- automatic/opus-tools/update.ps1
+++ automatic/opus-tools/update.ps1
@@ -6,11 +6,11 @@ $releases = 'https://www.opus-codec.org/downloads/'
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-			"(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
@@ -22,10 +22,11 @@ function global:au_GetLatest {
 	$url64 = $urls[1]
 	$version = $url32.split('-')[-2]
 
-	return @{ URL32 = $url32
-		URL64 = $url64
-		Version = $version
+	return @{ 	URL32 = $url32
+				URL64 = $url64
+				Version = $version
 	}
 }
 
 update -NoCheckChocoVersion
+Test-Package
