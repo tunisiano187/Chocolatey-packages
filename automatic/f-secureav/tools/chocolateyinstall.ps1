d4e4272cf2cdf28c2b2d8f962261611e4699458d
Tunisiano187 <fabian@bowlman.org>
2020-08-30 12:57:56 +0200

    Splatting

--- automatic/f-secureav/tools/chocolateyinstall.ps1
+++ automatic/f-secureav/tools/chocolateyinstall.ps1
@@ -3,16 +3,20 @@ $url32          = 'https://download.sp.f-secure.com/SE/Retail/installer/F-Secure
 $checksum32     = 'fe002d5581ca298946b9ce63190012914bc35a7717a6d4b6ce6e1ac1268fd62a'
 $checksumType32 = 'sha256'
 
-$osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
-if($osInfo.ProductType -eq 1) {
-    $packageArgs = @{
-        url             = $url32
-        fileType        = 'exe'
-        softwareName    = $env:ChocolateyPackageName
-        packageName     = $env:ChocolateyPackageName
-        silentArgs      = '/s'
-        checksum        = $checksum32
-        checksumType    = $checksumType32
-    }
-    Install-ChocolateyPackage @packageArgs
+$packageArgs = @{
+    url             = $url32
+    fileType        = 'exe'
+    softwareName    = $env:ChocolateyPackageName
+    packageName     = $env:ChocolateyPackageName
+    silentArgs      = '/s'
+    checksum        = $checksum32
+    checksumType    = $checksumType32
+}
+
+$OSIsServerVersion = if ([Int]3 -eq [Int](Get-CimInstance -Class Win32_OperatingSystem).ProductType) {$True} else {$False}
+if($OSIsServerVersion) {
+  Write-Warning "System not supported"
+  exit 0
+} else {
+  Install-ChocolateyPackage @packageArgs
 }
--- automatic/pinginfoview/tools/chocolateyInstall.ps1
+++ automatic/pinginfoview/tools/chocolateyInstall.ps1
@@ -1,16 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName = '{{PackageName}}'
+$packageName = >$env:ChocolateyPackageName
 $url = 'https://www.nirsoft.net/utils/pinginfoview.zip'
 $checksum = '9c074b15ff6d0bbf74f668a51907581f85eabec7cbaec42e4a03454719a95f45'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$packageArgs = @{
+    packageName     = $env:ChocolateyPackageName
+    url             = $url
+    UnzipLocation   = $toolsDir
+    softwareName    = '$packageName*'
+    checksum        = $checksum
+    checksumType    = $checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs
 
 Set-Content -Path ("$installFile.gui") `
             -Value $null
