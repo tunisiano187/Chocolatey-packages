d0bb0db21af030354e01d76b8bc4996d20d672d3
Tunisiano187 <fabian@bowlman.org>
2020-08-30 15:49:13 +0200

    ChecksumType

--- automatic/xolidosigndesktop/tools/chocolateyInstall.ps1
+++ automatic/xolidosigndesktop/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $packageName    = $env:ChocolateyPackageName
 $installerType  = 'EXE'
 $url32          = 'https://www.xolido.com/instaladores/SetupXolidoSign.exe'
-$checksumtype   = 'sha256'
 $checksum32     = '6cd3860665666a9efd13fd5ca33ee41807698f9c06788781094152811bf0db25'
+$checksumType   = 'sha256'
 
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
--- automatic/xolidosigndesktop/update.ps1
+++ automatic/xolidosigndesktop/update.ps1
@@ -8,6 +8,7 @@ function global:au_SearchReplace {
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
      }
 }
