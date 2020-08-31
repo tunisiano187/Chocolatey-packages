dd6ad4ce4b399e791f405747f4c10bba754b26ca
tunisiano187 <helpdesk.choc@gmail.com>
2020-08-31 12:15:30 +0000

    AU: 1 updated - citrix-sharefile-sync [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/3904cb186a00a5e9d4560b5f46d54da54b681292

--- automatic/citrix-sharefile-sync/tools/chocolateyInstall.ps1
+++ automatic/citrix-sharefile-sync/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 $installerType 			= 'MSI'
-$url 					= 'https://downloads.citrix.com/7110/ShareFileSync32_v3.24.106.0.msi?__gda__=1598876779_51df14cd9649b49c663362bb6f0bc6a2'
-$checksum 				= 'dcf05efb9b81ae50fb6d0b0cd6b983866ab79d93544c48ed8c010fef0f0d09ed'
-$checksumType 			= 'sha256'
-$url64 					= 'https://downloads.citrix.com/7110/ShareFileSync64_v3.24.106.0.msi?__gda__=1598876779_af8dd3d560d30426a7f328252abea25d'
-$checksum64 			= '4a4d085d22f6743b84f766d91395dd0b4e0ba2a2963257c1851e4ce7fbadec1c'
-$checksumType64 		= 'sha256'
+$url 					= 'https://downloads.citrix.com/7110/ShareFileSync32_v3.24.106.0.msi?__gda__=1598879382_c38c390a73da6daa17fbe6e449e1eb88'
+$checksum 				= ''
+$checksumType 			= ''
+$url64 					= 'https://downloads.citrix.com/7110/ShareFileSync64_v3.24.106.0.msi?__gda__=1598879382_6111f17bb03d1116452b67051247ebd5'
+$checksum64 			= ''
+$checksumType64 		= ''
 $silentArgs 			= '/quiet'
 $validExitCodes 		= @(0)
 
