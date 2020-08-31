97fb13fc99efb8a58a431402f7710535d8db0cd3
Tunisiano187 <fabian@bowlman.org>
2020-08-31 09:54:32 +0200

    Bitcoin

---
+++ automatic/bitcoin.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName    = $env:ChocolateyPackageName
+$installerType  = 'exe'
+$silentArgs     = '/S'
+$url64          = '{{DownloadUrlx64}}'
+$checksum64     = '{{Checksumx64}}'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/bitcoin.portable/update.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://github.com/bitcoin/bitcoin/releases'
+
+if ($MyInvocation.InvocationName -ne '.') {
+    function global:au_SearchReplace {
+        @{
+            'tools/chocolateyInstall.ps1' = @{
+                "(^[$]url64\s*=\s*)('.*')"            = "`$1'$($Latest.URL64)'"
+                "(^[$]checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
+                "(^[$]checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
+            }
+        }
+    }
+}
+
+function global:au_GetLatest {
+    Write-Verbose 'Get version'
+    $version = $(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match ".zip"} | Select-Object -First 1).href).split('/')[-1].replace('.zip', '').replace('v','')
+	Write-Verbose 'Get files'
+	$url64 = $("https://bitcoincore.org/bin/bitcoin-core-0.20.1/bitcoin-0.20.1-win64.zip").replace('0.20.1',$version)
+
+    return @{ URL64 = $url64; Version = $version }
+}
+
+update -ChecksumFor 64
