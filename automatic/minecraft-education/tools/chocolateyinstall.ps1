fc717cf0093ae9032a4c3b715158fd6de5f36374
Tunisiano187 <fabian@bowlman.org>
2020-08-31 12:46:26 +0200

    Allow reboot

--- automatic/minecraft-education/tools/chocolateyinstall.ps1
+++ automatic/minecraft-education/tools/chocolateyinstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   url             = $url
 
   silentArgs      = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
-  validExitCodes  = @(0, 3010, 1641, 1622)
+  validExitCodes  = @(0, 1603, 3010, 1641, 1622)
 
   softwareName    = "$packageName*"
   checksum        = $checksum
