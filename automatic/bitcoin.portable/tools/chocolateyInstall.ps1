2f711f3878843fd8b46561583d92f22b9cbb971b
tunisiano187 <helpdesk.choc@gmail.com>
2020-08-31 10:04:39 +0000

    AU: 2 updated - bitcoin.install bitcoin.portable [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/b8067960d1ee8eaa1b0a4756716c35e3e1e5a5b0

--- automatic/bitcoin.portable/tools/chocolateyInstall.ps1
+++ automatic/bitcoin.portable/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = '{{DownloadUrl}}'
 $checksum = '{{Checksum}}'
 $checksumType = 'sha256'
-$url64 = '{{DownloadUrlx64}}'
-$checksum64 = '{{Checksumx64}}'
+$url64 = 'https://bitcoincore.org/bin/bitcoin-core-0.20.1/bitcoin-0.20.1-win64.zip'
+$checksum64 = 'e59fba67afce011d32b5d723a3a0be12da1b8a34f5d7966e504520c48d64716d'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
