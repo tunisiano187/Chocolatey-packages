927da7ffb8a0cc21e9cbc42471221a951e3fa8ca
Tunisiano187 <fabian@bowlman.org>
2020-08-31 16:58:13 +0200

    <

--- automatic/pinginfoview/tools/chocolateyInstall.ps1
+++ automatic/pinginfoview/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName = >$env:ChocolateyPackageName
+$packageName = $env:ChocolateyPackageName
 $url = 'https://www.nirsoft.net/utils/pinginfoview.zip'
 $checksum = '9c074b15ff6d0bbf74f668a51907581f85eabec7cbaec42e4a03454719a95f45'
 $checksumType = 'sha256'
