3300f159a3c4eaa655f9072c8ed5bd7e6227e063
Tunisiano187 <fabian@bowlman.org>
2020-09-01 19:11:01 +0200

    Latest

--- automatic/citrix-sharefile-sync/update.ps1
+++ automatic/citrix-sharefile-sync/update.ps1
@@ -6,10 +6,8 @@ $releases = 'https://www.citrix.com/downloads/sharefile/clients-and-plug-ins/sha
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
-			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
 			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType64\s*=\s*)('.*')"	= "`$1'$($Latest.ChecksumType64)'"
 		}
