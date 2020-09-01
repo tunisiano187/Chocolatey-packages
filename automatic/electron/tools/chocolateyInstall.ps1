ab502a0beae13d4e466cb77deea358e8815900f8
tunisiano187 <helpdesk.choc@gmail.com>
2020-09-01 10:08:24 +0000

    AU: 2 updated - bitcoin.portable electron [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/ae8002a0023f3009053d2a4da47ca74ba627f98b

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v10.1.0/electron-v10.1.0-win32-ia32.zip'
-$checksum = 'c620305c568edbbb4f69532ce7cae01bc887f7fb87fbad8c6679a29b3c45065e'
+$url = 'https://github.com/electron/electron/releases/download/v10.1.1/electron-v10.1.1-win32-ia32.zip'
+$checksum = '79d76181518e9ddd7a242f91b63eb14ea821215b596fce75164bf26000bc9837'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v10.1.0/electron-v10.1.0-win32-x64.zip'
-$checksum64 = '7d438e9023b14440f3e5ad7152b8a763082c5935cdc4a6ce8710b785c217c293'
+$url64 = 'https://github.com/electron/electron/releases/download/v10.1.1/electron-v10.1.1-win32-x64.zip'
+$checksum64 = '8b8171514d53b2edc9a79a2094fb150b1ccb6ad363eb41ecdee8ce4a68907b06'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
