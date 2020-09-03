f5cb41a4f915c45243f7c6b6da8f12bc413b2be1
Tunisiano187 <fabian@bowlman.org>
2020-09-03 09:36:47 +0200

    64 bit only

--- automatic/bitcoin.portable/tools/chocolateyInstall.ps1
+++ automatic/bitcoin.portable/tools/chocolateyInstall.ps1
@@ -1,18 +1,12 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = $env:ChocolateyPackageName
-$checksum = '{{Checksum}}'
-$checksumType = 'sha256'
 $url64 = 'https://bitcoincore.org/bin/bitcoin-core-0.20.1/bitcoin-0.20.1-win64.zip'
 $checksum64 = 'e59fba67afce011d32b5d723a3a0be12da1b8a34f5d7966e504520c48d64716d'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
                              -UnzipLocation "$toolsDir" `
                              -Url64bit "$url64" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
                              -Checksum64 "$checksum64" `
                              -ChecksumType64 "$checksumType64"
 
