df38ddced67560647c23598dc92ef98b7fe88faf
tunisiano187 <helpdesk.choc@gmail.com>
2020-09-05 08:42:00 +0000

    AU: 1 updated - digikam [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/5a68c7adbdbfaa9a02342ec48f6b0e29de073152

--- automatic/digikam/tools/chocolateyInstall.ps1
+++ automatic/digikam/tools/chocolateyInstall.ps1
@@ -2,12 +2,12 @@
 $packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '/S'
-$url32          = 'https://download.kde.org/stable/digikam/7.0.0/digiKam-7.0.0-Win32.exe'
-$checksum32     = '5e3e7f6a196873d0d0391547badd06ea1d4cb627852cf0fd1b3037d3af4a7690'
+$url32          = 'https://download.kde.org/stable/digikam/7.1.0/digiKam-7.1.0-Win32.exe'
+$checksum32     = 'b413c1e37ed64fad48e4ea5f3cd1de5fe51c281da4e0eccff3c8a117edbc02b8'
 $checksumType32 = 'sha256'
-$url64          = 'https://download.kde.org/stable/digikam/7.0.0/digiKam-7.0.0-Win64.exe'
-$checksum64     = ''
-$checksumType64 = ''
+$url64          = 'https://download.kde.org/stable/digikam/7.1.0/digiKam-7.1.0-Win64.exe'
+$checksum64     = '51ca55fbf608d42ea5fb81eeca9592a385755a33ab959dcfdf8d1d7151776cf3'
+$checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
