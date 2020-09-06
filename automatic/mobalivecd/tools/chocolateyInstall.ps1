6e9ee6abe83df68a7cffdfeef364d4eb1238ec46
Tunisiano187 <fabian@bowlman.org>
2020-09-06 13:50:23 +0200

    MobaliveCD

---
+++ automatic/mobalivecd/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿try {
+  $toolsDir     = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $url          = 'http://mobalivecd.mobatek.net/en/MobaLiveCD_v2.1.exe'
+  $checksum     = ''
+  $checksumType = ''
+
+  #Install-ChocolateyPackage 'mobalivecd' 'EXE' '/SILENT' "$downUrl" -validExitCodes @(0)
+
+  $packageArgs = @{
+    packageName     = $env:ChocolateyPackageName
+    url             = $url
+    checksum        = $checksum
+    checksumType    = $checksumType
+    FileFullPath    = "${toolsDir}\MobaLiveCD_v2.1.exe"
+  }
+
+  Get-ChocolateyWebFile @packageArgs
+  Install-ChocolateyDesktopLink "${toolsDir}\MobaLiveCD_v2.1.exe"
+} catch {
+  throw "$($_.Exception.Message)"
+}
---
+++ automatic/mobalivecd/update.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://www.mobatek.net/labs_mobalivecd.html'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$url32 = "https://www.mobatek.net$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.exe'} | Sort-Object | Where-Object {$_.href -notmatch 'Sources'}).href)"
+	$version=$url32.split('/')[-1].split('v')[-1].replace('.exe','')
+	if($version -eq '2.1') {
+		$version = '2.1.0.1'
+	}
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32 -NoCheckChocoVersion
