b04bdba45ec57194259f93c96ee7fc9bfad58f1a
Tunisiano187 <fabian@bowlman.org>
2020-09-07 13:38:40 +0200

    [skip ci] H2Database

---
+++ automatic/h2database/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$url          = 'http://www.h2database.com/h2-setup-2012-07-13.exe'
+$checksum     = ''
+$checksumType = ''
+
+$packageArgs  = @{
+  fileType      	= 'EXE'
+  url             = $url
+  checksum        = $checksum
+  checksumType    = $checksumType
+  silentArgs    	= '/S'
+  validExitCodes	= @(0)
+}
+
+Install-ChocolateyPackage $packageArgs
---
+++ automatic/h2database/update.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'http://www.h2database.com/html/main.html'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url32\s*=\s*)('.*')"      	= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	Write-Output 'Check Folder'
+	$url32 = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.exe'}).href | Select-Object -First 1)
+
+	Write-Output 'Checking version'
+	$rssversion = 'https://h2database.com/html/newsfeed-rss.xml'
+	[xml]$result=(Invoke-WebRequest -Uri $rssversion -UseBasicParsing).Content
+	$version=$result.rss.channel.item[0].title.split(':|(')[1].trim()
+	Write-Output "Version : $version"
+
+	return @{ URL32 = $url32; Version = $version }
+}
+
+update -ChecksumFor 32
