0e98c9929c4214eb173f622f8436701319b3e9cf
tunisiano187 <helpdesk.choc@gmail.com>
2020-09-08 12:14:55 +0000

    AU: 2 updated - citrix-sharefile-sync pinginfoview [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/1e03d124315e8d32303d1cd1457e4d93598da56d

--- automatic/pinginfoview/tools/chocolateyInstall.ps1
+++ automatic/pinginfoview/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
 $url = 'https://www.nirsoft.net/utils/pinginfoview.zip'
-$checksum = '9c074b15ff6d0bbf74f668a51907581f85eabec7cbaec42e4a03454719a95f45'
+$checksum = '05c894741b3b9d0ead16b1d4f78cbe30efbeda1eeba43666aa7ec8c950730209'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
