2817fb395109cafe29ca0040ca7f6170df819434
Tunisiano187 <fabian@bowlman.org>
2020-09-08 17:53:31 +0200

    exe

--- automatic/citrix-sharefile-sync/tools/chocolateyInstall.ps1
+++ automatic/citrix-sharefile-sync/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
-$installerType 			= 'msi'
+$installerType 			= 'exe'
 $checksum 				= 'a8fcb8a7f50800c3c47bc90ade56437398610c6a9a8c54323716bfa9dc2ae33c'
 $checksumType 			= 'sha256'
 $checksum64 			= 'a8fcb8a7f50800c3c47bc90ade56437398610c6a9a8c54323716bfa9dc2ae33c'
 $checksumType64 		= 'sha256'
-$silentArgs 			= '/quiet /qn'
+$silentArgs 			= '/s /v"/qn"'
 $validExitCodes 		= @(0)
 
 $url 					= 'https://dl.sharefile.com/sync2win'
