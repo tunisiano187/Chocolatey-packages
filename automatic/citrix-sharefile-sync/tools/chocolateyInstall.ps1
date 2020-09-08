fcdf308dc080e074bfdcb662894df695289c5ce3
Tunisiano187 <fabian@bowlman.org>
2020-09-08 11:14:50 +0200

    /quiet /qn

--- automatic/citrix-sharefile-sync/tools/chocolateyInstall.ps1
+++ automatic/citrix-sharefile-sync/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $checksum 				= 'a8fcb8a7f50800c3c47bc90ade56437398610c6a9a8c54323716bfa9dc2ae33
 $checksumType 			= 'sha256'
 $checksum64 			= 'a8fcb8a7f50800c3c47bc90ade56437398610c6a9a8c54323716bfa9dc2ae33c'
 $checksumType64 		= 'sha256'
-$silentArgs 			= '/s /v"/qn"'
+$silentArgs 			= '/quiet /qn'
 $validExitCodes 		= @(0)
 
 $url 					= 'https://dl.sharefile.com/sync2win'
