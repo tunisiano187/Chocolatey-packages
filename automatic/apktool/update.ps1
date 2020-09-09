455f0e6e70a9b8154a6a56ce030970bc1ccf40d6
Tunisiano187 <fabian@bowlman.org>
2020-09-09 14:10:37 +0200

    APKTool

---
+++ automatic/apktool/update.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://github.com/iBotPeaches/Apktool/releases'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      			= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 			= "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumtype\s*=\s*)('.*')" 		= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	Write-Verbose 'Get files'
+	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.jar'} | Select-Object -First 1).href
+	Write-Verbose 'Checking version'
+	$version=$($url32).split('_')[-1].replace('.jar','')
+
+	Write-Verbose "Version : $version"
+	$url32 = "https://github.com$($url32)";
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
