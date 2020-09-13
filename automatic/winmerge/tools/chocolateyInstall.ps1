56c3ba7a332d1c022259ad1ac91187e920832288
tunisiano187 <helpdesk.choc@gmail.com>
2020-09-13 06:33:38 +0000

    AU: 1 updated - winmerge [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/ca2057a4e30fb0b4d695331e04131bae58947892

--- automatic/winmerge/tools/chocolateyInstall.ps1
+++ automatic/winmerge/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName    = 'WinMerge'
-$url32          = 'https://github.com//WinMerge/winmerge/releases/download/2.16.8/WinMerge-2.16.8-Setup.exe'
+$url32          = 'https://github.com/WinMerge/winmerge/releases/download/2.16.8/WinMerge-2.16.8-Setup.exe'
 $checksum32     = '0a2eebcd8a7ca35f7787a376e83b44f3650a07229b7ed96f1e986b9567dca89b'
 $checksumType32 = 'sha256'
-$url64          = 'https://github.com//WinMerge/winmerge/releases/download/2.16.8/WinMerge-2.16.8-x64-PerUser-Setup.exe'
-$checksum64     = '8c08c684e356e57e1b6a8c76ff131b048a9e389f9fc41162be8f93d4f232ba1b'
+$url64          = 'https://github.com/WinMerge/winmerge/releases/download/2.16.8/WinMerge-2.16.8-x64-Setup.exe'
+$checksum64     = 'eeb07fc8252fa6e54a6998b216b028db38df92ce268e23f9c5183ff57b5e95b7'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
