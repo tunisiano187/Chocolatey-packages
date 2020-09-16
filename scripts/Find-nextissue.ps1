cf274eac5700da5c1d7a055d08c19447d0d5a07c
Tunisiano187 <fabian@bowlman.org>
2020-09-16 17:42:24 +0200

    Folder

--- scripts/Find-nextissue.ps1
+++ scripts/Find-nextissue.ps1
@@ -9,10 +9,9 @@ $UserToken = $env:github_api_key
 
 Write-Output "Search the next package to import"
 $search = (Find-GitHubIssue -Type issue -Repo "$Owner/$Repository" -State open -Labels "ToCreateManualy" | Sort-Object -Property Number).Title.split('(|)')[1]
-$folder = Join-Path $PSScriptRoot "automatic/$search"
-Write-Output $folder
+$folder = Join-Path $PSScriptRoot "../automatic/$search"
 if(Test-Path $folder) {
-    Write-Warning "Package in the folder, the package $search needs to be finished and the issue closed"
+    Write-Warning "Package already in the folder, the package $search needs to be finished and the issue closed"
 } else {
     #git add :/automatic/$search/
     #git commit -m "Package $search downloaded"
