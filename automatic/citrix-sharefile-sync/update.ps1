2f9c969585c9bf2aafbc2c92967b2b85026191b4
Tunisiano187 <helpdesk.choc@gmail.com>
2020-09-16 14:11:30 +0200

    checksum 32 only

--- automatic/citrix-sharefile-sync/update.ps1
+++ automatic/citrix-sharefile-sync/update.ps1
@@ -8,8 +8,6 @@ function global:au_SearchReplace {
 		'tools/chocolateyInstall.ps1' = @{
 			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
-			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
-			"(^[$]checksumType64\s*=\s*)('.*')"	= "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
 }
