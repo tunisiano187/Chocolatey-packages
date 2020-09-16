6cdc46727dbc72d736a918d224e9369306334902
Tunisiano187 <helpdesk.choc@gmail.com>
2020-09-16 14:04:41 +0200

    url32

--- automatic/citrix-sharefile-sync/update.ps1
+++ automatic/citrix-sharefile-sync/update.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://dl.sharefile.com/sync2win'
+$releases = 'https://www.citrix.com/downloads/sharefile/clients-and-plug-ins/sharefile-sync-for-windows.html'
 
 function global:au_SearchReplace {
 	@{
@@ -18,7 +18,8 @@ function global:au_GetLatest {
 	$urls=$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.rel -match '.msi'}).rel)
 	$url32 = "https:$($urls[0])"
 
-    $version = $url32.split('?')[0].split('v')[-1].replace('.msi','')
+	$version = $url32.split('?')[0].split('v')[-1].replace('.msi','')
+	$url32 = "https://dl.sharefile.com/sync2win"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 
