49db9404f8803f896669526edeb00dd27b75a587
Tunisiano187 <fabian@bowlman.org>
2020-09-19 19:30:03 +0200

    IDrive

---
+++ automatic/idrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$url			= 'https://www.odrive.com/downloaddesktop?platform=win'
+$checksum		= 'fedb86168c71f7071dd077cfd982d8d55b391334e96fce1570087a017a38a7b6'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0,3010)
+  softwareName  = 'idrive*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/idrive/update.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://www.idrive.com/thankyou_win'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$url32 = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.exe'}).href | Select-Object -First 1)
+
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "Odrive.exe"
+	Invoke-WebRequest -Uri $url32 -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
