5cfe5e8d65f5fad2b2562ba070fd75396a3d563a
tunisiano187 <helpdesk.choc@gmail.com>
2020-09-20 08:50:04 +0000

    AU: 1 updated - scite [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/9936e11706cc2a1a9c59bb6ced99e198866ddb0c

--- automatic/scite/tools/chocolateyInstall.ps1
+++ automatic/scite/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 $packageName = $env:ChocolateyPackageName
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
-$url = 'https://www.ebswift.com/uploads/7/0/4/0/70403747/scite-4.4.4x86.msi'
-$checksum = '089605241148fbce117c3218a209e0956b9b74b1fd6abff01af494a7a7fb9bee'
+$url = 'https://www.ebswift.com/uploads/7/0/4/0/70403747/scite-4.4.5x86.msi'
+$checksum = '1206797d004fd70449081b1d15364b3d1e182f82e7785e66bcec2dd8435a2469'
 $checksumType = 'sha256'
-$url64 = 'https://www.ebswift.com/uploads/7/0/4/0/70403747/scite-4.4.4x64.msi'
-$checksum64 = 'd75c4d1e122df6172af018892caa3fbac58716b59e05bf9d98953aedce6b6853'
+$url64 = 'https://www.ebswift.com/uploads/7/0/4/0/70403747/scite-4.4.5x64.msi'
+$checksum64 = 'adfcbe6398a3e3582f05f198770fde6a61de27bd879fbbeb4370eeb5549c3735'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
