a3f12388b266ad9359ee3f5b052d46ae614562bc
tunisiano187 <helpdesk.choc@gmail.com>
2020-09-21 19:13:10 +0000

    AU: 1 updated - lbry [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/dd7d8054794edeb8a3976c4b1067379f8c8259b7

--- automatic/lbry/tools/chocolateyinstall.ps1
+++ automatic/lbry/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/lbryio/lbry-desktop/releases/download/v0.48.0-rc.1/LBRY_0.48.0-rc.1.exe'
-$checksum       = 'b962679efd2109a9d20028350f0319d563969ca2d6b224ba66e409629cc41c67'
+$url            = 'https://github.com/lbryio/lbry-desktop/releases/download/v0.48.0-rc.2/LBRY_0.48.0-rc.2.exe'
+$checksum       = '9c5499cc58a1450fe6c6b32766855bfd314a892ad205fae1f8d9a57b917de153'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
