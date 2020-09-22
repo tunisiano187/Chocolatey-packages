328e6130e446c7c53bfb8d21bf49ff76a7eff0e8
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-09-22 14:15:06 +0200

    Update update.ps1

--- automatic/dd/update.ps1
+++ automatic/dd/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
 	Write-Verbose 'Get files'
 	$url = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '.zip'} | Where-Object {$_.href -notmatch '.src'} | Where-Object {$_.href -match '0.5'}| Select-Object -First 1).href
 	Write-Verbose 'Checking version'
-	$version = $url.Split('-')[-1].replace('.zip',$env:ChocolateyPackageName).replace('beta','-beta')
+	$version = $url.Split('-')[-1].replace('.zip','').replace('beta','-beta')
 	Write-Verbose "Version : $version"
 	$url32 = "http://www.chrysocome.net/$($url)";
 
