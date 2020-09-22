ed19d1be50c30dbd878445dd1e0e94100f1c89c6
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-09-22 14:39:00 +0200

    Update update.ps1

--- automatic/wakemeonlan/update.ps1
+++ automatic/wakemeonlan/update.ps1
@@ -19,7 +19,7 @@ function global:au_GetLatest {
 	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
 	Expand-Archive $File -DestinationPath .\wol
 
-	$version=$(Get-Content .\wol\readme.txt | Where-Object {$_ -match 'WakeMeOnLan'})[0].split(' ')[-1].Replace('v',$env:ChocolateyPackageName)
+	$version=$(Get-Content .\wol\readme.txt | Where-Object {$_ -match 'WakeMeOnLan'})[0].split(' ')[-1].Replace('v','')
 	Remove-Item -Recurse .\wol -Force
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
