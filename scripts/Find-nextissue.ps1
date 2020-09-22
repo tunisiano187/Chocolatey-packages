f7ec8adfabbcd2920188e1f909658568b59550cc
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-09-22 15:03:17 +0200

    Update Find-nextissue.ps1

--- scripts/Find-nextissue.ps1
+++ scripts/Find-nextissue.ps1
@@ -8,7 +8,7 @@ if(!(Test-Path Env:github_api_key)) {
 
 Write-Output "Search the next package to import"
 $search = (Find-GitHubIssue -Type issue -Repo "$Owner/$Repository" -State open -Labels "ToCreateManualy" | Sort-Object -Property Number).Title.split('(|)')[1]
-$folder = (Join-Path $PSScriptRoot "../automatic/$search").FullName
+$folder = Join-Path $PSScriptRoot "../automatic/$search"
 if(Test-Path $folder) {
     Write-Warning "Package already in the folder, the package $search needs to be finished and the issue closed"
 } else {
