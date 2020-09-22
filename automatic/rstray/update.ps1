6b43814987fd03d6c5f419d93c999ab1ec75dcbc
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-09-22 14:33:38 +0200

    Update update.ps1

--- automatic/rstray/update.ps1
+++ automatic/rstray/update.ps1
@@ -19,7 +19,7 @@ function global:au_GetLatest {
 	Write-Verbose 'Check Folder'
 	$url32 = $(((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_.href -match '.zip'}).href)[0]
 	Write-Verbose 'Checking version'
-	$version = $url32.split('_')[-1].replace('.zip',$env:ChocolateyPackageName)
+	$version = $url32.split('_')[-1].replace('.zip','')
 	Write-Verbose "Version : $version"
 	$url32 = "https://github.com$($url32)";
 	Invoke-WebRequest -Uri $url32 -OutFile "$(Get-Location)\tools\redshift-tray.zip"
