5b2be3430a22a8f1b0b2af95c236e0007c47df5b
Tunisiano187 <fabian@bowlman.org>
2020-09-29 17:55:24 +0200

    Freac

--- automatic/freac.portable/tools/chocolateyInstall.ps1
+++ automatic/freac.portable/tools/chocolateyInstall.ps1
@@ -1,12 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Software Ideas Modeler.portable'
-$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = $env:ChocolateyPackageName
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64          = 'https://github.com/enzo1982/freac/releases'
+$chcksum64      = ''
+$ChecksumType64 = ''
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64          = 'https://github.com/enzo1982/freac/releases/download/v1.1.2/freac-1.1.2b-windows-x64.zip'
-    Checksum64     = '6B0363CE216DF714E243DD4780C96357C4D95A4CF79F167DF287DB041C393DB3'
-    ChecksumType64 = 'sha256'
+    Url64          = $url64
+    Checksum64     = $chcksum64
+    ChecksumType64 = $ChecksumType64
     UnzipLocation  = $toolsDir
 }
 
--- automatic/freac.portable/update.ps1
+++ automatic/freac.portable/update.ps1
@@ -1,18 +1,21 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 import-module au
 
 $releases = 'https://github.com/enzo1982/freac/releases'
 
 function global:au_SearchReplace {
-   @{
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
     }
 }
 
 function global:au_GetLatest {
     $url32 = "https://github.com$($((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.href -match "-windows.zip"} | Select-Object -First 1).href)"
+    $url64 = $url32.Replace('windows.zip','windows-x64.zip')
     $version = $url32 -split '-' | select-object -Last 1 -Skip 1
     $tags = Invoke-WebRequest 'https://api.github.com/repos/enzo1982/freac/releases' -UseBasicParsing | ConvertFrom-Json
     if($tag.tag_name -match $version) {
