a4a3525b432dc20e5602663e447b3e754b52aae1
Tunisiano187 <fabian@bowlman.org>
2020-09-29 19:30:17 +0200

    case

--- automatic/faceit/update.ps1
+++ automatic/faceit/update.ps1
@@ -8,7 +8,7 @@ function global:au_SearchReplace {
 		'tools/chocolateyInstall.ps1' = @{
 			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumtype\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
 		}
 	}
 }
