cf67b26916b76d74759cfd9bf89a02fa4b9ab318
Tunisiano187 <fabian@bowlman.org>
2020-09-29 06:54:09 +0200

    /latest

--- automatic/rubberduck/update.ps1
+++ automatic/rubberduck/update.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://github.com/rubberduck-vba/Rubberduck/releases'
+$releases = 'https://github.com/rubberduck-vba/Rubberduck/releases/latest'
 
 function global:au_SearchReplace {
     @{
