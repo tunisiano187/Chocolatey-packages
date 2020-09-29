b4191cdbf596e4ddaee52ff8d90fbef394d265c0
Tunisiano187 <fabian@bowlman.org>
2020-09-29 07:03:02 +0200

    Stable

--- automatic/rubberduck/update.ps1
+++ automatic/rubberduck/update.ps1
@@ -16,17 +16,17 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $url32 = "https://github.com$($((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.href -match "Rubberduck.Setup"} | Select-Object -First 1).href)"
     $version = $url32 -split 'v|/' | select-object -Last 1 -Skip 1
-    $tags = Invoke-WebRequest 'https://api.github.com/repos/rubberduck-vba/Rubberduck/releases' -UseBasicParsing | ConvertFrom-Json
-    foreach ($tag in $tags) {
-        if($tag.tag_name -match $version) {
-            if($tag.prerelease -match "true") {
-                $clnt = new-object System.Net.WebClient;
-                $clnt.OpenRead("$($url32)").Close();
-                $date = $([datetime]$clnt.ResponseHeaders["Last-Modified"];).ToString("yyyyMMdd")
-                $version = "$version-pre$($date)"
-            }
-        }
-    }
+    #$tags = Invoke-WebRequest 'https://api.github.com/repos/rubberduck-vba/Rubberduck/releases' -UseBasicParsing | ConvertFrom-Json
+    #if($tag.tag_name -match $version) {
+    #    foreach ($tag in $tags) {
+    #        if($tag.prerelease -match "true") {
+    #            $clnt = new-object System.Net.WebClient;
+    #            $clnt.OpenRead("$($url32)").Close();
+    #            $date = $([datetime]$clnt.ResponseHeaders["Last-Modified"];).ToString("yyyyMMdd")
+    #            $version = "$version-pre$($date)"
+    #        }
+    #    }
+    #}
 
     return @{ URL32 = $url32; Version = $version }
 }
