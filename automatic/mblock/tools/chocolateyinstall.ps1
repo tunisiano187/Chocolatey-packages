c38c2502caabc13208962a34c004e47a76ec9f37
Tunisiano187 <helpdesk.choc@gmail.com>
2020-10-02 11:20:25 +0200

    MBlock

---
+++ automatic/mblock/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$url            = ''
+$checksum       = ''
+$checksumType   = ''
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'mblock*'
+
+  checksum      = $checksum
+  checksumType  = $checksumType
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
