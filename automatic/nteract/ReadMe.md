e6ff2c5689e72e673127648caa1e175239d41e35
Tunisiano187 <fabian@bowlman.org>
2020-10-05 21:53:33 +0200

    Meta

---
+++ automatic/nteract/ReadMe.md
@@ -0,0 +1,17 @@
+[![](https://img.shields.io/chocolatey/v/nteract?color=green&label=nteract)](https://chocolatey.org/packages/nteract) [![](https://img.shields.io/chocolatey/dt/nteract)](https://chocolatey.org/packages/nteract)
+
+## nteract
+nteract is a literate coding environment that supports Python, R, JavaScript and other Jupyter kernels. 
+It wraps up the best of the web based Jupyter notebook and embeds it as a desktop application that allows
+you to open notebooks natively on your system.
+
+Use either with Python v2 or Python v3, standalone or within Anaconda/Miniconda distributions. This package 
+also tries to enable the default IPython kernel for your system:
+
+ * `python -m pip install ipykernel` or `conda install ipykernel`
+ * `python -m ipykernel install --user`
+
+### Package-specific issue
+If this package isn't up-to-date for some days, [Create an issue](https://github.com/tunisiano187/Chocolatey-packages/issues/new/choose)
+
+[![Patreon](https://cdn.jsdelivr.net/gh/tunisiano187/Chocolatey-packages@d15c4e19c709e7148588d4523ffc6dd3cd3c7e5e/icons/patreon.png)](https://www.patreon.com/bePatron?u=39585820)
---
+++ automatic/nteract/update.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://github.com/nteract/nteract/releases'
+
+function global:au_SearchReplace {
+    @{
+         "$($Latest.PackageName).nuspec" = @{
+             "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+         }
+     }
+ }
+
+function global:au_GetLatest {
+    $url32 = "https://github.com$($((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.href -match "nteract-Setup"} | Select-Object -First 1).href)"
+    $version = $url32 -split 'v|/' | select-object -Last 1 -Skip 1
+    $tags = Invoke-WebRequest 'https://api.github.com/repos/nteract/nteract/releases' -UseBasicParsing | ConvertFrom-Json
+    if($tag.tag_name -match $version) {
+        foreach ($tag in $tags) {
+            if($tag.prerelease -match "true") {
+                $clnt = new-object System.Net.WebClient;
+                $clnt.OpenRead("$($url32)").Close();
+                $date = $([datetime]$clnt.ResponseHeaders["Last-Modified"];).ToString("yyyyMMdd")
+                $version = "$version-pre$($date)"
+            }
+        }
+    }
+
+    return @{ URL32 = $url32; Version = $version }
+}
+
+update-package -ChecksumFor 32
