0ffa2c039a22a374a02d7b2657e8cd751207b9df
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-12 17:24:06 +0000

    AU: 1 updated - freac.portable [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/freac.portable/tools/chocolateyInstall.ps1
+++ automatic/freac.portable/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://github.com/enzo1982/freac/releases/download/v1.1.3/freac-1.1.3-windows.zip'
-$checksum       = ''
-$ChecksumType   = ''
+$checksum       = 'e325ce353515ebd86c4b124964ba54d1510fd1e9675e19eed90e460ad002bcab'
+$ChecksumType   = 'sha256'
 $url64          = 'https://github.com/enzo1982/freac/releases/download/v1.1.3/freac-1.1.3-windows-x64.zip'
-$checksum64     = ''
-$ChecksumType64 = ''
+$checksum64     = '585d29c3edba3611582969f3ebe2bd7f5c853990ecdacbda3bab6d2f0a4459e3'
+$ChecksumType64 = 'sha256'
 
 $packageArgs = @{
     PackageName     = $packageName
