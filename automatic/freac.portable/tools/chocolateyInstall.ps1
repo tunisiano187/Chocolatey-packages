14103b89f72f0d383e349a4ee8c5c4886a1a208e
Tunisiano187 <helpdesk.choc@gmail.com>
2020-10-12 19:07:06 +0200

    freac 64

--- automatic/freac.portable/tools/chocolateyInstall.ps1
+++ automatic/freac.portable/tools/chocolateyInstall.ps1
@@ -1,16 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32          = 'https://github.com/enzo1982/freac/releases'
+$checksum32     = ''
+$ChecksumType32 = ''
 $url64          = 'https://github.com/enzo1982/freac/releases'
 $checksum64     = ''
 $ChecksumType64 = ''
 
 $packageArgs = @{
-    PackageName    = $packageName
-    Url64          = $url64
-    Checksum64     = $chcksum64
-    ChecksumType64 = $ChecksumType64
-    UnzipLocation  = $toolsDir
+    PackageName     = $packageName
+    Url             = $url
+    Checksum        = $checksum
+    ChecksumType    = $ChecksumType
+    Url64           = $url64
+    Checksum64      = $chcksum64
+    ChecksumType64  = $ChecksumType64
+    UnzipLocation   = $toolsDir
 }
 
 # Download and unzip into a temp folder
--- automatic/freac.portable/update.ps1
+++ automatic/freac.portable/update.ps1
@@ -6,9 +6,12 @@ $releases = 'https://github.com/enzo1982/freac/releases'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
