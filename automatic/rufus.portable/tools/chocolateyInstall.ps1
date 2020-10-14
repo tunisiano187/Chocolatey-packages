71185599eb0eb225c989f81d9f815ee3ce32fb63
Tunisiano187 <helpdesk.choc@gmail.com>
2020-10-14 17:31:54 +0200

    Force

--- automatic/rufus.portable/tools/chocolateyInstall.ps1
+++ automatic/rufus.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 
-$url            = 'https://github.com/pbatard/rufus/releases/download/v3.11/rufus-3.11p.exe'
-$checksum       = 'a43bf34285d393f843b05d80f74c4790ed7f6ac636393ca5f6375ae0bb02e719'
-$checksumType   = 'sha256'
+$url            = ''
+$checksum       = ''
+$checksumType   = ''
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile    = Join-Path $toolsDir "rufusp.exe"
 
