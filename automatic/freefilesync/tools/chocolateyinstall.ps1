30385d300a32024d5767040fcb034f382777ce28
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-15 04:03:25 +0000

    Package download freefilesync

---
+++ automatic/freefilesync/tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+ 
+if (-not (Get-ChildItem -Path $toolsDir -Filter '*.exe')) {
+   Throw ("$env:ChocolateyPackageName installer executable not found!`n" +
+            "`tThis is an embedded package, so the most probable cause is that`n" +
+            "`tanti-virus/anti-malware software has incorrectly removed it.`n" +
+            "`tGo here for more info: https://www.freefilesync.org/faq.php#virus")
+}
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.exe').FullName
+ 
+# silent install requires AutoHotKey
+$ahkFile = Join-Path $toolsDir 'chocolateyInstall.ahk'
+$ahkProc = Start-Process -FilePath AutoHotkey -ArgumentList "$ahkFile" -PassThru
+Write-Debug "AutoHotKey start time:`t$($ahkProc.StartTime.ToShortTimeString())"
+Write-Debug "Process ID:`t$($ahkProc.Id)"
+ 
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'EXE'
+  file         = $fileLocation
+  softwareName = "$env:ChocolateyPackageName*"
+  silentArgs   = '/LANG=english'
+}
+ 
+if (-not (Test-Path $fileLocation)) {
+   if (get-process -id $ahkProc.Id -ErrorAction SilentlyContinue) {
+      stop-process -id $ahkProc.Id
+   }
+   Throw ("$env:ChocolateyPackageName installer executable not found!`n" +
+            "`tThis is an embedded package, so the most probable cause is that`n" +
+            "`tanti-virus/anti-malware software has incorrectly removed it.`n" +
+            "`tGo here for more info: https://www.freefilesync.org/faq.php#virus")
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+ 
+New-Item "$fileLocation.ignore" -Type file -Force | Out-Null
+ 
+if (get-process -id $ahkProc.Id -ErrorAction SilentlyContinue) {stop-process -id $ahkProc.Id}
+ 
+Write-Host ("If you like FreeFileSync, consider support with a donation at`n" + 
+            "`thttps://freefilesync.org/donate.php") -ForegroundColor Cyan
