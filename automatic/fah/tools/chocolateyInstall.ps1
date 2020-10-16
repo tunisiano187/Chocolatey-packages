da924df77c57464e6fd2cf80be15362bb7526593
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-16 16:24:38 +0000

    AU: 2 updated - fah lbry [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/fah/tools/chocolateyInstall.ps1
+++ automatic/fah/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
 $silentArgs = '/S'
-$url32       = ''
-$checksum32 = ''
+$url32       = 'https://download.foldingathome.org/releases/public/release/fah-installer/windows-10-32bit/v7.6/fah-installer_7.6.20_x86.exe'
+$checksum32 = 'a51e4b8f0056c4eb9847049c02149622ee96baefb113ec1c5b7851b8199ee2b8'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
