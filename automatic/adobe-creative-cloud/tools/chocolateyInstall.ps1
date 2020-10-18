2d38be1347126f1a95e89c9af410c59784c77ca3
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-18 04:27:50 +0000

    AU: 2 updated - adobe-creative-cloud brave-nightly [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/adobe-creative-cloud/tools/chocolateyInstall.ps1
+++ automatic/adobe-creative-cloud/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$url            = 'https://ccmdls.adobe.com/AdobeProducts/KCCC/1/win32/CreativeCloudSet-Up.exe' # download url
-$checksum       = ''
-$checksumType   = ''
+﻿$url            = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod&api_key=CCHomeWeb1' # download url
+$checksum       = '81ea7fcf82199348b04beac3531587cb9dbf1fc2346eb9c02dc193aca9546590'
+$checksumType   = 'sha256'
 
 $silentArgs     = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
