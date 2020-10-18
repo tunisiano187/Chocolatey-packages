afc9fc205886e89f60e32363a9e51c6134c062c3
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-18 17:14:03 +0000

    AU: 2 updated - faceit unison [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/faceit/tools/chocolateyinstall.ps1
+++ automatic/faceit/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url            = 'https://faceit-client.faceit-cdn.net/release/FACEIT-setup-latest.exe'
-$checksum       = '54a26077cc5065bc39956b04842bf7407c136d82641cc0bc96f1f129feaad374'
+$checksum       = 'e461c90d17dee7036d54ac4c458e486a9cc740d5c77aef05f2b2b8f192c04eaa'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
--- automatic/unison/tools/chocolateyInstall.ps1
+++ automatic/unison/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/bcpierce00/unison/releases/download/v2.51.3_rc2/unison-v2.51.3_rc2+ocaml-4.05.0.windows.zip'
-$checksum       = 'e95ef9bf69fe33819e2e01caa8bf07b3563834dac18e32a5072d4f5d367f9308'
+$url            = 'https://github.com/bcpierce00/unison/releases/download/v2.51.3_rc3/unison-v2.51.3_rc3+ocaml-4.05.0+x86_64.windows.zip'
+$checksum       = '7956c4b50006d6555bd1d44a63a27899b375aa09b22a2d40026abe08db3fb94f'
 $checksumType   = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
