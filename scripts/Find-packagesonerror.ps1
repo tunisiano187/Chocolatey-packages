21c0f8f63b5daaaf481b4b4618cd0c3f9dafae8f
Tunisiano187 <helpdesk.choc@gmail.com>
2020-10-20 19:53:55 +0200

    First commit about #2908

---
+++ scripts/Find-packagesonerror.ps1
@@ -0,0 +1,31 @@
+Install-Module psgithubsearch -ErrorAction SilentlyContinue
+Import-Module psgithubsearch
+if(!(Test-Path Env:github_api_key)) {
+    $Env:github_api_key   = $Github_personal_token          #Github personal access token
+}
+[string]$Owner = "tunisiano187"
+[string]$Repository = "Chocolatey-packages"
+$UserToken = $env:github_api_key
+$Headers = @{
+    Authorization='token '+$UserToken
+}
+
+$chocousers     = {tunisiano},{dtgm}
+foreach ($user in $chocousers) {
+    $chocoprofile = "https://chocolatey.org/profiles/$user"
+    $chocoprofile
+    $links = ((Invoke-WebRequest -Uri $chocoprofile -UseBasicParsing).links | Where-Object {$_.outerHTML -match "maintainer"}).href
+
+    foreach ($package in $links) {
+        $packageName    = $package.split("/")[-2]
+
+        $issues = (Find-GitHubIssue -Type issue -Repo "$Owner/$Repository" -Labels "NeedsUpdate" -Keywords $packageName | Sort-Object -Property Number).Title.split('(|)')[1]
+        $check=$issues | Where-Object {$_ -match "won't fix"}
+        if($check -eq 0) {
+            [string]$Label = "NeedsUpdate"
+            [string]$Title = "($packageName) Needs update"
+            [string]$Description = "([$packageName](https://chocolatey.org/packages/$packageName)) needs mainatainer"
+            New-GithubIssue -Title $Title -Description $Description -Label $Label -owner $Owner -Repository $Repository -Headers $Headers
+        }
+    }
+}
