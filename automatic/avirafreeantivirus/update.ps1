864b65d57a9e3aed0450154704cdfbd424ebbf91
Tunisiano187 <fabian@bowlman.org>
2020-10-21 20:47:30 +0200

    Get-version from install

--- automatic/avirafreeantivirus/update.ps1
+++ automatic/avirafreeantivirus/update.ps1
@@ -13,11 +13,22 @@ function global:au_SearchReplace {
 	}
 }
 
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion | select-object -first 1
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion | select-object -first 1
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
+}
+
 function global:au_GetLatest {
 	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "avira.exe"
 	Invoke-WebRequest -Uri $release -OutFile $File
-	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+	Start-Process $File -ArgumentList '/s' -Wait
 
+	$version = Get-Version("Avira")
 	$Latest = @{ URL32 = $release; Version = $version }
 	return $Latest
 }
