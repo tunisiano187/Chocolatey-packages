eb9d6c997a7ce0e30f97dab586bfd808155d06fe
Tunisiano187 <fabian@bowlman.org>
2020-10-21 20:14:45 +0200

    AviraFreeAntivirus

--- automatic/avirafreeantivirus/tools/chocolateyInstall.ps1
+++ automatic/avirafreeantivirus/tools/chocolateyInstall.ps1
@@ -1,7 +1,23 @@
-﻿$packageName = 'avirafreeantivirus'
-$installerType = 'EXE'
-$url = 'http://personal.avira-update.com/package/webloader/win32/pecl/avira_free_antivirus.exe'
-$silentArgs = ''
+﻿$ErrorActionPreference = 'Stop';
+
+$url            = 'http://personal.avira-update.com/package/webloader/win32/pecl/avira_free_antivirus.exe'
+$checksum       = ''
+$checksumType   = ''
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    fileType      = 'EXE'
+    url           = $url
+
+    softwareName  = "$($env:ChocolateyPackageName)*"
+
+    checksum      = $checksum
+    checksumType  = $checksumType
+
+    silentArgs    = "/s"
+    validExitCodes= @(0, 3010, 1641)
+  }
+  
+  Install-ChocolateyPackage @packageArgs
+  
---
+++ automatic/avirafreeantivirus/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$release = 'http://personal.avira-update.com/package/webloader/win32/pecl/avira_free_antivirus.exe'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumtype\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$File = Join-Path($(Split-Path $script:MyInvocation.MyCommand.Path)) "avira.exe"
+	Invoke-WebRequest -Uri $release -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+
+	$Latest = @{ URL32 = $release; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
