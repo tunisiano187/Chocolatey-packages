bb6d8cdd8ca28e7248590450358cbceea650d132
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-21 17:03:39 +0000

    AU: 1 updated - unison [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/unison/tools/chocolateyInstall.ps1
+++ automatic/unison/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/bcpierce00/unison/releases/download/v2.51.3_rc3/unison-v2.51.3_rc3+ocaml-4.05.0+x86_64.windows.zip'
-$checksum       = '7956c4b50006d6555bd1d44a63a27899b375aa09b22a2d40026abe08db3fb94f'
+$url            = 'https://github.com/bcpierce00/unison/releases/download/v2.51.3/unison-v2.51.3+ocaml-4.05.0+x86_64.windows.zip'
+$checksum       = '3e45b426ba2f286a1b5437ed9715c3d93f89df73927afae6bbcb8223bde02feb'
 $checksumType   = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
