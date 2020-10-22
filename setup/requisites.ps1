30ddb0a12d8d60eea2e8582526dacff2c2835e33
tunisiano187 <fabian@bowlman.org>
2020-10-22 15:40:10 +0000

    Disable IE First run Welcome screen

--- setup/requisites.ps1
+++ setup/requisites.ps1
@@ -1,1 +1,8 @@
 cup -y checksum keepass
+
+# http://techrena.net/disable-ie-set-up-first-run-welcome-screen/
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1 -PropertyType "DWord" -Force | Out-Null
+Write-Output "IE first run welcome screen has been disabled."
+
+Write-Output 'Setting Windows Update service to Manual startup type.'
+Set-Service -Name wuauserv -StartupType Manual
