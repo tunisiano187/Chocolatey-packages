2ffa9f272dd0a2ee644f99f992d31e3c441c9f9b
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-22 04:22:11 +0000

    AU: 1 updated - monero [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/monero/tools/chocolateyInstall.ps1
+++ automatic/monero/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://downloads.getmonero.org/cli/monero-win-x86-v0.17.1.0.zip'
-$checksum = 'd438ce08ebce2705b1de8469833ccda47c76401887751972086246cb3c59f041'
+$url = 'https://downloads.getmonero.org/cli/monero-win-x86-v0.17.1.1.zip'
+$checksum = '4d9fbb3fa5226b4c9928a8aebee9a57e1ca409c8e0499c8901d12dd19417f2d6'
 $checksumType = 'sha256'
-$url64 = 'https://downloads.getmonero.org/cli/monero-win-x64-v0.17.1.0.zip'
-$checksum64 = 'b942b584601faa504ed2eb5c6d7bdf62740826cbef63d33d35b48e414dd48f5d'
+$url64 = 'https://downloads.getmonero.org/cli/monero-win-x64-v0.17.1.1.zip'
+$checksum64 = 'e9e14bf95905d1d54fb490c384d50d7c972174d5f6febbfb55811713b8a1a470'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
