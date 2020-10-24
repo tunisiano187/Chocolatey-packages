f24d0afdf1b99b51ed94af19efe527b276bf5086
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-24 04:23:28 +0000

    AU: 4 updated - boinc brave-nightly electron rubberduck [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v10.1.4/electron-v10.1.4-win32-ia32.zip'
-$checksum = '585426edb38fd7468f05f008d47aab6eff3f78fdef4d8727d17aa4628176d7e9'
+$url = 'https://github.com/electron/electron/releases/download/v10.1.5/electron-v10.1.5-win32-ia32.zip'
+$checksum = 'cc59171f4236f4d8f9cb5e445cea04c52b2201c42a8e5e5089e41f91826b15b8'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v10.1.4/electron-v10.1.4-win32-x64.zip'
-$checksum64 = '182ba01acfe6c8366cc00e2390b7b80f1d5c30112529ff771e9cf04285717cbb'
+$url64 = 'https://github.com/electron/electron/releases/download/v10.1.5/electron-v10.1.5-win32-x64.zip'
+$checksum64 = 'e5f0bf09aea0d1def6839dc08e7a56bac5679bb1001ab3cb3088922391dba53c'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
