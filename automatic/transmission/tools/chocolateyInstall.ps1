41cab51babf390145c0491707d44d7526b9511e5
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-26 20:59:37 +0000

    AU: 1 updated - transmission [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/transmission/tools/chocolateyInstall.ps1
+++ automatic/transmission/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 $packageName = $env:ChocolateyPackageName
 $installerType = 'msi'
 $silentArgs = '/quiet /qn /norestart'
-$url = 'https://github.com/transmission/transmission/releases/download/3.00/transmission-3.00-x64.msi'
-$checksum = 'c34828a6d2c50c7c590d05ca50249b511d46e9a2a7223323fb3d1421e3f6b9d1'
+$url = 'https://github.com/transmission/transmission/releases/download/3.00/transmission-3.00-x86.msi'
+$checksum = 'eeab85327fa8a1299bb133d5f60f6674ca9e76522297202bbe39aae92dad4f32'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/transmission/transmission/releases/download/3.00/transmission-3.00-x86.msi'
-$checksum64 = 'eeab85327fa8a1299bb133d5f60f6674ca9e76522297202bbe39aae92dad4f32'
+$url64 = 'https://github.com/transmission/transmission/releases/download/3.00/transmission-3.00-x64.msi'
+$checksum64 = 'c34828a6d2c50c7c590d05ca50249b511d46e9a2a7223323fb3d1421e3f6b9d1'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0,3010)
 
