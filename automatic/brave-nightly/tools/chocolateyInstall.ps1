3a7ce195b5d52abf3b2b86109b072f8f7665c62e
tunisiano187 <helpdesk.choc@gmail.com>
2020-10-30 18:52:38 +0000

    AU: 1 updated - brave-nightly [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/brave-nightly/tools/chocolateyInstall.ps1
+++ automatic/brave-nightly/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file        = "$toolsPath\BraveBrowserSilentNightlySetup32.exe"
   file64      = "$toolsPath\BraveBrowserSilentNightlySetup.exe"
 }
-$version = '1.18.20-nightly'
+$version = '1.18.22-nightly'
 $version = $version.Split('-')[0]
 
 [version]$softwareVersion = $version
