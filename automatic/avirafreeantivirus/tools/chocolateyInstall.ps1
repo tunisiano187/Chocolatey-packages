dab7118bb1502b58eb47b9bca876753bf393733d
Tunisiano187 <helpdesk.choc@gmail.com>
2020-10-30 19:21:16 +0100

    Avira not maintained

--- automatic/avirafreeantivirus/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'avirafreeantivirus'
-$installerType = 'EXE'
-$url = 'http://personal.avira-update.com/package/webloader/win32/pecl/avira_free_antivirus.exe'
-$silentArgs = ''
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
