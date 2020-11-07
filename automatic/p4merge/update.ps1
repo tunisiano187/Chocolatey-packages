4b126ba24b1eea34218e1411b3e336ee8a7b12b0
Tunisiano187 <helpdesk.choc@gmail.com>
2020-11-07 10:19:05 +0100

    Force

--- automatic/p4merge/update.ps1
+++ automatic/p4merge/update.ps1
@@ -26,20 +26,20 @@ function global:au_GetLatest {
 			$ver = $item.replace(',','.')
 			$linktest = "https://cdist2.perforce.com/perforce/r$($ver)/doc/user/p4vnotes.txt"
 			Invoke-WebRequest -Uri $linktest -OutFile "$env:TEMP\p4v.txt"
-			$newversion = $($(Get-Content "$env:TEMP\p4v.txt" | Where-Object { $_ -match 'version'}).trim() | Where-Object { $_ -match '^Version'})[0].split(' ')[-1]
-			if([version]$version -lt [version]$newversion)
+            $newversion = $($(Get-Content "$env:TEMP\p4v.txt" | Where-Object { $_ -match 'version'}).trim() | Where-Object { $_ -match '^Version'})[0].split(' ')[-1]
+            if([version]$version -lt [version]$newversion)
 			{
 				$url32 = "https://cdist2.perforce.com/perforce/r$($ver)/bin.ntx86/p4vinst.exe"
 				$url64 = "https://cdist2.perforce.com/perforce/r$($ver)/bin.ntx64/p4vinst64.exe"
 
-				$version = $newversion
+                $version = $newversion
 			}
 		}
 		catch {
 			Write-Verbose "V$($item) Not found"
 		}
-	}
-
+    }
+if($version -eq "2020.2.3") { $version = 2020.2.20201107}
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
 }
