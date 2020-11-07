2689c305125b5bf2616f39655a37a4a56d0e46ce
tunisiano187 <helpdesk.choc@gmail.com>
2020-11-07 08:45:29 +0000

    AU: 2 updated - brave-nightly mblock [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf

--- automatic/mblock/tools/chocolateyinstall.ps1
+++ automatic/mblock/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = ''
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://dl.makeblock.com/mblock5/win32/V5.3.0.exe'
+$checksum       = 'ecb8a53af718a76dbc0c05515c241705bf3c148623fa0a62e7f6cb9088d8a561'
+$checksumType   = 'sha256'
 
 
 $packageArgs = @{
