afff3eee90fc2f2e315893708ed802f236c4090b
Tunisiano187 <fabian@bowlman.org>
2020-11-15 11:50:52 +0100

    remove line if we take it from list.txt

--- tools/looknewer.ps1
+++ tools/looknewer.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Continue';
 
+# Requisites
 Install-Module psgithubsearch -ErrorAction SilentlyContinue
 Import-Module psgithubsearch
 if(!(Test-Path Env:github_api_key)) {
@@ -14,13 +15,16 @@ $Headers = @{
 $search = ''
 $version = ''
 
+# Check if there is a waiting issue
 if(Find-GitHubIssue -Type issue -Repo "$Owner/$Repository" -State open) {
     Write-Warning "Some issues are still open"
     Write-Warning "Not checking for broken packages"
     exit 0;
 }
+# Wait to avoid api requests limit
 Start-Sleep 10;
 
+# Check if one package is waiting for maintainer action on Chocolatey
 if($Todo.Count -eq 0) {
     $chocoprofile = "https://chocolatey.org/profiles/tunisiano"
     $links = ((Invoke-WebRequest -Uri $chocoprofile -UseBasicParsing).links | Where-Object {$_.outerHTML -match "maintainer"}).href
@@ -28,18 +32,24 @@ if($Todo.Count -eq 0) {
     $version="/$($links.split('/')[-1])"
 }
 
+# If no package is waiting
+# Take a package that needs action on the Todo repository (Mostly dtgm packages)
 if($Todo.Count -eq 0) {
     Invoke-WebRequest -uri https://gitlab.com/chocolatey-packages/todo/-/raw/master/README.md -OutFile "$($env:TEMP)\list.txt"
     $Todo=$(Get-Content "$($env:TEMP)\list.txt" | Where-Object {$_ -notmatch '#'} | Where-Object {$_ -notmatch 'Count' } | Where-Object {$_ -notmatch '--'} | Select-Object -First 1).split('|')[-2]
     $ToDo=$ToDo.Trim()
 }
 
+# If no package is waiting
+# Take a package that is requested on the chocolatey-package-requests
 if($Todo.Count -eq 0) {
     $ToDo = Find-GitHubIssue -Type issue -Repo "chocolatey-community/chocolatey-package-requests" -State open -No assignee -SortBy updated | Where-Object {$_.Labels -match 'maint'} | Where-Object {$_.Title -match 'RFM'} | Select-Object -First 1
     $ToDo = $ToDo.Title.split(' ')[-1]
 }
 
+# Clean the search item
 $ToDo=$ToDo.trim()
+# Check if there is already a closed issue about this request and avoid search
 if($ToDo.Count -gt 0){
     $search = $ToDo
     $closed = Find-GitHubIssue -Type issue -Repo "$Owner/$Repository" -State closed -Keywords $search
@@ -47,11 +57,15 @@ if($ToDo.Count -gt 0){
         $search = ''
     }
 }
+
+# if the search var is empty, search in the list.txt file (obtained by seaching for [outdated] term)
 if($search -eq '') {
     $source = Join-Path $PSScriptRoot "Check/list.txt"
     $search = (Get-Content $source | Select-Object -First 1).split(' ')[0]
     $check=(choco search $search) | Where-Object {$_ -match $search} | Where-Object {$_ -match 'broken'}
-    if($check.Count -gt 0) {
+    get-content $source | Select-Object -Skip 1 | set-content "$source-temp"
+    Move-Item "$source-temp" $source -Force
+    if(($check.Count -gt 0) -or (Test-Path("../automatic/$search"))) {
         $search = ''
     }
 }
