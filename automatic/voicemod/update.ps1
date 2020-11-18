c1abba456b40f65ed9429a0f0218edbf7b7b6b37
Tunisiano187 <helpdesk.choc@gmail.com>
2020-11-18 17:17:19 +0100

    VoiceMod Free

---
+++ automatic/voicemod/update.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$url32 = 'https://www.voicemod.net/downloadVoicemod.php'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	choco install wormies-au-helpers -y
+	$url32 = Get-RedirectedUrl $url32
+	$version = $url32.Split('_')[-1].replace('.exe','')
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
