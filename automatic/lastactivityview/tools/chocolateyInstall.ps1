138d5981b8a0409c13f95a75d84355d1a5364bb1
tunisiano187 <helpdesk.choc@gmail.com>
2020-12-09 19:56:59 +0000

    Package download lastactivityview

---
+++ automatic/lastactivityview/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'lastactivityview'
+$url = 'http://www.nirsoft.net/utils/lastactivityview.zip'
+$checksum = '8572b903d316b55e89fa7c129a7c8d0e1da597158fcde8a7210b2b1a6e34df17'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+Set-Content -Path ("$installFile.gui") `
+            -Value $null
