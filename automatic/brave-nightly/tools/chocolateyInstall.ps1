f80c2e015497e437b3e5ab45b7a72111eac5e9cf
tunisiano187 <helpdesk.choc@gmail.com>
2020-12-15 04:11:02 +0000

    AU: 3 updated - brave-nightly lbry monero [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/4a24c6ee26d7383f7b14b00e2733df532f0a0201

--- automatic/brave-nightly/tools/chocolateyInstall.ps1
+++ automatic/brave-nightly/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file        = "$toolsPath\BraveBrowserSilentNightlySetup32.exe"
   file64      = "$toolsPath\BraveBrowserSilentNightlySetup.exe"
 }
-$version = '1.20.26-nightly'
+$version = '1.20.28-nightly'
 $version = $version.Split('-')[0]
 
 [version]$softwareVersion = $version
--- automatic/lbry/tools/chocolateyinstall.ps1
+++ automatic/lbry/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/lbryio/lbry-desktop/releases/download/v0.49.0/LBRY_0.49.0.exe'
-$checksum       = 'e2ee82dd6e7df5bbd0d54df3cebdb80bdc742cdad6f583a002554bb4c60d096e'
+$url            = 'https://github.com/lbryio/lbry-desktop/releases/download/v0.49.1-rc.1/LBRY_0.49.1-rc.1.exe'
+$checksum       = '2a012202b825c23577951e3ccc9d0d5288b51f8b8ff5f25f2e853e8fb4bffbf8'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
