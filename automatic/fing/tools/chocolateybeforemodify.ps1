abeac36527976ad0c72a8d7500de852c2998cca0
Tunisiano187 <helpdesk.choc@gmail.com>
2020-12-19 10:20:59 +0100

    Uninstall for updates

---
+++ automatic/fing/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Fing'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)
+
+  silentArgs    = "/allusers /S"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
+}
+
+Uninstall-ChocolateyPackage @packageArgs
