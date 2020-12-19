0255d4e436cef3bb6601a897b1a368649ab1c289
tunisiano187 <helpdesk.choc@gmail.com>
2020-12-19 12:39:34 +0000

    AU: 1 updated - ultracopier [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/8a17c5047789a6f6307f6a685a00770196c0bb40

--- automatic/ultracopier/tools/chocolateyInstall.ps1
+++ automatic/ultracopier/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:ChocolateyPackageName
-$url32='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.8/ultracopier-windows-x86-2.2.4.8-setup.exe'
-$url64='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.8/ultracopier-windows-x86_64-2.2.4.8-setup.exe'
-$checksum32='578b7ed7dc353f119c5006b8f6ebe1093926b1806bf954cf64e7e197e50e6153'
-$checksum64='18cec14974ae908c5c249ce7fc5880db991a467f6be8fa24995ca665e4dd891d'
+$url32='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.9/ultracopier-windows-x86-2.2.4.9-setup.exe'
+$url64='https://cdn.confiared.com/files.first-world.info/ultracopier/2.2.4.9/ultracopier-windows-x86_64-2.2.4.9-setup.exe'
+$checksum32='be8882f326f8f633323ee7ec7d6a7833aa2049d2dcb54937b9ca6f0fe045604a'
+$checksum64='ba4018e7f251c80d7ddd6a413b3e7f4cfc813d2c5624741780b94cb7db5d771f'
 
 $packageArgs = @{
   packageName   = $packageName
