218a8e7c63e654c8512c65a5c822402f49087424
Tunisiano187 <helpdesk.choc@gmail.com>
2020-12-22 15:31:37 +0100

    internal

--- automatic/fing/tools/chocolateybeforemodify.ps1
+++ automatic/fing/tools/chocolateybeforemodify.ps1
@@ -1,2 +1,11 @@
-﻿$uninstaller = $(./Get-InstalledApps.ps1 -NameRegex Fing).UninstallString.split('"')[1]
-Start-Process -FilePath $uninstaller -ArgumentList "/allusers /S"
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors	$uninstaller = $(./Get-InstalledApps.ps1 -NameRegex Fing).UninstallString.split('"')[1]
+$packageArgs = @{	Start-Process -FilePath $uninstaller -ArgumentList "/allusers /S" 
+  packageName   = $env:ChocolateyPackageName	
+  softwareName  = 'Fing'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique	
+  fileType      = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)	
+
+  silentArgs    = "/allusers /S"	
+  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx	
+}
+
+Uninstall-ChocolateyPackage @packageArgs
