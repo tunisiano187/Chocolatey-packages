0b4d27950811181b1ca755712141839fe26cad1f
tunisiano187 <helpdesk.choc@gmail.com>
2020-12-25 04:10:55 +0000

    AU: 2 updated - brave-nightly lbry [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/606623df81b476d823abc651678710d7ef547c5e

--- automatic/lbry/tools/chocolateyinstall.ps1
+++ automatic/lbry/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/lbryio/lbry-desktop/releases/download/v0.49.1/LBRY_0.49.1.exe'
-$checksum       = '3fea8e0da3cdd2644e6cc7fde9b94a53a84edc3d95f752a4807e600da6e376c4'
+$url            = 'https://github.com/lbryio/lbry-desktop/releases/download/v0.49.2-rc.2/LBRY_0.49.2-rc.1.exe'
+$checksum       = '727e7c50b5b3aba9e0cd623a97b40fe596c26f432d377fca05cbea6bdfbe97d1'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
