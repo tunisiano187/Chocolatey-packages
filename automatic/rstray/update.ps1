75afd0dce00db4fa5e12a5e2e41f05efbe53855a
Tunisiano187 <fabian@bowlman.org>
2020-12-31 11:12:22 +0100

    test

--- automatic/rstray/update.ps1
+++ automatic/rstray/update.ps1
@@ -6,11 +6,11 @@ $releases = 'https://github.com/ltGuillaume/Redshift-Tray/releases/latest'
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]version\s*=\s*)('.*')"      = "`$1'$($Latest.VERSION)'"
+			"(^[$]version\s*=\s*)('.*')"      	= "`$1'$($Latest.VERSION)'"
 		}
 		"tools\VERIFICATION.txt"      = @{
-			"(?i)(x86:).*"        = "`${1} $($Latest.URL32)"
-			"(?i)(checksum:).*" = "`${1} $($Latest.Checksum32)"
+			"(?i)(x86:).*"        				= "`${1} $($Latest.URL32)"
+			"(?i)(checksum:).*" 				= "`${1} $($Latest.Checksum32)"
 		}
 	}
 }
@@ -23,9 +23,10 @@ function global:au_GetLatest {
 	Write-Verbose "Version : $version"
 	$url32 = "https://github.com$($url32)";
 	Invoke-WebRequest -Uri $url32 -OutFile "$(Get-Location)\tools\redshift-tray.zip"
+	Get-FileHash -Path "$(Get-Location)\tools\redshift-tray.zip" -Algorithm SHA256
 	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ltGuillaume/Redshift-Tray/master/LICENSE" -OutFile "$(Get-Location)\tools\license.txt"
-	$Latest = @{ URL32 = $url32; Version = $version }
+	$Latest = @{ URL32 = $url32; Version = $version; Checksum32 = $checksum }
 	return $Latest
 }
 
-update -ChecksumFor 32 -NoCheckChocoVersion
+update -ChecksumFor none -NoCheckChocoVersion
