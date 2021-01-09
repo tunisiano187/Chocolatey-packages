b547bc8e56c33c5b4b165546591f62f73633af92
Tunisiano187 <fabian@bowlman.org>
2021-01-09 19:26:34 +0100

    https

--- automatic/jcpicker/update.ps1
+++ automatic/jcpicker/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$url32 = 'http://annystudio.com/jcpicker.exe'
+	$url32 = 'https://annystudio.com/jcpicker.exe'
 
 	$File = "$($env:TEMP)\jcpicker.exe"
 	Invoke-WebRequest -Uri $url32 -OutFile $File
