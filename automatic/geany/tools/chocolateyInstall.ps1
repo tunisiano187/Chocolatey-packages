6c565908e8c799e982ab3b0c4a0389f961d8e4d1
Tunisiano187 <fabian@bowlman.org>
2021-01-14 19:27:32 +0100

    Geany

--- automatic/geany/tools/chocolateyInstall.ps1
+++ automatic/geany/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'geany'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = 'http://download.geany.org/geany-1.36_setup.exe'
-$checksum = '1549b62f9e9fdde825ae45245bafc3a47ce85f4555f28b06e3a9c600340751df'
+$url32 = 'http://download.geany.org/geany-1.36_setup.exe'
+$checksum32 = '1549b62f9e9fdde825ae45245bafc3a47ce85f4555f28b06e3a9c600340751df'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
