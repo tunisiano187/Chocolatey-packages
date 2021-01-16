dbfd965d767d8ebcdbe76bbce0c33a48d7f4b97d
Aaron Price <ajp@aaronjp.co.uk>
2021-01-16 13:28:27 +0000

    Correct 64 bit install path
    Add correct 64 bit install path
    Add SHA256 checksum

--- automatic/gramps/tools/chocolateyInstall.ps1
+++ automatic/gramps/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $silentArgs         = '/S'
 $url                = 'https://github.com/gramps-project/gramps/releases/download/v5.1.3/GrampsAIO-5.1.3-1_win32.exe'
 $checksum           = 'da88cb39bf75ebcbbf22c9e276e682620ddb4885ca7c04f1d3922d818095c0e3'
 $checksumType       = 'sha256'
-$url64              = 'https://github.com/gramps-project/gramps/releases/download/v5.1.3/GrampsAIO-5.1.3-2_win32.exe'
-$checksum64         = 'bbe69ebcf2f35f456243081593dcc87a7175967302638bd475a3a87ab4feb785'
+$url64              = 'https://github.com/gramps-project/gramps/releases/download/v5.1.3/GrampsAIO-5.1.3-2_win64.exe'
+$checksum64         = '0962b59f62195c8d65f2c586903dd90d578840c5edbffe98defe23951186432b'
 $checksumType64     = 'sha256'
 $validExitCodes     = @(0)
 
