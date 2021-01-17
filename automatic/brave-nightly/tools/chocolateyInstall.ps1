bc405f4a773b733187e9c7602f825d3e6ee18c1b
tunisiano187 <helpdesk.choc@gmail.com>
2021-01-17 04:10:16 +0000

    AU: 1 updated - brave-nightly [skip ci]

--- automatic/brave-nightly/tools/chocolateyInstall.ps1
+++ automatic/brave-nightly/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file        = "$toolsPath\BraveBrowserSilentNightlySetup32.exe"
   file64      = "$toolsPath\BraveBrowserSilentNightlySetup.exe"
 }
-$version = '1.20.77-nightly'
+$version = '1.21.7-nightly'
 $version = $version.Split('-')[0]
 
 [version]$softwareVersion = $version
