74a9594eabf1b1ae330d6d1dc7cda02baea97f05
tunisiano187 <helpdesk.choc@gmail.com>
2021-01-19 11:57:30 +0000

    AU: 1 updated - p4merge [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/fc9fe37e492982f5f5057d459be4b59af511fb86

--- automatic/p4merge/tools/chocolateyinstall.ps1
+++ automatic/p4merge/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ $packageName      = $env:ChocolateyPackageName
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url              = 'https://cdist2.perforce.com/perforce/r20.3/bin.ntx86/p4vinst.exe'
 $url64            = 'https://cdist2.perforce.com/perforce/r20.3/bin.ntx64/p4vinst64.exe'
-$checksum         = '8b78cda0cd78e8fed73652a89df54b4f3f63737f83c038dbe37d7980b0dd216d'
+$checksum         = 'b6e10012dfe313525a8bad6d042083e9ce2657108e98f725555a67f307ca1788'
 $checksumType     = 'sha256'
-$checksum64       = '7912efe05339ac310b997b051be875b898e6419c7e6bb08958a50a570593329d'
+$checksum64       = 'aaf7a92b09d9c654f716755dc78fb0c0f5c01c65ad073e7cb0f8b40c8743ae7e'
 $checksumType64   = 'sha256'
 
 $packageArgs = @{
