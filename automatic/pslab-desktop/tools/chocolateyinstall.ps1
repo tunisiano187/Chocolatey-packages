1ea98aaef9ef1bfacef5b77dbcb86ff48fe39ca0
tunisiano187 <helpdesk.choc@gmail.com>
2021-01-22 13:52:49 +0000

    AU: 1 updated - pslab-desktop [skip ci]

--- automatic/pslab-desktop/tools/chocolateyinstall.ps1
+++ automatic/pslab-desktop/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url          = ''
-$checksum     = ''
-$checksumType = ''
+$url          = 'https://github.com/fossasia/pslab-desktop/releases/download/v2.5.2/PSLab-Desktop-Setup-2.5.2.exe'
+$checksum     = '8eb40300dfc5c141389fe160fa2ca6c67be3d3923085bdcc7ccfbd72629fe351'
+$checksumType = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
