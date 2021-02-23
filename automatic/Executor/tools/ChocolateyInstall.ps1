646a59d47aa27929d706847b1abb7c8c060c9602
tunisiano187 <helpdesk.choc@gmail.com>
2021-02-23 04:10:09 +0000

    AU: 1 updated - Executor [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/059e237b03d09c9154aaf5359c55ec5e0c729064

--- automatic/Executor/tools/ChocolateyInstall.ps1
+++ automatic/Executor/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
     PackageName     = $env:ChocolateyPackageName
     FileType        = 'exe'
     Url             = 'http://www.1space.dk/executor/ExecutorSetup.exe'
-    Checksum        = 'a119808acfdcc1fa8d71b1dc35cc40afec470cf59c0127176a30bde87c09d7b1'
+    Checksum        = '63d565ec701ba49c3eb58e814796aa9fbf8c690669d9a6167de1b8a3cf5955c0'
     ChecksumType    = 'sha256'
     SilentArgs      = '/SILENT /VERYSILENT /SUPRESSMSGBOXES /SP-'
 }
