414d3ca4f3a2b41cf31bc97464440b8e59598d8e
tunisiano187 <helpdesk.choc@gmail.com>
2021-04-05 04:12:27 +0000

    AU: 2 updated - gridcoinwallet rubberduck [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/cada6db3d563877d46e05ddfeaf0988cba22e0d0

--- automatic/gridcoinwallet/tools/chocolateyinstall.ps1
+++ automatic/gridcoinwallet/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url32		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.3.0.0/gridcoin-5.3.0-win32-setup.exe'
-$checksum32	    = 'd8e3af324430a20e43e49cbce5e02e96d9da2dfc306724fe8c638d8b48638527'
+$url32		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.3.1.0/gridcoin-5.3.1-win32-setup.exe'
+$checksum32	    = '9a81d3916f02b2902bc334d2de5978b8f7d0432779f4c76bee72fa2a0467e328'
 $checksumType32 = 'sha256'
-$url64		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.3.0.0/gridcoin-5.3.0-win64-setup.exe'
-$checksum64     = 'c754f665f7bb44b3d9adf970bf2127c854e20523a8cc017f583c535056355937'
+$url64		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.3.1.0/gridcoin-5.3.1-win64-setup.exe'
+$checksum64     = '3b2eae2c3a0cc2eaf869795cdd69426f3817ec30f22f4d321365a2833100d4c8'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
--- automatic/rubberduck/tools/chocolateyinstall.ps1
+++ automatic/rubberduck/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName  = $env:ChocolateyPackageName
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/rubberduck-vba/Rubberduck/releases/download/Prerelease-v2.5.1.5767/Rubberduck.Setup.2.5.1.5767-pre.exe' # download url
-$checksum     = '3dc2831c5aebb70f1768e011b0f94edaa61071ef49e2544f654100f3e6d7cfe5'
+$url          = 'https://github.com/rubberduck-vba/Rubberduck/releases/download/Prerelease-v2.5.1.5788/Rubberduck.Setup.2.5.1.5788-pre.exe' # download url
+$checksum     = '3ebb9728c31464ab6a76f3971f6d3b6b5e3f6ea037f63e4614aab12936d9b57e'
 $checksumType = 'sha256'
 
 $packageArgs = @{
