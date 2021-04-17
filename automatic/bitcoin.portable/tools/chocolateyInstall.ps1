0b4954e549052f321c21f26d4cec605c2fdc64c4
tunisiano187 <helpdesk.choc@gmail.com>
2021-04-17 04:12:18 +0000

    AU: 1 updated - bitcoin.portable [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/caeff71be445560a3d47e1ffdeea5fe0a4924d24

--- automatic/bitcoin.portable/tools/chocolateyInstall.ps1
+++ automatic/bitcoin.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com//bitcoin/bitcoin/archive/v0.21.0.zip'
-$checksum64 = '58fc5b9de765824c6ab798ceb4267fac9bd3d85fd57699c16f9ea05eadc84ffb'
+$url64 = 'https://github.com//bitcoin/bitcoin/archive/refs/tags/v0.21.1rc1.zip'
+$checksum64 = '5342671107ec572642beaef0024e04adedcbd94e147a96c9c38ae2d37ea3500f'
 $checksumType64 = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
