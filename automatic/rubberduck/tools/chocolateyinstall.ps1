0a77485f0faadcd2a3d206ef357b2ba2116b7091
tunisiano187 <helpdesk.choc@gmail.com>
2021-04-20 04:11:30 +0000

    AU: 1 updated - rubberduck [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/a99509944c213ee150a08e860ad10c81652ba94b

--- automatic/rubberduck/tools/chocolateyinstall.ps1
+++ automatic/rubberduck/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName  = $env:ChocolateyPackageName
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/rubberduck-vba/Rubberduck/releases/download/Prerelease-v2.5.2.5814/Rubberduck.Setup.2.5.2.5814-pre.exe' # download url
-$checksum     = '292e3fc35e516108b51b65c7c25c13e40283d0208038be229e641584e5d386d1'
+$url          = 'https://github.com/rubberduck-vba/Rubberduck/releases/download/Prerelease-v2.5.2.5816/Rubberduck.Setup.2.5.2.5816-pre.exe' # download url
+$checksum     = 'ae97bbe4cd8f0caded88f7dfd1bd37e9c93b6ded85ebeca8af1a97c7095f6b45'
 $checksumType = 'sha256'
 
 $packageArgs = @{
