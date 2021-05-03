ec5e96c9ff1671e54ac45b17a54535e8a753c02a
tunisiano187 <helpdesk.choc@gmail.com>
2021-05-03 11:09:17 +0000

    AU: 2 updated - bitcoin duplicati.portable [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/0be73eb8abc978a3bd4c541f7458e7f048ea6e11

--- automatic/duplicati.portable/tools/chocolateyinstall.ps1
+++ automatic/duplicati.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.0.6.0-2.0.6.0_experimental_2021-04-09/duplicati-2.0.6.0_experimental_2021-04-09.zip'
-$checksum       = '592b3e42082af5c79a4badcfe299fc823e6bd29f7b283e4bba2afa8626c8f912'
+$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.0.6.1-2.0.6.1_beta_2021-05-03/duplicati-2.0.6.1_beta_2021-05-03.zip'
+$checksum       = '15ca4f0257beeb50fb9a4a5461bcc8205c5fb6949835a1eceb9b59112505267f'
 $checksumType   = 'sha256'
 
 
