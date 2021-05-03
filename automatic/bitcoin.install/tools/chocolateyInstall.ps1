81a3f597ce5fe1e45fef9aa478ce696e04acdb9c
tunisiano187 <helpdesk.choc@gmail.com>
2021-05-03 11:36:23 +0000

    AU: 1 updated - bitcoin.install [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/03c99228398bad421da93bc60ff13eca932b0f61

--- automatic/bitcoin.install/tools/chocolateyInstall.ps1
+++ automatic/bitcoin.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '/S'
-$url64          = 'https://bitcoincore.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-win64-setup.exe'
-$checksum64     = '54050748ef4d4f000ea1ece472491b3e5fd546efc74ed52119354b2893f6624b'
+$url64          = 'https://bitcoincore.org/bin/bitcoin-core-0.21.1/bitcoin-0.21.1-win64-setup-unsigned.exe'
+$checksum64     = 'afdd0f1717a74af01b88631d17a2f29f89d21ca2e3be0fec0678e7a1e20712d5'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
