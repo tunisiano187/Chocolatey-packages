769a3144fd2ce2d3e11d54b23cec303e5a3785c9
Tunisiano187 <helpdesk.choc@gmail.com>
2021-05-10 12:24:13 +0200

    64 strip

--- automatic/brave-nightly/update.ps1
+++ automatic/brave-nightly/update.ps1
@@ -7,11 +7,8 @@ function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
 			"(^[$]url32\s*=\s*)('.*')"		= "`$1'$($Latest.URL32)'"
-			"(^[$]url64\s*=\s*)('.*')"		= "`$1'$($Latest.URL64)'"
 			"(^[$]checksum32\s*=\s*)('.*')"	= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksum64\s*=\s*)('.*')"	= "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType32\s*=\s*)('.*')"	= "`$1'$($Latest.ChecksumType32)'"
-			"(^[$]checksumType64\s*=\s*)('.*')"	= "`$1'$($Latest.ChecksumType64)'"
 		}
 	}
 }
