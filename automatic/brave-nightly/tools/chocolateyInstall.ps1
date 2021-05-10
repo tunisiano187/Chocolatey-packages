5314e84c06d126a94477af488162aeb793c09218
Tunisiano187 <helpdesk.choc@gmail.com>
2021-05-10 12:21:23 +0200

    New name

--- automatic/brave-nightly/tools/chocolateyInstall.ps1
+++ automatic/brave-nightly/tools/chocolateyInstall.ps1
@@ -4,24 +4,18 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageName      = $env:ChocolateyPackageName
 $url32            = 'https://github.com/brave/brave-browser/releases/download/v1.24.53/BraveBrowserSilentNightlySetup32.exe'
-$url64            = 'https://github.com/brave/brave-browser/releases/download/v1.24.53/BraveBrowserSilentNightlySetup.exe'
 $checksum32       = '2b0daf78daa8a8ab1234b1dc855b99f0c4564e46a23e25a9e01255be8fe28e6b'
-$checksum64       = 'f6efa3e03270ba99bb941409c9e5c1ee76c8f31c74003ac616937c3079dda9ca'
 $checksumType32   = 'sha256'
-$checksumType64   = 'sha256'
 
 $packageArgs = @{
   packageName     = $packageName
   fileType        = 'EXE'
   url             = $url32
-  url64           = $url64
 
   softwareName    = "$packageName*"
 
   checksum        = $checksum32
   checksumType    = $checksumType32
-  checksum64      = $checksum64
-  checksumType64  = $checksumType64
 
 
   validExitCodes= @(0)
--- automatic/brave-nightly/update.ps1
+++ automatic/brave-nightly/update.ps1
@@ -17,18 +17,16 @@ function global:au_SearchReplace {
 }
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
-	$url32 = $(((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_ -match 'BraveBrowserSilentNightlySetup32.exe'} | Select-Object -First 1).href)
-	$url64 = $(((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_ -match 'BraveBrowserSilentNightlySetup.exe'} | Select-Object -First 1).href)
+	$url32 = $(((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_ -match 'BraveBrowserStandaloneSilentNightlySetup.exe'} | Select-Object -First 1).href)
 	Write-Output 'Checking version'
 	$version = $url32.split('/')[5].replace('v','')
 	$version = "$version-nightly"
 	Write-Output "Version : $version"
 
 	$url32 = "https://github.com$($url32)";
-	$url64 = "https://github.com$($url64)";
-
-	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	
+	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
 }
 
-update
+update -ChecksumFor 32
