a674c8c488e6282c5956c267fcbe49646f6b243c
tunisiano187 <helpdesk.choc@gmail.com>
2021-05-16 04:10:07 +0000

    AU: 2 updated - freeplane rubberduck [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/ba31b9ec34fa0b130663c7e2164744a134ddf985

--- automatic/freeplane/tools/chocolateyInstall.ps1
+++ automatic/freeplane/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
-$url = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-1.8.11.exe/download'
+$url = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-1.9.1.exe/download'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
-$checksum = '448a65de1406d94ea6a28a8e0244e067bc5ec700085a5d1cab5157d4ebce10fc'
+$checksum = 'd31dd3555c5a26c0a0278d73ae64fd03ea6514913f39420a4a51adc57296ee41'
 $checksumType = 'sha256'
 
 Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -Url "$url" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
