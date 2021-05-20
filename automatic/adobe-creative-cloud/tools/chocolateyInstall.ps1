c4d78c87dfd593df0aaa19bb5468432eedbebd1a
tunisiano187 <helpdesk.choc@gmail.com>
2021-05-20 06:48:48 +0000

    AU: 1 updated - adobe-creative-cloud [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/bc1a65e2a9449af983220772af38bb178ee8acc1

--- automatic/adobe-creative-cloud/tools/chocolateyInstall.ps1
+++ automatic/adobe-creative-cloud/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$url            = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod&api_key=CCHomeWeb1' # download url
-$checksum       = '8b149a43a8a9606930407e27f494aa7486bfb203ab1d0aae0c4bddd7681e54f4'
+﻿$url            = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod' # download url
+$checksum       = '97cd4cd7793cb35483275cb108cf05109cb98c26d025b95c1e89c78f0983b736'
 $checksumType   = 'sha256'
 
 $silentArgs     = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
