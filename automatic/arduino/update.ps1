9a489de855bec479bd253ad37ac20b5a46ad36bd
Tunisiano187 <helpdesk.choc@gmail.com>
2021-05-28 12:27:42 +0200

    no install.ps1 update

--- automatic/arduino/update.ps1
+++ automatic/arduino/update.ps1
@@ -3,17 +3,6 @@ import-module au
 $releases = 'https://github.com/arduino/Arduino/releases'    
 
 
-function global:au_SearchReplace {
-    @{
-        'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      			= "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" 			= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" 		= "`$1'$($Latest.ChecksumType32)'"
-		}
-    }
-}
-
-
 function global:au_GetLatest {
     $url = $(((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_ -match '.tar.xz'} | Select-Object -First 1).href)
     
