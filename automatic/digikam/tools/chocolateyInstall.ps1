8c6d1ddbbc71e95bd257e11c59bb77e26c918dfd
Tunisiano187 <helpdesk.choc@gmail.com>
2021-06-12 19:38:49 +0200

    64bit only

--- automatic/digikam/tools/chocolateyInstall.ps1
+++ automatic/digikam/tools/chocolateyInstall.ps1
@@ -2,9 +2,6 @@
 $packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '/S'
-$url32          = 'https://download.kde.org/stable/digikam/7.1.0/digiKam-7.1.0-Win32.exe'
-$checksum32     = 'b413c1e37ed64fad48e4ea5f3cd1de5fe51c281da4e0eccff3c8a117edbc02b8'
-$checksumType32 = 'sha256'
 $url64          = 'https://download.kde.org/stable/digikam/7.1.0/digiKam-7.1.0-Win64.exe'
 $checksum64     = '51ca55fbf608d42ea5fb81eeca9592a385755a33ab959dcfdf8d1d7151776cf3'
 $checksumType64 = 'sha256'
@@ -13,9 +10,6 @@ $validExitCodes = @(0)
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
-                          -Url "$url32" `
-                          -Checksum "$checksum32" `
-                          -ChecksumType "$checksumType32" `
                           -url64bit "$url64" `
                           -Checksum64 "$checksum64" `
                           -ChecksumType64 "$checksumType64" `
--- automatic/digikam/update.ps1
+++ automatic/digikam/update.ps1
@@ -3,22 +3,9 @@ import-module au
 
 $releases = 'https://download.kde.org/stable/digikam/'
 
-function Get-Version($name) {
-	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
-	while($version_file.count -eq 0)
-	{
-		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
-		Start-Sleep -Seconds 1
-	}
-	return $version_file
-}
-
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url32\s*=\s*)('.*')"      	= "`$1'$($Latest.URL32)'"
-			"(^[$]checksum32\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
 			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -29,11 +16,11 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
 	$version = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '^\d+([.]\d+)?'} | ForEach-Object {($_.href -replace '[^.\d]', '')} | Measure-Object -Max).Maximum
-	$url32 = "https://download.kde.org/stable/digikam/$($version)/digiKam-$($version)-Win32.exe"
+	$url32 = Invoke-WebRequest -Uri "https://download.kde.org/stable/digikam/$($version)/"
 	$url64 = "https://download.kde.org/stable/digikam/$($version)/digiKam-$($version)-Win64.exe"
 
-	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	$Latest = @{ URL64 = $url64; Version = $version }
 	return $Latest
 }
 
-update
+update -ChecksumFor 64
