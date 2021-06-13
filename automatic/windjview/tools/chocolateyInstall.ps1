b0f609ba8588e4a37edd92c4b99d3fded9bc4735
tunisiano187 <helpdesk.choc@gmail.com>
2021-06-13 14:04:08 +0000

    AU: 2 updated - deepl windjview [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/e3a7cd58928734501143c1f46351df4776315cf2

--- automatic/windjview/tools/chocolateyInstall.ps1
+++ automatic/windjview/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$packageName = 'windjview'
+﻿$packageName = 'windjview'
 $installerType = 'exe'
 $silentArgs = '/S'
-$url = 'http://sourceforge.net/projects/windjview/files/WinDjView/2.1/WinDjView-2.1-Setup.exe/download'
-$checksum = 'bbf32f86dc8db03a73c1331a04d73b41bc80c52b'
-$checksumType = 'sha1'
+$url = 'https://sourceforge.net/projects/windjview/files/WinDjView/2.1/WinDjView-2.1-Setup.exe/download'
+$checksum = 'edd0016a9b528243e5eaff9677237059d3bc3ca7ea5dad28885c267728f61a98'
+$checksumType = 'sha256'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
