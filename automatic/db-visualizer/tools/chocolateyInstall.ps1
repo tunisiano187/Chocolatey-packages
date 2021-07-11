581e4a090725a69a4c051f5836258664283ee7e8
tunisiano187 <helpdesk.choc@gmail.com>
2021-07-11 04:11:17 +0000

    AU: 1 updated - db-visualizer [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/a858b89817df9c38c51303b5564128393a244373

--- automatic/db-visualizer/tools/chocolateyInstall.ps1
+++ automatic/db-visualizer/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32            = 'https://dbvis.com/product_download/dbvis-12.0.8/media/dbvis_windows-x32_12_0_8.exe'
-$url64            = 'https://dbvis.com/product_download/dbvis-12.0.8/media/dbvis_windows-x64_12_0_8.exe'
-$checksum         = 'aa7fea9a2b5bf9f9dbbc996b06f62f0e9b4a483bef057a8771241d16172d9004'
+$url32            = 'https://dbvis.com/product_download/dbvis-12.0.9/media/dbvis_windows-x32_12_0_9.exe'
+$url64            = 'https://dbvis.com/product_download/dbvis-12.0.9/media/dbvis_windows-x64_12_0_9.exe'
+$checksum         = 'c720c5acb5ddf7e879d2099dacedfd4229d5ac4de73a889c26b60b19c4e6f9d4'
 $checksumType     = 'sha256'
-$checksum64       = 'e8eb4c3d391bfde0ee0a34242bdbec621783f07be518b7f7de3789cbd0eb6e13'
+$checksum64       = '6258a77fa959885d1062f428b50c552f4c3f62f6f5caaac470c161d44591ccb5'
 $checksumType64   = 'sha256'
 
 $packageArgs = @{
