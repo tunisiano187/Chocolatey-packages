12432b4a303fb4c0af274f468130c99568989209
jwfxpr <jwfxpr@gmail.com>
2021-08-20 15:15:54 +0200

    Fix boinc update URL, roll back to stable 7.16.11

--- automatic/boinc/tools/chocolateyInstall.ps1
+++ automatic/boinc/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
 $silentArgs = '/S /v/qn'
-$url = 'https://boinc.berkeley.edu/dl/boinc_7.16.16_windows_x86_64.exe'
-$checksum = '4542e15073b7bd27c08ab0e9d8238c83e49980c94e4b34ebde07d6b5d7246bec'
+$url = 'https://boinc.berkeley.edu/dl/boinc_7.16.11_windows_x86_64.exe'
+$checksum = '28b1aa0f7edbd78402b7163bc483a909b156064860b8890e52667a597079fb33'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
--- automatic/boinc/update.ps1
+++ automatic/boinc/update.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://boinc.berkeley.edu/dl/'
+$releases = 'https://boinc.berkeley.edu/download.php'
 
 function global:au_SearchReplace {
 	@{
@@ -13,13 +13,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$installer = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'windows'} | Where-Object {$_ -match 'windows_x86_64.exe'} | Where-Object {$_ -match 'boinc_'}).href)[-1]
+	$installer = (((Invoke-WebRequest -Uri $releases -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36" -UseBasicParsing).Links | Where-Object {$_ -match 'windows_x86_64.exe'} | Where-Object {$_ -match 'boinc_'}).Href)
 	$version = Get-Version $installer
-	if($version -match '7.16.16') {
-		$version = '7.16.16.20201101'
-	}
 	Write-Output "Version : $version"
-	$url32 = "$($releases)$($installer)"
+	$url32 = "$($installer)"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
