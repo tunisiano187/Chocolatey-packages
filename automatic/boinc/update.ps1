fc0d74bd306d1a9053a837095afd2aed7ff3063c
Tunisiano187 <helpdesk.choc@gmail.com>
2021-08-29 18:35:53 +0200

    Don't Force

--- automatic/boinc/update.ps1
+++ automatic/boinc/update.ps1
@@ -15,9 +15,6 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
 	$installer = (((Invoke-WebRequest -Uri $releases -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36" -UseBasicParsing).Links | Where-Object {$_ -match 'windows_x86_64.exe'} | Where-Object {$_ -match 'boinc_'}).href)
 	$version = Get-Version $installer
-	if($version -match '7.16.16') {
-		$version = '7.16.16.20201101'
-	}
 	Write-Output "Version : $version"
 	$url32 = "$($installer)"
 
@@ -25,4 +22,4 @@ function global:au_GetLatest {
 	return $Latest
 }
 
-update -ChecksumFor 32 -NoCheckChocoVersion
+update -ChecksumFor 32
