69fe49bc4e36f3702f4573129c785e87aaaba5a4
tunisiano187 <helpdesk.choc@gmail.com>
2021-10-03 04:10:33 +0000

    AU: 1 updated - freeplane [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/cde5af1bed60bbf34e9aec040124779a080d1b62

--- automatic/freeplane/tools/chocolateyInstall.ps1
+++ automatic/freeplane/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
-$url = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-1.9.9.exe/download'
+$url = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-1.9.10.exe/download'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
-$checksum = '28fa5662f29c666365fdab96f496e31983ed06b619ab0b4bdf9e29523be44b3f'
+$checksum = '12855e56ed95694bf151960271ae1cd0a50b187b5d71a80e6182ef108ec0c474'
 $checksumType = 'sha256'
 
 Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -Url "$url" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
