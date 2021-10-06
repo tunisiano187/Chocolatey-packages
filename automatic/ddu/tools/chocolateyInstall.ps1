1a78d39336fee5b9d7d9d4059e187d94e53e184f
tunisiano187 <helpdesk.choc@gmail.com>
2021-10-06 04:11:53 +0000

    AU: 2 updated - ddu rubberduck [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/689cf4afec365466d0385e4265a897fbceccc31d

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url            = 'https://www.wagnardsoft.com/DDU/download/DDU%20v18.0.4.3.exe'
-$checksum       = '11f66382b11fef34999be37015817e37daf1ca72a2402adda1c784cfd7d7b7f8'
+$url            = 'https://www.wagnardsoft.com/DDU/download/DDU%20v18.0.4.4.exe'
+$checksum       = 'eceaad44fcdc7fc5b8792f39cefe2516f4bd4aa550250beb188d23cd99f4d21b'
 $checksumType   = 'sha256'
-$referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3711'
+$referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?f=5&t=3780'
 
 Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/test.html" #  Required to be allowed just after
 
--- automatic/rubberduck/tools/chocolateyinstall.ps1
+++ automatic/rubberduck/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName  = $env:ChocolateyPackageName
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/rubberduck-vba/Rubberduck/releases/download/Prerelease-v2.5.2.5973/Rubberduck.Setup.2.5.2.5973-pre.exe' # download url
-$checksum     = 'a78eeb3791e5f57a7ec68d91e3cced7a9a76b75ca087b522a4ecb95a27e1843d'
+$url          = 'https://github.com/rubberduck-vba/Rubberduck/releases/download/Prerelease-v2.5.2.5980/Rubberduck.Setup.2.5.2.5980-pre.exe' # download url
+$checksum     = 'e31b5ae78bf76be93dda205871bb8f83f32d750bdfb049b212687866e81e4212'
 $checksumType = 'sha256'
 
 $packageArgs = @{
