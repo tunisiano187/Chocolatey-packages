707318015cd793f52a104b876b5c7dac1badd806
Tunisiano187 <helpdesk.choc@gmail.com>
2021-10-08 11:42:06 +0200

    Binance

---
+++ automatic/binance/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = $env:ChocolateyPackageName
+$url = 'https://distfiles.audacious-media-player.org/audacious-4.1-beta1-win32.exe'
+$checksum = 'f853c7c04197b854a87ff63778faba9d942d9998f85689c61f8281a11a0f03c1'
+$checksumType = 'sha256'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+    packageName     = $packageName
+    fileType        = 'exe'
+    url             = $url
+    silentArgs      = $silentArgs
+    validExitCodes  = $validExitCodes
+    softwareName    = "$packageName*"
+    checksum        = $checksum
+    checksumType    = $checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
