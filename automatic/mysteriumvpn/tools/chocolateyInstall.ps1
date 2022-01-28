f3e93b8446f141d1b2e9ec1d077482209e232835
tunisiano187 <helpdesk.choc@gmail.com>
2022-01-28 12:55:40 +0000

    ps1 vars

--- automatic/mysteriumvpn/tools/chocolateyInstall.ps1
+++ automatic/mysteriumvpn/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName    = $env:ChocolateyPackageName
+$packageName    = $env:ChocolateyPackageName
 $url            = 'https://github.com/iBotPeaches/Apktool/releases/download/v2.6.0/apktool_2.6.0.jar'
 $checksum       = 'f750a3cd2c1f942f27f5f7fd5d17eada3bdaff0a6643f49db847e842579fdda5'
 $checksumType   = 'sha256'
@@ -9,14 +9,14 @@ $packageArgs = @{
     unzipLocation = $toolsDir
     fileType      = 'EXE'
     url           = $url
-  
+
     softwareName  = 'mysterium*'
-  
+
     checksum      = $checksum
     checksumType  = $checksumType
-  
+
     silentArgs   = '/S' # NSIS
     validExitCodes= @(0)
   }
-  
+
   Install-ChocolateyPackage @packageArgs
