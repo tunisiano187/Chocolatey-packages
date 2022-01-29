45c446c9e6d92e7b1482193b2784de424157cf91
tunisiano187 <helpdesk.choc@gmail.com>
2022-01-29 04:16:49 +0000

    Package download vibrancegui

---
+++ automatic/vibrancegui/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName  = 'vibrancegui' # arbitrary name for the package, used in messages
+$url          = 'http://vibrancegui.com/vibrance/download' # download url
+$checksum     = 'b2560705a8647cb8931097b32638049c1388e7e0'
+$checksumType = 'sha1'
+
+Install-ChocolateyZipPackage $packageName `
+  -Url $url `
+  -Checksum $checksum `
+  -ChecksumType $checksumType `
+  -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
