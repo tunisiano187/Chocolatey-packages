4966459a59c1329d1be16122019fd2ff2c0eb57d
tunisiano187 <fabian@bowlman.org>
2022-02-01 17:50:47 +0100

    First Commit

--- automatic/nano/tools/chocolateyInstall.ps1
+++ automatic/nano/tools/chocolateyInstall.ps1
@@ -1,12 +1,16 @@
-$packageName = 'nano'
-$url = 'http://www.nano-editor.org/dist/v2.5/NT/nano-2.5.3.zip'
-$checksum = 'b4ecf6ad6ae46e7c4e29b115b1503722133591d9'
-$checksumType = 'sha1'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = $env:ChocolateyPackageName
+$url            = 'http://www.nano-editor.org/dist/v2.5/NT/nano-2.5.3.zip'
+$checksum       = 'b4ecf6ad6ae46e7c4e29b115b1503722133591d9'
+$checksumtype   = 'sha1'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Url64bit "" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$packageArgs = @{
+    packageName   = $packageName
+    unzipLocation = $toolsDir
+    url           = $url
+
+    checksum      = $checksum
+    checksumType  = $checksumType
+}
+  
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/nano/update.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://github.com/lhmouse/nano-win/tags'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      			= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 			= "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumtype\s*=\s*)('.*')" 		= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	Write-Verbose 'Get files'
+	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.zip'} | Select-Object -First 1).href
+	Write-Verbose 'Checking version'
+	$version=$($url32).split('v')[-1].replace('.zip','')
+
+	Write-Verbose "Version : $version"
+	$url32 = "https://github.com$($url32)";
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
