7af30418a0badf0cc639ea8bfb974f66ca48549e
tunisiano187 <fabian@bowlman.org>
2022-02-04 14:49:49 +0100

    No iTag

--- automatic/itag/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$packageName        = $env:ChocolateyPackageName
-$installerType      = 'EXE'
-$silentArgs         = '/S'
-$url                = 'https://dl.johnsadventures.com/SwitcherSetup54.exe'
-$checksum           = '8f819e316091c11cc6150cb72a234058007ec22928dcd57b5635a4e3e055cc38'
-$checksumtype       = 'sha256'
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url -checksum $checksum -ValidExitCodes @(0,1223) -checksumType $checksumtype
