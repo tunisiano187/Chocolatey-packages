406a6b5394a8bd49665675f5e03b881aec548405
tunisiano187 <fabian@bowlman.org>
2022-02-07 20:09:58 +0100

    Update

--- automatic/keepass-plugin-webautotype/update.ps1
+++ automatic/keepass-plugin-webautotype/update.ps1
@@ -17,8 +17,8 @@ function global:au_GetLatest {
 	cup -y keepass
 	$url32 = "https://sourceforge.net/projects/webautotype/files/latest/download"
 	$version = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match "files\/v"}).href[0].split('/')[-2].replace('v','')
-	if($version -eq '6.4.0') {
-		$version = '6.4.0.20200906'
+	if($version -eq '6.8.1') {
+		$version = '6.8.1.20220207'
 	}
 
 	$Latest = @{ URL32 = $url32; Version = $version }
