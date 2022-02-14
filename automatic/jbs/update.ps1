636e57bada7b2bf1695da7d011aa483dd23db9f5
tunisiano187 <fabian@bowlman.org>
2022-02-14 22:35:00 +0100

    Force update

--- automatic/jbs/update.ps1
+++ automatic/jbs/update.ps1
@@ -23,6 +23,7 @@ function global:au_GetLatest {
 		Start-Sleep -Seconds 1
 	}
 	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($(join-path ${env:ProgramFiles(x86)} "johnsadventures.com\John's Background Switcher\BackgroundSwitcher.exe")).FileVersion
+	if($version -eq '5.4.0.1') {$version = '5.4.0.2'}
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
