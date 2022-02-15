f26defd5cf82981d1ae929b18d43f60a4e38ae72
tunisiano187 <fabian@bowlman.org>
2022-02-15 21:50:28 +0100

    Force

--- automatic/adobe-creative-cloud/tools/chocolateyInstall.ps1
+++ automatic/adobe-creative-cloud/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$url            = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v1/wam/download?sapCode=KCCC&productName=Creative%20Cloud&os=win&environment=prod' # download url
-$checksum       = 'f1ce9a3a681b1175f7b1e70c95f9b32efd2c72a6c5d816b32788e6a976ab1deb'
+$checksum       = 'f1ce9a3a681b1175f7bz1e70c95f9b32efd2c72a6c5d816b32788e6a976ab1deb'
 $checksumType   = 'sha256'
 
 $silentArgs     = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
