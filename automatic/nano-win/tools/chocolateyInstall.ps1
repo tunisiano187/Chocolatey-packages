3f0eac95dd8b62a39bc5fc6a5f7df22422442d08
tunisiano187 <fabian@bowlman.org>
2022-03-17 17:43:25 +0100

    nouveau lien

---
+++ automatic/nano-win/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName    = $env:ChocolateyPackageName
+$url            = 'https://github.com/lhmouse/nano-win/archive/refs/tags/v6.0.zip'
+$checksum       = '81734b6a02670c2f287951b3aa81e966ff12bef120d0590db9c21a0cd8cda0bc'
+$checksumtype   = 'sha256'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName   = $packageName
+    unzipLocation = $toolsDir
+    url           = $url
+
+    checksum      = $checksum
+    checksumType  = $checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs
