69fa318f4dc441a4f350ad868c2f76f7dead5377
tunisiano187 <fabian@bowlman.org>
2022-03-18 10:07:20 +0100

    link

--- automatic/nano/update.ps1
+++ automatic/nano/update.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://files.lhmouse.com/nano-win/?C=M&O=D'
+$releases = 'https://files.lhmouse.com/nano-win/'
 
 function global:au_SearchReplace {
 	@{
@@ -13,11 +13,12 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	Write-Verbose 'Get files'
-	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.7z'} | Select-Object -First 1).href
+	$filename = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.7z'} | Select-Object -Last 1).href
 	Write-Verbose 'Checking version'
-	$version=$($url32).split('v|g')[-2].replace('-','.')
+	$version=$($filename).split('v|g')[-2].replace('-','.')
 	$version=$version.Substring(0,$version.Length-1)
-
+	
+	$url32 = $releases + $filename
 	Write-Verbose "Version : $version"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
