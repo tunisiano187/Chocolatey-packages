5d8f4805f1ebf05d3ea0bd683d62475a88bf3cd9
tunisiano187 <fabian@bowlman.org>
2022-03-18 11:05:09 +0100

    Install-ChocolateyZipPackage

--- automatic/nano-win/tools/chocolateyInstall.ps1
+++ automatic/nano-win/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageName    = $env:ChocolateyPackageName
-$file           = 'https://files.lhmouse.com/nano-win/nano-win_9986_v6.2-16-g58bbad01a.7z'
+$file           = 'nano-win_9986_v6.2-16-g58bbad01a.7z'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-    packageName     = $packageName
-    Destination     = $toolsDir
-    file            = Join-Path $toolsDir $file
+    packageName   = $packageName
+    unzipLocation = $toolsDir
+    file          = Join-Path $toolsDir $file
 }
 
-Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyZipPackage @packageArgs
