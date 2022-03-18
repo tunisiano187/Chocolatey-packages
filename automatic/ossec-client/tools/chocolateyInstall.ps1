d06a6ff43d9f4a22203392359adb7e4ed5c0be0a
tunisiano187 <helpdesk.choc@gmail.com>
2022-03-18 09:30:52 +0000

    AU: 1 updated - ossec-client [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/5045381752fb5c3f9bdd6eeceea943ece014195b

--- automatic/ossec-client/tools/chocolateyInstall.ps1
+++ automatic/ossec-client/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url            = 'https://updates.atomicorp.com/channels/atomic/windows/ossec-agent-win32-3.6.0-12032.exe'
-$checksum       = '1dcaa817ce9b9fd992787b070fabc8e78c8fe13d07abe031e326b6b7dbb5e519'
+$url            = 'https://updates.atomicorp.com/channels/atomic/windows/ossec-agent-win32-3.7.0-24343.exe'
+$checksum       = '862b31fd64c70c760278a0d5408032e26dc6814a1ae24d64b5f3ae0ee2b343a4'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
