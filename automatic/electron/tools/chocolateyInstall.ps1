541985c74939d690ea18e1a9f17e872674a907a1
tunisiano187 <helpdesk.choc@gmail.com>
2022-03-31 04:15:37 +0000

    AU: 1 updated - electron [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/83dea0ad2cb8eb8eb0d561146deda56c86668f2a

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v18.0.0/electron-v18.0.0-win32-ia32.zip'
-$checksum = '612641b158ee4a7872ef7040a7d54b4a46a46e90852dee0777bcb16aa7453c39'
+$url = 'https://github.com/electron/electron/releases/download/v18.0.1/electron-v18.0.1-win32-ia32.zip'
+$checksum = 'b1344e34f7d02f6ca2e84d11c447419a210d5b288e45a068d1a5d8dd562b1aeb'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v18.0.0/electron-v18.0.0-win32-x64.zip'
-$checksum64 = '6706372e6590636d582a8287b6796d058e4987b4fbb16194167e2f82258e2d46'
+$url64 = 'https://github.com/electron/electron/releases/download/v18.0.1/electron-v18.0.1-win32-x64.zip'
+$checksum64 = '7a47cf32330f41cc94066137d2b5f12a0fea88aaf8be1ad5ed3202d564aa35eb'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
