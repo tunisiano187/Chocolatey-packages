97068279cc25beba6ad574eb7d0ed2e02150bba3
tunisiano187 <fabian@bowlman.org>
2022-04-01 14:12:58 +0200

    First Commit PacketStream

---
+++ automatic/packetstream/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$url			      = ''
+$checksum		    = ''
+$checksumtype   = ''
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = '/quiet'
+  validExitCodes= @(0,3010)
+  softwareName  = 'Packetstream*'
+  checksum      = $checksum
+  checksumType  = $checksumtype
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/packetstream/update.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$url32 = 'https://s3-us-west-2.amazonaws.com/packetstream-releases/latest/PacketStream.exe'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumtype\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+	$File = Join-Path $env:TEMP "PacketStream.exe"
+	Invoke-WebRequest -Uri $url32 -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
