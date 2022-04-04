d667ed1f952868519b4e6677f24e6208e1bc5707
Tunisiano187 <helpdesk.choc@gmail.com>
2022-04-04 07:26:06 +0000

    AU: 1 updated - mysteriumvpn [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/27fabf96024e04d38337bd1317ad9ed8bc6bd628

--- automatic/mysteriumvpn/tools/chocolateyInstall.ps1
+++ automatic/mysteriumvpn/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName    = $env:ChocolateyPackageName
-$url            = 'https://github.com/mysteriumnetwork/mysterium-vpn-desktop/releases/download/10.8.2/MysteriumVPN-Setup-10.8.2.exe'
-$checksum       = '9e5ee2a9d8f906c9eea5bc7da0ddaa0c979c1ab2b8508db5ee38965e3a57be47'
+$url            = 'https://github.com/mysteriumnetwork/mysterium-vpn-desktop/releases/download/10.8.3/MysteriumVPN-Setup-10.8.3.exe'
+$checksum       = '34b6c488f872c1f688bdb9b1888276678573476a1340f718c8bdce8f7eb96976'
 $checksumType   = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
