06673bee498608e10b23fbe1ad02e4eb45cb35b4
Tunisiano187 <fabian@bowlman.org>
2022-09-10 20:19:57 +0200

    Test Pestudio

--- automatic/pestudio/update.ps1
+++ automatic/pestudio/update.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://www.winitor.com/download'
+$releases = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
 
 function global:au_SearchReplace {
 	@{
@@ -15,7 +15,8 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	Write-Output 'Get Link'
-	$url32 = "https://www.winitor.com$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '.zip'}).href)"
+	#$url32 = "https://www.winitor.com$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '.zip'}).href)"
+	$url32 = $releases
 
 	Invoke-WebRequest -Uri $url32 -OutFile "$env:temp/pestudio.zip"
 	Expand-Archive -Path "$env:temp/pestudio.zip" -DestinationPath "$env:temp/pestudio" -Force
