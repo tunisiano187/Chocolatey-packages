4c84f2b0ed89a3654c8930c5a69319f047ec64cf
Tunisiano187 <fabian@bowlman.org>
2022-09-10 20:52:08 +0200

    Correction

--- automatic/pestudio/update.ps1
+++ automatic/pestudio/update.ps1
@@ -18,13 +18,12 @@ function global:au_GetLatest {
 	#$url32 = "https://www.winitor.com$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '.zip'}).href)"
 	$url32 = $releases
 
-	Invoke-WebRequest -Uri $url32 -OutFile "$env:temp/pestudio.zip"
+	Invoke-WebRequest -Uri $url32 -OutFile "$env:temp/pestudio.zip" -UseBasicParsing	
 	Expand-Archive -Path "$env:temp/pestudio.zip" -DestinationPath "$env:temp/pestudio" -Force
 	$File = (Get-ChildItem "$env:temp/pestudio.exe" -Recurse).FullName
 	$version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim().replace(',','.')
-	$r = Invoke-WebRequest -Uri $releases
-	$checksum = ($r.ParsedHTML.GetElementsByTagName('code') | Select-Object innerHTML).innerHTML
 	$checksumType = 'SHA256'
+	$checksum = (Get-FileHash -Algorithm $checksumType -Path $File).Hash
 
 	$Latest = @{ URL32 = $url32; Checksum32 = $checksum; ChecksumType32 = $checksumType; Version = $version }
 	return $Latest
