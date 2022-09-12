fbf9e60749244ecaa3a7ea0c331c62ba8cf8808e
tunisiano187 <helpdesk.choc@gmail.com>
2022-09-12 19:57:10 +0000

    Package download xfinder

---
+++ automatic/xfinder/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿try {
+  $downUrl = "http://www.eonet.ne.jp/~gakana/download/dl/xf11-11.cab"
+  $parentpath = "$(Split-Path -parent $(Split-Path -parent $MyInvocation.MyCommand.Definition))"
+  Get-ChocolateyWebFile 'xfinder' "${parentpath}\xfinder.cab" "$downUrl"
+  $cmd7z = "${Env:ProgramFiles}\7-Zip\7z.exe"
+  Start-Process -FilePath "$cmd7z" -Wait -WorkingDirectory "${parentpath}\tools" -ArgumentList "x ${parentpath}\xfinder.cab"
+
+  $target = Join-Path $parentpath 'tools\xf.exe'
+  Install-ChocolateyDesktopLink $target
+  
+  Write-ChocolateySuccess 'xfinder'
+} catch {
+  Write-ChocolateyFailure 'xfinder' "$($_.Exception.Message)"
+  throw 
+}
