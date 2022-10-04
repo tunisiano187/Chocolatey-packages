77751a09608d6ab92bb218d8fffb6e0aa7675d77
tunisiano187 <fabian@bowlman.org>
2022-10-04 21:19:52 +0200

    prerelease

--- automatic/duplicati.portable/update.ps1
+++ automatic/duplicati.portable/update.ps1
@@ -17,14 +17,12 @@ function global:au_GetLatest {
 	$tags = Invoke-WebRequest 'https://api.github.com/repos/duplicati/duplicati/releases' -UseBasicParsing | ConvertFrom-Json
     $url32 = (($tags | Select-Object -First 1).assets | Where-Object {$_ -match '.zip'} | Where-Object {$_ -notmatch '-signatures'} | Select-Object -First 1).browser_download_url
     $version = ($tags[0].tag_name).split('v|-')[1]
-    foreach ($tag in $tags) {
-        if($tag.tag_name -match $version) {
-            if($tag.prerelease -match "true") {
-                $clnt = new-object System.Net.WebClient;
-                $clnt.OpenRead("https://github.com$($installers)").Close();
-                $date = $([datetime]$clnt.ResponseHeaders["Last-Modified"];).ToString("yyyyMMdd")
-                $version = "$version-pre$($date)"
-            }
+    if($tags[0].tag_name -match $version) {
+        if($tags[0].prerelease -match "true") {
+            $clnt = new-object System.Net.WebClient;
+            $clnt.OpenRead($url32).Close();
+            $date = $([datetime]$clnt.ResponseHeaders["Last-Modified"];).ToString("yyyyMMdd")
+            $version = "$version-pre$($date)"
         }
     }
 
