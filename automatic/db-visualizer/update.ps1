a35ad1d0056b9249bb1a4122c171a906346f08e7
tunisiano187 <fabian@bowlman.org>
2022-10-04 16:55:50 +0200

    Update filters

--- automatic/db-visualizer/update.ps1
+++ automatic/db-visualizer/update.ps1
@@ -18,9 +18,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
-	$links = $(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match 'dbvis_windows'} | Where-Object  {$_.href -match 'exe'} | Where-Object {$_.href -notMatch '_jre'})).href
+	$links = $(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match 'dbvis_windows'} | Where-Object  {$_.href -match 'exe'} | Where-Object {$_.href -notMatch '_jre'})).href | Select-Object -First 1
 
-	$version = $links[0].split('-')[-1].replace('x32_','').replace('.exe','').replace('_','.')
+	$version = $links.split('-')[-1].replace('x32_','').replace('.exe','').replace('_','.').replace('x64.','')
 	if($version -eq '13.0.4') {
 		$version = '13.0.4.22060901'
 	}
