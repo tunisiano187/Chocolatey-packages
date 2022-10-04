f20b038fbfe11b67fc62c9e8b758ef76a3062402
tunisiano187 <fabian@bowlman.org>
2022-10-04 19:25:33 +0200

    no more 32bit version

--- automatic/db-visualizer/tools/chocolateyInstall.ps1
+++ automatic/db-visualizer/tools/chocolateyInstall.ps1
@@ -1,9 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32            = 'https://dbvis.com/product_download/dbvis-13.0.5/media/dbvis_windows-x32_13_0_5.exe'
 $url64            = 'https://dbvis.com/product_download/dbvis-13.0.5/media/dbvis_windows-x64_13_0_5.exe'
-$checksum         = 'ba174ccb3369ec25d7b628b26ae36ee4f650b7ac34ff9db534c8991292da256b'
-$checksumType     = 'sha256'
 $checksum64       = '76f0925b16ef7f6d9f4f669278a8015545c610b542a5d2c43aec36dde0a39795'
 $checksumType64   = 'sha256'
 
@@ -11,16 +8,13 @@ $packageArgs = @{
   packageName     = $env:ChocolateyPackageName
   unzipLocation   = $toolsDir
   fileType        = 'EXE'
-  url             = $url32
-  url64bit        = $url64
-
+  url             = $url64
+  
   softwareName    = 'DbVisualizer*'
 
-  checksum        = $checksum
-  checksumType    = $checksumType
-  checksum64      = $checksum64
-  checksumType64  = $checksumType64
-
+  checksum        = $checksum64
+  checksumType    = $checksumType64
+  
   silentArgs      = "-q"
 
   validExitCodes  = @(0)
--- automatic/db-visualizer/update.ps1
+++ automatic/db-visualizer/update.ps1
@@ -6,9 +6,6 @@ $releases = 'https://www.dbvis.com/download'
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
 			"(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -20,13 +17,12 @@ function global:au_GetLatest {
 	Write-Output 'Check Folder'
 	$links = $(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match 'dbvis_windows'} | Where-Object  {$_.href -match 'exe'} | Where-Object {$_.href -notMatch '_jre'})).href | Select-Object -First 1
 
-	$version = $links.split('-')[-1].replace('x32_','').replace('.exe','').replace('_','.').replace('x64.','')
+	$version = $links.split('-')[-1].replace('x64_','').replace('.exe','').replace('_','.')
 	if($version -eq '13.0.4') {
 		$version = '13.0.4.22060901'
 	}
 
-	$url32 = $links[0]
-	$url64 = $links[1]
+	$url64 = $links
 
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
