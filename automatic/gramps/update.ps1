3986b6d6a0007f437fc6d4596cb97eba645949fa
tunisiano187 <fabian@bowlman.org>
2022-10-05 18:56:43 +0200

    Github API

--- automatic/gramps/update.ps1
+++ automatic/gramps/update.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://github.com/gramps-project/gramps/releases/latest'
+$releases = 'https://api.github.com/repos/gramps-project/gramps/releases/latest'
 
 function global:au_SearchReplace {
 	@{
@@ -18,16 +18,15 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
-	$installer = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.exe'} | Sort-Object -Descending).href
-	$installer32 = $installer | Where-Object {$_ -match 'win32'} | Select-Object -First 1
-	$installer64 = $installer | Where-Object {$_ -match 'win64'} | Select-Object -First 1
+	$tags = Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json
+	$installer = ($tags[0].assets | where {$_.browser_download_url -match ".exe$"}).browser_download_url | Sort-Object -Descending
+	$url32 = $installer | Where-Object {$_ -match 'win32'} | Select-Object -First 1
+	$url64 = $installer | Where-Object {$_ -match 'win64'} | Select-Object -First 1
 	Write-Output 'Checking version'
 	$version=$($installer[0]).split('O')[-1].split('_')[0].trim().substring(1).replace('-','.')
 
 	Write-Output "Version : $version"
-	$url32 = "https://github.com$($installer32)";
-	$url64 = "https://github.com$($installer64)";
-
+	
 	if($version -eq '5.1.3.2') {
 		$version = '5.1.3.3'
 	}
