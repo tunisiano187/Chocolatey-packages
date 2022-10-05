caec17ba654d5b7d60a94bc86b872146ea11832a
tunisiano187 <fabian@bowlman.org>
2022-10-05 19:16:31 +0200

    Github API

--- automatic/gsmartcontrol/update.ps1
+++ automatic/gsmartcontrol/update.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://github.com/ashaduri/gsmartcontrol/releases/'
+$releases = 'https://api.github.com/repos/ashaduri/gsmartcontrol/releases/latest'
 
 function global:au_SearchReplace {
 	@{
@@ -17,13 +17,14 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
+	$tags = Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json
 	Write-Output 'Check Folder'
-	$installer = (((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'gsmartcontrol-'} | Where-Object {$_ -match ".exe"} | Where-Object {$_ -notmatch 'hotfix'} | Where-Object {$_ -notmatch 'sha256'}).href | Select-Object -First 2 | Sort-Object )
+	$installer = (($tags[0].assets | Where-Object {$_.browser_download_url -match 'gsmartcontrol-'} | Where-Object {$_.browser_download_url -match ".exe"} | Where-Object {$_.browser_download_url -notmatch 'hotfix'} | Where-Object {$_.browser_download_url -notmatch 'sha256'}).browser_download_url | Select-Object -First 2 | Sort-Object )
 	Write-Output 'Checking version'
 	$version=$installer[0].split('/')[-1].split('-')[1]
 	Write-Output "Version : $version"
-	$url32 = "https://github.com$($installer[0])";
-	$url64 = "https://github.com$($installer[1])";
+	$url32 = $installer | Where-Object {$_ -match 'win32'}
+	$url64 = $installer | Where-Object {$_ -match 'win64'}
 
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
 	return $Latest
