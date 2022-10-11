058ba8695367fbf700673851c79c64e996150fb6
tunisiano187 <fabian@bowlman.org>
2022-10-11 19:32:16 +0200

    0.2.0.20221010 [skip ci]

--- automatic/opus-tools/tools/chocolateyInstall.ps1
+++ automatic/opus-tools/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://archive.mozilla.org/pub/opus/win32/opus-tools-0.2-win32.zip'
-$checksum       = '99a7c07c46c24727d9798adfc53f5d1d82244e3c249476c22ddf0e1d76a7c86c'
+$checksum       = '99A7C07C46C24727D9798ADFC53F5D1D82244E3C249476C22DDF0E1D76A7C86C'
 $checksumType   = 'sha256'
 $url64          = 'https://archive.mozilla.org/pub/opus/win32/opus-tools-0.2-win32.zip'
 $checksum64     = '99a7c07c46c24727d9798adfc53f5d1d82244e3c249476c22ddf0e1d76a7c86c'
