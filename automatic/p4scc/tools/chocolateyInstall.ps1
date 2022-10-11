328f579b4c5024966c51231414a4a96c1e8c4520
tunisiano187 <helpdesk.choc@gmail.com>
2022-10-11 18:01:25 +0000

    Package download p4scc

---
+++ automatic/p4scc/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'p4scc'
+$installerType = 'EXE'
+$url = 'http://www.perforce.com/downloads/perforce/r12.1/bin.ntx86/p4sccinst.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
