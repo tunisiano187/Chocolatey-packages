3c9bbdb2a24f32e3d09d1546dd6d8ed0b61fc787
tunisiano187 <helpdesk.choc@gmail.com>
2022-10-11 19:47:14 +0000

    Package download chainlp

---
+++ automatic/chainlp/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+  $downUrl = 'http://chainlp.o.oo7.jp/ChainLP40b17.zip'
+  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+  Install-ChocolateyZipPackage 'chainlp' "$downUrl" "$toolsDir" 
+
+  Write-ChocolateySuccess 'chainlp'
+} catch {
+  Write-ChocolateyFailure 'chainlp' "$($_.Exception.Message)"
+  throw 
+}
