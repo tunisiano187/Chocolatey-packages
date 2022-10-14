3cbb8dba8b4b88a6450f36390455ad95766d3e43
tunisiano187 <helpdesk.choc@gmail.com>
2022-10-14 04:22:35 +0000

    Package download mightymoose

---
+++ automatic/mightymoose/tools/chocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+﻿$package = 'mightymoose'
+$file = (Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{DE3A8FE7-5918-471A-BF59-84C2C3328CCF}_is1").UninstallString
+Uninstall-ChocolateyPackage $package 'EXE' -file $file
