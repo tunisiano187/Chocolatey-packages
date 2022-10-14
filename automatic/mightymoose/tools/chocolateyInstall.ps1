5c981504ef1261d424818d08992a345cf41712a2
Tunisiano187 <fabian@bowlman.org>
2022-10-14 21:26:46 +0200

    Close #3143

--- automatic/mightymoose/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'mightmoose' 
-$installerType = 'EXE' 
-$url = 'http://continuoustests.com/ContinuousTests-v1.0.47.exe'  
-$url64 = $url 
-$silentArgs = '/SILENT' 
-$validExitCodes = @(0) 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
