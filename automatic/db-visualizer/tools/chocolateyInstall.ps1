fe29581353f47c1188e36c95465dc0622db87f19
tunisiano187 <helpdesk.choc@gmail.com>
2022-10-14 19:50:16 +0000

    AU: 2 updated - db-visualizer voicemod [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/054d1bb068256a0b4f21ee4c4977e0d13f866e6d

--- automatic/db-visualizer/tools/chocolateyInstall.ps1
+++ automatic/db-visualizer/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64            = 'https://dbvis.com/product_download/dbvis-14.0/media/dbvis_windows-x64_14_0.exe'
-$checksum64       = 'a6b209b4bd837907478df3a5c31911baf5f46a5db531e5bce827366df8560999'
+$url64            = 'https://dbvis.com/product_download/dbvis-14.0.1/media/dbvis_windows-x64_14_0_1.exe'
+$checksum64       = '2efa48edb9d935340763684a5e40c9584444e20919364568a627022e2d21726e'
 $checksumType64   = 'sha256'
 
 $packageArgs = @{
