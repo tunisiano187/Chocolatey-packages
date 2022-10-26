7c9c9373840ccfb1279514dcb3710e735f14d2b1
Tunisiano187 <fabian@bowlman.org>
2022-10-26 23:21:34 +0200

    module

--- automatic/brave-nightly/update.ps1
+++ automatic/brave-nightly/update.ps1
@@ -2,7 +2,8 @@ $ErrorActionPreference = 'Stop'
 import-module au
 
 # $releases = 'https://github.com/brave/brave-browser/releases'
-
+$Owner = "brave"
+$repo = "brave-browser"
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
@@ -11,16 +12,16 @@ function global:au_SearchReplace {
 			"(^[$]checksumType32\s*=\s*)('.*')"	= "`$1'$($Latest.ChecksumType32)'"
 		}
 	}
-}
+} 
 function global:au_GetLatest {
 	Write-Verbose 'Get files'
-	$tags = Invoke-WebRequest 'https://api.github.com/repos/brave/brave-browser/releases' -UseBasicParsing | ConvertFrom-Json
-	$url32 = ($tags[0].assets | Where-Object {$_.browser_download_url -match ".exe$"}).browser_download_url | Where-Object { $_ -match 'StandaloneSilent'} | Where-Object {$_ -notmatch '32.exe'} | Where-Object {$_ -notmatch 'Arm64'}
+	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
+	$url32 = $tags.assets.browser_download_url | Where-Object {$_ -match ".exe$"} | Where-Object { $_ -match 'StandaloneSilent'} | Where-Object {$_ -notmatch '32.exe'} | Where-Object {$_ -notmatch 'Arm64'}
 
 	Write-Verbose 'Checking version'
-	$version=($tags[0].name.Split(' ')[1]).replace('v','')
+	$version=($tags.name.Split(' ')[1]).replace('v','')
 
-	if($tags[0].prerelease -eq $true) {
+	if($tags.prerelease -eq $true) {
 		$version = "$version-nightly"
 	} elseif ($url32 -match 'Nightly') {
 		$version = "$version-nightly"
