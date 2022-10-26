432a159448e5fd21b707e2808b92c57150ca08da
Tunisiano187 <fabian@bowlman.org>
2022-10-26 21:52:45 +0200

    version from repo

--- automatic/devbox-p4merge/update.ps1
+++ automatic/devbox-p4merge/update.ps1
@@ -22,29 +22,9 @@ function global:au_BeforeUpdate() {
 
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
-	$version_folder = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '^r\d+([.]\d+)?'} | ForEach-Object {($_.href -replace '[^.\d]', '')} | Sort-Object -Descending)
-	$newversion='0.0'
-	$version='0.0'
-	foreach ($item in $version_folder) {
-		try {
-			$ver = $item.replace(',','.')
-			$linktest = "https://cdist2.perforce.com/perforce/r$($ver)/doc/user/p4vnotes.txt"
-			$license = "https://cdist2.perforce.com/perforce/r$($ver)/doc/user/p4v_license.txt"
-			Invoke-WebRequest -Uri $linktest -OutFile "$env:TEMP\p4v.txt"
-			Invoke-WebRequest -Uri $license -OutFile ".\tools\LICENSE.txt"
-            $newversion = $($(Get-Content "$env:TEMP\p4v.txt" | Where-Object { $_ -match 'version'}).trim() | Where-Object { $_ -match '^Version'})[0].split(' ')[-1]
-            if([version]$version -lt [version]$newversion)
-			{
-				$url64 = "https://cdist2.perforce.com/perforce/r$($ver)/bin.ntx64/p4vinst64.exe"
-
-                $version = $newversion
-			}
-		}
-		catch {
-			Write-Verbose "V$($item) Not found"
-		}
-    }
-	#Invoke-WebRequest -Uri $url64 -OutFile ".\tools\p4vinst64.exe"
+	$version=$(choco search ^p4merge$ | Where-Object {$_ -notmatch 'found.'} | Where-Object {$_ -notmatch 'Did you know'} | Where-Object {$_ -notmatch 'Chocolatey v'} | Where-Object {$_ -notmatch "NuGet"} | Select-Object -First 1).split(' ')[1]
+	$license = "https://cdist2.perforce.com/perforce/r$($version)/doc/user/p4v_license.txt"
+	$url64 = "https://cdist2.perforce.com/perforce/r$($version)/bin.ntx64/p4vinst64.exe"
 
 	$Latest = @{ URL32 = $url64; Version = $version; License = $license }
 	return $Latest
