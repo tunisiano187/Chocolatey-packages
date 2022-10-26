a966040874f0d9932081d716005b7034a24a3481
Tunisiano187 <fabian@bowlman.org>
2022-10-26 23:37:59 +0200

    .Substring(2)

--- automatic/devbox-p4merge/update.ps1
+++ automatic/devbox-p4merge/update.ps1
@@ -23,8 +23,9 @@ function global:au_BeforeUpdate() {
 function global:au_GetLatest {
 	Write-Output 'Check Folder'
 	$version=$(choco search ^p4merge$ | Where-Object {$_ -notmatch 'found.'} | Where-Object {$_ -notmatch 'Did you know'} | Where-Object {$_ -notmatch 'Chocolatey v'} | Where-Object {$_ -notmatch "NuGet"} | Select-Object -First 1).split(' ')[1]
-	$license = "https://cdist2.perforce.com/perforce/r$($version)/doc/user/p4v_license.txt"
-	$url64 = "https://cdist2.perforce.com/perforce/r$($version)/bin.ntx64/p4vinst64.exe"
+
+	$license = "https://cdist2.perforce.com/perforce/r$($version.Substring(2))/doc/user/p4v_license.txt"
+	$url64 = "https://cdist2.perforce.com/perforce/r$($version.Substring(2))/bin.ntx64/p4vinst64.exe"
 
 	$Latest = @{ URL32 = $url64; Version = $version; License = $license }
 	return $Latest
