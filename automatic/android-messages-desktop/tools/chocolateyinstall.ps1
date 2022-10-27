31f7773bf3733a137aaf75685894b51ba7d9a462
tunisiano187 <fabian@bowlman.org>
2022-10-27 18:00:58 +0200

    vars

--- automatic/android-messages-desktop/tools/chocolateyinstall.ps1
+++ automatic/android-messages-desktop/tools/chocolateyinstall.ps1
@@ -1,13 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$url            = ''
+$checksum       = ''
+$checksumType   = ''
+
+
 $packageArgs = @{
   packageName   = 'android-messages-desktop'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'https://github.com/chrisknepper/android-messages-desktop/releases/download/v0.7.1/android-messages-desktop-setup-0.7.1.exe'
+  url           = $url
   softwareName  = 'Android Messages*'
-  checksum      = 'F909E96B0C3027D09C3811BB112DBDB0BC3D85C6F687CAFFA60B35168704F358'
-  checksumType  = 'sha256'
+  checksum      = $checksum
+  checksumType  = $checksumType
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/S'
 }
