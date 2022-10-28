6cc449490e74ed8d26c42fe541713c530c6f4e90
Tunisiano187 <helpdesk.choc@gmail.com>
2022-10-28 07:23:41 +0000

    AU: 1 updated - electron [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/63f4c827b5d7166579a0af960269ddcac3df5dae

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v22.0.0-beta.1/electron-v22.0.0-beta.1-win32-ia32.zip'
-$checksum = 'c1f45aac04c9d9572355c9f3b7b908ebb5395ec2154c8cb9787e15388d2498ea'
+$url = 'https://github.com/electron/electron/releases/download/v22.0.0-beta.2/electron-v22.0.0-beta.2-win32-ia32.zip'
+$checksum = '213418a65a3d89b84caf82fbed375f0e086c8cb325da5c66d18a7f23f7122396'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v22.0.0-beta.1/electron-v22.0.0-beta.1-win32-x64.zip'
-$checksum64 = 'd0a08f147dc547232dcb6757f7bd4562f72fe54af243cc433c792d3bd321761b'
+$url64 = 'https://github.com/electron/electron/releases/download/v22.0.0-beta.2/electron-v22.0.0-beta.2-win32-x64.zip'
+$checksum64 = '1468283df08e14dd7a2c1d4b2cb0fd311384a3b6666bcc8eccb85087101fee17'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
