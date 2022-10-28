b54485aefd7a0c22c878468c3accd158dd091954
tunisiano187 <fabian@bowlman.org>
2022-10-28 20:56:14 +0200

    No message if no package requested

--- scripts/Find-nextissue.ps1
+++ scripts/Find-nextissue.ps1
@@ -8,10 +8,12 @@ if(!(Test-Path Env:github_api_key)) {
 
 Write-Output "Search the next package to import"
 $search = (Find-GitHubIssue -Type issue -Repo "$Owner/$Repository" -State open -Labels "ToCreateManualy" | Sort-Object -Property Number).Title.split('(|)')[1]
-$folder = Join-Path $PSScriptRoot "../automatic/$search"
-if(Test-Path $folder) {
-    Write-Warning "Package already in the folder, the package $search needs to be finished and the issue closed"
-} elseif ($search -ne '') {
-    $script = Join-Path $PSScriptRoot "Get-Package.ps1"
-    . $script $search.Tolower()
+if($search) {
+    $folder = Join-Path $PSScriptRoot "../automatic/$search"
+    if(Test-Path $folder) {
+        Write-Warning "Package already in the folder, the package $search needs to be finished and the issue closed"
+    } elseif ($search -ne '') {
+        $script = Join-Path $PSScriptRoot "Get-Package.ps1"
+        . $script $search.Tolower()
+    }
 }
