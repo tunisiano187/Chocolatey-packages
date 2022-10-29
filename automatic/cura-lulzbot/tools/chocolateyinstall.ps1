1936eb01583d69fcbbb71e07e7c67ef1bc9a6e80
tunisiano187 <helpdesk.choc@gmail.com>
2022-10-29 13:52:22 +0000

    Package download cura-lulzbot

---
+++ automatic/cura-lulzbot/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32      = 'https://gitlab.com/lulzbot3d/cura-le/release/-/raw/master/cura-lulzbot-3.6.37-win10-win32.exe'
+$checksum32 = 'bfa1588268ec59e371f68c1f7629fa93876c2743eff0590c8281d8d4618f402f'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url32
+  softwareName  = 'cura-lulzbot*'
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
