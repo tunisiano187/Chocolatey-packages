01e561f75ac4e509a627c7df5b6441e3a754cfdd
tunisiano187 <fabian@bowlman.org>
2022-10-29 17:26:06 +0200

    VT

--- automatic/wireedit/update.ps1
+++ automatic/wireedit/update.ps1
@@ -13,6 +13,16 @@ function global:au_SearchReplace {
      }
 }
 
+function global:au_BeforeUpdate {
+    Install-Module VirusTotalAnalyzer -Force
+    Import-Module VirusTotalAnalyzer -NoClobber -Force
+    $vt = (Get-VirusScan -ApiKey $env:VT_APIKEY -File $Latest.URL32).data.attributes.reputation
+    if ( $vt -gt 5 ) {
+        Write-Error "Ignoring $($Latest.PackageName) package due to virus total results - $vt positives"
+        return 'ignore'
+    }
+  }
+
 function global:au_GetLatest {
 	$url32="https://omnipacket.com/$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.msi'} | Where-Object {$_ -match 'WireEdit'}).href)"
 
