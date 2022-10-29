18d34fa02b2aad9aeead2b9d92ee1e28f5e4d13a
tunisiano187 <fabian@bowlman.org>
2022-10-29 11:25:02 +0200

    Working on #3156, https://github.com/chocolatey-community/chocolatey-package-requests/issues/1082

---
+++ automatic/ds4windows/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+
+$releases = 'https://github.com/Ryochan7/DS4Windows/releases/latest'
+$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
+$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x64:).*"                   = "`${1} $($Latest.URL64)"
+          "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL64)"
+          "(?i)(\s+checksum64:).*"            = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+
+function global:au_GetLatest {
+  $tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
+  $url = $tags.assets.browser_download_url | Where-Object {$_ -match ".zip$"}
+  $version = $tags.tag_name.Replace('v','')
+  if($tags.prerelease -match "true") {
+      $date = $tags.published_at.ToString("yyyyMMdd")
+      $version = "$version-pre$($date)"
+  }
+  @{
+    URL64 = $url
+    Version = $version.Replace('v','')
+    ReleaseNotes = "https://github.com/Ryochan7/DS4Windows/releases/tag/${version}"
+  }
+}
+
+update -ChecksumFor none
