e0342d07d955abbdabd7e2eb2cb28f1e75c304d7
tunisiano187 <fabian@bowlman.org>
2022-10-31 18:35:12 +0100

    iconUrl licenseUrl releaseNotes copyright

--- automatic/ivpn/update.ps1
+++ automatic/ivpn/update.ps1
@@ -8,9 +8,13 @@ $repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      	= "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+		"$($Latest.PackageName).nuspec" = @{
+			"(\<releaseNotes\>).*?(\</releaseNotes\>)" 	= "`${1}$($Latest.ReleaseNotes)`$2"
+			"(\<copyright>\>IVPN.net).*?(\</releaseNotes\>)"	= "`${1}$($Latest.ReleaseNotes)`$2"
 		}
 	}
 }
@@ -29,13 +33,14 @@ function global:au_GetLatest {
 	$url32 = ($tags[0].body.Split('
 ') | Where-Object {$_ -match '.exe'}).split('(|)')[-2]
 	$version = $tags[0].tag_name -split 'v|/' | select-object -Last 1
+	$releaseNotes="https://github.com/$($Owner)/$($repo)/releases/tag/$($tags[0].tag_name)"
 	if($tags[0].prerelease -match "true") {
 		$date = $tags[0].published_at.ToString("yyyyMMdd")
 		$version = "$version-pre$($date)"
 	}
 
-	$Latest = @{ URL32 = $url32; Version = $version }
+	$Latest = @{ URL32 = $url32; Version = $version; ReleaseNotes = $releaseNotes }
 	return $Latest
 }
 
-update
+update -NoCheckChocoVersion
