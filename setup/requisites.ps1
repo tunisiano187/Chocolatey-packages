b82bf8b8557d7b97c9da003198b5e743abef2a3f
tunisiano187 <fabian@bowlman.org>
2022-11-01 21:18:49 +0100

    Disable telemetry

--- setup/requisites.ps1
+++ setup/requisites.ps1
@@ -22,4 +22,11 @@ Set-Service -Name wuauserv -StartupType Manual
 #$(Get-DnsClientServerAddress).ServerAddresses
 
 Install-Module VirusTotalAnalyzer -Force
+Install-Module -Name PowerShellForGitHub -Force
+    $secureString = ("$env:github_api_key" | ConvertTo-SecureString -AsPlainText -Force)
+    $cred = New-Object System.Management.Automation.PSCredential "username is ignored", $secureString
+    Set-GitHubAuthentication -Credential $cred
+    $secureString = $null # clear this out now that it's no longer needed
+    $cred = $null # clear this out now that it's no longer needed
+Set-GitHubConfiguration -DisableTelemetry
 #. "$PSScriptRoot\au_setup.ps1"
