d90a39a3bc30e21517ac47bc3f2602d30ef4f05c
tunisiano187 <fabian@bowlman.org>
2022-11-01 16:09:20 +0100

    Close #3163

--- automatic/ds4windows/tools/chocolateybeforemodify.ps1
+++ automatic/ds4windows/tools/chocolateybeforemodify.ps1
@@ -2,9 +2,9 @@
 
 $p = Get-Process -Name "DS4Windows" -ea 0
 if (!$p) {
-    Write-Host "$packageName is not running"
+    "$packageName is not running"
     return
 }
 
-Write-Host "$packageName is running, trying to gracefully shut it down before upgrade/uninstall"
+"$packageName is running, trying to gracefully shut it down before upgrade/uninstall"
 Stop-Process -InputObject $p -Force
