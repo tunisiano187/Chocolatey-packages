40572ef933b4f682bfbf8e4ad01f99e3c81942ed
tunisiano187 <helpdesk.choc@gmail.com>
2022-11-03 12:14:53 +0000

    ps1 vars

--- automatic/powerguivsx/tools/ChocolateyInstall.ps1
+++ automatic/powerguivsx/tools/ChocolateyInstall.ps1
@@ -1,5 +1,5 @@
-try 
-{ 
+try
+{
 	#$targetUrl = "http://visualstudiogallery.msdn.microsoft.com/01516103-d487-4a7e-bb40-c15ec709afa3/file/31518/10/PowerGUIVsx.vsix"
 
 	$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
@@ -8,12 +8,12 @@ try
 	$targetFileName = [System.IO.Path]::GetFileName($targetUrl)
 
 	Write "Loading and installing file:[$targetFileName] from URL:[$targetUrl]"
-	Install-ChocolateyVsixPackage "PowerGUIVSX" $targetUrl 
-	  
+	Install-ChocolateyVsixPackage "PowerGUIVSX" $targetUrl
+
 	Write-ChocolateySuccess 'PowerGUIVSX'
 }
-catch 
+catch
 {
   	Write-ChocolateyFailure 'PowerGUIVSX' "$($_.Exception.Message)"
-  	throw 
+  	throw
 }
