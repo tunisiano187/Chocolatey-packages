d1b3c42b0684e87224ab64a86acdb72dbd9fd1af
tunisiano187 <helpdesk.choc@gmail.com>
2022-11-04 13:55:33 +0000

    AU: 1 updated - traffic-monitor [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/6607d94d966e0bdcedfa07d6e3041abd91583f73

--- automatic/traffic-monitor/tools/chocolateyinstall.ps1
+++ automatic/traffic-monitor/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 #$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
 $url32          = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.84/TrafficMonitor_V1.84_x86.zip'
-$checksum32     = ''
-$checksumType32 = ''
+$checksum32     = '6B5D3091528E39F6F6F64F8725AB8BD4423EB9A6648706D4AD40BBFC7C0E3755'
+$checksumType32 = 'sha256'
 $url64          = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.84/TrafficMonitor_V1.84_x64.zip'
-$checksum64     = ''
-$checksumType64 = ''
+$checksum64     = '3B2DF6DB0540CAD6F9D5F813D693951AA538F5A6410469737EDB11BEC2A40928'
+$checksumType64 = 'sha256'
 
 $shortcutsPath = [Environment]::GetFolderPath("Programs")
 
