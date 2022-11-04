eef613d683f58d324e86f3933db530bfde1cd61c
tunisiano187 <fabian@bowlman.org>
2022-11-04 08:12:04 +0100

    Close #3171

--- automatic/eclipse-classic-juno/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$name='Eclipse'
-$url32='http://mirror.ibcp.fr/pub/eclipse/eclipse/downloads/drops4/R-4.2.2-201302041200/eclipse-SDK-4.2.2-win32.zip'
-$url64='http://eclipse.ialto.com/eclipse/downloads/drops4/R-4.2.2-201302041200/eclipse-SDK-4.2.2-win32-x86_64.zip'
-
-Install-ChocolateyZipPackage "$name" "$url32" C:\Eclipse "$url64" 
-
