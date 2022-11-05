45838230bd41df1b7861dc2b986f8519365a404a
tunisiano187 <fabian@bowlman.org>
2022-11-05 16:15:30 +0100

    Automate

--- automatic/gifsicle/tools/chocolateyinstall.ps1
+++ automatic/gifsicle/tools/chocolateyinstall.ps1
@@ -1,23 +1,27 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'gifsicle'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://eternallybored.org/misc/gifsicle/releases/gifsicle-1.89-win32.zip'
-$url64      = 'https://eternallybored.org/misc/gifsicle/releases/gifsicle-1.89-win64.zip'
+$packageName    = $env:ChocolateyPackageName
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32          = 'https://eternallybored.org/misc/gifsicle/releases/gifsicle-1.89-win32.zip'
+$checksum32     = ''
+$checksum32Type = ''
+$url64          = 'https://eternallybored.org/misc/gifsicle/releases/gifsicle-1.89-win64.zip'
+$checksum64     = ''
+$checksum64Type = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  Url           = $url
+  Url           = $url32
   Url64Bit      = $url64
 
-  softwareName  = 'gifsicle*'
+  softwareName  = "$($packageName)*"
 
-  Checksum      = 'D9B2B29F383B5235BE6C3518131FEBD55C1A4BB48CB39975CBAC13E073094938'
-  ChecksumType  = 'sha256'
-  Checksum64    = '8C1FE58644FDDC8FC1E3CCD97A048630F8E1E0DF196931825DFBE5C3F93882F5'
-  ChecksumType64= 'sha256'
+  Checksum      = $checksum32
+  ChecksumType  = $checksum32Type
+  Checksum64    = $checksum64
+  ChecksumType64= $checksum64Type
 
 }
 
