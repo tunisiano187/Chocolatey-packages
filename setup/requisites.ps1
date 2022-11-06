2b51695287e0f48b9a1d94b5b1737870c6c881d3
Tunisiano187 <fabian@bowlman.org>
2022-11-06 18:59:40 +0100

    optipng

--- setup/requisites.ps1
+++ setup/requisites.ps1
@@ -1,4 +1,5 @@
 choco upgrade -y chocolatey checksum keepass vt-cli
+choco remove -y optipng
 
 # http://techrena.net/disable-ie-set-up-first-run-welcome-screen/
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1 -PropertyType "DWord" -Force | Out-Null
