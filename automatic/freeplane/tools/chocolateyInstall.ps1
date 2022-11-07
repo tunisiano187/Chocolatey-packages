a869b91169b781394a1f468320322cb6c25cc69d
tunisiano187 <helpdesk.choc@gmail.com>
2022-11-07 04:17:44 +0000

    AU: 3 updated - ds4windows freeplane pestudio [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/a59ccc19871d70b217c18581fafc692eb2cb7968

--- automatic/freeplane/tools/chocolateyInstall.ps1
+++ automatic/freeplane/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
-$url = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-with-Java-1.10.3.exe/download'
+$url = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/Freeplane-Setup-with-Java-1.10.5.exe/download'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
-$checksum = 'fd630509ecd6b1bb5adf434b8ec316681b55d354bb5067aee18988fed6b512f6'
+$checksum = '56b4bc50cf85b82f0f4b1fd5302e08e28d5b167b73bf25c3eb8c3ba5c06fb271'
 $checksumType = 'sha256'
 
 Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -SilentArgs "$silentArgs" -Url "$url" -validExitCodes $validExitCodes -Checksum "$checksum" -ChecksumType "$checksumType"
--- automatic/pestudio/tools/chocolateyInstall.ps1
+++ automatic/pestudio/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
-$checksum       = '36f5c38d24bd9c53891c48f3732082252c3ca490fc92aa513dbe15f9eee5f0fe'
+$checksum       = '0b5605b47a75c10f0988928979e1b8730ef85e876c4f6276a09d04ab1a96833e'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
