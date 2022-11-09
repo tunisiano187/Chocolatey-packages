0e3200c74264eab0e2d2ad14c98d86989a665f27
tunisiano187 <fabian@bowlman.org>
2022-11-09 18:32:31 +0100

    64

--- automatic/datamash/update.ps1
+++ automatic/datamash/update.ps1
@@ -7,6 +7,10 @@ function global:au_SearchReplace {
         ".\tools\chocolateyinstall.ps1" = @{
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
         "$($Latest.PackageName).nuspec" = @{
 			"(\<copyright\>).*?(\</copyright\>)" 	= "`${1}© $((Get-Date).year) Assaf Gordon`$2"
@@ -35,4 +39,4 @@ function global:au_GetLatest {
     }
 }
 
-update
+update -NoCheckChocoVersion
