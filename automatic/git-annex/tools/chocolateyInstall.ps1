c5beb7dfe976b4dc17afb3e77c43a2466424e361
tunisiano187 <fabian@bowlman.org>
2022-11-13 14:02:38 +0100

    Git-annex (git-annex) #3212

--- automatic/git-annex/tools/chocolateyInstall.ps1
+++ automatic/git-annex/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$packageName = 'git-annex'
-$installerType = 'exe'
-$silentArgs = '/S /D=C:\Program Files\Git'
-$url = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe'
-$checksum = 'b9b479d4f137c79563ed27b61577c9a5794aa2fc83d9bf700a4d0f6816c98be2'
-$checksumType = 'sha256'
-$validExitCodes = @(0)
+$packageName        = $env:ChocolateyPackageName
+$installerType      = 'exe'
+$silentArgs         = '/S /D=C:\Program Files\Git'
+$url32              = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe'
+$checksum32         = 'b9b479d4f137c79563ed27b61577c9a5794aa2fc83d9bf700a4d0f6816c98be2'
+$checksumType32     = 'sha256'
+$validExitCodes     = @(0)
 
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $ahkExe = 'AutoHotKey'
@@ -14,7 +14,7 @@ Start-Process $ahkExe $ahkFile
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
-                          -Url "$url" `
+                          -Url "$url32" `
                           -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+                          -Checksum "$checksum32" `
+                          -ChecksumType "$checksumType32"
---
+++ automatic/git-annex/update.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$releases = 'https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe.info'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url32\s*=\s*)('.*')"      	= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$file = Join-Path $env:TEMP "git-annex.exe.info"
+	Invoke-WebRequest -Uri $releases -OutFile $file
+	$info = Get-Content $file
+	$url32 = $releases.Replace('.info','')
+	$version = ($info.split(" ") | Where-Object {$_ -match '"[0-9][0-9]'}).split('"') | Where-Object {$_ -match "\."}
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update
