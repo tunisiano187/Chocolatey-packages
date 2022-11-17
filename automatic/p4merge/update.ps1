3652444a271230ff3f6736e088843f9e24dcac7b
tunisiano187 <fabian@bowlman.org>
2022-11-17 19:05:54 +0100

    VT

--- automatic/p4merge/update.ps1
+++ automatic/p4merge/update.ps1
@@ -19,17 +19,8 @@ function global:au_SearchReplace {
 }
 
 
-function global:au_BeforeUpdate() {
-	Write-Output "Downloading $($Latest.Version) installer file"
-	Get-RemoteFiles -Purge -NoSuffix
-	Import-Module VirusTotalAnalyzer -NoClobber -Force
-	New-VirusScan -ApiKey $env:VT_APIKEY -Url $Latest.URL32
-	Start-Sleep -Seconds 60
-	$vt = (Get-VirusScan -ApiKey $env:VT_APIKEY -Url $Latest.URL32).data.attributes.reputation
-	if ( $vt -gt 5 ) {
-	  Write-Error "Ignoring $($Latest.PackageName) package due to virus total results - $vt positives"
-	  return 'ignore'
-	}
+function global:au_BeforeUpdate($Package) {
+	Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
