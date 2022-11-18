e7b8edb6042b210d9748ae6df9ea999194c5e462
tunisiano187 <helpdesk.choc@gmail.com>
2022-11-18 19:32:34 +0000

    AU: 2 updated - brave-nightly traffic-monitor [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/18f635260bb43af19a71f512066f0e485dde0491

--- automatic/traffic-monitor/tools/chocolateyinstall.ps1
+++ automatic/traffic-monitor/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 #$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
-$url32          = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.84/TrafficMonitor_V1.84_x86.zip'
-$checksum32     = '6B5D3091528E39F6F6F64F8725AB8BD4423EB9A6648706D4AD40BBFC7C0E3755'
+$url32          = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.84.1/TrafficMonitor_V1.84.1_x86.zip'
+$checksum32     = '47C95D4BB315FC89353EC6F84FFF31760C3C609282B076F9082E1AE37E86D9DC'
 $checksumType32 = 'sha256'
-$url64          = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.84/TrafficMonitor_V1.84_x64.zip'
-$checksum64     = '3B2DF6DB0540CAD6F9D5F813D693951AA538F5A6410469737EDB11BEC2A40928'
+$url64          = 'https://github.com/zhongyang219/TrafficMonitor/releases/download/V1.84.1/TrafficMonitor_V1.84.1_x64.zip'
+$checksum64     = '7217C298D56936C58E36F8B98FCEA50537AE935D57BC85C946C6385B6FFDD7CC'
 $checksumType64 = 'sha256'
 
 $shortcutsPath = [Environment]::GetFolderPath("Programs")
