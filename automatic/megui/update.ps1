68747e518d13f1f51022a902023a1c9bae10578f
tunisiano187 <fabian@bowlman.org>
2022-11-25 19:36:36 +0100

    url

--- automatic/megui/update.ps1
+++ automatic/megui/update.ps1
@@ -18,7 +18,9 @@ function global:au_AfterUpdate($Package) {
 
 function global:au_GetLatest {
 	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.zip'}).href | Select-Object -First 1
+	$ver = $url32.Split('-')[-2]
 	$version = "1.0.$($url32.Split('-')[-2])"
+	$url32 = "https://jztkft.dl.sourceforge.net/project/megui/megui-stable/$ver/MeGUI-$ver-32.zip"
 
 	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
