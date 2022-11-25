6c821c2cc34348d87f974db1ee5cf7465e4199ac
tunisiano187 <helpdesk.choc@gmail.com>
2022-11-25 18:08:41 +0000

    Package download megui

---
+++ automatic/megui/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$packageName = 'megui'
+$url = 'https://sourceforge.net/projects/megui/files/megui-stable/2896/MeGUI-2896-32.zip/download'
+$checksum = 'ff54154d9d7d9da354517f6ee89de051a07239cf1964935f3fea9e73590c85d2'
+$checksumType = 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = Join-Path $toolsDir "$($packageName).exe"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+
+# create empty sidecar so shimgen creates shim for GUI rather than console
+Set-Content -Path "$installFile.gui" `
+            -Value $null
