45d443c2b452e7fe79468bee929bed6e42c76ab1
Tunisiano187 <fabian@bowlman.org>
2022-12-03 10:22:58 +0100

    @packageargs

--- automatic/powderplayer/tools/chocolateyInstall.ps1
+++ automatic/powderplayer/tools/chocolateyInstall.ps1
@@ -1,15 +1,19 @@
-$packageName = 'powderplayer'
-$installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://github.com/jaruba/PowderPlayer/releases/download/v1.20/PowderPlayer_v1.20.exe'
-$checksum = '7f365e1455e0a25361495ec428431fbf80a12dca20110ed7263f0ea78d00efc1'
-$checksumType = 'sha256'
+$installerType  = 'exe'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url32          = 'https://github.com/jaruba/PowderPlayer/releases/download/v1.20/PowderPlayer_v1.20.exe'
+$checksum32     = '7f365e1455e0a25361495ec428431fbf80a12dca20110ed7263f0ea78d00efc1'
+$checksumType32 = 'sha256'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+$packageArgs = @{
+    packageName     = $env:ChocolateyPackageName
+    FileType        = $installerType
+    SilentArgs      = $silentArgs
+    
+    url             = $url32
+    checksum        = $checksum32
+    checksumType    = $checksumType32
+    ValidExitCodes  = $validExitCodes
+}
+
+Install-ChocolateyPackage @packageArgs
