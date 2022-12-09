1e5093ee355a2df32375f5afbe50473e2cca7fda
Tunisiano18 <tunisiano187@users.noreply.github.com>
2022-12-09 17:00:56 +0100

    Update update.ps1

--- automatic/freeplane/update.ps1
+++ automatic/freeplane/update.ps1
@@ -17,7 +17,7 @@ function global:au_AfterUpdate($Package) {
 }
 
 function global:au_GetLatest {
-	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '-setup-'} | Where-Object {$_ -match '.exe'} | Where-Object {$_ -match "https"}).href | Select-Object -First 1
+	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '-Setup-'} | Where-Object {$_ -match '.exe'} | Where-Object {$_ -match "https"}).href | Select-Object -First 1
 	#$version = $url32.split('-')[-1].split("x")[0].replace('.e','')
 	$version = Get-Version $url32
 
