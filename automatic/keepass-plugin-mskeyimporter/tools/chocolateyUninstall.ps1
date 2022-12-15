3850e4e7f5a69cc44dd2d2d42a598ee84d985420
tunisiano187 <helpdesk.choc@gmail.com>
2022-12-15 14:01:15 +0000

    Package download keepass-plugin-mskeyimporter

---
+++ automatic/keepass-plugin-mskeyimporter/tools/chocolateyUninstall.ps1
@@ -0,0 +1,62 @@
+# powershell v2 compatibility
+$psVer = $PSVersionTable.PSVersion.Major
+if ($psver -ge 3) {
+  function Get-ChildItemDir {Get-ChildItem -Directory $args}
+} else {
+  function Get-ChildItemDir {Get-ChildItem $args}
+}
+
+$packageName = 'keepass-plugin-mskeyimporter'
+$typName = 'MicrosoftKeyImporterPlugin.dll'
+$packageSearch = 'KeePass Password Safe'
+
+try {
+Write-Verbose "Checking KeePass is not running..."
+if (Get-Process -Name "KeePass" `
+                -ErrorAction SilentlyContinue) {
+  Write-Warning "$($packageSearch) is running. Please save any opened databases and close $($packageSearch) before attempting to uninstall KeePass plugins."
+  throw
+}
+
+Write-Verbose "Searching registry for installed KeePass..."
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                            -ErrorAction:SilentlyContinue `
+           | Where-Object {$_.DisplayName -like "$packageSearch*"} `
+           | ForEach-Object {$_.InstallLocation}
+$installPath = $regPath
+if (! $installPath) {
+  Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
+  $binRoot = Get-BinRoot
+  $portPath = Join-Path $binRoot "keepass"
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (! $installPath) {
+  Write-Verbose "Searching $env:Path for unregistered install..."
+  $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
+  if (! $installFullName) {
+    $installPath = [io.path]::GetDirectoryName($installFullName)
+  }
+}
+if (! $installPath) {
+  Write-Warning "$($packageSearch) not found."
+  throw
+}
+Write-Verbose "`t...found."
+
+Write-Verbose "Searching for plugin directory..."
+$pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
+if ($pluginPath.Count -eq 0) {
+  throw "Plugin directory not found."
+}
+Write-Verbose "`t...found."
+
+Write-Verbose "Removing plugin files..."
+$pluginTypFile = Join-Path $pluginPath $typName
+Remove-Item -Path $pluginTypFile `
+            -Force `
+            -ErrorAction Continue
+} catch {
+  throw $_.Exception
+}
