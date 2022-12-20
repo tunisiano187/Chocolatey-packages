34c2cae73f2eeb54635c008210e42fa9b14ad243
tunisiano187 <fabian@bowlman.org>
2022-12-20 14:14:23 +0100

    32

--- automatic/initool/tools/chocolateyInstall.ps1
+++ automatic/initool/tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
-$packageName = 'initool'
-$url = 'https://github.com/dbohdan/initool/releases/download/v0.9.0/initool-v0.9.0-win32.zip'
-$checksum = '16a9296d1e47d372676dc0d2cb0f2cc1df8c62c14ef8f9aafa3321e9f2f448f9'
-$checksumType = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$packageName    = 'initool'
+$url32          = 'https://github.com/dbohdan/initool/releases/download/v0.10.0/initool-v0.10.0-e5daa7c-win32.zip'
+$checksum32     = ''
+$checksumType32 = ''
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
