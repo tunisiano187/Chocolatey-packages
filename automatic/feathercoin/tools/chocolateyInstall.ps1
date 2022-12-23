e259005228da1d7e1bca0a44ce9bc6586ea1d8d7
tunisiano187 <fabian@bowlman.org>
2022-12-23 11:10:01 +0100

    feathercoin

--- automatic/feathercoin/tools/chocolateyInstall.ps1
+++ automatic/feathercoin/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-$SilentArgs         = '/S'
-$Url                = 'https://www.feathercoin.com/dl/feathercoin-0.8.7.1-setup.exe'
-$Checksum           = ''
-$Checksumtype       = ''
+﻿$SilentArgs         = '/S'
+$Url                = 'https://github.com/FeatherCoin/Feathercoin/releases/download/v0.19.1/feathercoin-0.19.1-win64-setup.exe'
+$Checksum           = '23b80ed7f17821b8a4340ddc8154e86774d5dfd77975305b4bcc91b01318969c'
+$Checksumtype       = 'sha256'
 $ValidExitCodes     = @(0)
 
 $packageArgs = @{
---
+++ automatic/feathercoin/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$fileType 		= 'exe'
+$silentArgs 	= '/S'
+$validExitCodes = @(0)
+$unPath 		= "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$unString 		= (Get-ItemProperty "$unPath\$packageName*" UninstallString).UninstallString
+
+$packageArgs	= @{
+	PackageName     = $env:ChocolateyPackageName
+	FileType		= $fileType
+	SilentArgs		= $silentArgs
+	File			= $unString
+	ValidExitCodes	= $validExitCodes
+}
+
+Uninstall-ChocolateyPackage @packageArgs
