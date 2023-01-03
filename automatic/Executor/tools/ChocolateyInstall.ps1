05e4bde47d3b833d4401f2616c28edc394db22ee
tunisiano187 <fabian@bowlman.org>
2023-01-03 20:05:35 +0100

    silent

--- automatic/Executor/tools/ChocolateyInstall.ps1
+++ automatic/Executor/tools/ChocolateyInstall.ps1
@@ -6,6 +6,6 @@ $packageArgs = @{
     Url             = 'http://www.1space.dk/executor/ExecutorSetup.exe'
     Checksum        = 'ae6fa48557c7aef38c648c0ee33785d5b0c9a557f0f6a23b8395091d7299ec2b'
     ChecksumType    = 'sha256'
-    SilentArgs      = '/SILENT /VERYSILENT /SUPRESSMSGBOXES /SP-'
+    SilentArgs      = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 Install-ChocolateyPackage @packageArgs
