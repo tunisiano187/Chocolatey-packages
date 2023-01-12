e22fdd432322b97e4072d995d38d9df35b6e7721
Tunisiano187 <fabian@bowlman.org>
2023-01-12 21:20:06 +0100

    choc

--- automatic/devbox-p4merge/update.ps1
+++ automatic/devbox-p4merge/update.ps1
@@ -12,14 +12,13 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	Write-Output 'Check Folder'
-	$version=$(choco search ^p4merge$ | Where-Object {$_ -notmatch 'found.'} | Where-Object {$_ -notmatch 'Did you know'} | Where-Object {$_ -notmatch 'Chocolatey v'} | Where-Object {$_ -notmatch "NuGet"} | Select-Object -First 1).split(' ')[1]
+	$choc=$(choco search p4merge | Where-Object {$_ -match "p4merge"})
+	$version = $choc.Split(" ")[1]
 
-	$license = "https://cdist2.perforce.com/perforce/r$($version.Substring(2))/doc/user/p4v_license.txt"
-	$url64 = "https://cdist2.perforce.com/perforce/r$($version.Substring(2))/bin.ntx64/p4vinst64.exe"
+	$url64 = "https://cdist2.perforce.com/perforce/r$($version)/bin.ntx64/p4vinst64.exe"
 
-	$Latest = @{ URL32 = $url64; Version = $version; License = $license }
+	$Latest = @{ URL32 = $url64; Version = $version }
 	return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
