d1ce33686f6aa126ea0510a3f9c4b995d33604ec
tunisiano187 <helpdesk.choc@gmail.com>
2023-01-25 19:10:02 +0000

    ps1 vars

--- automatic/par2cmdline/tools/chocolateyInstall.ps1
+++ automatic/par2cmdline/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$url            = 'https://github.com//Parchive/par2cmdline/releases/download/v0.6.13/par2cmdline-0.6.13-x86-win32.zip'
+$url            = 'https://github.com//Parchive/par2cmdline/releases/download/v0.6.13/par2cmdline-0.6.13-x86-win32.zip'
 $checksum       = '96a5d354e9eb0908a5c57b3dcfaae299'
 $checksumType   = 'md5'
 $url64          = 'https://github.com//Parchive/par2cmdline/releases/download/v0.6.13/par2cmdline-0.6.13-x86-win32.zip'
@@ -11,13 +11,13 @@ $packageArgs = @{
     fileType      = 'EXE'
     Url           = $url
     Url64bit      = $url64
-  
+
     UnzipLocation = $toolsDir
     checksum      = $Checksum
     checksumType  = $ChecksumType
     checksum64    = $Checksum64
     checksumType64= $ChecksumType64
-  
+
 }
-  
+
 Install-ChocolateyZipPackage @packageArgs
