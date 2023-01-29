5acd87885fbe29efbc01f470d740b0547c325563
tunisiano187 <helpdesk.choc@gmail.com>
2023-01-29 10:32:07 +0000

    AU: 1 updated - ddu [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/ec472caea5e3cc5bc83f6255861483e3b66abe50

--- automatic/ddu/tools/chocolateyInstall.ps1
+++ automatic/ddu/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $url            = 'https://www.wagnardsoft.com/DDU/download/DDU%20v18.0.6.0.exe'
-$checksum       = '390f263a4b65af6a81521e6d5998055e117571de96fe2c25b6ce6b9cab513827'
+$checksum       = '48ebdaf2e7a2ede56e91e0800f8a1128f0e8883e4d9feb3d46808d8218dfe8fc'
 $checksumType   = 'sha256'
-$referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?t=4316&sid=1b09cc3b6e31f7805cc6e0a83cab96a1'
+$referer        = 'https://www.wagnardsoft.com/forums/viewtopic.php?t=4316&sid=b1dd9bb9496c9ac1251cfff1dd1f99c0'
 
 Invoke-WebRequest -Uri $referer -OutFile "$env:TEMP/test.html" #  Required to be allowed just after
 
