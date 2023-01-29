ed2cf824a56fa94e5c6271ffc41294d8d91cc915
Tunisiano187 <fabian@bowlman.org>
2023-01-29 11:40:06 +0100

    Openstego.install

--- automatic/openstego.install/tools/chocolateyInstall.ps1
+++ automatic/openstego.install/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
-$packageName = 'openstego.install'
-$installerType = 'exe'
-$silentArgs = '/S'
-$url = 'http://sourceforge.net/projects/openstego/files/openstego/openstego-0.6.1/Setup-OpenStego-0.6.1.exe/download'
-$checksum = 'c343f8dadb6ca89e42a0d43bca36161d95f952a4'
-$checksumType = 'sha1'
-$validExitCodes = @(0)
+$url32          = 'http://sourceforge.net/projects/openstego/files/openstego/openstego-0.6.1/Setup-OpenStego-0.6.1.exe/download'
+$checksum32     = 'c343f8dadb6ca89e42a0d43bca36161d95f952a4'
+$checksumType32 = 'sha1'
 
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+$packageArgs = @{
+    packageName            = $env:ChocolateyPackageName
+    fileType               = 'EXE'
+    url                    = $url32
+    checksum               = $checksum32
+    checksumType           = $checksumType32
+    silentArgs             = '/S'
+    validExitCodes         = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
