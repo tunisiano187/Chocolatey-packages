dcdf9c386ab592dcbac58c0c79630c5df9d49628
tunisiano187 <helpdesk.choc@gmail.com>
2023-02-12 16:54:25 +0000

    AU: 2 updated - nano nano-win [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/47f9d221561a8a2591c9302dee05a6baa74a4dd3

--- automatic/nano-win/tools/chocolateyInstall.ps1
+++ automatic/nano-win/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://files.lhmouse.com/nano-win/nano-win_10155_v7.1-12-g14f292a7c.7z'
-$checksum       = 'dce659505c33f6def27048d44d5998dcbe70ae4361c55fe32398af33bc83682e'
+$url            = 'https://files.lhmouse.com/nano-win/nano-win_10169_v7.2-14-gb7943a37a.7z'
+$checksum       = '752f5bdc1e1f2a419bcb6660f4122f6f35f9c7937d59fb6d336bb4c25942ced8'
 $checksumtype   = 'sha256'
 
 $packageArgs = @{
