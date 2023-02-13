edf157e875148b638441b2de52fa834d542f5198
tunisiano187 <helpdesk.choc@gmail.com>
2023-02-13 04:18:40 +0000

    AU: 1 updated - leanify [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/37645670e47225dea85b42a8b4f3abc489bb9a1e

--- automatic/leanify/tools/chocolateyInstall.ps1
+++ automatic/leanify/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$url            = 'https://github.com/JayXon/Leanify/releases/download/v0.4.2/Leanify_win32.7z'
-$checksum       = '234f3db3c9ccf87a85da787557ceda61'
-$checksumType   = 'md5'
-$url64          = 'https://github.com/JayXon/Leanify/releases/download/v0.4.2/Leanify_win64.7z'
-$checksum64     = '5273e8cf586f2e7f3a2669821741e1c4'
-$checksumType64 = 'md5'
+﻿$url            = 'https://github.com/JayXon/Leanify/releases/download/v0.4.3/Leanify_win32.7z'
+$checksum       = 'cf6d3af3c123ae1e3e7385d025e2b4d90e79a7bdac6cdea95876b2e476d313f7'
+$checksumType   = 'sha256'
+$url64          = 'https://github.com/JayXon/Leanify/releases/download/v0.4.3/Leanify_win64.7z'
+$checksum64     = 'e6d0552e5d3fceb7d603cc2b2fae65c7e4f5cfb9915882842363699f3245d744'
+$checksumType64 = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
