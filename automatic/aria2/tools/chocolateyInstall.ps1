14e064f7de0212d851e1e130b42853fb90ec92e2
Tunisiano187 <fabian@bowlman.org>
2023-02-13 20:58:28 +0100

    aria2c splatting

--- automatic/aria2/tools/chocolateyInstall.ps1
+++ automatic/aria2/tools/chocolateyInstall.ps1
@@ -1,5 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName    = $env:ChocolateyPackageName
 $url            = 'https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-32bit-build1.zip'
 $checksum       = 'c35a1bfa06726914006888929c6ce147e50b1d9fca8d1dc89bcd2c926f8c16de'
 $checksumType   = 'sha256'
@@ -8,11 +7,15 @@ $checksum64     = 'c82df5415125b438d72443923fea7f5f9fda1f326d0dbfac6aab16d58dbb7
 $checksumType64 = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Url64 "$url64" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
-                             -Checksum64 "$checksum64" `
-                             -ChecksumType64 "$checksumType64"
+$packageArgs = @{
+    packageName     = $env:ChocolateyPackageName
+    UnzipLocation   = $toolsDir
+    Url             = $url
+    checksum        = $checksum
+    checksumType    = $checksumType
+    Url64           = $url64
+    checksum64      = $checksum64
+    checksumType64  = $checksumType64
+}
+
+Install-ChocolateyZipPackage @packageArgs
