f3dd8c8bf68d2933fe6dc2cc1686f82b9c45022f
Tunisiano187 <fabian@bowlman.org>
2023-02-14 21:04:18 +0100

    (wfilecheck) update requested #3289

--- automatic/wfilecheck/tools/chocolateyInstall.ps1
+++ automatic/wfilecheck/tools/chocolateyInstall.ps1
@@ -1,22 +1,28 @@
-﻿$packageName = 'wfilecheck'
-$urlArray = @('https://sourceforge.net/projects/whirlpoolfilech/files/0.4/WFileCheck32.exe', 'https://sourceforge.net/projects/whirlpoolfilech/files/0.4/WFileCheck64.exe')
-$url = $urlArray[0]
-$checksum = '6beca15e394d9c2070614145b97a972f9ec0c66d286a62e3120c5772fcd3628e'
-$checksumType = 'sha256'
-$url64 = $urlArray[1]
-$checksum64 = '934087fafb8de7776cc79da395dc4329680fbbb033efea3e415c28bfe90dcdec'
+﻿$url            = ''
+$checksum       = '6beca15e394d9c2070614145b97a972f9ec0c66d286a62e3120c5772fcd3628e'
+$checksumType   = 'sha256'
+$url64          = ''
+$checksum64     = '934087fafb8de7776cc79da395dc4329680fbbb033efea3e415c28bfe90dcdec'
 $checksumType64 = 'sha256'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installFile = Join-Path $toolsDir "$($packageName).exe"
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile    = Join-Path $toolsDir "$($packageName).exe"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  FileFullPath    = $installFile
+
+  url             = $url
+  checksum        = $checksum
+  checksumType    = $checksumType
+
+  url64bit        = $url64
+  checksum64      = $Checksum64
+  checksumType64  = $ChecksumType64
+}
+
 try {
-  Get-ChocolateyWebFile -PackageName "$packageName" `
-                        -FileFullPath "$installFile" `
-                        -Url "$url" `
-                        -Url64bit "$url64" `
-                        -Checksum "$checksum" `
-                        -ChecksumType "$checksumType" `
-                        -Checksum64 "$checksum64" `
-                        -ChecksumType64 "$checksumType64"
+  Get-ChocolateyWebFile @packageArgs
+  
   Set-Content -Path ("$installFile.gui") `
               -Value $null
 } catch {
---
+++ automatic/wfilecheck/update.ps1
@@ -0,0 +1,33 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://sourceforge.net/projects/whirlpoolfilech/'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      			= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 			= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 		= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      		= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType64)'"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$urls = (Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.href -match '.exe$'}
+	$version = $urls[0].href.split('/') | Where-Object {$_ -match "[0]."}
+	$url32 = "$(($urls | Where-Object {$_.href -match "32"}).href)/download"
+	$url64 = "$(($urls | Where-Object {$_.href -match "64"}).href)/download"
+
+	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	return $Latest
+}
+
+update
