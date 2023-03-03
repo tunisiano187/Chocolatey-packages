98e4c21a173c570009b901d3598bcb1f3cdb37e5
tunisiano187 <helpdesk.choc@gmail.com>
2023-03-03 04:18:22 +0000

    AU: 1 updated - pestudio [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/bd94f9c5cc0b9f5e67b858604d5d68f26321627a

--- automatic/pestudio/tools/chocolateyInstall.ps1
+++ automatic/pestudio/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
-$checksum       = 'e1ca8e305c080ef6880173bd7f17f7e4773c420f8545db9a72f6958f1aeddc0a'
+$checksum       = '93c9ccb715de39370070656d3b1959f01dd4dc16acbb0ad9a6a3cff32c1bcb00'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
