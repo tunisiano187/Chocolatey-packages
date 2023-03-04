45dee07416cdf1fb053b0443f4da52e7b7e5f449
tunisiano187 <helpdesk.choc@gmail.com>
2023-03-04 04:19:16 +0000

    AU: 2 updated - electron litecoin [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/2b7cdd026b085b9d741a41abbf760f524c424220

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v23.1.1/electron-v23.1.1-win32-ia32.zip'
-$checksum = '2dd921d340e8fa3b2d494c05988f8db657212d8f08cf7e71124a8ffbec4f3e6e'
+$url = 'https://github.com/electron/electron/releases/download/v23.1.2/electron-v23.1.2-win32-ia32.zip'
+$checksum = '9ec63511754780b9fff80be27e01980b43a08bda7721abbdbcf2a96a945afb57'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v23.1.1/electron-v23.1.1-win32-x64.zip'
-$checksum64 = '18fce453ad305440a70bba5adf4cc608675fbbaab1f1180f83aac2f249591b04'
+$url64 = 'https://github.com/electron/electron/releases/download/v23.1.2/electron-v23.1.2-win32-x64.zip'
+$checksum64 = 'fc5bab259eabb3548e02ffc64d5d6228f31fb628a2a422a8dcb4ded19e6bd30e'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
--- automatic/litecoin/tools/chocolateyInstall.ps1
+++ automatic/litecoin/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '/S'
-$url            = 'https://download.litecoin.org/litecoin-0.21.2.1/win/litecoin-0.21.2.1-win64-setup.exe'
-$checksum       = 'd4462b58811bc35c3160c204cc85e94ebe27e57b816ed5bd607f7e22cf3d4131'
+$url            = 'https://download.litecoin.org/litecoin-0.21.2.2/win/litecoin-0.21.2.2-win64-setup.exe'
+$checksum       = 'fe5b2a5f747972940bf4d6eb489c11f3c739bb443168c28b277c0130cc737d35'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
 
