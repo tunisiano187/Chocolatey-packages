660178b699238907a10421742a7e420075c73b58
Tunisiano187 <fabian@bowlman.org>
2023-03-11 22:15:59 +0100

    deepl

--- automatic/deepl/update.ps1
+++ automatic/deepl/update.ps1
@@ -3,6 +3,16 @@ import-module au
 
 $release = 'https://appdownload.deepl.com/windows/0install/DeepLSetup.exe'
 
+function Get-Version($name) {
+	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+	while($version_file.count -eq 0)
+	{
+		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
+		Start-Sleep -Seconds 1
+	}
+	return $version_file
+}
+
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
@@ -20,7 +30,10 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
 	$File = Join-Path $env:TEMP "DeepLSetup.exe"
 	Invoke-WebRequest -Uri $release -OutFile $File
-	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+	#$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+
+	. $File /s
+	$version = Get-Version('deepl')
 
 	$Latest = @{ URL32 = $release; Version = $version }
 	return $Latest
