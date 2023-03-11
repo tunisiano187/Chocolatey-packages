2a2433d15a58901d088b6128931fad8f29142858
tunisiano187 <helpdesk.choc@gmail.com>
2023-03-11 04:18:58 +0000

    AU: 1 updated - voicemod [skip ci] https://gist.github.com/7032dea203873812b435f152ee3794bf/0beced14c26ca27e9369c687ffae6d234e380577

--- automatic/voicemod/tools/chocolateyinstall.ps1
+++ automatic/voicemod/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url          = 'https://www.voicemod.net/b2c/preview/VoicemodSetup_2.39.2.0.exe'
-$checksum     = 'e439e53ba92612b1b0d39bc3d73946524c86cedeb6746c770ee6abf3c077b057'
+$url          = 'https://www.voicemod.net/b2c/v2/VoicemodSetup_2.40.0.0.exe'
+$checksum     = '42fe62cf1f6a623c9525d0e4ba704d591c1764f20582a313c509f56a82b189d3'
 $checksumType = 'sha256'
 
 $packageArgs = @{
