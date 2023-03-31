2acabbc455002dd8ec686f2962428019654edf89
tunisiano187 <helpdesk.choc@gmail.com>
2023-03-31 19:07:07 +0000

    ps1 vars

--- automatic/tigervnc/update.ps1
+++ automatic/tigervnc/update.ps1
@@ -44,7 +44,7 @@ function global:au_GetLatest {
 	$version = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_.href -match "[0-9].[0-9]"} | Where-Object {$_.href -notmatch 'css'}).href[0].split('/')[-2]
 	$url32 = "https://sourceforge.net/projects/tigervnc/files/stable/$version/tigervnc-$version.exe/download"
 	$url64 = "https://sourceforge.net/projects/tigervnc/files/stable/$version/tigervnc64-$version.exe/download"
-	
+
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version; Options = $options }
 	return $Latest
 }
