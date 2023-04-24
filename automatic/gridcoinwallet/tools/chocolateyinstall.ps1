f90c72bdd93e18b2be5a064c8985ed7aab5147f2
tunisiano187 <helpdesk.choc@gmail.com>
2023-04-24 06:20:07 +0000

    AU: 1 updated - gridcoinwallet [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/7b62e7a5786d823b19642e67175c3e5d740fa58f

--- automatic/gridcoinwallet/tools/chocolateyinstall.ps1
+++ automatic/gridcoinwallet/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url32		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.4.4.0/gridcoin-5.4.4-win32-setup.exe'
-$checksum32	    = '147e2fd40f041724d63368507cbd588fd02e1bd6547b311c68a4e69d876345dd'
+$url32		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.4.5.0/gridcoin-5.4.5-win32-setup.exe'
+$checksum32	    = '105c51a4eac6df7c7e218b6f77e276762e5504af8bfa695e30d4339c9d5d1709'
 $checksumType32 = 'sha256'
-$url64		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.4.4.0/gridcoin-5.4.4-win64-setup.exe'
-$checksum64     = 'd71351c63bf2b7ee291303f034554fdfd7c0523ec414c27f23739338e347f9ad'
+$url64		      = 'https://github.com/gridcoin-community/Gridcoin-Research/releases/download/5.4.5.0/gridcoin-5.4.5-win64-setup.exe'
+$checksum64     = 'b86bef6dde60cc9e39c6648cbe897c4bcf99064acf3e57979cbd1e591d169910'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
