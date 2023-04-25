4d42adc88b5337a5f8013bc0ac52b6705c7aca9a
tunisiano187 <helpdesk.choc@gmail.com>
2023-04-25 06:18:59 +0000

    AU: 1 updated - pawns [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/f65e370e85c6cac31de34e402064096fc7fab66e

--- automatic/pawns/tools/chocolateyinstall.ps1
+++ automatic/pawns/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url64          = 'https://cdn.pawns.app/download/app/latest/windows/Pawns%20Setup.exe'
-$checksum64     = '4527721be9479d699d243d90bee3a738e957120045417d9422ead17dc7eb735e'
+$checksum64     = '05ac2968613a9796acea6a0dde6138ef33d7c00b033e6a584e674716f13d352a'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
