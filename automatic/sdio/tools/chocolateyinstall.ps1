0a4eb3be45b32213b4281c65c01837bb8ac7bd99
Fabian Clerbois <tunisiano187@gmail.com>
2023-05-22 19:53:40 +0200

    don't remove old ones

--- automatic/sdio/tools/chocolateyinstall.ps1
+++ automatic/sdio/tools/chocolateyinstall.ps1
@@ -7,6 +7,7 @@ if(([int]$os[0] -le [int]6) -and ([int]$os[1] -le [int]3)) {
     exit 0;
 }
 
+
 $SpecificFolder = 'SDIO_1.12.12.753'
 
 $packageArgs = @{
@@ -19,24 +20,4 @@ $packageArgs = @{
     checksumType    = 'sha256'
 }
 
-Remove-Item "$ToolsDir\SDIO_*" -Force -Recurse # Cleanup old versions
 Install-ChocolateyZipPackage @packageArgs
-
-if (!(Test-Path -path "$ENV:ALLUSERSPROFILE\SDIO")) {
-    New-Item "$ENV:ALLUSERSPROFILE\SDIO" -ItemType Directory
-}
-
-$shortcutName    = 'Snappy Driver Installer Origin'
-$fileName32      = 'SDIO_R753.exe'
-$fileName64      = 'SDIO_x64_R753.exe'
-$fullVersion     = '1.12.12.753'
-$FileFullpath32  = Join-Path $ToolsDir\SDIO_$fullVersion $fileName32
-$FileFullpath64  = Join-Path $ToolsDir\SDIO_$fullVersion $fileName64
-
-if (Get-OSArchitectureWidth -eq 64) {
-    Install-ChocolateyShortcut -targetPath $FileFullpath64 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk"
-    Install-ChocolateyShortcut -targetPath $FileFullpath64 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk"
-} else {
-    Install-ChocolateyShortcut -targetPath $FileFullpath32 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk"
-    Install-ChocolateyShortcut -targetPath $FileFullpath32 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk"
-}
