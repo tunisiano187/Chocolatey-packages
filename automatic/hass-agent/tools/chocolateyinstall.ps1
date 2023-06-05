234e2bd9898318580cd62461265d7840b48dc090
Fabian Clerbois <tunisiano187@gmail.com>
2023-06-05 16:54:51 +0200

    checksum

--- automatic/hass-agent/tools/chocolateyinstall.ps1
+++ automatic/hass-agent/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
 
   url             = 'http://sdupdt.itsupport247.net/tpupdate/Bonjour64_3.1.0.1.msi'
   checksum        = '46e31e284da64d6c2d366352b8a8abcf7db28d3e2a870d8fcf15c4a6fe0a6dd1'
-  checksumType64  = 'sha256'
+  checksumType    = 'sha256'
 
   silentArgs	    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
