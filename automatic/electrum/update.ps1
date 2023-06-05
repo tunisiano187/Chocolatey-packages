be6264d343094ba4cf4b0ff7a06ac18f08770d1f
Fabian Clerbois <tunisiano187@gmail.com>
2023-06-05 17:39:15 +0200

    update when .install passed

--- automatic/electrum/update.ps1
+++ automatic/electrum/update.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = 'https://download.electrum.org/?C=M;O=D'
-
 function global:au_SearchReplace {
 	@{
 		"$($Latest.PackageName).nuspec" = @{
@@ -13,7 +11,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 	Write-Verbose 'Check Version'
-	$version = $((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_.href -match '/$'} | Select-Object -First 1).href.replace('/','')
+	$version = $(choco search electrum.install | Where-Object {$_ -match 'electrum'}).split(' ')[1]
 	Write-Verbose "Version : $version"
 	$url32 = "https://download.electrum.org/$($version)/electrum-$($version)-setup.exe"
 
