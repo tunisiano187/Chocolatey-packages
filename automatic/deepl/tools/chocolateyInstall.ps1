be8f4dff42558efd48580b3e0f8b31684001252e
tunisiano187 <helpdesk.choc@gmail.com>
2023-06-14 16:24:39 +0000

    AU: 2 updated - deepl ultracopier [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/1087b548148faefb70493f09de3cf92a8aa8323a

--- automatic/deepl/tools/chocolateyInstall.ps1
+++ automatic/deepl/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $url            = 'https://appdownload.deepl.com/windows/0install/DeepLSetup.exe'
-$checksum       = 'ccdde4fda138a52c9e1e3d9c230acbe413e4dc121bdbbd1dd43b1eb36b966219'
+$checksum       = '0eec1294807d2cf7acea57a0d5f95e66b1dc47e6c86877cc45a14a5cff005e9f'
 $checksumtype   = 'sha256'
 
 $packageArgs = @{
