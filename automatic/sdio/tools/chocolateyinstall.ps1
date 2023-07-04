391f2359c99d963ed9347225d123b46bb1769c6a
Tunisiano18 <tunisiano187@users.noreply.github.com>
2023-07-04 14:55:42 +0200

    Update chocolateyinstall.ps1

--- automatic/sdio/tools/chocolateyinstall.ps1
+++ automatic/sdio/tools/chocolateyinstall.ps1
@@ -1,5 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.12.12.753.zip'
+$checksum = 'c24c4d805947b473c5f9abf3fa3b2168b1aaf8b282d612d004fd60774da49193'
+$checksumtype = 'sha256'
 
 $os=(Get-WMIObject win32_operatingsystem).Version.split('.')
 if(([int]$os[0] -le [int]6) -and ([int]$os[1] -le [int]3)) {
@@ -15,9 +18,9 @@ $packageArgs = @{
     unzipLocation   = $toolsDir
     fileType        = 'ZIP'
     SpecificFolder  = $SpecificFolder
-    url             = 'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.12.12.753.zip'
-    checksum        = 'c24c4d805947b473c5f9abf3fa3b2168b1aaf8b282d612d004fd60774da49193'
-    checksumType    = 'sha256'
+    url             = $url
+    checksum        = $checksum
+    checksumType    = $checksumtype
 }
 
 Install-ChocolateyZipPackage @packageArgs
