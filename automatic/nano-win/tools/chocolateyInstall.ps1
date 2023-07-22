deb6cf45e2fb392069d921ec7fe2201795974dbd
tunisiano187 <helpdesk.choc@gmail.com>
2023-07-22 17:33:49 +0000

    AU: 1 updated - nano-win [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/ea3bad7384dae5f813440deb09dda353aa348953

--- automatic/nano-win/tools/chocolateyInstall.ps1
+++ automatic/nano-win/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://files.lhmouse.com/nano-win/nano-win_10180_v7.2-25-g44c0d924b.7z'
-$checksum       = '4012ed5fd32d3784c7e13d0b48d7a0991dc974e2b21b68a303124060fb1a90d1'
+$url            = 'https://github.com/mysteriumnetwork/mysterium-vpn-desktop/releases/download/10.14.4/MysteriumVPN-Setup-10.14.4.exe'
+$checksum       = 'd97c0ba16c5371fa9c1844bccb0cf2ec59f34f9d9d3667525bbfcdbdb4044b12'
 $checksumtype   = 'sha256'
 
 $packageArgs = @{
