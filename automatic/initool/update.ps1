d1792d24e54941e3f76554de0372ac0529345320
Tunisiano187 <fabian@bowlman.org>
2023-08-10 20:04:43 +0200

    change requested by the author

--- automatic/initool/update.ps1
+++ automatic/initool/update.ps1
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
 		}
 		"$($Latest.PackageName).nuspec" = @{
 			"(\<releaseNotes\>).*?(\</releaseNotes\>)"		= "`${1}$($Latest.ReleaseNotes)`$2"
-			"(\<copyright\>).*?(\</copyright\>)" 	= "`${1}© $((Get-Date).year) Danyil Bohdan`$2"
+			"(\<copyright\>).*?(\</copyright\>)" 	= "`${1} © $((Get-Date).year) D. Bohdan`$2"
 		}
 	}
 }
