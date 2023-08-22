f557ff3a8467140f13a32639b90736a0766bca4e
Fabian Clerbois <tunisiano187@gmail.com>
2023-08-22 10:24:21 +0200

    Update

---
+++ automatic/jitsi-meet-electron/README.md
@@ -0,0 +1,20 @@
+# <img src="https://raw.githubusercontent.com/jitsi/jitsi-meet-electron/master/resources/icons/icon_48x48.png" width="48" height="48"/> [Jitsi Meet Electron](https://github.com/jitsi/jitsi-meet-electron)
+
+## Features
+
+- Works with any Jitsi Meet deployment
+- Local settings
+- Builtin auto-updates
+- Remote control
+- Always-On-Top window
+
+## Package information
+
+This will not use the installer, which installs jitsi-meet only for the current used.
+Instead Chocolatey's means of installing a zip-package are used.
+Thus the package will be installed for all users and requires administrative rights to update.
+
+This is an automatic package that will update itself as soon as a new version is released.
+It is therefore suggested to use `choco-upgrade-all-at-startup` or `choco-upgrade-all-at`, to make sure you always have the latest version.
+
+![screenshot](https://raw.githubusercontent.com/jitsi/jitsi-meet-electron/master/screenshot.png)
---
+++ automatic/jitsi-meet-electron/update.ps1
@@ -0,0 +1,90 @@
+﻿import-module au
+
+$domain   = 'https://github.com'
+$ghproj = 'jitsi/jitsi-meet-electron'
+$releases = "$domain/$ghproj/releases/latest"
+
+$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
+$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    }
+    ".\jitsi-meet-electron.nuspec" = @{
+      "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
+    }
+    ".\tools\VERIFICATION.txt" = @{
+      "(?i)(URL:).*"             = "`${1} $($Latest.URL32)"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
+}
+
+
+function global:au_BeforeUpdate {
+  set-alias 7z $Env:chocolateyInstall\tools\7z.exe
+
+  $toolsp="$PSScriptRoot\tools"
+  $downtemp="$PSScriptRoot\temp"
+  Remove-Item "$downtemp" -Recurse -Force -ea ignore
+  mkdir "$downtemp"
+  $downf = "$downtemp\jitsi.exe"
+  #$ProgressPreference = 'SilentlyContinue'
+  Invoke-WebRequest $Latest.URL32 -OutFile  $downf
+  7z x $downf "-o$downtemp"
+
+  Remove-Item "$toolsp\*.7z"
+  Move-Item "$downtemp\`$PLUGINSDIR\*.7z" $toolsp
+  Remove-Item "$downtemp" -Recurse -Force -ea ignore
+
+  #Based on Get-RemoteFiles
+
+  $f32="app-32.7z"
+  $f64="app-64.7z"
+  $Algorithm = 'sha256'
+
+  $global:Latest.Checksum32 = Get-FileHash "$toolsp\$f32" -Algorithm $Algorithm | ForEach-Object Hash
+  $global:Latest.ChecksumType32 = $Algorithm
+  $global:Latest.FileName32 = $f32
+
+  $global:Latest.Checksum64 = Get-FileHash "$toolsp\$f64" -Algorithm $Algorithm | ForEach-Object Hash
+  $global:Latest.ChecksumType64 = $Algorithm
+  $global:Latest.FileName64 = $f32
+
+  Invoke-WebRequest https://raw.githubusercontent.com/jitsi/jitsi-meet-electron/master/LICENSE -OutFile "$toolsp\LICENSE"
+  7z e "$toolsp\$f32" "-o$toolsp" "LICENSE*"
+
+}
+
+
+function global:au_GetLatest {
+  $tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
+  $url = $tags.assets.browser_download_url | Where-Object {$_ -match ".exe$"}
+  $version = $tags.tag_name.Replace('v','')
+  if($tags.prerelease -match "true") {
+      $date = $tags.published_at.ToString("yyyyMMdd")
+      $version = "$version-pre$($date)"
+  }
+  
+  $releaseNotesUrl = $tags.html_url
+
+  @{
+    URL32 = $url
+    Version = $version
+    ReleaseNotes = $releaseNotesUrl
+  }
+}
+
+try {
+  update -ChecksumFor none
+} catch {
+  $ignore = 'Unable to connect to the remote server'
+  if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
