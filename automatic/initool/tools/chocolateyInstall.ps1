109e685802cb3140b746e07c4d2f6db9961b0f1a
tunisiano187 <helpdesk.choc@gmail.com>
2023-10-14 06:16:24 +0000

    AU: 1 updated - initool [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/324bef3d577475c878b65faeeb31a1cb933f3899

--- automatic/initool/tools/chocolateyInstall.ps1
+++ automatic/initool/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName    = 'initool'
-$url32          = 'https://github.com/dbohdan/initool/releases/download/v0.13.0/initool-v0.13.0-d75e3e3-win32.zip'
-$checksum32     = '185a21002551ff37fe77a9d23467dabef48aa41cbc5d49975efda1cd37ea2978'
+$url32          = 'https://github.com/dbohdan/initool/releases/download/v0.14.0/initool-v0.14.0-ac4b61e-win32.zip'
+$checksum32     = 'd9a0d691ae34d186745d311dce87b726c1414117f530339453a901c3d74295c0'
 $checksumType32 = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
