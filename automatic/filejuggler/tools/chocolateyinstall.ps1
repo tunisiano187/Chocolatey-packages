7ef6641a0def2b550bda47664b0eeffd3296e5b4
tunisiano187 <helpdesk.choc@gmail.com>
2023-10-24 06:17:48 +0000

    AU: 1 updated - filejuggler [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/af2971cf1d74cefff449f69bd97a0cc150494400

--- automatic/filejuggler/tools/chocolateyinstall.ps1
+++ automatic/filejuggler/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $packageName    = $env:ChocolateyPackageName
 $installerType  = 'EXE'
 $silentArgs     = '/qb'
 $url            = 'https://www.filejuggler.com/download/filejuggler.exe'
-$checksum       = 'f44fff362397a39faffbec40aee782b5e9305cad1d1aaaf55cd4615ecdf46a17'
+$checksum       = 'c91c87dc2c044fb9da0315806b3a599ed164b04f2d8f1f9e5aede7a4a2b8ee10'
 $checksumtype	= 'sha256'
 
 $scriptPath     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
