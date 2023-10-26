8f475d0b468033dd07327317b6d2144b24954d14
tunisiano187 <helpdesk.choc@gmail.com>
2023-10-26 06:18:16 +0000

    AU: 1 updated - fing [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/033687e448d2a8229b1a7eeb63f67a79a77c1e6f

--- automatic/fing/tools/chocolateyinstall.ps1
+++ automatic/fing/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
 $silentArgs = '/S'
 $url32 = 'https://get.fing.com/fing-desktop-releases/win/Fing.exe'
-$checksum32 = 'a8b3bcb4e1098c9cca1c64a84c709aee1e375df32f157c3d401b2a812e299e18'
+$checksum32 = 'b39bff36533b882086f61f1cf0f4bb28e43c090b19e4f55fa588013c998c2a3c'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
