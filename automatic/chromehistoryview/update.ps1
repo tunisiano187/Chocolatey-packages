93d26f0e01c88519f50e949c3725dcfb688596af
Tunisiano187 <fabian@bowlman.org>
2023-11-26 19:07:35 +0100

    checksum

--- automatic/chromehistoryview/update.ps1
+++ automatic/chromehistoryview/update.ps1
@@ -11,6 +11,7 @@ function global:au_SearchReplace {
 		'tools/chocolateyInstall.ps1' = @{
 			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 		}
 	}
 }
