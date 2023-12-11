248301f48abbf484972c4b1a6dd72940789628c2
Tunisiano187 <fabian@bowlman.org>
2023-12-11 21:30:54 +0100

    link

--- automatic/bitcoin.portable/update.ps1
+++ automatic/bitcoin.portable/update.ps1
@@ -30,11 +30,11 @@ function global:au_GetLatest {
     $folder = "https://bitcoincore.org/bin/$(((Invoke-WebRequest -Uri 'https://bitcoincore.org/bin/' -UseBasicParsing).Links | Where-Object {$_.href -match $version}| Select-Object -Last 1).href)"
 
 	Write-Verbose 'Get files'
-	$file = $(((Invoke-WebRequest -Uri $folder -UseBasicParsing).Links | Where-Object {$_.href -match ".zip"}).href)
+	$file = $(((Invoke-WebRequest -Uri $folder -UseBasicParsing).Links | Where-Object {$_.href -match ".zip"} | Where-Object {$_.href -match "win64"}).href)
     $url64 = "$folder$file"
 	#$url64 = "https://github.com/$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match ".zip"} | Select-Object -First 1).href)"
 
     return @{ URL64 = $url64; Version = $version }
 }
 
-update -ChecksumFor 64 -NoCheckChocoVersion
+update -ChecksumFor 64
