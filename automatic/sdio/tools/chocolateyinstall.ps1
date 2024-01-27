fc9c686398b92aee3973296993397d405e7a2219
Tunisiano187 <fabian@bowlman.org>
2024-01-27 20:41:44 +0100

    not working

--- automatic/sdio/tools/chocolateyinstall.ps1
+++ automatic/sdio/tools/chocolateyinstall.ps1
@@ -16,23 +16,3 @@ $packageArgs = @{
 
 Remove-Item "$ToolsDir\SDIO_*" -Force -Recurse # Cleanup old versions
 Install-ChocolateyZipPackage @packageArgs
-
-if (!(Test-Path -path "$ENV:ALLUSERSPROFILE\SDIO")) {
-    New-Item "$ENV:ALLUSERSPROFILE\SDIO" -ItemType Directory
-}
-
-$shortcutName    = 'Snappy Driver Installer Origin'
-$baseVersion     = '707'
-$fileName32      = 'SDIO_R707.exe'
-$fileName64      = 'SDIO_x64_R707.exe'
-$fullVersion     = '1.5.8.707'
-$FileFullpath32  = Join-Path $ToolsDir\SDIO_$fullVersion $fileName32
-$FileFullpath64  = Join-Path $ToolsDir\SDIO_$fullVersion $fileName64
-
-if (Get-OSArchitectureWidth -eq 64) {
-    Install-ChocolateyShortcut -targetPath $FileFullpath64 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk"
-    Install-ChocolateyShortcut -targetPath $FileFullpath64 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk"
-} else {
-    Install-ChocolateyShortcut -targetPath $FileFullpath32 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk"
-    Install-ChocolateyShortcut -targetPath $FileFullpath32 -WorkingDirectory "%ALLUSERSPROFILE%\SDIO" -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk"
-}
