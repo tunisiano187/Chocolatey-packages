06f79d9ef5909f984380d122f61ac298e26e38f9
tunisiano187 <helpdesk.choc@gmail.com>
2024-02-07 06:08:35 +0000

    Package download logitech-options

---
+++ automatic/logitech-options/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
+$checksum = '8A8882C14F4ABBEF7756A2E58A69E26AF7D2191A26195C51F4325029552C96AE'
+
+Confirm-Win10
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = $url
+    checkum        = $checksum
+    checksumType   = 'sha256'
+    fileType       = 'EXE'
+    silentArgs     = '/quiet /update:false'
+    validExitCodes = @(0)   
+}
+
+Install-ChocolateyPackage @packageArgs
