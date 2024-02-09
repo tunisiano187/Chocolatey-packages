cedf913e000c9c2c2dd58f822b6618a8870a7473
Tunisiano187 <fabian@bowlman.org>
2024-02-09 20:17:11 +0100

    first version

--- automatic/logitech-options/tools/chocolateyinstall.ps1
+++ automatic/logitech-options/tools/chocolateyinstall.ps1
@@ -1,7 +1,8 @@
 $ErrorActionPreference = 'Stop'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
-$checksum = '8A8882C14F4ABBEF7756A2E58A69E26AF7D2191A26195C51F4325029552C96AE'
+#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download01.logi.com/web/ftp/pub/techsupport/options/options_installer.exe'
+$checksum       = '8A8882C14F4ABBEF7756A2E58A69E26AF7D2191A26195C51F4325029552C96AE'
+$checksumType   = 'sha256'
 
 Confirm-Win10
 
@@ -9,7 +10,7 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = $url
     checkum        = $checksum
-    checksumType   = 'sha256'
+    checksumType   = $checksumType
     fileType       = 'EXE'
     silentArgs     = '/quiet /update:false'
     validExitCodes = @(0)
---
+++ automatic/logitech-options/update.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.exe'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$url32 = $releases
+	$File = "$($env:TEMP)/logioptionsplus.exe"
+	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32
