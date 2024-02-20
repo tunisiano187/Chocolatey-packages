79b02b456219e7aa3caca2bd11dc2b69a0f3d890
tunisiano187 <helpdesk.choc@gmail.com>
2024-02-20 06:19:20 +0000

    AU: 2 updated - electron unison [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/7dc5be3f45292724c4dd4968f272bda43c30e7f1

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v28.2.3/electron-v28.2.3-win32-ia32.zip'
-$checksum = 'fa9c98b9ead4eeb73d751e253d181484abf8d875cf4dfae9af8b3313b7c8454c'
+$url = 'https://github.com/electron/electron/releases/download/v29.0.0/electron-v29.0.0-win32-ia32.zip'
+$checksum = '6352881301983524721c92875d3f8ac36d291f9fc92e29ec546bedc38ccdf7aa'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v28.2.3/electron-v28.2.3-win32-x64.zip'
-$checksum64 = '037ecd7935b3a0c6ade0976427cd109da3a66c01d13bc4ab08c810fb876c9974'
+$url64 = 'https://github.com/electron/electron/releases/download/v29.0.0/electron-v29.0.0-win32-x64.zip'
+$checksum64 = '2639165790888edb61ef42de397766aaacc4e455feb8827fe0da8e66b360c2b2'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
--- automatic/unison/tools/chocolateyInstall.ps1
+++ automatic/unison/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/bcpierce00/unison/releases/download/v2.53.3/unison-2.53.3-windows-x86_64.zip'
-$checksum       = '5e8607adb6402fb57b10d5cf2d2cc1a717473698023d3edeb5060ea42d79713c'
+$url            = 'https://github.com/bcpierce00/unison/releases/download/v2.53.4/unison-2.53.4-windows-x86_64.zip'
+$checksum       = '1276b54b870089e87b977931b66866f2a442631bc121d3504a38cc1c614fc4cd'
 $checksumType   = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
