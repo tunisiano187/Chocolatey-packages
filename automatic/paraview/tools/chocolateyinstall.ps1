2a62b34102fae5b31ec5ea390be252faf0b38750
tunisiano187 <helpdesk.choc@gmail.com>
2024-02-24 16:50:13 +0000

    ps1 vars

--- automatic/paraview/tools/chocolateyinstall.ps1
+++ automatic/paraview/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.6&type=binary&os=Windows&downloadFile=ParaView-5.6.1-Windows-msvc2015-64bit.exe' # download url, HTTPS preferred
 $checksum       = ''
 $checksumType   = 'sha256'
@@ -7,10 +7,10 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url           = $url
-  softwareName  = 'paraview*' 
+  softwareName  = 'paraview*'
   checksum      = $checksum
   checksumType  = $checksumType
-  
+
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
 }
