e4056149655b0b3ba0704980d2a400b3509962a2
tunisiano187 <helpdesk.choc@gmail.com>
2024-02-24 13:31:59 +0000

    Package download paraview

---
+++ automatic/paraview/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.6&type=binary&os=Windows&downloadFile=ParaView-5.6.1-Windows-msvc2015-64bit.exe' # download url, HTTPS preferred
+
+$packageArgs = @{
+  packageName   = 'paraview'
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'paraview*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '7E755DC7E6D3F0A4DC130BEC8D2C512E2A5A9457E3A02AFFD934CE56B650C9C0'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+}
+
+Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
