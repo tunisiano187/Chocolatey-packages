45938d88158c0a18a378038948c823ee8e4f6854
tunisiano187 <helpdesk.choc@gmail.com>
2024-02-24 18:40:37 +0000

    AU: 1 updated - deepl [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/67c076adbbfbabfd91d21b609a0f0e49f86a3531

--- automatic/deepl/tools/chocolateyInstall.ps1
+++ automatic/deepl/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $url            = 'https://appdownload.deepl.com/windows/0install/DeepLSetup.exe'
-$checksum       = 'cac980c207b8653c98a3fb671b2bf1ff59e49e8b36bb74a757935129a6f870c8'
+$checksum       = '0037c9895723fb712b57b144cbb429f319ab5a3c1e4c44a3ffefa351486bcdaf'
 $checksumtype   = 'sha256'
 
 $packageArgs = @{
