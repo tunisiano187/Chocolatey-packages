d9a689823099d9a5221c41de6a433e8ded54187a
tunisiano187 <helpdesk.choc@gmail.com>
2024-02-25 14:26:01 +0000

    AU: 1 updated - paraview [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/e37917adbed7c0e987908de130a8951111c928b6

--- automatic/paraview/tools/chocolateyinstall.ps1
+++ automatic/paraview/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.6&type=binary&os=Windows&downloadFile=ParaView-5.6.1-Windows-msvc2015-64bit.exe' # download url, HTTPS preferred
-$checksum       = ''
+﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.12/&type=binary&os=Windows&downloadFile=ParaView-5.12.0-RC3-Windows-Python3.10-msvc2017-AMD64.msi' # download url, HTTPS preferred
+$checksum       = 'f20c996478662d95f9ddcf81960073f7e9da3a04689922b832c0ade2af656925'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
