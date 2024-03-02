cf0b2c1a14c6fa58fe3bacfa81d2aa89b4d55286
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-02 11:41:59 +0000

    Package download newfiletime

---
+++ automatic/newfiletime/tools/ChocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'newfiletime'
+$Url32 = 'https://www.softwareok.com/Download/NewFileTime.zip'
+$Checksum32 = '708e3b67769296e77ae6ca03bbceda86d76d57f1a6e80c08bd8a7aa021e4a89f'
+$ChecksumType32 = 'sha256'
+$Url64 = 'https://www.softwareok.com/Download/NewFileTime_x64.zip'
+$Checksum64 = 'c7cb619e82b087a168697793001a9ef73e13515f8edc7748d1cd046174bfc0b4'
+$ChecksumType64 = 'sha256'
+$UrlExtra = 'https://www.softwareok.com/Download/NewFileTime_Unicode.zip'
+$ChecksumExtra = '7ba7839c5ace0f8c542d11e2a2e698d482f213222b6ef2ea15871f3336ca898e'
+$ChecksumTypeExtra = 'sha256'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    Url32          = $Url32
+    Checksum32     = $Checksum32
+    ChecksumType32 = $ChecksumType32
+    Url64          = $Url64
+    Checksum64     = $Checksum64
+    ChecksumType64 = $ChecksumType64
+    UnzipLocation  = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    Url            = $UrlExtra
+    Checksum       = $ChecksumExtra
+    ChecksumType   = $ChecksumTypeExtra
+    UnzipLocation  = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
