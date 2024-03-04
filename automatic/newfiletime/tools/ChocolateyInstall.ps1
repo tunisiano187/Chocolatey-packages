73bdd0b1d8636b7766b18f2ea98094c0f18e2f37
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-04 17:55:03 +0000

    AU: 1 updated - newfiletime [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/9effc41a64d25ce1841d6342b4d1fae270ad537d

--- automatic/newfiletime/tools/ChocolateyInstall.ps1
+++ automatic/newfiletime/tools/ChocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $PackageName = 'newfiletime'
 $Url32 = 'https://www.softwareok.com/Download/NewFileTime.zip'
-$Checksum32 = '@{Algorithm=SHA256; Hash=DC90D88E4189D073B8CB05E76D899F11D41F636E1137821E333DCAC392D0B835; Path=C:\Users\appveyor\AppData\Local\Temp\1\chocolatey\NewFileTime\NewFileTime.zip}'
+$Checksum32 = 'DC90D88E4189D073B8CB05E76D899F11D41F636E1137821E333DCAC392D0B835'
 $ChecksumType32 = 'sha256'
 $Url64 = 'https://www.softwareok.com/Download/NewFileTime_x64.zip'
-$Checksum64 = '@{Algorithm=SHA256; Hash=1518DD3C7D39BDD75E551C373484966F520C20D8F5BEFD5469F070AF9EEBFB83; Path=C:\Users\appveyor\AppData\Local\Temp\1\chocolatey\NewFileTime\NewFileTime.zip}'
+$Checksum64 = '1518DD3C7D39BDD75E551C373484966F520C20D8F5BEFD5469F070AF9EEBFB83'
 $ChecksumType64 = 'sha256'
 $UrlExtra = 'https://www.softwareok.com/Download/NewFileTime_Unicode.zip'
-$ChecksumExtra = '@{Algorithm=SHA256; Hash=0E7F0F66CA43A7A83478468AAA4FF17206C15820B4234AA5AE68FE6A4AA8DFD2; Path=C:\Users\appveyor\AppData\Local\Temp\1\chocolatey\NewFileTime\NewFileTime.zip}'
+$ChecksumExtra = '0E7F0F66CA43A7A83478468AAA4FF17206C15820B4234AA5AE68FE6A4AA8DFD2'
 $ChecksumTypeExtra = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
