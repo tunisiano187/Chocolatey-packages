6ec182b881498d58d3fae503c2d1d3607f838f90
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-04 18:22:24 +0000

    Package download quaternion

---
+++ automatic/quaternion/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = "quaternion"
+  file           = "$toolsDir\quaternion_windows.zip"
+  file64         = "$toolsDir\quaternion_windows.zip"
+  destination    = "$toolsDir"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item $toolsDir\*.zip -ea 0
