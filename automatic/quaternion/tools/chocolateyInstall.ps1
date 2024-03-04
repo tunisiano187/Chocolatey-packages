e4f44fd9073df0cf45b9ec6bb65dcd9bf6442644
Fabian Clerbois <tunisiano187@gmail.com>
2024-03-04 21:03:03 +0100

    test

--- automatic/quaternion/tools/chocolateyInstall.ps1
+++ automatic/quaternion/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file = "$toolsDir\quaternion_windows.zip"
 
 $packageArgs = @{
   packageName    = "quaternion"
   file           = "$toolsDir\quaternion_windows.zip"
-  file64         = "$toolsDir\quaternion_windows.zip"
   destination    = "$toolsDir"
 }
 
-Get-ChocolateyUnzip @packageArgs
+Expand-Archive -Path $file -DestinationPath $toolsDir
 
-Remove-Item $toolsDir\*.zip -ea 0
+#Remove-Item $toolsDir\*.zip -ea 0
--- automatic/quaternion/update.ps1
+++ automatic/quaternion/update.ps1
@@ -9,7 +9,7 @@ function global:au_SearchReplace {
 	@{
 		"legal\VERIFICATION.txt"      = @{
 			"(?i)(32-bit:).*"        				= "`${1} $($Latest.URL32)"
-			"(?i)(checksum32 =).*" 				= "`${1} $($Latest.Checksum32)"
+			"(?i)(checksum32 :).*" 				= "`${1} $($Latest.Checksum32)"
 		}
 	}
 }
@@ -22,14 +22,15 @@ function global:au_GetLatest {
 	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
 	$url32 = $tags.assets.browser_download_url | Where-Object {$_ -match ".zip$"}
 	Invoke-WebRequest -Uri $url32 -OutFile ".\tools\quaternion_windows.zip"
+	$checksum32 = (Get-FileHash -Path ".\tools\quaternion_windows.zip" -Algorithm "sha256").Hash
 	$version = $tags.tag_name.Replace('v','')
 	if($tags.prerelease -match "true") {
 		$date = $tags.published_at.ToString("yyyyMMdd")
 		$version = "$version-pre$($date)"
 	}
-	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/quotient-im/Quaternion/master/LICENSE.spdx" -OutFile "$(Get-Location)\legal\license.txt"
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/quotient-im/Quaternion/dev/COPYING" -OutFile "$(Get-Location)\legal\license.txt"
 
-	$Latest = @{ URL32 = $url32; Version = $version }
+	$Latest = @{ URL32 = $url32; Checksum32 = $checksum32; Version = $version }
 	return $Latest
 }
 
