203a0875d1d3dd3b019188d3aad2784aaf5e3345
Tunisiano18 <fabian@bowlman.org>
2024-03-07 12:47:00 +0100

    solve #3348

--- automatic/monosnap/tools/chocolateyinstall.ps1
+++ automatic/monosnap/tools/chocolateyinstall.ps1
@@ -1,14 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$url          = ''
+$checksum     = ''
+$checksumType = ''
+
 $packageArgs = @{
   packageName    = 'monosnap'
-  fileType       = 'msi'
-  url            = 'https://static.monosnap.com/windows/Monosnap.Web.Installer.msi'
-  silentArgs     = '/quiet'
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'monosnap*'
-  checksum       = 'ECEAC3760067D7FDDD3672894297803A4623208456F48C389C5F1FD4F32314B6'
-  checksumType   = 'sha256'
+  checksum       = $checksum
+  checksumType   = $checksumType
   toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
 Install-ChocolateyPackage @packageArgs
