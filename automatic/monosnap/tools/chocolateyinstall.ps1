2a8434d33ce20267cd33a54ecd538a94dd3063a8
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-07 11:01:50 +0000

    Package download monosnap

---
+++ automatic/monosnap/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'monosnap'
+  fileType       = 'msi'
+  url            = 'https://static.monosnap.com/windows/Monosnap.Web.Installer.msi'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+  softwareName   = 'monosnap*'
+  checksum       = 'ECEAC3760067D7FDDD3672894297803A4623208456F48C389C5F1FD4F32314B6'
+  checksumType   = 'sha256'
+  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
+Install-ChocolateyPackage @packageArgs
