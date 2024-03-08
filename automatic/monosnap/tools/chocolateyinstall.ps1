52c7660d463da35b5989cb7a483cee7245fb2192
Tunisiano18 <fabian@bowlman.org>
2024-03-08 16:53:23 +0100

    stop-process

--- automatic/monosnap/tools/chocolateyinstall.ps1
+++ automatic/monosnap/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
-
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $url          = 'https://static.monosnap.com/windows/MonosnapSetup.exe'
 $checksum     = '2331279833e97ac40ad10a14cd2ee73da2ad2e106566b25c26b8a168a4d0f77e'
 $checksumType = 'sha256'
@@ -13,6 +13,9 @@ $packageArgs = @{
   softwareName   = 'monosnap*'
   checksum       = $checksum
   checksumType   = $checksumType
-  toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  toolsDir       = $toolsDir
 }
+
+Start-Process "$toolsDir\stop-process.ps1"
+
 Install-ChocolateyPackage @packageArgs
---
+++ automatic/monosnap/tools/stop-process.ps1
@@ -0,0 +1,2 @@
+﻿Start-Sleep -Seconds 60
+Stop-Process -Name "Monosnap"
