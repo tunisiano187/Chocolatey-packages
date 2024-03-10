e3ba5d638efe47e3ddc9e0fa974da3c559913481
Tunisiano18 <fabian@bowlman.org>
2024-03-10 13:21:26 +0100

    mediathekview

---
+++ automatic/mediathekview/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$desktopLink = "$([System.Environment]::GetFolderPath('DesktopDirectory'))\$env:ChocolateyPackageName.lnk"
+
+if (Test-Path $desktopLink) {
+  Remove-Item -force -ea 0 $desktopLink
+}
+
+Write-Warning "If MediaThekView was installed to a custom directory, this needs to be cleaned up manually..."
---
+++ automatic/mediathekview/update.ps1
@@ -0,0 +1,57 @@
+﻿Import-Module AU
+import-module "$PSScriptRoot/../../scripts/au_extensions.psm1"
+
+$releases = 'https://download.mediathekview.de/stabil/'
+
+function global:au_BeforeUpdate($Package) {
+  $licenseFile = "$PSScriptRoot\legal\LICENSE.txt"
+  if (Test-Path $licenseFile) { rm -Force $licenseFile }
+
+  iwr -UseBasicParsing -Uri $($Package.nuspecXml.package.metadata.licenseUrl -replace 'blob', 'raw') -OutFile $licenseFile
+  if (!(Get-ValidOpenSourceLicense -path "$licenseFile")) {
+    throw "Unknown license download. Please verify it still contains distribution rights."
+  }
+
+  Get-RemoteFiles -Purge -NoSuffix
+}
+function global:au_AfterUpdate($Package) {
+  Update-Changelog -useIssueTitle
+  Invoke-VirusTotalScan $Package
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = 'MediathekView\-[\d\.]+.*\.zip$'
+  $url32 = $download_page.Links | ? href -match $re | select -last 1 -expand href | % {
+    if (!$_.StartsWith("http")) {
+      [uri]::new($releases, [string]$_)
+    } else {
+      $_
+    }
+  }
+
+  $verRe = 'View-|-win(?:32)?\.zip'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+  @{
+    URL32       = [uri]$url32
+    Version     = [version]$version32
+    PackageName = 'MediathekView'
+  }
+}
+
+update -ChecksumFor none -NoCheckUrl
