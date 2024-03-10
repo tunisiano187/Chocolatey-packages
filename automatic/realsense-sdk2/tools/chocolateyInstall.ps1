0eb18e882b7ce66b7fa5027a9e2757ecdc72af0a
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-10 15:32:14 +0000

    Package download realsense-sdk2

---
+++ automatic/realsense-sdk2/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
+ 
+$pp = Get-PackageParameters
+
+$packageArgs = @{
+    PackageName  = 'realsense.sdk'
+    FileType     = 'exe'
+    SoftwareName = 'Intel® RealSense™ SDK 2.0'
+    url          = 'https://github.com/IntelRealSense/librealsense/releases/download/v2.29.0/Intel.RealSense.SDK.exe'
+    checksum     = '47c94297f98b800e010e7f85701f0fd54b46a451796324d21fbf17bc6c78fd73'
+    checksumType = "sha256"
+    silentArgs   = "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/NOCANCEL", "/SP-", 
+                   "/LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).Install.log`"", 
+                   (Get-InstallComponents $pp), (Get-InstallOptions $pp)
+    validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+$installDir = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
