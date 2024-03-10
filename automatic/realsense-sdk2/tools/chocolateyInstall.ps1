51fef95c24700c5c012cdf8dd8425c96ea37114b
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-10 17:08:41 +0000

    AU: 1 updated - realsense-sdk2 [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/2e8106c69c8b0e82e50ddc08fbb6451e144e4940

--- automatic/realsense-sdk2/tools/chocolateyInstall.ps1
+++ automatic/realsense-sdk2/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
@@ -6,9 +6,9 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $pp = Get-PackageParameters
 
 $packageName    = $env:ChocolateyPackageName
-$url            = ''
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://github.com/IntelRealSense/librealsense/releases/download/v2.54.2/Intel.RealSense.SDK-WIN10-2.54.2.5684.exe'
+$checksum       = 'b6bded035e2aef5263aa983faefbdd4c350e84f238e6bbbaaab5ac4de58bf0b7'
+$checksumType   = 'sha256'
 
 $packageArgs = @{
     PackageName  = $packageName
