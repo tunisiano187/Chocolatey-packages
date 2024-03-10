1e35e9dee075f828784aad6b10ca0ebe75d1455a
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-10 16:56:00 +0000

    Package download todobackup

---
+++ automatic/todobackup/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://down.easeus.com/product/tb_free?ref=/backup-software/tb-free.html'
+
+$packageArgs = @{
+  packageName   = 'todobackup'
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  softwareName  = 'todobackup'
+  checksum      = 'C892D1FC4A91135D220851BE47D472B964C09F46336F73FFF2A30A1B5D8D36DC'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs 
