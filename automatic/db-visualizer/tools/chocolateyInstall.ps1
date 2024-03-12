04337e84c6a5d938950068559ddd6f01a0a21de2
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-12 06:20:22 +0000

    AU: 1 updated - db-visualizer [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/77ff4bdfe633cbc36390462dc6ad4518e745d21a

--- automatic/db-visualizer/tools/chocolateyInstall.ps1
+++ automatic/db-visualizer/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64            = 'https://www.dbvis.com/product_download/dbvis-24.1.2/media/dbvis_windows-x64_24_1_2.exe'
-$checksum64       = '75321afa06ffb9da55f4548b28bde87e2b97f1c4055a12a5d5f5951f5f222f8a'
+$url64            = 'https://www.dbvis.com/product_download/dbvis-24.1.3/media/dbvis_windows-x64_24_1_3.exe'
+$checksum64       = '96f3b2b7d7cd7402d32307fde8448b1a687f49ae3606d833e6e2dad81f3c6a2a'
 $checksumType64   = 'sha256'
 
 $packageArgs = @{
