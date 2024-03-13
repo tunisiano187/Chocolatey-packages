6920ef6914ea4d7b764f1294af28a4b2c4e8ccab
Tunisiano18 <fabian@bowlman.org>
2024-03-13 20:03:40 +0100

    tsedat

---
+++ automatic/tsedat/update.ps1
@@ -0,0 +1,30 @@
+Import-Module au
+
+$releases = 'https://sequencepublishing.com/cgi-bin/download.cgi?thesage'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*[$]url(32)?\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*[$]checksum(32)?\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*[$]checksumType\s*=\s*)('.*')"  = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_AfterUpdate($Package) {
+  Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+  $url32 = Get-RedirectedUrl $releases
+
+  $realnum = $url32 -split 'TheSage_Setup_|.exe' | Select-Object -Last 1 -Skip 1
+
+  $version = $realnum -replace ('-', '.');
+
+  $Latest = @{ URL32 = $url32; Version = $version }
+  return $Latest
+}
+
+update -ChecksumFor 32
