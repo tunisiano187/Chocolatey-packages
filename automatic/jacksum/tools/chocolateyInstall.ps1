1971a3526eaf6ba432024c91c5b9201189e97c06
Tunisiano18 <fabian@bowlman.org>
2024-03-13 18:12:49 +0100

    quote

--- automatic/jacksum/tools/chocolateyInstall.ps1
+++ automatic/jacksum/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = $env:ChocolateyPackageName
 $url = 'https://github.com/jonelo/jacksum-fbi-windows/releases/download/v2.6.0/jacksum-3.7.0-hashgarten-0.15.0-windows-explorer-integration-2.6.0.zip'
-$toolsDir = '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $checksum = '36b95119c3c87fb1ede8d4a664f3ca46739192662a96a4de07e39a1b24bff7d5'
 $checksumType = 'sha256'
 
