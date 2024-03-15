6887cf231731e9ddf695f878d0c9cf2bfe4904ef
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-15 07:32:24 +0000

    AU: 1 updated - faceit [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/c426e869a0a1a471dc9f686a5ed5215f3a2c6f25

--- automatic/faceit/tools/chocolateyinstall.ps1
+++ automatic/faceit/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url            = 'https://faceit-client.faceit-cdn.net/release/FACEIT-setup-latest.exe'
-$checksum       = '6ad1ac1ad5eabbefbf5a4c956592ccb317ba11d7c7a9cf40530539a2c5491452'
+$checksum       = '330225dec34192c76ce23d67f8c6b00da7a389787f1ae75a06f5567a5760e8ea'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
