92901a5ca267ba9e9bcd60b34bdce5a0e916f32c
Tunisiano18 <fabian@bowlman.org>
2024-03-20 20:28:53 +0100

    no release update

--- automatic/dogecoin/update.ps1
+++ automatic/dogecoin/update.ps1
@@ -1,10 +1,6 @@
 $ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = "https://github.com/dogecoin/dogecoin/releases/latest"
-$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
-$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
-
 function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
@@ -16,7 +12,6 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $choc=$(choco search dogecoin.install | Where-Object {$_ -match "dogecoin.install"})
 	$version = $choc.Split(" ")[1]
-    Update-Metadata -key "releaseNotes" -value $tags.html_url
 
     return @{
         Version = $version
