ebc3c7bad5856286f354f10d1b21c107ca868c03
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-21 19:20:33 +0000

    AU: 1 updated - bonjour [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/b2806771bb8965b88efcb0b00f475d302b2d44f6

--- automatic/bonjour/tools/chocolateyinstall.ps1
+++ automatic/bonjour/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
-$checksum = '0b3d970ecdcc431465f623a16f7b4fdf4e7baee335a2dc1e36c0602f97d81be0'
-$checksumType = 'sha256'
+$checksum = 'cd6891afada9f940bc551a215a6c40f51a93b140026375bd30b1ee686dc73bd2'
+$checksumType = 'SHA256'
 
-$checksum64 = '0b3d970ecdcc431465f623a16f7b4fdf4e7baee335a2dc1e36c0602f97d81be0'
-$checksumType64 = 'sha256'
+$checksum64 = '541c30b2d72705afe75649f97e3daf677b8576e6e73d6f78f7265a0ded61011f'
+$checksumType64 = 'SHA256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
