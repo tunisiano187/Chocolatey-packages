81cab08ec5c680c1248f1a71564e3efd713abca7
Tunisiano18 <fabian@bowlman.org>
2024-03-21 18:17:37 +0100

    test

--- automatic/bonjour/tools/chocolateyinstall.ps1
+++ automatic/bonjour/tools/chocolateyinstall.ps1
@@ -1,16 +1,22 @@
 $ErrorActionPreference = 'Stop';
 
+$checksum = '0b3d970ecdcc431465f623a16f7b4fdf4e7baee335a2dc1e36c0602f97d81be0'
+$checksumType = 'sha256'
+
+$checksum64 = '0b3d970ecdcc431465f623a16f7b4fdf4e7baee335a2dc1e36c0602f97d81be0'
+$checksumType64 = 'sha256'
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType		= "msi"
 
   url           = 'http://sdupdt.itsupport247.net/tpupdate/Bonjour_3.1.0.1.msi'
-  checksum      = '7d7b2986d4fd35cc98af3607b1a5e2e004d6ab1116a4a9db37a66c17b63b6397'
-  checksumType  = 'sha256'
+  checksum      = $checksum
+  checksumType  = $checksumType
 
   url64bit      = 'http://sdupdt.itsupport247.net/tpupdate/Bonjour64_3.1.0.1.msi'
-  checksum64    = '46e31e284da64d6c2d366352b8a8abcf7db28d3e2a870d8fcf15c4a6fe0a6dd1'
-  checksumType64= 'sha256'
+  checksum64    = $checksum64
+  checksumType64= $checksumType64
 
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
