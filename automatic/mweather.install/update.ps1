ab25b8d12c23bfec80f3366070b34d0106154709
Tunisiano18 <fabian@bowlman.org>
2024-03-22 19:52:01 +0100

    mweather

---
+++ automatic/mweather.install/update.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$release = 'https://www.nirsoft.net/utils/mweather_setup.exe'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$File = Join-Path $env:TEMP "mweather_setup.zip"
+	Invoke-WebRequest -Uri 'https://www.nirsoft.net/utils/mweather.zip' -OutFile $File
+	Expand-Archive -Path $File -DestinationPath "$env:TEMP\MWeather"
+	$inf = Get-Content -Path "$env:TEMP\MWeather\mweather.txt"
+	$version=($inf | Where-Object {$_ -match 'MetarWeather v'}).split('v') | Select-Object -Last 1
+
+	$Latest = @{ URL32 = $release; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32 -NoCheckChocoVersion
