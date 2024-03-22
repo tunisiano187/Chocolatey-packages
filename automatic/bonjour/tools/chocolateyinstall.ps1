a786f7e37b3838c0cb643cd9472d178629635d59
Tunisiano18 <fabian@bowlman.org>
2024-03-22 19:34:57 +0100

    url update

--- automatic/bonjour/tools/chocolateyinstall.ps1
+++ automatic/bonjour/tools/chocolateyinstall.ps1
@@ -1,20 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$checksum = 'cd6891afada9f940bc551a215a6c40f51a93b140026375bd30b1ee686dc73bd2'
-$checksumType = 'SHA256'
+$url              = ''
+$checksum         = 'cd6891afada9f940bc551a215a6c40f51a93b140026375bd30b1ee686dc73bd2'
+$checksumType     = 'SHA256'
 
-$checksum64 = '541c30b2d72705afe75649f97e3daf677b8576e6e73d6f78f7265a0ded61011f'
-$checksumType64 = 'SHA256'
+$url64            = ''
+$checksum64       = '541c30b2d72705afe75649f97e3daf677b8576e6e73d6f78f7265a0ded61011f'
+$checksumType64   = 'SHA256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType		= "msi"
 
-  url           = 'http://sdupdt.itsupport247.net/tpupdate/Bonjour_3.1.0.1.msi'
+  url           = $url
   checksum      = $checksum
   checksumType  = $checksumType
 
-  url64bit      = 'http://sdupdt.itsupport247.net/tpupdate/Bonjour64_3.1.0.1.msi'
+  url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= $checksumType64
 
--- automatic/bonjour/update.ps1
+++ automatic/bonjour/update.ps1
@@ -6,8 +6,10 @@ $releases = 'https://www.apple.com/itunes/download/win'
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
 			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
 			"(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
 			"(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 		}
