20a81df66c4e885b490ec71916e6a8e0c19cdc68
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-23 15:58:53 +0000

    Package download xmeters

---
+++ automatic/xmeters/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://entropy6.com/xmeters/downloads/XMetersSetup.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'xmeters'
+  checksum 		= '7E88AC356DBCF8B33A17D52AB0178EA516471E7BC4D7D1C9814104FD74D584B3'
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes = @(0, 1116, 1641, 3010)
+  
+}
+
+Install-ChocolateyPackage @packageArgs
