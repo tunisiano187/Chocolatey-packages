aa8fcef136c98421c2c3cef613895720688c88b7
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-24 09:27:25 +0000

    Package download netbeans

---
+++ automatic/netbeans/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageName	= 'netbeans'
+$fileType		= 'exe'
+$filePath		= Get-Item $toolsDir\*.exe
+$softwareName	= 'NetBeans*'
+$silentArgs		= '--silent'
+
+$packageArgs = @{
+	PackageName    	= $packageName
+	FileType       	= $fileType
+	File           	= $filePath
+	SoftwareName	= $softwareName
+	SilentArgs		= $silentArgs
+	ValidExitCodes	= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
---
+++ automatic/netbeans/tools/chocolateyUninstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Uninstall glassfish server
+$regeditItem 		= 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\nbi-glassfish*'
+$UninstallProperty 	= Get-ItemProperty -Path $regeditItem -Name 'UninstallString'
+
+$packageName  	= 'glassfish'
+$fileType     	= 'EXE'
+$silentArgs   	= '--silent'
+$uninstallPath	= $UninstallProperty.UninstallString
+
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs -File $uninstallPath -ValidExitCodes @(0)
+
+# Uninstall Netbeans
+$regeditItem 		= 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\nbi-nb-base*'
+$UninstallProperty	= Get-ItemProperty -Path $regeditItem -Name 'UninstallString'
+
+$packageName  	= 'netbeans'
+$fileType     	= 'EXE'
+$silentArgs   	= '--silent'
+$uninstallPath	= $UninstallProperty.UninstallString
+
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $fileType -SilentArgs $silentArgs -File $uninstallPath -ValidExitCodes @(0)
