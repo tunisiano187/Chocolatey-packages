9fce75a127c80077bee1d479c29a537e6569e292
Fabian Clerbois <tunisiano187@gmail.com>
2024-03-26 15:14:03 +0100

    mweather

--- automatic/mweather.portable/update.ps1
+++ automatic/mweather.portable/update.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 import-module au
 
-$release = 'https://www.nirsoft.net/utils/mweather_setup.exe'
+$release = 'https://www.nirsoft.net/utils/mweather.zip'
 
 function global:au_SearchReplace {
 	@{
@@ -18,13 +18,13 @@ function global:au_AfterUpdate($Package) {
 }
 
 function global:au_GetLatest {
-	$File = Join-Path $env:TEMP "mweather_setup.zip"
+	$File = Join-Path $env:TEMP "mweather.zip"
 	if(Test-Path -Path $File) {
 		Remove-Item -Path $File
 	}
 	Invoke-WebRequest -Uri 'https://www.nirsoft.net/utils/mweather.zip' -OutFile $File
-	Expand-Archive -Path $File -DestinationPath "$env:TEMP\MWeather_setup" -Force
-	$inf = Get-Content -Path "$env:TEMP\MWeather_setup\mweather.txt"
+	Expand-Archive -Path $File -DestinationPath "$env:TEMP\MWeather" -Force
+	$inf = Get-Content -Path "$env:TEMP\MWeather\mweather.txt"
 	$version=($inf | Where-Object {$_ -match 'MetarWeather v'}).split('v') | Select-Object -Last 1
 
 	$Latest = @{ URL32 = $release; Version = $version }
