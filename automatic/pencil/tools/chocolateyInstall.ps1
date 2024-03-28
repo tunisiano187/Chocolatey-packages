de03c11f7d07d865deaf8970a91ad39362b1cc01
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-28 17:34:56 +0000

    Package download pencil

---
+++ automatic/pencil/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$version = "3.1.1"
+
+$packageArgs = @{
+  packageName    = $Env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = "$toolsPath\Pencil-$version.ga.exe"
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Pencil*'
+}
+Install-ChocolateyInstallPackage @packageArgs
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" "" }}
