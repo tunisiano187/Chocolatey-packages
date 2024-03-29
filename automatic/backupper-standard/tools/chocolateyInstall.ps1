5a4fec54c86d370695839a962aafb891ecbf3228
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-29 10:13:17 +0000

    Package download backupper-standard

---
+++ automatic/backupper-standard/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = 'https://www.ubackup.com/ss/download/adb/AOMEIBackupperStd.exe'
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = $toolsDir
+    fileType      = 'EXE'
+    url           = $url
+    softwareName  = 'AOMEI Backupper Standard*'
+    checksum      = '4391ab2f786785643ca89a56dc3725f9d91fb7fed11d60334265d0107c4da024'
+    checksumType  = 'sha256'
+    silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
