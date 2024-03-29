794177affe482ecc646e3ad2e8d4095723f20455
Tunisiano18 <fabian@bowlman.org>
2024-03-29 16:49:27 +0100

    test

---
+++ scripts/Exclude-package.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+  Updates nuspec file description from README.md
+
+.DESCRIPTION
+  This script should be called in au_AfterUpdate to put the text in the README.md
+  into description tag of the Nuspec file. The current description will be replaced.
+  Function will throw an error if README.md is not found.
+
+.PARAMETER SkipFirst
+  Number of start lines to skip from the README.md, by default 0.
+
+.PARAMETER SkipLast
+  Number of end lines to skip from the README.md, by default 0.
+
+.EXAMPLE
+  function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 2 }
+#>
+function Exclude-Package {
+    param(
+        [Parameter(Mandatory = $true, ParameterSetName = "issue")]
+        [long]$issueNumber,
+        [Parameter(Mandatory = $true)]
+        [string]$title
+    )
+    Write-Host "number : $issueNumber"
+    Write-Host "Title : $title"
+    $extract = $($title.split('(|)')[1])
+    Write-Host "Extract : $extract"
+    $folder = Join-Path $PSScriptRoot "../automatic/$extract"
+    Write-Host "Test : $(Test-Path -Path $folder)"
+}
