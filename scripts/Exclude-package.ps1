c03bd8e3cdd6f61a6218c9ff33df1b33b7c313cc
Tunisiano18 <fabian@bowlman.org>
2024-03-29 18:37:42 +0100

    [skip ci] add to exclude

--- scripts/Exclude-package.ps1
+++ scripts/Exclude-package.ps1
@@ -46,6 +46,7 @@ function Exclude-Package {
         Remove-Item -Path $folder -Recurse -Force -ErrorAction Continue
         Remove-Item -Path "../icons/$extract.*" -ErrorAction Continue
       }
+      Add-Content -Path "../tools/Check/exclude.txt" -Value $title
       Update-GitHubIssue -OwnerName $Owner -RepositoryName $repository -Issue $issueNumber -State Closed
       git commit -am  "[skip ci] exclude $title"
 
