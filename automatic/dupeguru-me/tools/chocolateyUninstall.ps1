b571d13a1e3ea900b12223c4f5869fc2bf4c045f
Tunisiano187 <helpdesk.choc@gmail.com>
2024-03-30 15:22:24 +0000

    [skip ci] exclude dupeguru-me

--- automatic/dupeguru-me/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-try {
-  $packageName = 'dupeguru-me'
-  $packageSearch = "dupeguru " + "music" + "*"
-  $packageVersion = '6.8.1'
-  $uninstallPackage = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like $packageSearch -and ($_.Version -eq $packageVersion) }
-  $uninstallResults = $uninstallPackage.Uninstall()
-} catch {
-  throw $_.Exception
-}
