0ff91acc59edaa0421e57635cf5040fa02ded611
Tunisiano18 <fabian@bowlman.org>
2024-03-30 18:38:31 +0100

    checksum

--- automatic/newsbin/update.ps1
+++ automatic/newsbin/update.ps1
@@ -21,9 +21,11 @@ function global:au_GetLatest {
 	$releasepage = ((Invoke-WebRequest -Uri $releases).Links | Where-Object {$_.href -match "download.php?"} | Select-Object -First 1).href
 	$version=get-version $releasepage
 	$url32 = $releasepage.Replace('&amp;','&')
+	$checksumType = "SHA512"
+	$checksum = Get-RemoteChecksum -Url $url32 -Algorithm $checksumType
 	
-	$Latest = @{ URL32 = $url32; Version = $version}
+	$Latest = @{ URL32 = $url32; Version = $version; Checksum32 = $checksum; ChecksumType32 = $checksumType}
 	return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
