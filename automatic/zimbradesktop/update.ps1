39c5f8b79c112e8fe5b13e8d7d65b6dff667728c
Tunisiano18 <fabian@bowlman.org>
2024-03-31 19:15:41 +0200

    remove 32

--- automatic/zimbradesktop/update.ps1
+++ automatic/zimbradesktop/update.ps1
@@ -6,8 +6,8 @@ $releases = "https://www.zimbra.com/zimbra-desktop-download/"
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
      }
