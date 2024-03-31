69dd4d75a0fef7538281a0792b44f31f3518de0e
tunisiano187 <helpdesk.choc@gmail.com>
2024-03-31 10:07:57 +0000

    AU: 1 updated - newsbin [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/eeba73b05c9984da97baa15569aa427fc2a3f2ed

--- automatic/newsbin/tools/chocolateyInstall.ps1
+++ automatic/newsbin/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName        = $env:ChocolateyPackageName
 $installerType      = 'exe'
 $silentArgs         = '/S /D=C:\Program Files\Newsbin'
-$url                = 'http://www.newsbin.com/downloads/nb672-full.exe'
-$checksum           = 'a02b43c90598585d87a1b63d6edcb4da92df5c760465b85d74006445a4aa2eb4'
-$checksumType       = 'sha1'
+$url                = 'https://www.newsbin.com/download.php?reg=0&filename=nb682.exe&version=6.82'
+$checksum           = 'b3c35b1231bf99bb8f344b5991d7ca6814652914f81b11553c65103cd786cec1d831566a6fb41f76607d6d9c8a43b70489cbcf16107ba6e669752036a0d25652'
+$checksumType       = 'SHA512'
 $validExitCodes = @(0)
 
 $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
