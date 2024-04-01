1f9827c78fe99f2c56f13f671b18989072d1b321
Tunisiano18 <fabian@bowlman.org>
2024-04-01 12:00:21 +0200

    force

--- automatic/zimbradesktop/tools/chocolateyInstall.ps1
+++ automatic/zimbradesktop/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-﻿$packageName = 'zimbra-desktop'
-$installerType = 'msi'
-$silentArgs = '/quiet /qn /norestart'
-$url = 'https://files.zimbra.com/downloads/zxui/4.36.0/Zimbra_Desktop_4.36.0_90a2ecb8c_20240205134908.exe'
-$checksum = 'e014d1d71fb5b8cef990d720861f5f70a3bd0a029f538127799ab727a6f6a354'
-$checksumType = 'sha256'
-$validExitCodes = @(0)
+﻿$packageName        = 'zimbra-desktop'
+$installerType      = 'exe'
+$silentArgs         = '/S'
+$url                = 'https://files.zimbra.com/downloads/zxui/4.36.0/Zimbra_Desktop_4.36.0_90a2ecb8c_20240205134908.exe'
+$checksum           = 'e014d1d71fb5b8cef990d720861f5f70a3bd0a029f538127799ab727a6f6a354'
+$checksumType       = 'sha256'
+$validExitCodes     = @(0)
 
 $MinimumWindowsVersion = "10.0.0"
 if((Get-CimInstance win32_operatingsystem).Version -lt $MinimumWindowsVersion) {
