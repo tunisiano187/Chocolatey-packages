13d7103ad273953716ca6bca413097a2d5fcf7d2
Tunisiano18 <tunisiano187@users.noreply.github.com>
2024-04-02 19:02:28 +0000

    Where-Object {$_.href -notmatch "debug" } |

--- automatic/bitcoin.portable/update.ps1
+++ automatic/bitcoin.portable/update.ps1
@@ -28,7 +28,7 @@ function global:au_GetLatest {
 
     Write-Verbose 'Get version'
     $version = $tags.name.Split(' ')[-1]
-    $folder = "https://bitcoincore.org/bin/$(((Invoke-WebRequest -Uri 'https://bitcoincore.org/bin/' -UseBasicParsing).Links | Where-Object {$_.href -match $version}| Select-Object -Last 1).href)"
+    $folder = "https://bitcoincore.org/bin/$(((Invoke-WebRequest -Uri 'https://bitcoincore.org/bin/' -UseBasicParsing).Links | Where-Object {$_.href -match $version} | Where-Object {$_.href -notmatch "debug" } | Select-Object -Last 1).href)"
 
 	Write-Verbose 'Get files'
 	$file = $(((Invoke-WebRequest -Uri $folder -UseBasicParsing).Links | Where-Object {$_.href -match ".zip"} | Where-Object {$_.href -match "win64"}).href)
