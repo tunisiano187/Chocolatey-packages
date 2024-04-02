6e9763e9c87074434f1f843d2e212fc338dd7107
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-02 16:52:32 +0000

    AU: 2 updated - nexusfile.install nexusfile.portable [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/b695e75f8a65f17a2bf4cfed83c9a6e626313c84

--- automatic/nexusfile.install/tools/chocolateyInstall.ps1
+++ automatic/nexusfile.install/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$packageName        = $env:ChocolateyPackageName
+﻿$packageName        = $env:ChocolateyPackageName
 $installerType      = 'exe'
 $silentArgs         = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url                = 'http://www.xiles.net/pds/NexusFileSetup5.3.3.exe'
-$checksum           = '3ca2bbd1072dadfbc87989f3018be89fe544a70c'
-$checksumType       = 'sha1'
+$url                = 'https://s3.ap-northeast-2.amazonaws.com/net.xiles.public/download/nexusfile_setup_5.4.1.exe'
+$checksum           = 'a76ae2de6ab0e072866133092668af788a7a3d1e664444e7fd7c4b12686ac900'
+$checksumType       = 'sha256'
 $validExitCodes     = @(0)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
--- automatic/nexusfile.portable/tools/chocolateyInstall.ps1
+++ automatic/nexusfile.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-$packageName        = $env:ChocolateyPackageName
-$url                = 'http://www.xiles.net/pds/nexusfile5.3.3.zip'
-$checksum           = '73f5d57cfbe8e811f853a9d43139443ac50141d6'
-$checksumType       = 'sha1'
+﻿$packageName        = $env:ChocolateyPackageName
+$url                = 'https://s3.ap-northeast-2.amazonaws.com/net.xiles.public/download/nexusfile_5.4.1.zip'
+$checksum           = 'eec39d8987a681da1cb9a48727167328edc30ea29c6acebb900fafb3e7dd7907'
+$checksumType       = 'sha256'
 $toolsDir           = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
