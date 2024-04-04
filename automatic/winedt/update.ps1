c77110ab466b3834044eeed986ba729d17e8b8a8
Tunisiano18 <fabian@bowlman.org>
2024-04-04 21:05:29 +0200

    [skip ci] WinEdt

---
+++ automatic/winedt/update.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = "https://www.winedt.com/download.html"
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+    $link = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '.exe'})
+	$url32="https://www.winedt.com/$($link.href)"
+
+    $version = (Get-Version $link.outerHTML).Version
+    
+	$Latest = @{ URL32 = $url32; Version = $version }
+
+    return $Latest
+}
+
+update -ChecksumFor 32
