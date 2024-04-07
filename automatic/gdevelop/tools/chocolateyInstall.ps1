738203da1221905df050e8654d754b6dbba35394
Tunisiano18 <fabian@bowlman.org>
2024-04-07 16:36:30 +0200

    silent arg

--- automatic/gdevelop/tools/chocolateyInstall.ps1
+++ automatic/gdevelop/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName        = $env:ChocolateyPackageName
 $installerType      = 'exe'
-$silentArgs         = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$silentArgs         = '/S'
 $url                = 'https://github.com/4ian/GDevelop/releases/download/v5.3.198/GDevelop-5-Setup-5.3.198.exe'
 $checksum           = 'b51b42b89832649b1d80a1590d4797696138e90452cc1bb0e0cff88566e81349'
 $checksumType       = 'sha256'
