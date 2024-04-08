7f37770b85c8ff87f2e9b23a43baa40852557be9
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-08 14:10:31 +0000

    AU: 2 updated - capture2text deepl [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/5cc962aea1397e3f800888cfcf31a4786494c404

--- automatic/capture2text/tools/chocolateyInstall.ps1
+++ automatic/capture2text/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$packageName    = $env:ChocolateyPackageName
-$url            = ''
-$checksum       = '20b8713a452c2e974d0851fd9934da99a072b051dd157932698d739d85bb0268'
+﻿$packageName    = $env:ChocolateyPackageName
+$url            = 'https://sourceforge.net/projects/capture2text/files/Capture2Text/Capture2Text_v4.6.3/Capture2Text_v4.6.3_32bit.zip/download'
+$checksum       = '789f2d29f855cfc4533fea71c8dc5ce7dfd58c26e16b83159f77385b8d8f8afe'
 $checksumType   = 'sha256'
-$url64          = ''
-$checksum64     = 'ac0d3ca8ac1bff23875f9752c13787eae0fda5c60828ef420eee1c500119d65a'
+$url64          = 'https://sourceforge.net/projects/capture2text/files/Capture2Text/Capture2Text_v4.6.3/Capture2Text_v4.6.3_64bit.zip/download'
+$checksum64     = 'd33847064c4edf726c61b35f3b753470546be671561cdb35fba85dfd1ad170fa'
 $checksumType64 = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
