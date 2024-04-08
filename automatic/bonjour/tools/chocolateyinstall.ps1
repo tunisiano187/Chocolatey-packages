4e1b9b42176a731664788d962fc050edd12d230b
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-08 16:50:45 +0000

    ps1 vars

--- automatic/bonjour/tools/chocolateyinstall.ps1
+++ automatic/bonjour/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $url              = 'https://www.apple.com/itunes/download/win32'
 $checksum         = 'cd6891afada9f940bc551a215a6c40f51a93b140026375bd30b1ee686dc73bd2'
@@ -14,7 +14,7 @@ $fileFullPath     = "$toolsDir\itunes.zip"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   FileFullPath  = $fileFullPath
-  
+
   url           = $url
   Checksum      = $checksum
   checksumType  = $checksumType
