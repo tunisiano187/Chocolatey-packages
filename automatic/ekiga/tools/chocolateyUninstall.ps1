a3601c657c58ccfeccdfc8d0a9375332279b8001
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-11 16:02:57 +0000

    Package download ekiga

---
+++ automatic/ekiga/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+try {
+  $packageName = 'ekiga'
+  $fileType = 'exe'
+  $silentArgs = '/S'
+  $validExitCodes = @(0)
+  $osBitness = Get-ProcessorBits
+  if ($osBitness -eq 64) {
+    $unPath = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+  } else {
+    $unPath = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+  }
+  $unString = (Get-ItemProperty $unPath\$packageName* UninstallString).UninstallString
+  Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unString" -validExitCodes $validExitCodes
+} catch {
+  throw $_.Exception
+}
