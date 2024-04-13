a7bba95ec63bc4501b61791f712e5a227e1c8321
Tunisiano18 <fabian@bowlman.org>
2024-04-13 18:26:39 +0200

    get-childitem

--- automatic/searchmyfiles/tools/chocolateyInstall.ps1
+++ automatic/searchmyfiles/tools/chocolateyInstall.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = 'Stop'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $file32         = "$(Join-Path $toolsDir -ChildPath 'searchmyfiles.zip')"
 $file64         = "$(Join-Path $toolsDir -ChildPath 'searchmyfiles-x64.zip')"
-$installFile    = Join-Path $toolsDir "$($env:ChocolateyPackageName).exe"
 
 $unzipArgs = @{
     PackageName     = $env:ChocolateyPackageName
@@ -12,6 +11,8 @@ $unzipArgs = @{
 }
 
 Get-ChocolateyUnzip @unzipArgs
+
+$installFile    = (get-childitem -Filter "*.exe" -Recurse).FullName
 
 Set-Content -Path ("$installFile.gui") `
             -Value $null
