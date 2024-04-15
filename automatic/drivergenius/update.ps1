350c2ea88fb6d6717d097912b57cb6d0da4af643
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-15 17:48:03 +0200

    tools

--- automatic/drivergenius/update.ps1
+++ automatic/drivergenius/update.ps1
@@ -6,9 +6,9 @@ $releases = 'https://www.driver-soft.com/download.html'
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-			"(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-			"(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
 		}
 	}
 }
