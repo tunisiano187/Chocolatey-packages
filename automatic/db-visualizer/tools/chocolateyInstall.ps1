09a2e379569e195646e0e05d8abce242e3d213e9
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-16 05:23:07 +0000

    AU: 2 updated - db-visualizer electron [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/01a9a80a0f20b98d053a01ea859df0ccafeef820

--- automatic/db-visualizer/tools/chocolateyInstall.ps1
+++ automatic/db-visualizer/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64            = 'https://www.dbvis.com/product_download/dbvis-24.1.3/media/dbvis_windows-x64_24_1_3.exe'
-$checksum64       = '96f3b2b7d7cd7402d32307fde8448b1a687f49ae3606d833e6e2dad81f3c6a2a'
+$url64            = 'https://www.dbvis.com/product_download/dbvis-24.1.4/media/dbvis_windows-x64_24_1_4.exe'
+$checksum64       = 'ecfe93936e3390b33a2c615a412e3a6205207eba87c8b9ef980967639e797afb'
 $checksumType64   = 'sha256'
 
 $packageArgs = @{
--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v29.3.0/electron-v29.3.0-win32-ia32.zip'
-$checksum = '02e449a761f08ff710c3ed04a64940efbf6358071362b964c0f8e2f88f132a5d'
+$url = 'https://github.com/electron/electron/releases/download/v30.0.0/electron-v30.0.0-win32-ia32.zip'
+$checksum = '17ad6e44b9c44f6cc51db623c19dea888051ee9b5b1de3dbb79199d5a2e0cca6'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v29.3.0/electron-v29.3.0-win32-x64.zip'
-$checksum64 = '4c7adf366cd0747d072aec23ff22837c9b016336a0bc26d64bffdd7869ad4bd2'
+$url64 = 'https://github.com/electron/electron/releases/download/v30.0.0/electron-v30.0.0-win32-x64.zip'
+$checksum64 = '13a51d6a9f023937208334a76ec1888c6ea208925f8404f468acafd50c8ce178'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
