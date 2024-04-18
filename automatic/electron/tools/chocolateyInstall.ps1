bf107b59d36556d682e7793dde167d56ded2a3d2
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-18 17:58:13 +0000

    AU: 2 updated - electron soundvolumeview [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/a85b9b5bf38b6810acd038d52faf9dea2764c61f

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v30.0.0/electron-v30.0.0-win32-ia32.zip'
-$checksum = '17ad6e44b9c44f6cc51db623c19dea888051ee9b5b1de3dbb79199d5a2e0cca6'
+$url = 'https://github.com/electron/electron/releases/download/v30.0.1/electron-v30.0.1-win32-ia32.zip'
+$checksum = '4430530331cbfd52632ece0cd2b55fe24a73cbe9c37833cfa8692234724dddf1'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v30.0.0/electron-v30.0.0-win32-x64.zip'
-$checksum64 = '13a51d6a9f023937208334a76ec1888c6ea208925f8404f468acafd50c8ce178'
+$url64 = 'https://github.com/electron/electron/releases/download/v30.0.1/electron-v30.0.1-win32-x64.zip'
+$checksum64 = 'ca6801afcaa80e16420d5b3a637d5f468ae439c681f3221a1bf6d20ffcc00110'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
