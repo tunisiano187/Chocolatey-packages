2603ae8e4ac08c670d904ffd62a2e8c4fb833eec
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-18 12:55:29 +0200

    mailpv

--- automatic/mailpv.portable/update.ps1
+++ automatic/mailpv.portable/update.ps1
@@ -6,6 +6,7 @@ function global:au_SearchReplace {
 		"legal\VERIFICATION.txt"      = @{
 			"(?i)(x86:).*"        				= "`${1} $($Latest.URL32)"
 			"(?i)(checksum:).*" 				= "`${1} $($Latest.Checksum32)"
+			"(?i)(checksumtype:).*" 			= "`${1} $($Latest.ChecksumType32)"
 		}
 	}
 }
@@ -16,15 +17,19 @@ function global:au_AfterUpdate($Package) {
 
 function global:au_GetLatest {
 	$url32 = "https://www.nirsoft.net/toolsdownload/mailpv.zip"
-	Invoke-WebRequest -Uri $url32 -OutFile "tools/mailpv.zip"
+	$strHeader = @{
+		"referer"="https://www.nirsoft.net/utils/mailpv.html"
+	}
+	Invoke-WebRequest -Uri $url32 -OutFile "tools/mailpv.zip" -UserAgent $(Get-UserAgent) -Headers $strHeader
+	$checksum = Get-FileHash -Path "tools/mailpv.zip" -Algorithm $env:ChocolateyChecksumType
 	$pageContent = Invoke-WebRequest -Uri "https://www.nirsoft.net/utils/mailpv.html"
 	$regexPattern = 'PassView v(\d+(\.\d+)*)'
 	$versionMatch = $pageContent.Content | Select-String -Pattern $regexPattern -AllMatches
 	$version = $versionMatch.Matches[0].Groups[1].Value
 	Update-Metadata -key "copyright" -value "© $(Get-Date -Format "yyyy") NirSoft"
 
-	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	$Latest = @{ URL32 = $url32; Checksum32 = $checksum; ChecksumType32 = $env:ChocolateyChecksumType ; Version = $version }
 	return $Latest
 }
 
-update
+update -NoCheckUrl -ChecksumFor none
