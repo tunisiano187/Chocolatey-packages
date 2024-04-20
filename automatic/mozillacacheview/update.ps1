99f0c07b6b54c4086576c9b83b84099f0e03af56
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-20 17:57:07 +0200

    checksum

--- automatic/mozillacacheview/update.ps1
+++ automatic/mozillacacheview/update.ps1
@@ -17,14 +17,15 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
 	$url32 = "https://www.nirsoft.net/utils/mzcacheview.zip"
 	Invoke-WebRequest -Uri $url32 -OutFile "tools/mzcacheview.zip"
+	$checksum32 = Get-FileHash -Path "tools/mzcacheview.zip" -Algorithm $env:ChocolateyChecksumType
 	$pageContent = Invoke-WebRequest -Uri "https://www.nirsoft.net/utils/mozilla_cache_viewer.html"
 	$regexPattern = 'MZCacheView v(\d+(\.\d+)*)'
 	$versionMatch = $pageContent.Content | Select-String -Pattern $regexPattern -AllMatches
 	$version = $versionMatch.Matches[0].Groups[1].Value
 	Update-Metadata -key "copyright" -value "© $(Get-Date -Format "yyyy") NirSoft"
 
-	$Latest = @{ URL32 = $url32; Version = $version }
+	$Latest = @{ URL32 = $url32; Checksum32 = $checksum32; ChecksumType32 = $env:ChocolateyChecksumType; Version = $version }
 	return $Latest
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
