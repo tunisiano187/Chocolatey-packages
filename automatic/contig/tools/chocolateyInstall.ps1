181b51b9df9459a6c710b52eaca097d803413a76
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-20 20:03:34 +0200

    Contig

--- automatic/contig/tools/chocolateyInstall.ps1
+++ automatic/contig/tools/chocolateyInstall.ps1
@@ -1,19 +1,15 @@
-﻿$packageName = 'contig'
-$url = 'https://download.sysinternals.com/files/Contig.zip'
-$checksum = '1c176a2dbe80e60941f8e00926828158df1bf94530ce936fe5bb84a477355b5b'
+﻿$packageName  = 'contig'
+$url          = 'https://download.sysinternals.com/files/Contig.zip'
+$checksum     = '1c176a2dbe80e60941f8e00926828158df1bf94530ce936fe5bb84a477355b5b'
 $checksumType = 'sha256'
-$url64 = "$url"
-$checksum64 = "$checksum"
-$checksumType64 = "checksumType"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -UnzipLocation "$toolsDir" `
-                             -Url64bit "$url64" `
                              -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
-                             -Checksum64 "$checksum64" `
-                             -ChecksumType64 "$checksumType64"
+                             -ChecksumType "$checksumType"
+                            
 Write-Verbose "Accepting license..."
 $regRoot = 'HKCU:\Software\Sysinternals'
 $regPkg = 'C'
