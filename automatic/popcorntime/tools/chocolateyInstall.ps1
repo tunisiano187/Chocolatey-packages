089d7ac86df7186d03cdc6043ce3fd1aa5fce3cc
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-20 14:36:12 +0200

    64bits/win64 only

--- automatic/popcorntime/tools/chocolateyInstall.ps1
+++ automatic/popcorntime/tools/chocolateyInstall.ps1
@@ -1,8 +1,5 @@
 ﻿$packageName = 'popcorntime'
 $installerType = 'exe'
-$url = 'https://github.com/popcorn-official/popcorn-desktop/releases/download/v0.5.0/Popcorn-Time-0.5.0-win32-0.44.5-Setup.exe'
-$checksum = '2c299c5c4a4b04767abab14e23d4c0a4e751d8bf1699e7f4cdf32ab96ca63c7c'
-$checksumType = 'sha256'
 $url64 = 'https://github.com/popcorn-official/popcorn-desktop/releases/download/v0.5.0/Popcorn-Time-0.5.0-win64-0.44.5-Setup.exe'
 $checksum64 = 'b781fa573f6366e1e1428e0f3b21efd22b94e3a96db753488b73132a9db72e11'
 $checksumType64 = 'sha256'
@@ -12,10 +9,7 @@ $validExitCodes = @(0)
 Install-ChocolateyPackage -PackageName "$packageName" `
                           -FileType "$installerType" `
                           -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
-                          -Url64 "$url64" `
-                          -Checksum64 "$checksum64" `
-                          -ChecksumType64 "$checksumType64" `
+                          -Url "$url64" `
+                          -Checksum "$checksum64" `
+                          -ChecksumType "$checksumType64" `
                           -ValidExitCodes $validExitCodes
