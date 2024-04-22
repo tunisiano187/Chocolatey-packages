ec65a8a921fa1018e696870b00d2385199a57e3f
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-22 19:38:08 +0200

    url

--- automatic/Yandex-browser/update.ps1
+++ automatic/Yandex-browser/update.ps1
@@ -6,9 +6,9 @@ $release = "https://browser.yandex.com/download?banerid=6500000000&statpromo=tru
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
         }
      }
 }
