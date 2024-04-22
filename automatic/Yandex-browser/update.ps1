4d26039409fc2fffd7a5df7b9f560025dd947a6c
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-22 17:23:32 +0000

    ps1 vars

--- automatic/Yandex-browser/update.ps1
+++ automatic/Yandex-browser/update.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 import-module au
 
 $release = "https://browser.yandex.com/download?banerid=6500000000&statpromo=true&partner_id=default"
@@ -19,7 +19,7 @@ function global:au_AfterUpdate($Package) {
 
 function global:au_GetLatest {
 	$url32 = (Get-RedirectedUrl -url $release).split('?')[0] -replace "/lite/","/"
-    
+
     $ver = ($url32.Split("/") | Where-Object {$_ -match "_"}).split('_')
 	$version = "$($ver[0]).$($ver[1]).$($ver[2]).$($ver[3])"
 
