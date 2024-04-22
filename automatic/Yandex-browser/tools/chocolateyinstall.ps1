b6022d4c3044f57ed29ff06f0750a668c582276c
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-22 19:11:15 +0200

    yandex-browser

---
+++ automatic/Yandex-browser/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+
+$url          = ''
+$checksum     = ''
+$checksumType = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'Yandex-browser*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum      = $checksum
+  checksumType  = $checksumType
+
+  silentArgs    = "--silent"
+  #validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/Yandex-browser/update.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$release = "https://browser.yandex.com/download?banerid=6500000000&statpromo=true&partner_id=default"
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$url32 = (Get-RedirectedUrl -url $release).split('?')[0] -replace "/lite/","/"
+    
+    $ver = ($url32.Split("/") | Where-Object {$_ -match "_"}).split('_')
+	$version = "$($ver[0]).$($ver[1]).$($ver[2]).$($ver[3])"
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 32
