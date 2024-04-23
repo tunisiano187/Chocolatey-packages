06eb3461b760c56591d53748323c896e80f02772
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-23 05:19:36 +0000

    AU: 1 updated - Yandex-browser [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/e260444c9169b98499e15741ddde4bae37f53c8d

--- automatic/Yandex-browser/tools/chocolateyinstall.ps1
+++ automatic/Yandex-browser/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
-$url          = ''
-$checksum     = ''
-$checksumType = ''
+$url          = 'https://cachev2-kiv03.cdn.yandex.net/download.cdn.yandex.net/browser/int/24_4_1_901_56713/en/Yandex.exe'
+$checksum     = '45fc4234238e0f17c82efe97c277a92bf449ae0bf1236757124610f47bc46ab3'
+$checksumType = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
