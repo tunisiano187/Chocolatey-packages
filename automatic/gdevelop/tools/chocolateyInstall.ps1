e4f5a0ba51da6d08da367257a9cc1f8a832cd826
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-26 17:55:32 +0000

    AU: 1 updated - gdevelop [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/df6fdaf8903cf88217d020a0e30dd8253e2db15c

--- automatic/gdevelop/tools/chocolateyInstall.ps1
+++ automatic/gdevelop/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName        = $env:ChocolateyPackageName
 $installerType      = 'exe'
 $silentArgs         = '/S'
-$url                = 'https://github.com/4ian/GDevelop/releases/download/v5.3.200/GDevelop-5-Setup-5.3.200.exe'
-$checksum           = '88753ca956cc25b12112832ed90065f3b78605d10669f71cf5d0789f055b5b94'
+$url                = 'https://github.com/4ian/GDevelop/releases/download/v5.3.201/GDevelop-5-Setup-5.3.201.exe'
+$checksum           = '72402d9872bcc739065ef3958976640385b7e7a77e875ec7671ccd95286e58fb'
 $checksumType       = 'sha256'
 $validExitCodes     = @(0)
 
