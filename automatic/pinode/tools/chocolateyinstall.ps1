85e13f2834eac17e71b105a869056b772f3fe428
tunisiano187 <helpdesk.choc@gmail.com>
2024-04-28 08:58:20 +0000

    AU: 1 updated - pinode [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/cdb55308f0b375286353e6443e54499f04576810

--- automatic/pinode/tools/chocolateyinstall.ps1
+++ automatic/pinode/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName      = $env:ChocolateyPackageName
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url              = 'https://downloads.minepi.com/Pi%20Network%20Setup%200.4.11.exe'
-$checksum         = 'ea4843db2e26c8de59344c09d63cfcead8c1cdb2cb9eadf141fff5112fa06ca5'
+$checksum         = '65e124e93afca3b8cf982683a3e9f13f5ee85fc11a6ffc76b31d8c2e33e013b1'
 $checksumType     = 'sha256'
 
 $packageArgs = @{
