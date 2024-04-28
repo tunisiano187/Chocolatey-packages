714153e50520c7dc06ec9a88fc9341befaa44e0d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-28 10:38:26 +0200

    Force

--- automatic/pinode/update.ps1
+++ automatic/pinode/update.ps1
@@ -21,6 +21,9 @@ function global:au_GetLatest {
 	$url = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match ".exe"}).href
 	[version]$version=$url.replace("%20"," ").Split(" ")[-1].replace('.exe','')
 
+	if($version -eq "0.4.11") {
+		$version = '0.4.11.2024042801'
+	}
 	$Latest = @{ URL32 = $url; Version = $version }
 	return $Latest
 }
