d050b1df3d7888d21fb8d281858f812aa03ee24c
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-29 08:56:08 +0000

    Exclude adguard-chrome [skip ci]

--- automatic/adguard-chrome/tools/chocolateyuninstall.ps1
+++
@@ -1,9 +0,0 @@
-$bits = Get-ProcessorBits
-$packageName = 'adguard-chrome'
-$extensionID = 'bgnkhhnnamicmpeenaelnjfhikgbkllg'
-
-if ($bits -eq 64) {
-    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
-   }else{
-    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
-}
