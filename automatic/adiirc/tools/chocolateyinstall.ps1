4d49faf2e2ae92b84f47accaadb687f5c9c85465
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-29 12:40:28 +0200

    addirc

--- automatic/adiirc/tools/chocolateyinstall.ps1
+++ automatic/adiirc/tools/chocolateyinstall.ps1
@@ -1,22 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.adiirc.com/AdiIRC.3.3.exe'
-$url64      = 'https://www.adiirc.com/AdiIRC64.3.3.exe'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://www.adiirc.com/AdiIRC.3.3.exe'
+$checksum       = ''
+$url64          = 'https://www.adiirc.com/AdiIRC64.3.3.exe'
+$checksum64     = ''
+$checksumType   = ''
+
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
-  softwareName  = 'AdiIRC*'
-  checksum      = 'C486E52BF1EC1EF9BC668675A6EFCD39143C9235B2890A5DDF782B4F0E7C3189'
-  checksumType  = 'sha256'
-  checksum64    = 'D974BD427BE660D3C54E88BAA898B7B64C58D0EF7A6C8043C32985BEC0026AB2'
-  checksumType64= 'sha256'
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $toolsDir
+  fileType        = 'EXE'
+  url             = $url
+  url64bit        = $url64
+  softwareName    = 'AdiIRC*'
+  checksum        = $checksum
+  checksumType    = $checksumType
+  checksum64      = $checksum64
+  checksumType64  = $checksumType
 
-  validExitCodes= @(1223, 0, 3010, 1641)
-  silentArgs   = '/S'
+  validExitCodes  = @(1223, 0, 3010, 1641)
+  silentArgs      = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
---
+++ automatic/adiirc/update.ps1
@@ -0,0 +1,39 @@
+﻿import-module au
+
+$domain     = 'https://www.adiirc.com'
+$releases   = 'https://www.adiirc.com/download.php'
+
+function global:au_SearchReplace {
+  @{
+    "tools\chocolateyInstall.ps1" = @{
+      "(^[$]url\s*=\s*)('.*')"              = "`$1'$($Latest.URL32)'"
+      "(^[$]url64\s*=\s*)('.*')"            = "`$1'$($Latest.URL64)'"
+      "(^[$]checksum\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum32)'"
+      "(^[$]checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
+      "(^[$]checksumType\s*=\s*)('.*')"     = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    
+    $urls = $download_page.Links.href | Where-Object {$_ -match "exe$"} | Select-Object -First 2
+    $url64 = $urls | Where-Object {$_ -match "64"}
+    $url = $urls | Where-Object {$_ -notmatch $url64}
+    
+	  $version = (Get-Version ($download_page.Links.href | Where-Object {$_ -match "download.php\?v="} | Select-Object -First 1)).Version
+	
+    return @{
+        URL32 = $domain + $url
+        URL64 = $domain + $url64
+        Version = $version
+    }
+}
+
+update
