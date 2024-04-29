e95fd08d4c460bd7380fb67b227b36b87ea45716
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-29 10:50:44 +0200

    depedency

--- automatic/freeplane/update.ps1
+++ automatic/freeplane/update.ps1
@@ -1,7 +1,7 @@
 import-module au
 
 $releases = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/'
-$bugversion = "1.11.13"
+$bugversion = ""
 
 function global:au_SearchReplace {
 	@{
@@ -23,8 +23,9 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
 	$url32 = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match '-Setup-'} | Where-Object {$_ -match '.exe'} | Where-Object {$_ -match "https"}).href | Select-Object -First 1
 	$version = ($url32.Split('-|/') | Where-Object {$_ -match ".exe"}).replace('.exe','').replace('u','-u')
-	
-	$version = Get-FixVersion -Version $version -OnlyFixBelowVersion $bugversion
+	if($version -eq "1.11.12") {
+		$version = "1.11.12.202442901"
+	}
 	$CorrettoVersion = $(choco search corretto11jdk | Where-Object {$_ -match 'corretto11jdk'}).split(' ')[1]
 
 	$Latest = @{ URL32 = $url32; Version = $version; FileName32 = "Freeplane.exe"; CorrettoVersion = $CorrettoVersion }
