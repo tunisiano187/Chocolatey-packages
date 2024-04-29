93398488fcea619a8e49a8b453f11403f237d9bf
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-29 20:06:41 +0200

    advancedsystemtweaker

--- automatic/advancedsystemtweaker/tools/chocolateyInstall.ps1
+++ automatic/advancedsystemtweaker/tools/chocolateyInstall.ps1
@@ -1,16 +1,21 @@
-﻿$packageName = 'advancedsystemtweaker'
-$installerType = 'EXE'
-$url = 'http://www.tweaking.com/files/setups/tweaking.com_advanced_system_tweaker_setup.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+﻿$ErrorActionPreference = "Stop"
 
-try {
+$url 			= 'http://www.tweaking.com/files/setups/tweaking.com_advanced_system_tweaker_setup.exe'
+$checksum 		= ''
+$checksumType 	= ''
 
-	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
-	
-	Write-ChocolateySuccess $packageName
-	
-} catch {
-	Write-ChocolateyFailure $packageName $($_.Exception.Message)
-	throw 
+$packageArgs = @{
+	packageName   = $env:ChocolateyPackageName
+	fileType      = 'EXE'
+	url           = $url
+  
+	softwareName  = 'amazon-chime*'
+  
+	checksum      = $checksum
+	checksumType  = $checksumType
+  
+	silentArgs    = '/S'
+	validExitCodes= @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 }
+
+Install-ChocolateyPackage @packageArgs
