9072817992ed2c129350f10cec01a6b663147372
Tunisiano187 <helpdesk.choc@gmail.com>
2024-04-30 19:17:43 +0200

    first commit

--- automatic/agentsvn/tools/chocolateyinstall.ps1
+++ automatic/agentsvn/tools/chocolateyinstall.ps1
@@ -1,8 +1,27 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$packageName = 'agentsvn'
-$installerType = 'exe'
-$silentArgs = '/sp- /silent /norestart /suppressmsgboxes'
-$url = 'http://www.zeusedit.com/agent/bin/asvn-v256.exe'
+$packageName    = 'agentsvn'
+$url32          = 'http://www.zeusedit.com/agent/bin/asvn-v256.exe'
+$checksum32     = ''
+$checksumType   = ''
+$url64          = 'http://www.zeusedit.com/agent/bin/asvn-v256.exe'
+$checksum64     = ''
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url
+$packageArgs = @{
+    packageName     = $packageName
+    fileType        = 'EXE'
+    softwareName    = "$($packageName)*"
+  
+    checksum        = $checksum32
+    checksumType    = $checksumType
+    url             = $url32
+  
+    checksum64      = $checksum64
+    checksumType64  = $checksumType
+    url64bit        = $url64
+  
+    silentArgs      = '/sp- /silent /norestart /suppressmsgboxes'
+    validExitCodes  = @(0)
+}
+  
+Install-ChocolateyZipPackage @packageArgs
--- automatic/agentsvn/tools/chocolateyuninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName = 'agentsvn'
-$installerType = 'exe'
-$silentArgs = '/sp- /silent /norestart /suppressmsgboxes'
-
-if (Test-Path '${env:ProgramFiles(x86)}\AgentSVN') {
-    Uninstall-ChocolateyPackage $packageName $installerType $silentArgs '${env:ProgramFiles(x86)}\AgentSVN\unins000.exe'
-}
