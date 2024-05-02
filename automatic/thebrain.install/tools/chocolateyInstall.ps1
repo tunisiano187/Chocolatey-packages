b59adc555995202701673a4e707e93cbb22c5c6b
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-02 00:31:40 +0000

    AU: 1 updated - thebrain.install [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/2cb443a9c8da22e192d130c6b4fc25bbf91a3eec

--- automatic/thebrain.install/tools/chocolateyInstall.ps1
+++ automatic/thebrain.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '-q -overwrite'
-$url            = 'https://updater.thebrain.com/files/TheBrain%2014.0.38.0%20Installer.exe'
-$checksum       = '3760bd316279218c28359205db4008a02c8d9b9bb8b85c593ebbd06a24f9352e'
+$url            = 'https://updater.thebrain.com/files/TheBrain%2014.0.42.0%20Installer.exe'
+$checksum       = '8c5de3f30901c454186b5ed9843068e5f5c408fe688019ccd3b2d9224dc9c1a1'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
 
