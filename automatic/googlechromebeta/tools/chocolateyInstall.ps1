7103860186df338a1d32bfd2c898ef07fb628ec2
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-02 14:20:29 +0200

    googlechromebeta

---
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
+
+$version = '125.0.6422.14-beta'
+if ($version -eq (Get-ChromeBetaVersion)) {
+  Write-Host "Google Chrome Beta $version is already installed."
+  return
+}
+
+$packageArgs = @{
+  packageName            = 'googlechrome'
+  fileType               = 'MSI'
+  url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
+  url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
+  checksum               = '85ace5cc873720664edf54ec05202d5ca6c907028bfbdccac7a7d34410c7b272'
+  checksum64             = '4617a48b4ee18d64f056669f391cdb098f3c4f59710b53ae17241e9e8de88ea4'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes         = @(0)
+}
+
+if (Get-Chrome32bitInstalled) { 'url64bit', 'checksum64', 'checksumType64' | ForEach-Object { $packageArgs.Remove($_) } }
+Install-ChocolateyPackage @packageArgs
