716cb991f1c68abb562fa2f6836e5138bb0d89af
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-05 21:05:20 +0200

    airshipper

---
+++ automatic/airshipper/update.ps1
@@ -0,0 +1,40 @@
+﻿import-module au
+
+$releases = 'https://github.com/veloren/airshipper/releases/latest'
+$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
+$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
+
+function global:au_SearchReplace {
+	@{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*url(32)?\:\s*).*"        = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum(32)?\:\s*).*"   = "`${1}$($Latest.Checksum32)"
+      "(?i)(^\s*checksum\s*type\:\s*).*" = "`${1}$($Latest.ChecksumType32)"
+    }
+	}
+}
+
+function global:au_BeforeUpdate($Package) {
+  Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_AfterUpdate($Package) {
+  Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
+	$url32 = $tags.assets.browser_download_url | Where-Object {$_ -match ".msi$"}
+	$version = $tags.tag_name.Replace('v','')
+	Update-Metadata -key "releaseNotes" -value $tags.html_url
+	Update-Metadata -key "licenseUrl" -value $((Get-GitHubLicense -OwnerName $Owner -RepositoryName $repo).download_url)
+	if($tags.prerelease -match "true") {
+    	$date = $tags.published_at.ToString("yyyyMMdd")
+    	$version = "$version-pre$($date)"
+	}
+
+  	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update
