05083ab8306aa26b832ab2b7679c940d35c6c51c
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-05 17:27:04 +0200

    AgentSVN

--- automatic/agentsvn/tools/chocolateyinstall.ps1
+++ automatic/agentsvn/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32          = 'https://www.zeusedit.com/agent/bin/asvn32-v276.zip'
-$checksum32     = ''
-$checksumType   = ''
-$url64          = 'https://www.zeusedit.com/agent/bin/asvn32-v276.zip'
-$checksum64     = ''
+$checksum32     = 'cd3e8f366c7b247dc310dd67413028004c1d4e5b71557451460ed37241b561b6'
+$checksumType   = 'sha256'
+$url64          = 'https://www.zeusedit.com/agent/bin/asvn64-v276.zip'
+$checksum64     = 'f8cfd51389d8f479b3461156776c11e8420bcd5cf731a4711ab66370ccd8aab6'
 
 $packageArgs = @{
     packageName     = $packageName
-    fileType        = 'EXE'
-    softwareName    = "$($packageName)*"
     unzipLocation   = $toolsDir
 
     checksum        = $checksum32
@@ -21,9 +19,21 @@ $packageArgs = @{
     checksum64      = $checksum64
     checksumType64  = $checksumType
     url64bit        = $url64
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$File = (Get-ChildItem -Path $toolsDir -Filter "*.exe").FullName
+
+$packageArgs = @{
+    packageName     = $packageName
+    fileType        = 'EXE'
+    softwareName    = "$($packageName)*"
+    
+    file            = $File
 
     silentArgs      = '/sp- /silent /norestart /suppressmsgboxes'
     validExitCodes  = @(0)
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
