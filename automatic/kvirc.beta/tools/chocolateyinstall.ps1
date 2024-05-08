93eba9079676e19638f5c7cb5a3365bca4f53e15
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-08 15:25:38 +0200

    beta

---
+++ automatic/kvirc.beta/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://github.com/kvirc/KVIrc/releases/download/4.2.0/KVIrc-4.2.0-Equilibrium.exe'
+$checksum       = ''
+$checksumType   = ''
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'KVIrc*'
+  checksum      = $checksum
+  checksumType  = $checksumType
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/kvirc.beta/update.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://nightly.kvirc.net/win-x86_64/'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$url32 = "https://nightly.kvirc.net$((Invoke-WebRequest -Uri $releases).Links.href | Where-Object {$_ -match ".exe$"} | select-object -Last 1)"$version = ($tags.tag_name -split 'v|/' | Where-Object { $_ -match "."}).Trim()
+	$version = $url32 -split '-' | Where-Object {$_ -match '[0-9]\.[0-9]'}
+	$version = "$version-dev"
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update
