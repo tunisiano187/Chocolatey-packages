8874bcd7964d7460ab7bd3410a8275e813a3d9ee
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-08 13:02:28 +0000

    Package download kvirc [skip ci]

---
+++ automatic/kvirc/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/kvirc/KVIrc/releases/download/4.2.0/KVIrc-4.2.0-Equilibrium.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'KVIrc*'
+  checksum      = '44d862cf06495faa8b70ec916542a083d161b219fb0606b4c72f7cc2c7fcef83'
+  checksumType  = 'sha256'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
