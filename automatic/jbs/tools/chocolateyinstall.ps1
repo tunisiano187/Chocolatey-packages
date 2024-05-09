198f3af99a69ddd70282fd0a4a44a718570fd797
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-09 16:33:26 +0000

    AU: 1 updated - jbs [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/83065a05aa54ebc59c7a43344b90d60064d4a624

--- automatic/jbs/tools/chocolateyinstall.ps1
+++ automatic/jbs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $packageName= $env:ChocolateyPackageName
 $installerType      = 'EXE'
 $silentArgs = '/S'
-$url        = 'https://dl.johnsadventures.com/SwitcherSetup57.exe'
-$checksum   = '03226011dca68d348ca4e4814b22dcafbaea23f514dc8ad698489fbb8b2a280d'
+$url        = 'https://dl.johnsadventures.com/SwitcherSetup58.exe'
+$checksum   = 'b412edcdfb873329a894a7950e9330002f774e5c6d512244d7bc564503063e68'
 $checksumtype = 'sha256'
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url -checksum $checksum -ValidExitCodes @(0,1223) -checksumType $checksumtype
