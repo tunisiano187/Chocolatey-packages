476bc78322253d52a842490f2b3d741ce4364024
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-09 10:39:59 +0000

    AU: 2 updated - adiirc kvirc [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/f012d9b7715e9e0ac97fc164ef986c5a9cf2f15e

--- automatic/adiirc/tools/chocolateyinstall.ps1
+++ automatic/adiirc/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://www.adiirc.com/AdiIRC.3.3.exe'
-$checksum       = ''
-$url64          = 'https://www.adiirc.com/AdiIRC64.3.3.exe'
-$checksum64     = ''
-$checksumType   = ''
+$url            = 'https://www.adiirc.com/AdiIRC.4.4.exe'
+$checksum       = 'd4c0fabcd44a1db0e5b8cfb9dfb996785db6487b1e500d5a2eff28b90159090b'
+$url64          = 'https://www.adiirc.com/AdiIRC64.4.4.exe'
+$checksum64     = 'e5ba3854f59f9088643b7dcdf20289a66604b3607ce56130c2b8dcfad1b132bb'
+$checksumType   = 'sha256'
 
 
 $packageArgs = @{
--- automatic/kvirc/tools/chocolateyinstall.ps1
+++ automatic/kvirc/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/kvirc/KVIrc/releases/download/4.2.0/KVIrc-4.2.0-Equilibrium.exe'
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://github.com/kvirc/KVIrc/releases/download/5.2.2/KVIrc-5.2.2-Quasar-x86_64.exe'
+$checksum       = 'ebbf9dc260f080b0c0b664639ac5c3d21077de7b2393ae9437f9964c137c1849'
+$checksumType   = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
