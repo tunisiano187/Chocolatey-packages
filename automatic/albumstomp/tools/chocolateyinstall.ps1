07086e63bc87e351a60a135d9a5456ad5c0914c6
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-10 14:53:40 +0200

    albumstomp

--- automatic/albumstomp/tools/chocolateyinstall.ps1
+++ automatic/albumstomp/tools/chocolateyinstall.ps1
@@ -1,17 +1,17 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://stompsoftware.com/downloadfiles/albumstomp/SetupAlbumStompV2.exe'
+$url                = 'https://stompsoftware.com/downloadfiles/albumstomp/SetupAlbumStompV2.exe'
+$checksum           = ''
+$checksumType       = ''
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
     fileType       = 'exe'
     url            = $url
 
     softwareName   = 'AlbumStomp*'
 
-    checksum       = '2E7DB665D7D4061BF6D32D4A43FFA61139536F2FECA358539F56B73894573989'
-    checksumType   = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+    checksum       = $checksum
+    checksumType   = $checksumType
 
     # validExitCodes= @(0, 3010, 1641)
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
