f8fb031f8540ab6726ce0f05e5c439ce3ce2a878
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-11 15:00:14 +0200

    default checksumType from template [skip ci]

--- setup/requisites.ps1
+++ setup/requisites.ps1
@@ -40,5 +40,27 @@ Copy-Item scripts/Update-Metadata.ps1 $moduleWormiesPath
 
 . .\scripts\Invoke-VirusTotalScan.ps1
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-$env:ChocolateyChecksumType = "SHA512"
+
+# Define $env:ChocolateyChecksumType from chocolateyInstall file
+choco new test --outputdirectory $env:TEMP\Chocolatey\
+$fichier = "$env:TEMP\Chocolatey\test\tools\chocolateyInstall.ps1"
+
+# Read file content
+$contenu = Get-Content $fichier -Raw
+
+# Define the regex filter
+$pattern = "checksumType\s*=\s*'([^']*)'"
+
+# Find the checksumType
+if ($contenu -match $pattern) {
+    $checksumType = $Matches[1]
+    # Set to $env:ChocolateyChecksumType
+    $env:ChocolateyChecksumType = $checksumType
+} else {
+    Write-Output "Hash type not found set it to default."
+    $env:ChocolateyChecksumType = "SHA512"
+}
+
+Write-Output "Hash type is $checksumType."
+
 RefreshEnv.cmd
