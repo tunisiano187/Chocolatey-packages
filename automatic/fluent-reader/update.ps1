5cec02a2cbb14e326451c1c27acef14d5831488e
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-11 17:53:52 +0200

    license

--- automatic/fluent-reader/update.ps1
+++ automatic/fluent-reader/update.ps1
@@ -28,6 +28,7 @@ function global:au_GetLatest {
 	$url64 = $urls | Where-Object {$_ -match "x64"}
 	$version = $tags.tag_name.Replace('v','')
 	Update-Metadata -key "releaseNotes" -value $tags.html_url
+	Update-Metadata -key "licenseUrl" -value $(Get-GithubRepositoryLicense $Owner $repo)
 	if($tags.prerelease -match "true") {
 		$date = $tags.published_at.ToString("yyyyMMdd")
 		$version = "$version-pre$($date)"
