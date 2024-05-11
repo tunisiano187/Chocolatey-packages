b0cb0656412e52b9c2c9f7906a70660db7fd7253
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-11 16:23:08 +0200

    fluent-reader [skip ci]

--- automatic/fluent-reader/ReadMe.md
+++ automatic/fluent-reader/ReadMe.md
@@ -1,8 +1,25 @@
 [![](https://img.shields.io/chocolatey/v/fluent-reader?color=green&label=fluent-reader)](https://chocolatey.org/packages/fluent-reader) [![](https://img.shields.io/chocolatey/dt/fluent-reader)](https://chocolatey.org/packages/fluent-reader)
 
-## PKGTITLE
+## Fluent Reader
 
-PKGDesc
+## Features
+
+<p align="center">
+  <img src="https://github.com/yang991178/fluent-reader/raw/master/docs/imgs/screenshot.jpg">
+</p>
+
+- A modern UI inspired by Fluent Design System with full dark mode support.
+- Read locally or sync with self-hosted services compatible with Fever or Google Reader API.
+- Sync with RSS Services including Inoreader, Feedbin, The Old Reader, BazQux Reader, and more.
+- Importing or exporting OPML files, full application data backup & restoration.
+- Read the full content with the built-in article view or load webpages by default.
+- Search for articles with regular expressions or filter by read status.
+- Organize your subscriptions with folder-like groupings.
+- Single-key [keyboard shortcuts](https://github.com/yang991178/fluent-reader/wiki/Support#keyboard-shortcuts).
+- Hide, mark as read, or star articles automatically as they arrive with regular expression rules.
+- Fetch articles in the background and send push notifications.
+
+Support for other RSS services are [under fundraising](https://github.com/yang991178/fluent-reader/issues/23).
 
 ### Package-specific issue
 If this package isn't up-to-date for some days, [Create an issue](https://github.com/tunisiano187/Chocolatey-packages/issues/new/choose)
---
+++ automatic/fluent-reader/update.ps1
@@ -0,0 +1,45 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://api.github.com/repos/yang991178/fluent-reader/releases/latest'
+$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
+$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumtype\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      	= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 	= "`$1'$($Latest.Checksum64)'"
+		}
+	}
+}
+
+function global:au_BeforeUpdate {
+	Get-RemoteFiles -Purge -NoSuffix
+	Invoke-WebRequest -Uri "https://github.com/Embarcadero/Dev-Cpp/blob/master/COPYING.txt" .\tools\LICENSE.txt
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
+	$urls = $tags.assets.browser_download_url | Where-Object {$_ -match ".exe$"}
+	$url32 = $urls | Where-Object {$_ -match "x86"}
+	$url64 = $urls | Where-Object {$_ -match "x64"}
+	$version = $tags.tag_name.Replace('v','')
+	Update-Metadata -key "releaseNotes" -value $tags.html_url
+	if($tags.prerelease -match "true") {
+		$date = $tags.published_at.ToString("yyyyMMdd")
+		$version = "$version-pre$($date)"
+	}
+
+	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	return $Latest
+}
+
+update
