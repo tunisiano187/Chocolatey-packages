e5b1aa78d6118f3a1723682a20d536902931d8e3
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-12 11:09:23 +0000

    AU: 1 updated - fluent-reader [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/21e1b739255dc0b611e61984f452b86c30c5bcf6

--- automatic/fluent-reader/tools/chocolateyinstall.ps1
+++ automatic/fluent-reader/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
