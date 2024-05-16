918c70218502b77604f023af35f80d141295a9d7
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-16 17:07:18 +0200

    anyvideoconverter

---
+++ automatic/anyvideoconverter/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'http://www.any-video-converter.com/avc-free.exe'
+
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}
+
+
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+    $url32 = $releases
+
+	$File = "$($env:TEMP)\$($releases -split '/' | Select-Object -Last 1)"
+	Invoke-WebRequest -Uri $url32 -OutFile $File
+    $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+
+    return @{ URL32 = $url32; Version = $version }
+}
+
+update -ChecksumFor 32
