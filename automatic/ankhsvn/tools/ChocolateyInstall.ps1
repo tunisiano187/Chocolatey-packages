cc7bb5340b11a1b4ee9a93e1e1757ea7260576ff
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-16 09:11:42 +0000

    Package download ankhsvn [skip ci]

---
+++ automatic/ankhsvn/tools/ChocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+	'AnkhSvn' 'msi' '/passive' `
+	'https://ankhsvn.open.collab.net/files/documents/175/11775/AnkhSvn-2.5.12703.msi'
---
+++ automatic/ankhsvn/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+﻿$package = 'AnkhSvn'
+
+try {
+  # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
+  $msiid = '{5EF9F880-EEE2-4386-8BF4-854D06955104}'
+  Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msIid /qb" -validExitCodes @(0)
+  
+  # the following is all part of error handling
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)" 
+}
