9903ddb6425d50f55a13cf832be2e028d81b04d9
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-17 08:58:33 +0000

    ps1 vars

--- automatic/drivermax/tools/chocolateyinstall.ps1
+++ automatic/drivermax/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
-$packageName  = $env:ChocolateyPackageName 
+$packageName  = $env:ChocolateyPackageName
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://www.drivermax.com/soft/dmx/drivermax.exe'
 $checksum     = '1C906D8A663FBE88EED21C8D38F8DA7F66EFA962445A14B48846EAE53F658BE4'
@@ -11,10 +11,10 @@ $packageArgs = @{
   fileType       = 'EXE'
   url            = $url
   validExitCodes = @(0,1)
-  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS' 
-  softwareName   = 'DriverMax*' 
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS'
+  softwareName   = 'DriverMax*'
   checksum       = $checksum
   checksumType   = $checksumType
 }
-  
+
 Install-ChocolateyPackage @packageArgs
