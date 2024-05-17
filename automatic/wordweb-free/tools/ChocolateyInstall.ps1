358beb09b17ab55111109ba10282e5495956f514
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-17 11:49:14 +0000

    Package download wordweb-free [skip ci]

---
+++ automatic/wordweb-free/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageName    = 'wordweb-free'
+$url            = "$toolsDir\wordweb10.exe"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = $url
+  validExitCodes = @(0)
+  silentArgs     = '-s1'
+  softwareName   = 'WordWeb'
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -force | Out-Null
+Remove-Item $toolsDir\*.ignore -force | Out-Null
+
+# UPDATE INSTRUCTIONS:
+# Update the binary with the latest version
+
