013cb8f4815241c7d812f451b780fb783307ae6d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-18 10:28:57 +0200

    x86

--- automatic/wamp-server/update.ps1
+++ automatic/wamp-server/update.ps1
@@ -21,12 +21,12 @@ function global:au_AfterUpdate($Package) {
 }
 
 function global:au_GetLatest {
-    $url32 = "$($releases)$((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'exe'}).href)[0])"
+    $url32 = "$($releases)$((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'exe'}).href) | Where-Object {$_ -match 'x86'} | Select-Object -First 1)"
     $url64 = $url32.replace('x86','x64')
     $version = Get-Version $url32
 	#$version = $url32.split('/').split('_')[-2].replace('wampserver','')
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
-choco ipgrade -y KB2919355
-update
+
+update -NoCheckChocoVersion
