40e40e018cd7a5a0349d4e8e1743ebc0dfdcbfa4
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-18 16:52:21 +0000

    ps1 vars

--- automatic/performancetest/tools/chocolateyinstall.ps1
+++ automatic/performancetest/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName    = 'performancetest'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.passmark.com/downloads/petst.exe'
@@ -6,14 +6,14 @@ $checksum       = '3D526B7DE6FEF477436C895258760B56C62645FEAE22E4AED2187BC8D4442
 
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = $toolsDir  
+  unzipLocation  = $toolsDir
   fileType       = 'EXE'
   url            = $url
   validExitCodes = @(0)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName   = 'PerformanceTest*'
   checksum       = $checksum
-  checksumType   = 'sha256' 
+  checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
