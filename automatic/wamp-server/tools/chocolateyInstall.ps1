3dafc0038059ebd694403f24194edabbfa821130
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-18 12:59:33 +0200

    64bits only

--- automatic/wamp-server/tools/chocolateyInstall.ps1
+++ automatic/wamp-server/tools/chocolateyInstall.ps1
@@ -2,11 +2,8 @@
 $packageName      = $env:ChocolateyPackageName
 $installerType    = 'EXE'
 $url32            = 'http://wampserver.aviatechno.net/files/install/wampserver3.3.5_x64.exe'
-$url64            = 'http://wampserver.aviatechno.net/files/install/wampserver3.3.5_x64.exe'
 $checksum32Type   = 'sha256'
-$checksum64Type   = 'sha256'
 $checksum32       = '644f35f0ff3b9bfc7f7e4ec2389474875f3a3fc773bb18c9d85477b7ad05b484'
-$checksum64       = '644f35f0ff3b9bfc7f7e4ec2389474875f3a3fc773bb18c9d85477b7ad05b484'
 $silentArgs       = '/DIR="C:\wamp" /VERYSILENT /SUPPRESSMSGBOXES'
 $validExitCodes   = @(0)
 
@@ -14,14 +11,11 @@ $packageArgs = @{
   packageName     = $packageName
   fileType        = $installerType
   url             = $url32
-  url64			      = $url64
   silentArgs      = $silentArgs
   validExitCodes  = $validExitCodes
   softwareName    = 'wamp-server*'
   checksum        = $checksum32
-  checksum64      = $checksum64
   checksumType    = $checksum32Type
-  checksum64Type  = $checksum64Type
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/wamp-server/update.ps1
+++ automatic/wamp-server/update.ps1
@@ -1,16 +1,13 @@
 $ErrorActionPreference = 'Stop'
 import-module au
 
-$releases = "http://wampserver.aviatechno.net/"
+$releases = "https://wampserver.aviatechno.net/"
 
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
             "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
-            "(^[$]checksum32Type\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
 			"(^[$]checksum64Type\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
      }
@@ -21,12 +18,13 @@ function global:au_AfterUpdate($Package) {
 }
 
 function global:au_GetLatest {
-    $url32 = "$($releases)$((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'exe'}).href) | Where-Object {$_ -match 'x86'} | Select-Object -First 1)"
-    $url64 = $url32.replace('x86','x64')
-    $version = Get-Version $url32
-	#$version = $url32.split('/').split('_')[-2].replace('wampserver','')
+    $url32 = "$($releases)$((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match 'exe'}).href) | Where-Object {$_ -match 'x64'} | Select-Object -First 1)"
+    if ($url32 -match "(\d+\.\d+\.\d+)_x64\.exe") {
+        $version = $matches[1]
+    }
+
 	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update -NoCheckChocoVersion
+update -ChecksumFor 32 -NoCheckChocoVersion
