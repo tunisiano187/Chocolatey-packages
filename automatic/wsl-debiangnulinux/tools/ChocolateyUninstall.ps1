9c1dc50e1bb830bf133880f908bed7d1295d8930
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-19 18:39:26 +0000

    Exclude wsl-debiangnulinux [skip ci]

--- automatic/wsl-debiangnulinux/tools/ChocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$packageName    = 'wsl-debiangnulinux'
-$shortcutName   = 'Debian.lnk'
-
-wslconfig /unregister Debian
-wslconfig /list
-
-Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
-Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
