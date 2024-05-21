af091b823357af0400fbfd106aaff79ed0d00a2c
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-21 12:10:39 +0000

    AU: 1 updated - performancetest [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/eb47e7671d429ff267a80f0b24beee5cc79b24a4

--- automatic/performancetest/tools/chocolateyinstall.ps1
+++ automatic/performancetest/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'performancetest'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.passmark.com/downloads/petst.exe'
-$checksum       = '3D526B7DE6FEF477436C895258760B56C62645FEAE22E4AED2187BC8D44425C3'
+$checksum       = '22b5fd188eb93fe8ebc3abedb541388cb6e16fc3b3f5ca7f1f8d27bfd46a9a81'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
