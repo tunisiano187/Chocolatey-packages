ed3e7e44156d004a719e4653a8152f2255ca3086
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-21 14:37:34 +0000

    Package download windows-iso-downloader [skip ci]

---
+++ automatic/windows-iso-downloader/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'windows-iso-downloader' 
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://www.heidoc.net/php/Windows%20ISO%20Downloader.exe'
+$checksum      = 'D9E52B0DBB5D3540016497C913E4186433C2ECD14E359EB841D5D93C8B021AEF'
+$ExeFile       = 'Windows ISO Downloader.exe'
+$ShortcutName  = 'Microsoft Windows and Office ISO Download Tool.lnk'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  FileFullPath  = "$toolsDir\$ExeFile"
+  softwareName  = ''
+  }
+
+Get-ChocolateyWebFile @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$ShortcutName" -targetPath "$toolsDir\$ExeFile" -WorkingDirectory "$toolsDir\$packageName"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName" -targetPath "$toolsDir\$ExeFile" -WorkingDirectory "$toolsDir\$packageName"
