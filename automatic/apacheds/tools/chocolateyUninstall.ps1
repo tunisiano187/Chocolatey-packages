2cbdcf7bf387c2a7964a07d8c64a64c6ff68bb3d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-24 17:26:56 +0000

    Exclude apacheds [skip ci]

--- automatic/apacheds/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = 'apacheds'
-
-if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-. "$PSScriptRoot\Install-ChocolateyService030.ps1"
-. "$PSScriptRoot\Uninstall-ChocolateyZipPackage030.ps1"
-
-Uninstall-ChocolateyService030 "$packageName"
-
-Uninstall-ChocolateyZipPackage030 "$packageName"
