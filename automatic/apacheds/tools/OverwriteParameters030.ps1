fff977df5fc3e65cdb10d19d7508d143bbb81788
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-24 16:55:54 +0000

    ps1 vars

--- automatic/apacheds/tools/OverwriteParameters030.ps1
+++ automatic/apacheds/tools/OverwriteParameters030.ps1
@@ -6,19 +6,19 @@ function OverwriteParameters030 {
 		$MATCH_PATTERN = "/([a-zA-Z]+)=([`"'])?(.*)([`"'])?"
 		$PARAMATER_NAME_INDEX = 1
 		$VALUE_INDEX = 3
-		
+
 		if($packageParameters -match $MATCH_PATTERN){
-			$results = $packageParameters | Select-String $MATCH_PATTERN -AllMatches 
-			$results.matches | % { 
+			$results = $packageParameters | Select-String $MATCH_PATTERN -AllMatches
+			$results.matches | ForEach-Object {
 			$arguments.Add(
 				$_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
-				$_.Groups[$VALUE_INDEX].Value.Trim()) 
+				$_.Groups[$VALUE_INDEX].Value.Trim())
 			}
-		}     
-		
+		}
+
 		if($arguments.ContainsKey("InstallLocation")) {
 			$global:installLocation = $arguments["InstallLocation"];
 			Write-Host "Value variable installLocation changed to $global:installLocation"
 		}
-	}	
+	}
 }
--- automatic/apacheds/tools/Uninstall-ChocolateyZipPackage030.ps1
+++ automatic/apacheds/tools/Uninstall-ChocolateyZipPackage030.ps1
@@ -6,8 +6,8 @@ param(
 		Write-ChocolateyFailure "Uninstall-ChocolateyZipPackage030" "Missing PackageName input parameter."
 		return
 	}
-	
-	ChildItem "$env:ChocolateyInstall\lib\${packageName}.*" -Recurse -Filter "${packageName}Install.zip.txt" | 
+
+	ChildItem "$env:ChocolateyInstall\lib\${packageName}.*" -Recurse -Filter "${packageName}Install.zip.txt" |
 	ForEach-Object{ $installLocation = (Get-Content $_.FullName | Select-Object -First 1);
 		if (("$installLocation" -match "${packageName}|adt-bundle-windows-x86_64") -and (Test-Path -Path "$installLocation")) {
 			Write-Host "Uninstalling by removing directory $installLocation";
