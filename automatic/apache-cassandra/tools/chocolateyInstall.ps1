dcb74d93fc1b1e4c84903909c7c4e57b06c8e647
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-25 10:07:06 +0000

    AU: 1 updated - apache-cassandra [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/84882e3c3ee17960ecb764da1a0e1fa66b8ebf01

--- automatic/apache-cassandra/tools/chocolateyInstall.ps1
+++ automatic/apache-cassandra/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'Cassandra'
-$url            = 'http://apache.mirror.anlx.net/cassandra/3.7/apache-cassandra-3.7-bin.tar.gz'
-$checksum       = ''
-$checksumType   = ''
+$url            = 'https://dlcdn.apache.org/cassandra/4.1.5/apache-cassandra-4.1.5-bin.tar.gz'
+$checksum       = 'ed184c361482e8b34f75537a1ac83755286313d6dcb0e11293813fb8ce4afbf5'
+$checksumType   = 'sha256'
 $installDir     = (Get-ToolsLocation +"/$packageName")
 
 $packageArgs = @{
