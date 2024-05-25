2164b3534e2341ce26fd20f2602ee2f0386ec9aa
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-25 13:56:06 +0200

    Force

--- automatic/apache-cassandra/tools/chocolateyInstall.ps1
+++ automatic/apache-cassandra/tools/chocolateyInstall.ps1
@@ -3,14 +3,15 @@ $url            = 'https://dlcdn.apache.org/cassandra/4.1.5/apache-cassandra-4.1
 $checksum       = 'ed184c361482e8b34f75537a1ac83755286313d6dcb0e11293813fb8ce4afbf5'
 $checksumType   = 'sha256'
 $installDir     = (Get-ToolsLocation +"/$packageName")
+$unzipFolder    = Join-Path $env:TEMP "chocolatey\$packageName"
 
 $packageArgs = @{
     packageName     = $env:ChocolateyPackageName
     url             = $url
     checksum        = $checksum
     checksumType    = $checksumType
-    unzipLocation   = (Join-Path $env:chocolateyPackageFolder tmp)
+    unzipLocation   = $unzipFolder
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder tmp) -destination "$installDir"
+Get-ChocolateyUnzip -fileFullPath $unzipFolder -destination "$installDir"
