5fd9278f5b35ef3d1290608ae19593bcd6fd4f2b
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-28 13:47:20 +0200

    Force

--- automatic/faceit/update.ps1
+++ automatic/faceit/update.ps1
@@ -21,9 +21,10 @@ function global:au_GetLatest {
 	$File = Join-Path $env:TEMP "FACEIT-setup-latest.exe"
 	Invoke-WebRequest -Uri $release -OutFile $File
 	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+	$checksum = Get-FileHash -Path $File -Algorithm $env:ChocolateyChecksumType
 
-	$Latest = @{ URL32 = $release; Version = $version }
+	$Latest = @{ URL32 = $release; Checksum32 = $checksum; ChecksumType32 = $env:ChocolateyChecksumType; Version = $version }
 	return $Latest
 }
 
-update -ChecksumFor 32 -NoCheckChocoVersion
+update -ChecksumFor none -NoCheckChocoVersion
