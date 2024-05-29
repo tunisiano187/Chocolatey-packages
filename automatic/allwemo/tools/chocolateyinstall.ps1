408eb28c3d9fe1e207a96eab97a84bceafe38ab3
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-29 16:50:29 +0000

    Package download allwemo [skip ci]

---
+++ automatic/allwemo/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'allwemo'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'MSI'
+  file           = "$toolsDir\AllWemoSetup.msi"
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/quiet /qn /norestart'
+  softwareName   = 'AllWemo*'
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item "$toolsDir\*.msi" -EA SilentlyContinue | Out-Null
