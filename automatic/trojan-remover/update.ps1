057c078ba22f04c9bc5cd4aff975852bcba57d02
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-29 18:34:51 +0200

    trojan-remover #3636

---
+++ automatic/trojan-remover/update.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://www.simplysup.com/tremover/download_full.html'
+
+function global:au_SearchReplace {
+	@{
+		"tools\VERIFICATION.txt" = @{
+			"(url32\s+:).*"  			= "`${1} $($Latest.URL32)"
+			"(checksum32\s+:).*" 		= "`${1} $($Latest.Checksum32)"
+			"(checksumtype\s+:).*" 		= "`${1} $($Latest.ChecksumType32)"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$page = Invoke-WebRequest -Uri $releases
+	$url = $page.Links.href | Where-Object {$_ -match "exe$"} | Select-Object -First 1
+	$File = Join-Path $PSScriptRoot "tools\trjsetup.exe"
+	
+	Invoke-WebRequest -Uri $url -OutFile $File
+	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion
+
+	$Latest = @{ URL32 = $url; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor 32 -NoCheckChocoVersion
