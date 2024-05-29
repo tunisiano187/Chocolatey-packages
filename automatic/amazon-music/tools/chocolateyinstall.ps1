95e720cf39131be041c38c802b6e92b9c20290a8
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-29 00:16:49 +0000

    ps1 vars

--- automatic/amazon-music/tools/chocolateyinstall.ps1
+++ automatic/amazon-music/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName  = $env:ChocolateyPackageName
-$url          = 'https://amazon.com/images/G/01/digital/music/morpho/installers/20200905/0031046272/AmazonMusicInstaller.exe' 
+$url          = 'https://amazon.com/images/G/01/digital/music/morpho/installers/20200905/0031046272/AmazonMusicInstaller.exe'
 $checksum     = '73A2946C961D01B23FCE6BFF0FA26B71D47F00CFF32B7C7BB2BED3ABD72AA2E4'
 $checksumType = 'sha256'
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = 'EXE' 
+  fileType      = 'EXE'
   url           = $url
-  softwareName  = 'Amazon Music' 
+  softwareName  = 'Amazon Music'
   checksum      = $checksum
   checksumType  = $checksumType
   silentArgs    = '--unattendedmodeui none'
@@ -17,4 +17,4 @@ $packageArgs = @{
 
 Start-WaitandStop "Amazon Music Helper"
 Start-WaitandStop "Amazon Music"
-Install-ChocolateyPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
