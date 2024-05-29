0ad25dc5fa6ea0b5231c98c72149026a2389fab5
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-29 18:31:44 +0000

    Package download ad-acl-scanner [skip ci]

---
+++ automatic/ad-acl-scanner/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'ad-acl-scanner' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName     = 'ADACLScan.ps1'
+$shortcutName = 'AD ACL Scanner'
+$FileFullpath = "$toolsDir\$fileName"
+
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:USERPROFILE\Desktop\$shortcutName.lnk" -targetPath $FileFullpath -WorkingDirectory "$toolsDir"
+
---
+++ automatic/ad-acl-scanner/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'ad-acl-scanner'
+$shortcutName = 'AD ACL Scanner' 
+
+Remove-Item "$ENV:USERPROFILE\Desktop\$shortcutName.lnk" -Force -ErrorAction SilentlyContinue
