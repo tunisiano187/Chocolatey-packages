bf34f0ad7879516dd81e9d587e59b2d22b57f114
Tunisiano187 <helpdesk.choc@gmail.com>
2024-05-30 08:21:35 +0000

    Package download captureflux [skip ci]

---
+++ automatic/captureflux/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$packageName  = 'captureflux' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://paul.glagla.free.fr/fichiers/captureflux_60en.zip'
+$checksum     = '5CBD61413D72F15446B980F7A8AE5AC0D572A93BD3C103F11CC78032DC32F49C'
+$shortcutName = 'CaptureFlux.lnk'
+$exe          = 'CaptureFlux_60en.exe'
+
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  url64          = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe"       
+
