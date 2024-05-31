060aad6f8052720196fd86dd70233936ec9e59fe
tunisiano187 <helpdesk.choc@gmail.com>
2024-05-31 00:34:07 +0000

    AU: 3 updated - amazon-music odrive trojan-remover [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/a3993570f3b640221188f3da010f6c45ced8438c

--- automatic/odrive/tools/chocolateyinstall.ps1
+++ automatic/odrive/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $url			= 'https://www.odrive.com/downloaddesktop?platform=win'
-$checksum		= '8e1ab1abc01ad29fd4162473fb9febcc0fe1f2773ac4ea457279b9ee9f820fe1'
+$checksum		= 'cce427a219343cc890b65dd7aafc08d06961099df3ee5a855656c1f920d5ec1e'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
