b03e0d90062d6619df775d521132553e63507047
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-01 00:15:57 +0000

    ps1 vars

--- automatic/vmwarevsphereclient/tools/ChocolateyInstall.ps1
+++ automatic/vmwarevsphereclient/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 # Download URLs for VMware vSphere Client (2089791)
 # https://kb.vmware.com/s/article/2089791
-$packageName    = 'vmwarevsphereclient' 
+$packageName    = 'vmwarevsphereclient'
 $url            = 'https://vsphereclient.vmware.com/vsphereclient/9/1/0/3/8/9/1/VMware-viclient-all-6.0.0-9103891.exe'
 $checksum       = 'D37203F2344E44016501FB269E92D5FAC577B11FF319C3C653ED5D762F82DE64'
-$silentArgs     = '/s /w /v" /qb"' 
-$validExitCodes = @(0) 
+$silentArgs     = '/s /w /v" /qb"'
+$validExitCodes = @(0)
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,8 +14,8 @@ $packageArgs = @{
   validExitCodes= $validExitCodes
   silentArgs    = $silentArgs
   softwareName  = 'VMware vSphere Client 6*'
-  checksum      = $checksum 
-  checksumType  = 'sha256' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
