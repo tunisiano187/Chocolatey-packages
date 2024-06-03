f1ff481d646322cde65d63c212b12b9f045d1fa8
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-03 09:36:07 +0000

    Package download amazon-photos [skip ci]

---
+++ automatic/amazon-photos/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'amazon-photos'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://d29x207vrinatv.cloudfront.net/win/AmazonPhotosSetup.exe'
+$checksum      = '47B668F665973B0B4E85B66D389C4204CD26A646ADCE615DED79C5C35713A047'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = "-quiet"
+  softwareName   = 'Amazon Photos'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+Start-Sleep -s 5
+Start-WaitandStop "amazonphotos"
