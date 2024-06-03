b457c525eb5d96e81e2d159c7c435ccde226d32d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-03 11:32:30 +0200

    update not required

--- automatic/beebeep/tools/chocolateyuninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'beebeep'
-$ShortcutName = 'beeBEEP'
-
-Remove-Item "$ENV:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
