fef5934e597e102227e1c7e065be2251e879cf43
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-08 13:53:58 +0000

    Package download getdataback-simple [skip ci]

---
+++ automatic/getdataback-simple/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'getdataback-simple'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://www.runtime.org/gdbprosetup.exe'
+$checksum       = '4B894A3905EAAADD46BB9B5E7668AFAAC21F4B8573FEBB6225F52EE3E1B40D0A'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir  
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName   = 'GetDataBack*'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs
