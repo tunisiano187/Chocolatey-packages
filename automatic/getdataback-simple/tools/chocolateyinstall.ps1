7eee0a8e1918c38e7e574d062eb52aa1c2e0660f
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-08 16:49:42 +0200

    getdata

--- automatic/getdataback-simple/tools/chocolateyinstall.ps1
+++ automatic/getdataback-simple/tools/chocolateyinstall.ps1
@@ -3,6 +3,7 @@ $packageName    = 'getdataback-simple'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.runtime.org/gdbprosetup.exe'
 $checksum       = '4B894A3905EAAADD46BB9B5E7668AFAAC21F4B8573FEBB6225F52EE3E1B40D0A'
+$checksumType   = ''
 
 $packageArgs = @{
   packageName    = $packageName
@@ -13,7 +14,7 @@ $packageArgs = @{
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName   = 'GetDataBack*'
   checksum       = $checksum
-  checksumType   = 'sha256' 
+  checksumType   = $checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
