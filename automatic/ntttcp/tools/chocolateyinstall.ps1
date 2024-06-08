e6dc9c224062ebcbe689267e393fb7b663d32c5a
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-08 20:24:19 +0200

    ntttcp

--- automatic/ntttcp/tools/chocolateyinstall.ps1
+++ automatic/ntttcp/tools/chocolateyinstall.ps1
@@ -1,17 +1,17 @@
-$packageName = 'ntttcp'
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://gallery.technet.microsoft.com/NTttcp-Version-528-Now-f8b12769/file/159655/1/NTttcp-v5.33.zip'
-$checksum    = '090FE61913513BD55A56E59354E50B0B1DDAB17721446345223D05DEDE4B8F0F'
+$packageName  = 'ntttcp' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://gallery.technet.microsoft.com/NTttcp-Version-528-Now-f8b12769/file/159655/1/NTttcp-v5.33.zip'
+$checksum     = '090FE61913513BD55A56E59354E50B0B1DDAB17721446345223D05DEDE4B8F0F'
+$checksumType = ''
+$file         = Join-Path $toolsDir $($url.Split('/')[-1])
 
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP'
+  FileFullPath  = $file
   url           = $url
   checksum      = $checksum
-  checksumType  = 'sha256'
+  checksumType  = $checksumType
 }
 
-Install-ChocolateyZipPackage @packageArgs
-
+Get-ChocolateyWebFile  @packageArgs
