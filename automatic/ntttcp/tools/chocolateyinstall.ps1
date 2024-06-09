bb35c22d7ce4df6252eeeaeca66386ff5f4ee190
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-09 00:15:15 +0000

    ps1 vars

--- automatic/ntttcp/tools/chocolateyinstall.ps1
+++ automatic/ntttcp/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$packageName  = 'ntttcp' 
+$packageName  = 'ntttcp'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://gallery.technet.microsoft.com/NTttcp-Version-528-Now-f8b12769/file/159655/1/NTttcp-v5.33.zip'
 $checksum     = '090FE61913513BD55A56E59354E50B0B1DDAB17721446345223D05DEDE4B8F0F'
