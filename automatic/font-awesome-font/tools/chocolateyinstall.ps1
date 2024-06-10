be05af43eaa70435e26d91a1c12bc64b9b7905b4
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-10 09:13:43 +0000

    ps1 vars

--- automatic/font-awesome-font/tools/chocolateyinstall.ps1
+++ automatic/font-awesome-font/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName = 'font-awesome-font'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = "$toolsDir\fontawesome-free-$ENV:ChocolateyPackageVersion-desktop.zip"
@@ -9,12 +9,12 @@ $packageArgs = @{
   fileType      = 'ZIP'
   url           = $url
   }
-  
+
 Install-ChocolateyZIPPackage @packageArgs
- 
+
 Install-ChocolateyFont "$toolsDir\fontawesome-free-$ENV:ChocolateyPackageVersion-desktop\otfs\Font Awesome 5 Brands-Regular-400.otf"
 Install-ChocolateyFont "$toolsDir\fontawesome-free-$ENV:ChocolateyPackageVersion-desktop\otfs\Font Awesome 5 Free-Regular-400.otf"
 Install-ChocolateyFont "$toolsDir\fontawesome-free-$ENV:ChocolateyPackageVersion-desktop\otfs\Font Awesome 5 Free-Solid-900.otf"
- 
+
 Remove-Item "$toolsDir\fontawesome-free-$ENV:ChocolateyPackageVersion-desktop" -Recurse -ErrorAction SilentlyContinue | Out-Null
 Remove-Item "$toolsDir\*.zip" -ErrorAction SilentlyContinue | Out-Null
