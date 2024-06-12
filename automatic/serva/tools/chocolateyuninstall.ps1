f93eaa5325f5b64415a2f5f0a6c3526d4588571d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-12 18:18:57 +0000

    Exclude serva [skip ci]

--- automatic/serva/tools/chocolateyuninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName = 'serva'
-
-remove-item "$env:Public\Desktop\Serva.lnk" -Force -ErrorAction SilentlyContinue
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Serva.lnk" -Force -ErrorAction SilentlyContinue
-
