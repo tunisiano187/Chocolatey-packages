d690d00daa3fed8d2228641ab3f52efb5411f0a2
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-16 14:14:15 +0000

    ps1 vars

--- automatic/warzone2100/tools/chocolateyinstall.ps1
+++ automatic/warzone2100/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName  = 'warzone2100'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32        = 'https://downloads.sourceforge.net/project/warzone2100/releases/4.1.3/warzone2100_win_x86_installer.exe' 
+$url32        = 'https://downloads.sourceforge.net/project/warzone2100/releases/4.1.3/warzone2100_win_x86_installer.exe'
 $checksum32   = '1AC853090FF513ED568EFBC3A2337E1D75619DAF75BBC9EF5625B57EC5549F4E'
-$url64        = 'https://downloads.sourceforge.net/project/warzone2100/releases/4.1.3/warzone2100_win_x64_installer.exe' 
+$url64        = 'https://downloads.sourceforge.net/project/warzone2100/releases/4.1.3/warzone2100_win_x64_installer.exe'
 $checksum64   = '7898FCEFA2A77EA5ABE6C2FCA218505BCCB90A49151E9C727DB0821F980983B0'
 $checksumType = 'sha256'
 
