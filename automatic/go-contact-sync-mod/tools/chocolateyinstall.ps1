e8cfc659a9a07a2f2e1a2467d0a6b37305077663
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-16 12:44:01 +0000

    Package download go-contact-sync-mod [skip ci]

---
+++ automatic/go-contact-sync-mod/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿# https://sourceforge.net/projects/googlesyncmod/files/
+$ErrorActionPreference = 'Stop'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = 'go-contact-sync-mod'
+  fileType       = 'MSI'
+  file           = "$toolsDir\SetupGCSM-$ENV:ChocolateyPackageVersion.msi"
+  silentArgs     = '/quiet /qn /norestart'
+  softwareName   = 'Go Contact Sync Mod*' 
+  validExitCodes = @(0, 3010, 1641)
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.msi -Force | Out-Null
