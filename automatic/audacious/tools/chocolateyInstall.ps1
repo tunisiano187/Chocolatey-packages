94f121e06057f84c23afeabfb1924f5c1146b10d
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-19 18:43:24 +0000

    AU: 2 updated - audacious vhdattach [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/ae55444fd5965f0a45b75b9d34c8b64fe32b16f8

--- automatic/audacious/tools/chocolateyInstall.ps1
+++ automatic/audacious/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://distfiles.audacious-media-player.org/audacious-4.4-beta1-win32.exe'
-$checksum = 'e26e4fb2eb7a9ca1d539b51f43ec384df477359f49d0f676c6ca1de3921f8a9f'
+$url = 'https://distfiles.audacious-media-player.org/audacious-4.4-win32.exe'
+$checksum = '1b28b177a191155182d6b0fa9754e0c5853923fd59a677fe2c82c1e81a784251'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
