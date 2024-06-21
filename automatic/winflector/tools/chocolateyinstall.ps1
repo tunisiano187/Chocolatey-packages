443e886e85727ef23df1e51c94483c62abde84a3
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-21 16:15:41 +0000

    ps1 vars

--- automatic/winflector/tools/chocolateyinstall.ps1
+++ automatic/winflector/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName    = 'winflector'
 $softwareName   = 'Winflector*'
 $installerType  = 'EXE'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'http://www.winflector.com/store/free-version/index/id/395' 
+$url            = 'http://www.winflector.com/store/free-version/index/id/395'
 $checksum       = 'B1BD04CEE4670BB70948622BDE467DFC1D51FDBFE89D0BA9C5E501870055C293'
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0, 3010, 1641)
@@ -20,7 +20,7 @@ $packageArgs = @{
   silentArgs    = $silentArgs
   softwareName  = $softwareName
   checksum      = $checksum
-  checksumType  = 'sha256' 
+  checksumType  = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs  
+Install-ChocolateyPackage @packageArgs
