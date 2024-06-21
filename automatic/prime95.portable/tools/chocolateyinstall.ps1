3e576e2d13745bec7829b216a79696ca9e5a0073
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-21 09:41:11 +0000

    Package download prime95.portable [skip ci]

---
+++ automatic/prime95.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'prime95.portable' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://www.mersenne.org/ftp_root/gimps/p95v303b6.win32.zip'
+$checksum     = 'CA712CEB3C3A9642ACB6C6A45B9F3164A1F0AAEC72DAB4E47083AB07916B03F4'
+$url64        = 'http://www.mersenne.org/ftp_root/gimps/p95v303b6.win64.zip'
+$checksum64   = '533EDA6A585CD047B3D41FBA85BD52798AD6F4D8572B25A5F01FFDAAE11509DA'
+$ExeFile      = 'prime95.exe'
+$ShortcutName = 'Prime95'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  url64         = $url64
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+  checksum64    = $checksum64
+  checksumType64= 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$ShortcutName.lnk" -targetPath "$env:ChocolateyInstall\lib\$packageName\tools\$ExeFile"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$toolsDir\$ExeFile" -WorkingDirectory "$toolsDir"
