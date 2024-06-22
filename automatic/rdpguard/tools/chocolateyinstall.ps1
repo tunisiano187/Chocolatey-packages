dd42c8bf667053946f02b47d78e135e971a96343
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-22 16:32:23 +0000

    AU: 1 updated - rdpguard [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/c9839f98fba4e1b0b9cc12bbe9b4aecd654b1f8e

--- automatic/rdpguard/tools/chocolateyinstall.ps1
+++ automatic/rdpguard/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'rdpguard'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://rdpguard.com/download/rdpguard-9-7-1.exe'
-$checksum       = 'b8a619ac524f4883d499f2a31724524cbd548938babb2c066cb2d7bfb24fe371'
+$url            = 'https://rdpguard.com/download/rdpguard-9-7-3.exe'
+$checksum       = '3f6c5c49b56e6f634999be60aef8776ab5d8055ceb52ce7cd7d876d497a91f14'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
