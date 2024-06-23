64e2652339e7576f1c1d8727c7e409b02001d487
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-23 19:09:38 +0200

    closes (tageditor) update requested #3723

--- automatic/tageditor/tools/ChocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'tageditor' 
-$shortcutName = 'Tag Editor.lnk'
-
-Uninstall-BinFile tageditor
-Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
-Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
