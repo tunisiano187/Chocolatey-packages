b6f131ac8a481088ddf3680dd69848b6c5b8adea
Tunisiano187 <helpdesk.choc@gmail.com>
2024-06-26 12:31:35 +0000

    Package download xmedia-recode [skip ci]

---
+++ automatic/xmedia-recode/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$TodaysVersion = ($ENV:ChocolateyPackageVersion -replace '[.]','')
+ 
+$packageArgs = @{
+  packageName    = "$ENV:ChocolateyPackageName"
+  fileType       = 'EXE'
+  file           = "$toolsDir\XMediaRecode"+$TodaysVersion+"_setup.exe"
+  file64         = "$toolsDir\XMediaRecode"+$TodaysVersion+"_x64_setup.exe"
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item "$toolsDir\*.exe" -ErrorAction SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS:
+# Replace the .EXE files
