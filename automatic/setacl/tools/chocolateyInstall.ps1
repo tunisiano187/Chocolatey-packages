6c867335aa45446a8a46daf563e93d5604dade0c
tunisiano187 <helpdesk.choc@gmail.com>
2024-06-29 00:16:01 +0000

    ps1 vars

--- automatic/setacl/tools/chocolateyInstall.ps1
+++ automatic/setacl/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'setacl' 
+$ErrorActionPreference = 'Stop'
+$packageName    = 'setacl'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://helgeklein.com/downloads/SetACL/current/SetACL%203.1.2%20(executable%20version).zip'
 $checksum       = 'BA74399A70963C156580180455FBFC0FA68EA673A64EB89010A46273C7D478CC'
@@ -7,7 +7,7 @@ $checksum       = 'BA74399A70963C156580180455FBFC0FA68EA673A64EB89010A46273C7D47
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
+  fileType      = 'ZIP'
   url           = $url
   checksum      = $checksum
   checksumType  = 'sha256'
