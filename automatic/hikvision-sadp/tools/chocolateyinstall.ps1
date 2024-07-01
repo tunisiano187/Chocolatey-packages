988cc46bbf12471d7699e7f8e3d08a10b19ba32e
tunisiano187 <helpdesk.choc@gmail.com>
2024-07-01 00:15:31 +0000

    ps1 vars

--- automatic/hikvision-sadp/tools/chocolateyinstall.ps1
+++ automatic/hikvision-sadp/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'hikvision-sadp' 
+$ErrorActionPreference = 'Stop'
+$packageName    = 'hikvision-sadp'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://oversea-download.hikvision.com//uploadfile/Soft/Tools/SADP/for%20windows/V3.0.1.7/SADPTool.zip'
 $checksum       = '0FD9185E2E12B1C696D1A9D4818F974CF82A5FE0A6C721C808A27AA74EF52223'
@@ -7,13 +7,13 @@ $checksum       = '0FD9185E2E12B1C696D1A9D4818F974CF82A5FE0A6C721C808A27AA74EF52
 $packageArgs = @{
   packageName   = 'sadp'
   unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
+  fileType      = 'ZIP'
   url           = $url
   checksum      = $checksum
   checksumType  = 'sha256'
 }
 
-Install-ChocolateyZipPackage @packageArgs 
+Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
   packageName    = $packageName
@@ -23,7 +23,7 @@ $packageArgs = @{
   validExitCodes = @(0)
   softwareName   = 'SADPTool'
 }
- 
+
 Install-ChocolateyInstallPackage @packageArgs
 
 Remove-Item "$toolsDir\SADPTool.*" -Force -EA SilentlyContinue | Out-Null
