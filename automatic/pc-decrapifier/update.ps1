c7b2e3d9c6ac3e844195270048971efead10597e
Fabian Clerbois <tunisiano187@gmail.com>
2024-07-02 09:46:47 +0200

    pc-decrapifier

---
+++ automatic/pc-decrapifier/update.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_GetLatest {
+	$version = "3.0.1.20240702"
+
+	$Latest = @{ URL32 = 'https://www.bowlman.org'; Version = $version}
+
+    return $Latest
+}
+
+update -ChecksumFor none
