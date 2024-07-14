fc4f03558c53b74ede2a2dbdb1dc506ec1a91161
tunisiano187 <helpdesk.choc@gmail.com>
2024-07-14 11:22:17 +0000

    AU: 2 updated - prime95.portable vhdattach [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/16264f1590aabb0a0390a0d7429411f53f8a2347

--- automatic/prime95.portable/tools/chocolateyinstall.ps1
+++ automatic/prime95.portable/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'prime95.portable'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.mersenne.org/download/software/v30/30.19/p95v3019b20.win32.zip'
+$url          = 'https://download.mersenne.ca/gimps/v30/30.19/p95v3019b20.win32.zip'
 $checksum     = 'a77c9297bd80dffcb7e2e7e6e89db674126c06a2fe8e769dcec06da2e66e8516'
 $checksumType = 'sha256'
-$url64        = 'https://www.mersenne.org/download/software/v30/30.19/p95v3019b20.win64.zip'
+$url64        = 'https://download.mersenne.ca/gimps/v30/30.19/p95v3019b20.win64.zip'
 $checksum64   = 'd9475f2ff3f4a6a701abc49a86a66126cb48abd10bda6fa87039d98fa8756bca'
 $ExeFile      = 'prime95.exe'
 $ShortcutName = 'Prime95'
