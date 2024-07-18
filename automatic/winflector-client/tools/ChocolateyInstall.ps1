b54c6a0e2074f059eb7df28be68c865f5b25bbf8
tunisiano187 <helpdesk.choc@gmail.com>
2024-07-18 08:27:11 +0000

    AU: 1 updated - winflector-client [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/e4c075873c07e220b4dac3dc423410bfd4a013ef

--- automatic/winflector-client/tools/ChocolateyInstall.ps1
+++ automatic/winflector-client/tools/ChocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $softwareName   = 'Windows Driver Package - Era software*'
 $installerType  = 'EXE'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://www.winflector.com/store/free-version/index/id/521'
-$checksum       = '58fb9a7d216418438d6a6bfa727b14ec04f0e2691110d7905832eaacf33ee6d0'
+$url            = 'https://www.winflector.com/store/free-version/index/id/530'
+$checksum       = 'b8662d806c1935dd7d8e4abd4de679dd0df67c23a3464f4a405475dba9fbd7e6'
 $checksumType   = 'sha256'
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0, 3010, 1641)
