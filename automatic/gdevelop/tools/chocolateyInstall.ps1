1721875bb9bb6b5c9ef90d5f1f907002dba1a57e
Tunisiano187 <helpdesk.choc@gmail.com>
2024-07-18 16:20:08 +0000

    Package download gdevelop [skip ci]

---
+++ automatic/gdevelop/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName        = $env:ChocolateyPackageName
+$installerType      = 'exe'
+$silentArgs         = '/S'
+$url                = 'https://github.com/4ian/GDevelop/releases/download/v5.4.205/GDevelop-5-Setup-5.4.205.exe'
+$checksum           = '4f1fc71fcf031ef13bfa5a2b79845a254dc8966851b69eb847a87f130dcac255'
+$checksumType       = 'sha256'
+$validExitCodes     = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
