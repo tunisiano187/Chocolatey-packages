d7d6c192c2ef5c5f2de3ec9240757acdc9336e1d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-08-02 19:31:06 +0200

    url64

--- automatic/digikam/update.ps1
+++ automatic/digikam/update.ps1
@@ -21,6 +21,7 @@ function global:au_GetLatest {
 	Write-Output 'Check Folder'
 	$version = ((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object  {$_.href -match '^\d+([.]\d+)?'} | ForEach-Object {($_.href -replace '[^.\d]', '')} | Measure-Object -Max).Maximum
 	$url64 = ((Invoke-WebRequest -Uri "$($releases)$($version)/").Links | Where-Object {$_.href -match 'Qt6-Win64.exe'}).href | Select-Object -First 1
+	$url64 = "$($releases)$($version)/$($url64)"
 
 	$Latest = @{ URL64 = $url64; Version = $version }
 	return $Latest
