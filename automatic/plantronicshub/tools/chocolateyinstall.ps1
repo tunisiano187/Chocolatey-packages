e82f155398f60a0f23112b784317430677b3cfd4
Tunisiano187 <helpdesk.choc@gmail.com>
2024-08-25 15:59:43 +0000

    Package download plantronicshub [skip ci]

---
+++ automatic/plantronicshub/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+
+  url          = 'https://www.poly.com/content/dam/www/software/PlantronicsHubInstaller.exe'
+  checksum     = 'e43484d770f3da729d8ba090604ad971a3db201ec7dfa80ad21cfe3cd3b38a52'
+  checksumType = 'sha256'
+
+  silentArgs   = '/install /quiet /norestart'
+}
+
+Install-ChocolateyPackage @packageArgs
