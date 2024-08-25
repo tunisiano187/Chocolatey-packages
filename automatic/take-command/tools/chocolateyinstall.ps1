b0bcaddf4a7491b90a91476943a79f238b47b331
Tunisiano187 <helpdesk.choc@gmail.com>
2024-08-25 16:49:33 +0000

    Package download take-command [skip ci]

---
+++ automatic/take-command/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'take-command'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\tcmd.exe"
+$MajorVer    = (Get-Item $url).VersionInfo.FileMajorPart
+
+$packageArgs = @{
+  packageName  = $packageName
+  fileType     = 'EXE'
+  file         = $url
+  silentArgs   = '/Q'
+  softwareName = "Take Command $MajorVer"
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $url -Force -ErrorAction SilentlyContinue | Out-Null
