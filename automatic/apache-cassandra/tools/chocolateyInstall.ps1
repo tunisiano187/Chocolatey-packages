24f2272e27a2bd37932f37d7cd69c7b7cb19749f
tunisiano187 <helpdesk.choc@gmail.com>
2024-08-27 08:28:07 +0000

    AU: 1 updated - apache-cassandra [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/f53ef450bb93c55fa19449aaa62ef666005fb38f

--- automatic/apache-cassandra/tools/chocolateyInstall.ps1
+++ automatic/apache-cassandra/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName    = 'Cassandra'
-$url            = 'https://dlcdn.apache.org/cassandra/5.0-rc1/apache-cassandra-5.0-rc1-bin.tar.gz'
-$checksum       = '5efb04e14cc455eea426677ce92bcf3e423591c9261c714fd6d8bae6951d959c'
+$url            = 'https://dlcdn.apache.org/cassandra/5.0-rc2/apache-cassandra-5.0-rc2-bin.tar.gz'
+$checksum       = 'd91e5a78870f78a0d23ba41ede599da8cdd78e47570a16dc71fdbbf28effcb05'
 $checksumType   = 'sha256'
 $installDir     = (Get-ToolsLocation +"/$packageName")
 $unzipFolder    = Join-Path $env:TEMP "chocolatey\$packageName"
