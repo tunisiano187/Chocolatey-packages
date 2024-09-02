e37d2b5270aafc17990a612e4504f1229e706148
Tunisiano187 <fabian@bowlman.org>
2024-09-02 09:02:40 +0200

    Back

---
+++ automatic/backupper-server/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+Import-Module ..\..\scripts\au_extensions.psm1
+
+$release = 'https://www.ubackup.com/server.html'
+
+function global:au_SearchReplace {
+	@{
+    ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url(32)?\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum(32)?\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType(32)?\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+  Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+  $page = Invoke-WebRequest -Uri $release
+  $url32 = "https://www.ubackup.com$($page.Links.href | Where-Object {$_ -match ".exe$"} | Where-Object {$_ -match "Server"} | Select-Object -First 1)"
+  . ..\..\scripts\Get-FileVersion.ps1
+  $FileVersion = Get-FileVersion $url32
+
+  $Latest = @{ URL32 = $url32; Version = $FileVersion.Version; Checksum32 = $FileVersion.Checksum; ChecksumType32 = $FileVersion.ChecksumType }
+	return $Latest
+}
+
+update -ChecksumFor none
