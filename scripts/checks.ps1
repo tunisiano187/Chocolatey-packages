c7fb0bc893da5dcfdfd64818aefa860fdf101639
Tunisiano187 <fabian@bowlman.org>
2024-09-06 11:02:56 +0200

    https://www.patreon.com/bePatron?u=39585820

--- scripts/checks.ps1
+++ scripts/checks.ps1
@@ -17,7 +17,7 @@ import-module Wormies-AU-Helpers
 $nuspec=Get-ChildItem ./*.nuspec -Recurse; foreach ($file in $nuspec) { Update-Metadata -key owners -value "tunisiano" -NuspecFile $file.FullName }
 git commit -am "Updating owner"
 Write-Host "updating tunisiano187/choco-packages to chocolatey-packages"
-$md=Get-ChildItem ./*.md -Recurse; foreach ($file in $md) { ((Get-Content $file.FullName) -replace 'https://cdn.jsdelivr.net/gh/tunisiano187/choco-packages@f986b7f5de3afc021180256752805698d4efbc38/icons/patreon.png','https://cdn.jsdelivr.net/gh/tunisiano187/Chocolatey-packages@d15c4e19c709e7148588d4523ffc6dd3cd3c7e5e/icons/patreon.png') -replace “tunisiano187/Choco-packages”, “tunisiano187/Chocolatey-packages” | Set-Content -Path $file.FullName }
+$md=Get-ChildItem ./*.md -Recurse; foreach ($file in $md) { (((Get-Content $file.FullName) -replace 'https://cdn.jsdelivr.net/gh/tunisiano187/choco-packages@f986b7f5de3afc021180256752805698d4efbc38/icons/patreon.png','https://cdn.jsdelivr.net/gh/tunisiano187/Chocolatey-packages@d15c4e19c709e7148588d4523ffc6dd3cd3c7e5e/icons/patreon.png') -replace “tunisiano187/Choco-packages”, “tunisiano187/Chocolatey-packages”) -replace 'https://www.patreon.com/bePatron?u=39585820','https://www.patreon.com/bePatron?u=39585820' | Set-Content -Path $file.FullName }
 git commit -am "Updating repository"
 Write-Host "Updating Packages.md"
 scripts/ListPackages.ps1
