70a29670d411c28d07c4a0a08fd5d462076cde19
tunisiano187 <helpdesk.choc@gmail.com>
2024-09-06 08:29:34 +0000

    AU: 2 updated - apache-cassandra xmedia-recode [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/82842f442c5e4352a3c2b131c3a9d26235b45f19

--- automatic/apache-cassandra/tools/chocolateyInstall.ps1
+++ automatic/apache-cassandra/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName    = 'Cassandra'
-$url            = 'https://dlcdn.apache.org/cassandra/5.0-rc2/apache-cassandra-5.0-rc2-bin.tar.gz'
-$checksum       = 'd91e5a78870f78a0d23ba41ede599da8cdd78e47570a16dc71fdbbf28effcb05'
+$url            = 'https://dlcdn.apache.org/cassandra/5.0.0/apache-cassandra-5.0.0-bin.tar.gz'
+$checksum       = 'bda1e57004cc964ec403f9b8c941cda9d4bb1225f786f2b1cd7dde3c71033465'
 $checksumType   = 'sha256'
 $installDir     = (Get-ToolsLocation +"/$packageName")
 $unzipFolder    = Join-Path $env:TEMP "chocolatey\$packageName"
