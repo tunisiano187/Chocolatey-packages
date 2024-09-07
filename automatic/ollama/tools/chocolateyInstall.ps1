3a77d5246f39529a6fb8cae027666e0ef1287571
Tunisiano187 <fabian@bowlman.org>
2024-09-07 18:49:40 +0200

    Ollama first commit

--- automatic/ollama/tools/chocolateyInstall.ps1
+++ automatic/ollama/tools/chocolateyInstall.ps1
@@ -1,22 +1,16 @@
 $ErrorActionPreference = 'Stop';
-$packageName = 'Ollama'
-$url = 'https://github.com/ollama/ollama/releases/download/v0.1.26/OllamaSetup.exe'
-$installerType = 'exe'
-$checksum = 'CACB2123E27CE31C065B723061EF6784308D77840AC0D554DD7696BEB23FC542'
-$checksumType = 'sha256'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$url            = 'https://github.com/ollama/ollama/releases/download/v0.1.26/OllamaSetup.exe'
+$checksum       = 'CACB2123E27CE31C065B723061EF6784308D77840AC0D554DD7696BEB23FC542'
+$checksumType   = 'sha256'
 
-$installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$downloadFile = Join-Path $installPath "$($packageName)Setup.exe"
+$packageArgs = @{
+    packageName            = $env:ChocolateyPackageName
+    fileType               = 'EXE'
+    url                    = $url
+    checksum               = $checksum
+    checksumType           = $checksumType
+    silentArgs             = '/S'
+    validExitCodes         = @(0)
+}
 
-Get-ChocolateyWebFile -PackageName $packageName `
-                      -FileFullPath $downloadFile `
-                      -Url $url `
-                      -Checksum $checksum `
-                      -ChecksumType $checksumType
-Install-ChocolateyInstallPackage -PackageName $packageName `
-                                 -FileType $installerType `
-                                 -SilentArgs $silentArgs `
-                                 -File $downloadFile `
-                                 -ValidExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
