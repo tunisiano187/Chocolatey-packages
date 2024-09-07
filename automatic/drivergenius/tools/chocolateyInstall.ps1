d97e6703061769f5f21c40bde62492e0baddd563
tunisiano187 <helpdesk.choc@gmail.com>
2024-09-07 08:29:18 +0000

    AU: 1 updated - drivergenius [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/c126fa4596f0db164d9503222f882a8e30152477

--- automatic/drivergenius/tools/chocolateyInstall.ps1
+++ automatic/drivergenius/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType  = 'exe'
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $url            = 'https://www.driver-soft.com/products/DriverGenius_Setup_24.0.exe'
-$checksum       = 'efd7d56a39fde9ce5f1609e962b0edb42221294947d188a68a7ceba6b7811b24'
+$checksum       = 'ab4127079529dd80c08e83e192772c2e17e7dd77af6f163f47a683014d4ba04e'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
 
