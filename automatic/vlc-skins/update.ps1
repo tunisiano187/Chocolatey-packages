a5fca9f0df56a8c683d72227e889c4a684edbed1
Tunisiano18 <tunisiano187@users.noreply.github.com>
2024-09-09 20:50:10 +0200

    First update version (#3804)
    Working on issue #3802

---
+++ automatic/vlc-skins/update.ps1
@@ -0,0 +1,37 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://www.videolan.org/vlc/skins2/vlc-skins.zip'
+
+function global:au_SearchReplace {
+	@{
+		"legal\VERIFICATION.txt" = @{
+			"(url32+:).*"  			= "`${1} $($Latest.URL32)"
+			"(checksum32+:).*" 		= "`${1} $($Latest.Checksum32)"
+			"(checksumtype+:).*" 	= "`${1} $($Latest.ChecksumType32)"
+		}
+	}
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$url32 = $releases
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileInfos = Get-FileVersion -url $url32 -keep
+	$fileContent = Get-Content -Path $FileInfos.TempFile
+	if ($fileContent -contains $checksum) {
+		Write-Debug "Checksum $checksum match, won't be updating the file"
+		$version = "0.0"
+	} else {
+		Move-Item -Path $FileInfos.TempFile -Destination "tools\$($FileInfos.FileName)"
+		$version = $FileInfos.Version
+	}
+	
+	$Latest = @{ URL32 = $url32; Version = $version; Checksum32 = $FileInfos.CHECKSUM; ChecksumType32 = $FileInfos.ChecksumType }
+	return $Latest
+}
+
+update -ChecksumFor none
