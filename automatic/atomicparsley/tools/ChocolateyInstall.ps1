f821e6e466fc40260ecaee00c641b6bd9e370af4
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-10 16:41:47 +0200

    Realase folder

--- automatic/atomicparsley/tools/ChocolateyInstall.ps1
+++ automatic/atomicparsley/tools/ChocolateyInstall.ps1
@@ -3,10 +3,11 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = 'atomicparsley'
-  unzipLocation = $toolsDir
+  unzipLocation = "$toolsDir\Release"
   FileFullPath  = "$toolsDir\AtomicParsleyWindows.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
 Move-Item -Path "$toolsDir\Release\*.exe" -Destination "$toolsDir"
+Remove-Item "$toolsDir\Release" -ErrorAction SilentlyContinue -Force | Out-Null
 Remove-Item "$toolsDir\*.zip" -ErrorAction SilentlyContinue -Force | Out-Null
