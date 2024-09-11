377e37ed536f26be4ac37ee01f57d76557f28d00
tunisiano187 <helpdesk.choc@gmail.com>
2024-09-11 19:27:05 +0000

    AU: 1 updated - protonpass [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/170bdf54252b41960b0e46b0ab5b38c64d1d960b

--- automatic/protonpass/tools/chocolateyinstall.ps1
+++ automatic/protonpass/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$url           = ''
 $url64          = 'https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup_1.22.3.exe'
-$checksum64     = ''
-$checksumType64 = ''
+$checksum64     = 'd7a876b53c72c787fae9de0b51fdb791a3e3a4a6e9daad60424c4bb76be584f0'
+$checksumType64 = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
