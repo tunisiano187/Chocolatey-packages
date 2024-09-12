3dde7610471a88209770291723319064509cb56c
Tunisiano <helpdesk.choc@gmail.com>
2024-09-12 11:44:15 +0000

    ps1 vars [skip ci]

--- automatic/allway-sync/tools/chocolateyinstall.ps1
+++ automatic/allway-sync/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://allwaysync.com/content/download/allwaysync.exe'
 $checksum    = 'EFD10324D81669FA47ECE5CA98D1C043C4717CB81E97BEF926C21C110532A0F2'
@@ -7,15 +7,15 @@ $checksum64  = '6A2B489E07964975B666CC82492DF21EE9A5DB51B4827E39D0694F6FCAF08D89
 
 $packageArgs = @{
   packageName    = 'allway-sync'
-  fileType       = 'exe' 
+  fileType       = 'exe'
   url            = $url
   checksum       = $checksum
-  checksumType   = 'sha256'  
+  checksumType   = 'sha256'
   url64bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-  softwareName   = 'Allway Sync*' 
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName   = 'Allway Sync*'
   }
-  
+
 Install-ChocolateyPackage @packageArgs
