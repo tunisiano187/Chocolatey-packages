4cc3bab1bddd6eccb2c402b2c5f68904a329337d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-12 16:19:20 +0200

    snort

---
+++ automatic/snort/update.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = "https://www.snort.org/downloads"
+
+function global:au_SearchReplace {
+    @{
+        "legal\VERIFICATION.txt"      = @{
+			"(?i)(x86:).*"        				= "`${1} $($Latest.URL32)"
+			"(?i)(checksum:).*" 				= "`${1} $($Latest.Checksum32)"
+			"(?i)(checksumtype:).*" 			= "`${1} $($Latest.ChecksumType32)"
+		}
+     }
+}
+
+function global:au_BeforeUpdate {
+    . ..\..\scripts\Get-FileVersion.ps1
+    $FileVersion = Get-FileVersion -url $Latest.URL32 -keep
+    $Latest.Checksum32      = $FileVersion.Checksum
+    $Latest.ChecksumType32  = $FileVersion.ChecksumType
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UserAgent "Chocolatey cli"
+    $url32 = "https://www.snort.org$(($page.Links | Where-Object {$_ -match 'installer'}).href)"
+
+	$version = (Get-Version $url32.Replace('_','.')).Version
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor none -NoCheckUrl
