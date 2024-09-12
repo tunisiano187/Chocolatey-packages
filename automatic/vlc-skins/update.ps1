469ae4a7554776fee01a2f9923b5fbcddbd68dca
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-12 13:44:03 +0200

    try to limit updates

--- automatic/vlc-skins/update.ps1
+++ automatic/vlc-skins/update.ps1
@@ -9,6 +9,7 @@ function global:au_SearchReplace {
 			"(url32+:).*"  			= "`${1} $($Latest.URL32)"
 			"(checksum32+:).*" 		= "`${1} $($Latest.Checksum32)"
 			"(checksumtype+:).*" 	= "`${1} $($Latest.ChecksumType32)"
+			"(pagehash+:).*" 		= "`${1} $($Latest.PageHash)"
 		}
 	}
 }
@@ -21,6 +22,7 @@ function global:au_GetLatest {
 	. ..\..\scripts\Get-FileVersion.ps1
 	$FileInfos = Get-FileVersion -url $releases -keep
 	$fileContent = Get-Content -Path '.\legal\VERIFICATION.txt'
+	$hash=''
 	if ($fileContent -match $FileInfos.checksum) {
 		Write-Debug "Checksum $checksum match, won't be updating the file"
 		$version = "0.0"
@@ -29,9 +31,18 @@ function global:au_GetLatest {
 		$response = Invoke-WebRequest -Uri $releases -Method Head
 		$FileDate = [datetime]::Parse($response.Headers["Last-Modified"])
 		$version = "{0:0000}.{1:00}.{2:00}" -f $FileDate.Year, $FileDate.Month, $FileDate.Day
+		$pageurl = 'https://www.videolan.org/vlc/skins.html?sort=date_mod'
+		$page = (New-TemporaryFile).FullName
+		Invoke-WebRequest -Uri $pageurl -OutFile $page
+		$hash=(Get-FileHash $page).Hash
+		if ($fileContent -match $hash) {
+			Write-Debug "Checksum $hash match, won't be updating the file"
+			$version = "0.0"
+		}
 	}
 
-	$Latest = @{ URL32 = $releases; Version = $version; Checksum32 = $FileInfos.CHECKSUM; ChecksumType32 = $FileInfos.ChecksumType }
+
+	$Latest = @{ URL32 = $releases; Version = $version; Checksum32 = $FileInfos.CHECKSUM; ChecksumType32 = $FileInfos.ChecksumType; PageHash = $hash }
 	return $Latest
 }
 
