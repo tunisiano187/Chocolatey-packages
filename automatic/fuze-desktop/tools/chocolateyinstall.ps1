13ed6773aa213f5725b81445d5f392740b17cf90
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-12 19:25:54 +0200

    fuze

--- automatic/fuze-desktop/tools/chocolateyinstall.ps1
+++ automatic/fuze-desktop/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://api.fuze.com/citadel/v1/applications/fuzeapp/windows/versions/latest/download/perMachineInstaller'
-$checksum     = ''
-$checksumType = ''
+$url          = 'https://d1yyftelocodol.cloudfront.net/windows/23.11.14510/FuzeInstallerPerMachine.exe'
+$checksum     = 'ed336beb1e21c87900fda866d471e8d944916689c3e1d623e52d687e34449799'
+$checksumType = 'sha256'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
