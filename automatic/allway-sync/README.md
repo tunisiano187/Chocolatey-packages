9655a1ccc621837784df2e6f5116fbb7fbd3741d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-12 13:50:18 +0200

    allway-sync

--- automatic/allway-sync/README.md
+++ automatic/allway-sync/README.md
@@ -2,27 +2,13 @@
 
 ## allway-sync (Install)
 
----
-### [choco://allway-sync](choco://allway-sync)
-To use choco:// protocol URLs, install [(unofficial) choco:// Protocol support ](https://chocolatey.org/packages/choco-protocol-support)
-
----
-
 ![Screenshot of Allway Sync](http://allwaysync.com/content/img/screenshots/sync.png)
 
 Allway Sync uses innovative synchronization algorithms to synchronize your data between desktop PCs, laptops, USB drives, remote FTP/SFTP and WebDAV servers, various online data storages and more. Data compression and encryption are supported. It combines bulletproof reliability with extremely easy-to-use interface. Allway Sync also can synchronize your PC data with a Mac via Sync Gateway.
 
-**SOCIAL MEDIA:**
+** SOCIAL MEDIA: **
 [Facebook](https://www.facebook.com/AllwaySync) [Twitter](https://twitter.com/allwaysync)
 
-
-**[PACKAGE NOTES](https://github.com/bcurran3/ChocolateyPackages/blob/master/allway-sync/readme.md)**
-    
----
-
-**Click here to [Patreon-ize](https://www.patreon.com/bcurran3) the package maintainer.**
-
----
 
 #### [choco://allway-sync](choco://allway-sync)
 To use choco:// protocol URLs, install [(unofficial) choco:// Protocol support ](https://chocolatey.org/packages/choco-protocol-support)
--- automatic/allway-sync/tools/chocolateyinstall.ps1
+++ automatic/allway-sync/tools/chocolateyinstall.ps1
@@ -1,19 +1,20 @@
 $ErrorActionPreference = 'Stop'
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://allwaysync.com/content/download/allwaysync.exe'
-$checksum    = 'EFD10324D81669FA47ECE5CA98D1C043C4717CB81E97BEF926C21C110532A0F2'
-$url64       = 'https://allwaysync.com/content/download/allwaysync-x64.exe'
-$checksum64  = '6A2B489E07964975B666CC82492DF21EE9A5DB51B4827E39D0694F6FCAF08D89'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://allwaysync.com/content/download/allwaysync.exe'
+$checksum     = 'EFD10324D81669FA47ECE5CA98D1C043C4717CB81E97BEF926C21C110532A0F2'
+$checksumType = 'sha256'
+$url64        = 'https://allwaysync.com/content/download/allwaysync-x64.exe'
+$checksum64   = '6A2B489E07964975B666CC82492DF21EE9A5DB51B4827E39D0694F6FCAF08D89'
 
 $packageArgs = @{
   packageName    = 'allway-sync'
   fileType       = 'exe'
   url            = $url
   checksum       = $checksum
-  checksumType   = 'sha256'
+  checksumType   = $checksumType
   url64bit       = $url64
   checksum64     = $checksum64
-  checksumType64 = 'sha256'
+  checksumType64 = $checksumType
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName   = 'Allway Sync*'
   }
