9f425be2203962ac24cf774e5885371cd92f2418
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-13 12:39:15 +0200

    dual-monitor-tools

---
+++ automatic/dual-monitor-tools/update.ps1
@@ -0,0 +1,40 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+$releases = 'https://sourceforge.net/projects/dualmonitortool/rss?path=/'
+
+function global:au_SearchReplace {
+	@{
+		"legal\VERIFICATION.txt"      = @{
+			"(?i)(x86:).*"        				= "`${1} $($Latest.URL32)"
+			"(?i)(checksum:).*" 				= "`${1} $($Latest.Checksum32)"
+			"(?i)(^\s*checksum\s*type\:\s*).*" 	= "`${1}$($Latest.ChecksumType32)"
+		}
+	}
+}
+
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32 -keep
+	Move-Item -Path $FileVersion.TempFile -Destination "tools\$($FileVersion.FileName)"
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$File = "$env:TEMP\dualmonitortool.xml"
+	Invoke-WebRequest -Uri $releases -OutFile $File
+	$xml = Get-Content $File
+	$links=$xml | Where-Object {$_ -match '.msi/download'} | Select-Object -First 1
+	$url32 = Get-RedirectedUrl ($links.Split('<|>') | Where-Object {$_ -match '.msi/download'})
+	$version = (Get-Version $url32).Version
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor none
