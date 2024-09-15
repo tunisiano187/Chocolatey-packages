7550f2714158d820cf9027352888f4f2d91bd976
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-15 15:15:20 +0200

    before update

--- automatic/backupper-server/update.ps1
+++ automatic/backupper-server/update.ps1
@@ -13,6 +13,13 @@ function global:au_SearchReplace {
 	}
 }
 
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+}
+
 function global:au_AfterUpdate($Package) {
   Invoke-VirusTotalScan $Package
 }
@@ -20,10 +27,15 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
   $page = Invoke-WebRequest -Uri $release
   $url32 = "https://www.ubackup.com$($page.Links.href | Where-Object {$_ -match ".exe$"} | Where-Object {$_ -match "Server"} | Select-Object -First 1)"
+  $url = "https://www.aomeitech.com/download.html"
+  $response = Invoke-WebRequest -Uri $url
+  $versionPattern = 'v<span\s+data--release-product="AB"\s+data--release="version">(\d+\.\d+\.\d+)<\/span>'
+  $match = [regex]::Match($response.Content, $versionPattern)
+	$version = $match.Groups[1].Value
   . ..\..\scripts\Get-FileVersion.ps1
   $FileVersion = Get-FileVersion $url32
 
-  $Latest = @{ URL32 = $url32; Version = $FileVersion.Version; Checksum32 = $FileVersion.Checksum; ChecksumType32 = $FileVersion.ChecksumType }
+  $Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
 }
 
