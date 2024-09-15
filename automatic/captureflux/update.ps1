9274e4bae16f2b802e95807d51bcbc6ebb59a92e
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-15 15:26:18 +0200

    beforeUpdate

--- automatic/captureflux/update.ps1
+++ automatic/captureflux/update.ps1
@@ -20,17 +20,10 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
 	$page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url32 = "https://paulglagla.com/$($page.Links.href | Where-Object {$_ -match "zip$"} | Select-Object -First 1)"
-
-	$ExtractFolder = Join-Path $env:TEMP "Chocolatey/captureflux"
-	if(Test-Path $ExtractFolder) {
-		Remove-Item -Path $ExtractFolder -Recurse -Force
-	}
-	$File = Join-Path $env:TEMP $($url32.Split('/')[-1])
-	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
-	Expand-Archive -Path $File -DestinationPath $ExtractFolder
-	$checksum = (Get-FileHash -Path $File -Algorithm $env:ChocolateyChecksumType).Hash
-	$File = (Get-ChildItem -Path $ExtractFolder -Filter "*.exe").FullName
-	$version=[System.Diagnostics.FileVersionInfo]::GetVersionInfo($File).FileVersion.trim()
+	$regexPattern = '(\d+(\.\d+)*)</b>'
+	$versionMatch = $page.Content | Select-String -Pattern $regexPattern -AllMatches
+	$version = $versionMatch.Matches[0].Groups[1].Value
+	
 
 	$Latest = @{ URL32 = $url32; Version = $version; Checksum32 = $checksum; ChecksumType32 = $env:ChocolateyChecksumType }
 	return $Latest
