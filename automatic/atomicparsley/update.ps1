0523757eea0a7efa19bd0c73d206ff300bde77ff
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-15 14:28:18 +0200

    version

--- automatic/atomicparsley/update.ps1
+++ automatic/atomicparsley/update.ps1
@@ -31,7 +31,7 @@ function global:au_AfterUpdate($Package) {
 function global:au_GetLatest {
 	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
 	$url32 = $tags.assets.browser_download_url | Where-Object {$_ -match "Windows.zip$"}
-	$version = $tags.tag_name.Replace('v','')
+	$version = "$($tags.tag_name.Replace('v','').split('.')[0]).$($tags.tag_name.Replace('v','').split('.')[1])"
 	Update-Metadata -key "releaseNotes" -value $tags.html_url
 	Update-Metadata -key "licenseUrl" -value $((Get-GitHubLicense -OwnerName $Owner -RepositoryName $repo).download_url)
 	if($tags.prerelease -match "true") {
