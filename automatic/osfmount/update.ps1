0c8ef10e2a6cbab8c3617966a280c5adc7bd2835
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-16 18:29:00 +0200

    download only if updated

--- automatic/osfmount/update.ps1
+++ automatic/osfmount/update.ps1
@@ -11,18 +11,28 @@ function global:au_SearchReplace {
 	}
 }
 
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32 -keep
+	Move-Item -Path $FileVersion.TempFile -Destination "tools\osfmount.exe"
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+}
+
 function global:au_AfterUpdate($Package) {
 	Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
+	$releases='https://www.osforensics.com/tools/mount-disk-images.html'
+	$page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$regexPattern = 'OSFMount V(\d+(\.\d+)*)'
+	$versionMatch = $page.Content | Select-String -Pattern $regexPattern -AllMatches
+	$version = $versionMatch.Matches[0].Groups[1].Value
 	$url32 = "https://www.osforensics.com/downloads/osfmount.exe"
-	. ..\..\scripts\Get-FileVersion.ps1
-	$FileInfos = Get-FileVersion $url32 -keep
-	Move-Item -Path $FileInfos.TempFile -Destination "tools\osfmount.exe"
 
-	$Latest = @{ URL32 = $url32; Checksum32 = $FileInfos.CHECKSUM; ChecksumType32 = $FileInfos.ChecksumType; Version = $FileInfos.Version }
+	$Latest = @{ URL32 = $url32; Version = $version }
 	return $Latest
 }
 
-update -NoCheckUrl -ChecksumFor none -NoCheckChocoVersion
+update -NoCheckUrl -ChecksumFor none
