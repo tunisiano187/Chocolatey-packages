116e0ac1357c3a95f5f5d6234b5e16464b2b8a5e
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-16 18:31:29 +0200

    No download

--- automatic/packetstream/update.ps1
+++ automatic/packetstream/update.ps1
@@ -3,16 +3,6 @@ import-module au
 
 $url32 = 'https://s3-us-west-2.amazonaws.com/packetstream-releases/latest/PacketStream.exe'
 
-function Get-Version($name) {
-	$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
-	while($version_file.count -eq 0)
-	{
-		$version_file=$(../../tools/Get-InstalledApps.ps1 -ComputerName $env:COMPUTERNAME -NameRegex $name).DisplayVersion
-		Start-Sleep -Seconds 1
-	}
-	return $version_file
-}
-
 function global:au_SearchReplace {
 	@{
 		'tools/chocolateyInstall.ps1' = @{
@@ -28,9 +18,6 @@ function global:au_AfterUpdate($Package) {
 }
 
 function global:au_GetLatest {
-	$File = Join-Path $env:TEMP "PacketStream.exe"
-	Invoke-WebRequest -Uri $url32 -OutFile $File -UseBasicParsing
-
 	$pageContent = Invoke-WebRequest -Uri "https://packetstream.software.informer.com/" -UseBasicParsing
 	$regexPattern = 'PacketStream \s*(\d+(\.\d+)*)'
 	$versionMatch = $pageContent.Content | Select-String -Pattern $regexPattern -AllMatches
