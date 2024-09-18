274fe572abd72534603482d7b3cb515235b63c74
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-18 18:49:29 +0200

    winpcap

--- manual/winpcap/tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿Write-Output "Running Autohotkey uninstaller"
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$ahkScript = "$toolsPath\winpcapInstall.ahk"
-AutoHotkey $ahkScript $packageArgs.fileFullPath
