32960c95b9dda1ddfce27fab01a72771658d93a0
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-18 15:28:47 +0200

    AU: 2 updated - electron nuclear [skip ci]

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v32.1.0/electron-v32.1.0-win32-ia32.zip'
-$checksum = '23cbecd7114a69a507ab27659d904e2ea3a3c2a82ac65a9635af89dec2a0544f'
+$url = 'https://github.com/electron/electron/releases/download/v32.1.1/electron-v32.1.1-win32-ia32.zip'
+$checksum = 'b91d4449a188ce69e5eda831feecec4f9837a32aec4dec540731a8f6bb1b7834'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v32.1.0/electron-v32.1.0-win32-x64.zip'
-$checksum64 = 'af31750386553869fd8ec0ad65e75621d2426443a51282b19ec3c4f5e80ae607'
+$url64 = 'https://github.com/electron/electron/releases/download/v32.1.1/electron-v32.1.1-win32-x64.zip'
+$checksum64 = 'ce32b239271edd725e2f53a95d65a3a01df228289d94dfefdb749eb8991af3ce'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
