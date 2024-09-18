75549dd9cff5a860a94bd057d1dcabdf8a385121
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-18 18:37:49 +0200

    winpcap

--- automatic/winpcap/tools/chocolateyInstall.ps1
+++ automatic/winpcap/tools/chocolateyInstall.ps1
@@ -1,15 +1,17 @@
 $ErrorActionPreference = 'Stop'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = "$toolsDir\WinPcap_4_1_3.exe"
+$ahkExe        = 'AutoHotKey'
+$ahkFile       = "$toolsDir\winpcapInstall.ahk"
 
 $packageArgs = @{
-  packageName    = 'WinPcap'
-  fileFullPath   = "$(Get-PackageCacheLocation)\WinPcapInstall.exe"
-  url            = 'https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe'
-  checksum       = 'fc4623b113a1f603c0d9ad5f83130bd6de1c62b973be9892305132389c8588de'
-  checksumType   = 'sha256'
-}
-Get-ChocolateyWebFile @packageArgs
+  packageName  = $packageName
+  fileType     = 'EXE'
+  file         = $url
+  silentArgs   = '/S'
+  softwareName = "Winpcap"
+  }
 
-Write-Output "Running Autohotkey installer"
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$ahkScript = "$toolsPath\winpcapInstall.ahk"
-AutoHotkey $ahkScript $packageArgs.fileFullPath
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $url -Force -ErrorAction SilentlyContinue | Out-Null
