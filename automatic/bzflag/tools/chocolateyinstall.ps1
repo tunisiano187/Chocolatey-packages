742bf715b4fa473c1087b11ad32d2f925079388c
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-20 11:19:41 +0200

    remove useless var

--- automatic/bzflag/tools/chocolateyinstall.ps1
+++ automatic/bzflag/tools/chocolateyinstall.ps1
@@ -1,6 +1,5 @@
 $packageName    = 'bzflag'
 $installerType  = 'exe'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://download.bzflag.org/bzflag/windows/2.4.10/bzflag-2.4.10.exe'
 $checksum       = 'A33119ABACF6B896E523C777BBF918D042D8618BE9DBF0369644FB49009F2ECF'
 $checksumType   = 'sha256'
