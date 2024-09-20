8ce3423cd0691d5c9ebe4ea7f745d542f74ca6a9
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-20 11:07:31 +0200

    bzflag

--- automatic/bzflag/tools/chocolateyinstall.ps1
+++ automatic/bzflag/tools/chocolateyinstall.ps1
@@ -3,6 +3,7 @@ $installerType  = 'exe'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://download.bzflag.org/bzflag/windows/2.4.10/bzflag-2.4.10.exe' 
 $checksum       = 'A33119ABACF6B896E523C777BBF918D042D8618BE9DBF0369644FB49009F2ECF'
+$checksumType   = 'sha256'
 $silentArgs     = '/S'
 $validExitCodes = @(0)
 
@@ -14,7 +15,7 @@ $packageArgs = @{
   silentArgs    = $silentArgs
   softwareName  = 'BZFlag*'
   checksum      = $checksum 
-  checksumType  = 'sha256' 
+  checksumType  = $checksumType
 }
 
 Install-ChocolateyPackage @packageArgs  
