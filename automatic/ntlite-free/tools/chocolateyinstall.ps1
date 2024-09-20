eb6e47af312b937fde3ac9436675271780b44c97
Tunisiano <helpdesk.choc@gmail.com>
2024-09-20 18:43:36 +0000

    ps1 vars [skip ci]

--- automatic/ntlite-free/tools/chocolateyinstall.ps1
+++ automatic/ntlite-free/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'http://downloads.ntlite.com/files/NTLite_setup_x86.exe' 
+$url            = 'http://downloads.ntlite.com/files/NTLite_setup_x86.exe'
 $checksum       = 'FA2AC26D0A4E7930B9A19373EB0640CFE8F1863812498482B84C575246D15275'
-$url64          = 'http://downloads.ntlite.com/files/NTLite_setup_x64.exe' 
+$url64          = 'http://downloads.ntlite.com/files/NTLite_setup_x64.exe'
 $checksum64     = '402314245E6CFED2E3E61C0D305FF99593F0D8C623D1718B4372B4D827381B5B'
 
 $packageArgs = @{
@@ -14,8 +14,8 @@ $packageArgs = @{
   validExitCodes = @(0)
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName   = 'NTLite*'
-  checksum       = $checksum 
-  checksumType   = 'sha256' 
+  checksum       = $checksum
+  checksumType   = 'sha256'
   checksum64     = $checksum64
   checksumType64 = 'sha256'
 }
