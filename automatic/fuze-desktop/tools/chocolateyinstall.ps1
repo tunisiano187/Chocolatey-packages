6105a9aaae1cb4a850bc87692fd843a776ae584d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-22 10:19:35 +0200

    Documentation

--- automatic/fuze-desktop/tools/chocolateyinstall.ps1
+++ automatic/fuze-desktop/tools/chocolateyinstall.ps1
@@ -2,6 +2,10 @@
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://d1yyftelocodol.cloudfront.net/windows/23.11.14510/FuzeInstallerPerMachine.exe'
+# This url comes from the redirection of https://api.fuze.com/citadel/v1/applications/fuzeapp/windows/versions/latest/download/perMachineInstaller
+# The script is : 
+# $releases = "https://api.fuze.com/citadel/v1/applications/fuzeapp/windows/versions/latest/download/perMachineInstaller" // Url on the website that is always the latest
+# $url = Get-RedirectedUrl $releases // redirected url (to have the file still available if there is a newer version)
 $checksum     = 'ed336beb1e21c87900fda866d471e8d944916689c3e1d623e52d687e34449799'
 $checksumType = 'sha256'
 
