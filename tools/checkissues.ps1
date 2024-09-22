9edf862ca51ef1cbc73c4be837b575346dad0181
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-22 18:45:43 +0200

    close finished issue

--- tools/checkissues.ps1
+++ tools/checkissues.ps1
@@ -1,3 +1,19 @@
 ﻿$Issues=Get-GitHubIssue -OwnerName Tunisiano187 -RepositoryName chocolatey-packages -State Open | Where-Object {$_.IssueNumber -ne 3784}
+$search = $Issues.title.Split('(|)')[1]
 
-Write-Output $Issues
+$page = Invoke-WebRequest -Uri "https://community.chocolatey.org/packages/$($search)"
+if($page.Links | Where-Object {$_.href -match 'tunisiano'}) {
+    if($page.Links | Where-Object {$_.href -match 'tunisiano187/Chocolatey-packages'}) {
+        Install-Module -Name PowerShellForGitHub -Force
+        Import-Module PowerShellForGitHub
+        $secureString = ("$env:GITHUB_TOKEN" | ConvertTo-SecureString -AsPlainText -Force)
+        $cred = New-Object System.Management.Automation.PSCredential "username is ignored", $secureString
+        Set-GitHubAuthentication -Credential $cred
+        $secureString = $null # clear this out now that it's no longer needed
+        $cred = $null # clear this out now that it's no longer needed
+        Set-GitHubConfiguration -DisableTelemetry
+
+        Update-GitHubIssue -OwnerName Tunisiano187 -RepositoryName chocolatey-packages -Issue $issues.number -State Closed
+    }
+}
+Write-Output "$Issues.title closed"
