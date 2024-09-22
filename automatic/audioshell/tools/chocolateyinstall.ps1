1223b67f2491a55112d7e8a030e58268b94842d5
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-22 11:18:32 +0200

    Fix

--- automatic/audioshell/tools/chocolateyinstall.ps1
+++ automatic/audioshell/tools/chocolateyinstall.ps1
@@ -1,20 +1,22 @@
 $ErrorActionPreference = 'Stop'
 
-$packageName= 'audioshell'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url        = 'http://www.softpointer.com/downloads/AudioShell236.exe'
-$validExitCodes= @(0)
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkExe = 'AutoHotKey'
-$ahkFile = "$toolsDir\audioshell.ahk"
+$packageName    = 'audioshell'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url            = 'http://www.softpointer.com/downloads/AudioShell236.exe'
+$checksum       = '0EB44EF06A4EBD8FAA919DD59CFC692974209EF804586491FE6BC9B10965067B'
+$checksumType   = 'sha256'
+$validExitCodes = @(0)
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = "$toolsDir\audioshell.ahk"
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Audioshell*'
-  checksum      = '0EB44EF06A4EBD8FAA919DD59CFC692974209EF804586491FE6BC9B10965067B'
-  checksumType  = 'sha256'
+  checksum      = $checksum
+  checksumType  = $checksumType
   silentArgs    = $silentArgs
   validExitCodes= $validExitCodes
 }
