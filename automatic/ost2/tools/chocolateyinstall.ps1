86d8a231bbc3e6188117b5afa75ec7e06ddf4b6d
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-23 18:57:26 +0200

    ost2

--- automatic/ost2/tools/chocolateyinstall.ps1
+++ automatic/ost2/tools/chocolateyinstall.ps1
@@ -1,8 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'ost2' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'http://d.4team.biz/files/ost2_setup.exe'
+$url            = 'https://d.4team.biz/files/ost2_setup.exe'
 $checksum       = '07BBADB855D3B8DF3D614388F6F5D9A4FA87DE885937EE99C909868298D177FA'
+$checksumType   = 'sha256'
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = "$toolsDir\OST2install.ahk"
 
@@ -14,7 +15,7 @@ $packageArgs = @{
   fileType       = 'exe' 
   url            = $url
   checksum       = $checksum
-  checksumType   = 'sha256'  
+  checksumType   = $checksumType  
   silentArgs     = '/S'
   validExitCodes = @(0,2)
   softwareName   = 'OST2*' 
---
+++ automatic/ost2/update.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = 'Stop'
+import-module au
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+		}
+	}
+}	 
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$releases='https://www.ost2.com/fr/thankyou'
+	$page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$regexPattern = 'Version: (\d+(\.\d+)*)'
+	$versionMatch = $page.Content | Select-String -Pattern $regexPattern -AllMatches
+	$version = $versionMatch.Matches[0].Groups[1].Value
+	$url32 = "https://d.4team.biz/files/ost2_setup.exe"
+
+	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
+}
+
+update -NoCheckUrl -ChecksumFor none
