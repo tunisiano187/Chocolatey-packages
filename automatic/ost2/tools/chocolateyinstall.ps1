62b69415c2596f981628a69a018f5089781c0152
tunisiano187 <helpdesk.choc@gmail.com>
2024-09-24 16:59:53 +0000

    AU: 2 updated - gnu-units ost2 [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/364700ab97fe9ba1a252b1c2dae4e74aae206873

--- automatic/ost2/tools/chocolateyinstall.ps1
+++ automatic/ost2/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $packageName    = 'ost2'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://d.4team.biz/files/ost2_setup.exe'
-$checksum       = ''
-$checksumType   = ''
+$checksum       = 'AF6A47B25AB834435E19AAB119E60FB6B89FA9AB21FE23B7424491C473BF7B6A'
+$checksumType   = 'sha256'
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = "$toolsDir\OST2install.ahk"
 
