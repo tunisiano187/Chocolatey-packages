8174f4953a1e407ce0d094ccf15e6f982be57f83
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-26 19:43:43 +0200

    document

--- tools/checkissues.ps1
+++ tools/checkissues.ps1
@@ -1,9 +1,12 @@
 ﻿$Issues=Get-GitHubIssue -OwnerName Tunisiano187 -RepositoryName chocolatey-packages -State Open | Where-Object {$_.IssueNumber -ne 3784}
 $search = $Issues.title.Split('(|)')[1]
+$search
 
 $page = Invoke-WebRequest -Uri "https://community.chocolatey.org/packages/$($search)" -UseBasicParsing
 if($page.Links | Where-Object {$_.href -match 'tunisiano'}) {
+    Write-Output "tunisiano is maintainer"
     if($page.Links | Where-Object {$_.href -match 'tunisiano187/Chocolatey-packages'}) {
+        Write-Output "package published"
         Install-Module -Name PowerShellForGitHub -Force
         Import-Module PowerShellForGitHub
         $secureString = ("$env:GITHUB_TOKEN" | ConvertTo-SecureString -AsPlainText -Force)
