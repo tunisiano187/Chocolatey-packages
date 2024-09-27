0caa39dcf2bd6ef39ca61b536369bec4bd5d1cec
Tunisiano187 <helpdesk.choc@gmail.com>
2024-09-27 16:20:23 +0000

    Package download metafox [skip ci]

---
+++ automatic/metafox/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'metafox'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$TodaysVersion = $env:ChocolateyPackageVersion -replace '[.]',''
+
+$packageArgs = @{
+  packageName    = $packageName
+  softwareName   = 'MetaFox*'  
+  fileType       = 'EXE'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'  
+  file           = "$toolsDir\MetaFox_"+$TodaysVersion+".exe"
+  validExitCodes = @(0)  
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item "$toolsDir\*.exe" -EA SilentlyContinue | Out-Null
