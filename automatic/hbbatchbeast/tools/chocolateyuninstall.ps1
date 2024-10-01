f5a552d8c6a87792b9a20b2d3bd4dddb5e2182b3
Tunisiano187 <helpdesk.choc@gmail.com>
2024-10-01 12:21:52 +0000

    Exclude hbbatchbeast [skip ci]

--- automatic/hbbatchbeast/tools/chocolateyuninstall.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$packageName  = 'hbbatchbeast'
-$shortcutName = 'HBBatchBeast.lnk'
-
-Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
-Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
