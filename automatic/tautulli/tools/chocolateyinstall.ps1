9fcbf7388e869e72f8fbe4741da901536600fbd7
Tunisiano187 <helpdesk.choc@gmail.com>
2024-10-01 16:20:29 +0000

    Package download tautulli [skip ci]

---
+++ automatic/tautulli/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'tautulli' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file         = "$toolsDir\Tautulli-"+$ENV:ChocolateyPackageVersion+".zip"
+$extractDir   = "Tautulli-"+$ENV:ChocolateyPackageVersion
+$shortcutName = 'Tautulli.lnk'
+
+Get-ChocolateyUnzip -FileFullPath $file -Destination $toolsDir
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$extractDir\Tautulli.py" -WorkingDirectory "$toolsDir\$extractDir" -IconLocation "$toolsDir\$extractDir\data\interfaces\default\images\favicon\favicon.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$extractDir\Tautulli.py" -WorkingDirectory "$toolsDir\$extractDir" -IconLocation "$toolsDir\$extractDir\data\interfaces\default\images\favicon\favicon.ico"
+
+$WhoAmI=whoami
+icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
---
+++ automatic/tautulli/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'tautulli'
+$shortcutName = 'Tautulli.lnk'
+
+Remove-Item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
