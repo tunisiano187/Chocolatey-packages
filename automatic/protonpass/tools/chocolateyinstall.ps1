d94c2a1192161ef3cee72dda47749f670afc6801
Tunisiano187 <helpdesk.choc@gmail.com>
2024-10-08 19:33:02 +0200

    Workstation

--- automatic/protonpass/tools/chocolateyinstall.ps1
+++ automatic/protonpass/tools/chocolateyinstall.ps1
@@ -4,6 +4,11 @@ $url64          = 'https://proton.me/download/pass/windows/ProtonPass_Setup_1.23
 $checksum64     = '2844543a3c76d401d9db2a0be96889cdb9c2408036e3edaa1e7227708e853045'
 $checksumType64 = 'sha256'
 
+if (!(Get-IsWinWorkstation)) {
+  Write-Warning "  ** This package is for Windows Workstation OSes only. Aborting."
+  throw
+}
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
