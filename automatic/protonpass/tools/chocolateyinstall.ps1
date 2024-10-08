135aa10537c3c7eb49b271bbde156d42f23c19da
tunisiano187 <helpdesk.choc@gmail.com>
2024-10-08 18:08:45 +0000

    AU: 1 updated - protonpass [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/6a1445e108978df1147ee82132dea91466b9264d

--- automatic/protonpass/tools/chocolateyinstall.ps1
+++ automatic/protonpass/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64          = 'https://proton.me/download/pass/windows/ProtonPass_Setup_1.23.1.exe'
-$checksum64     = '2844543a3c76d401d9db2a0be96889cdb9c2408036e3edaa1e7227708e853045'
+$checksum64     = '2844543A3C76D401D9DB2A0BE96889CDB9C2408036E3EDAA1E7227708E853045'
 $checksumType64 = 'sha256'
 
 if (!(Get-IsWinWorkstation)) {
