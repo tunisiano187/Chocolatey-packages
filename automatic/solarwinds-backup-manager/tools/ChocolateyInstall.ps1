488cb9c5e30f8ffa2f59c745f96360f9b080aed1
tunisiano187 <helpdesk.choc@gmail.com>
2024-10-09 12:40:47 +0000

    AU: 3 updated - ntlite-free protonpass solarwinds-backup-manager [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/a952c0f131d7ae9668933695392592ba3f5307c7

--- automatic/solarwinds-backup-manager/tools/ChocolateyInstall.ps1
+++ automatic/solarwinds-backup-manager/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $packageName    = 'solarwinds-backup-manager'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://cdn.cloudbackup.management/maxdownloads/mxb-windows-x86_x64.exe'
-$checksum       = '3EBB18ED290F09AA9A84B179D91ED2A5C2E08396CC17163CF2D059571E4179D8'
+$checksum       = '4657D4E2A1B0ECB683CA154F7AE598B38F8E9513B6666C3D03D8ABCA46C2504D'
 $checksumType   = 'sha256'
 
 Write-Host "  ** Backup Manager will open its web console in your default browser after installing." -ForeGround Magenta
