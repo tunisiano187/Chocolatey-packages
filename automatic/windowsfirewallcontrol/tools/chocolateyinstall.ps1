a227345ff7d3f662d25f2913938b35e7b2db36ba
tunisiano187 <helpdesk.choc@gmail.com>
2024-10-17 16:29:28 +0000

    AU: 1 updated - windowsfirewallcontrol [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/fda220e4880a990f21288d6b157a07df640ae23f

--- automatic/windowsfirewallcontrol/tools/chocolateyinstall.ps1
+++ automatic/windowsfirewallcontrol/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url            = 'https://www.binisoft.org/download/wfc6setup.exe'
-$checksum       = '16442002148e25cc009ad69e6292ce763eeb93108a9008c35019ec2ca7252f32'
+$checksum       = '3598f39c809c7fb3c3f301bb4c5764459eae483b5c3f96dd6cf3de0f0d476087'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
