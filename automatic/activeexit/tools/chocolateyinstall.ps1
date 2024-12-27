bacc2e3408f1188954b084819f8bb84f401c3f4f
tunisiano187 <helpdesk.choc@gmail.com>
2024-12-27 08:15:09 +0000

    ps1 vars

--- automatic/activeexit/tools/chocolateyinstall.ps1
+++ automatic/activeexit/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName   = 'activeexit'
+$packageName   = 'activeexit'
 $installerType = 'EXE'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'http://www.winability.com/files/ActiveExitLite-17.3.0-setup.exe' 
+$url           = 'http://www.winability.com/files/ActiveExitLite-17.3.0-setup.exe'
 $checksum      = '2A12FCC744857DA856B152FF72B3354D9832B6F5502FEE947EBCE07B3F8492E9'
 $silentArgs    = ''
 $validExitCodes= @(0)
@@ -12,13 +12,13 @@ Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = 'EXE' 
+  fileType      = 'EXE'
   url           = $url
-  softwareName  = 'ActiveExit*' 
+  softwareName  = 'ActiveExit*'
   checksum      = $checksum
   checksumType  = 'sha256'
   silentArgs    = $silentArgs
   validExitCodes= $validExitCodes
 }
 
-Install-ChocolateyPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
