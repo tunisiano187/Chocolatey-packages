7df4826fd0e35ded3393af2cf85b221aa5364986
Tunisiano187 <helpdesk.choc@gmail.com>
2025-01-19 15:12:16 +0100

    zip way

--- automatic/sd-card-formatter/update.ps1
+++ automatic/sd-card-formatter/update.ps1
@@ -13,6 +13,13 @@ function global:au_SearchReplace {
 	}
 }
 
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+}
+
 function global:au_AfterUpdate($Package) {
 	Invoke-VirusTotalScan $Package
 }
@@ -34,4 +41,4 @@ function global:au_GetLatest {
 	}
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
