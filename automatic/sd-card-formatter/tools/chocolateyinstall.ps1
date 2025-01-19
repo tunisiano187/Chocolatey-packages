6fcb244aedc4971d255c0c763840931f6b433317
Tunisiano187 <helpdesk.choc@gmail.com>
2025-01-19 15:08:34 +0100

    zip way

--- automatic/sd-card-formatter/tools/chocolateyinstall.ps1
+++ automatic/sd-card-formatter/tools/chocolateyinstall.ps1
@@ -1,14 +1,17 @@
 ﻿$ErrorActionPreference     = 'Stop'
-$url                       = 'https://www.sdcard.org/downloads/formatter/eula_windows/SD%20Card%20Formatter%20Setup.exe'
-$Checksum                  = '0243f9adae333f8a158d8137c29a64f411a0c3dc7827715d49483235852db091'
-$ChecksumType              = 'sha256'
+$url                       = 'https://www.sdcard.org/downloads/formatter/eula_windows/SDCardFormatterv5_WinEN.zip'
+$Checksum                  = ''
+$ChecksumType              = ''
+
+$curDir	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = Join-Path $curDir "tools"
 
 $UnzipArgs = @{
     PackageName     = $env:chocolateyPackageName
     Url32           = $url
     Checksum32      = $Checksum
     checksumType    = $ChecksumType
-    UnzipLocation   = $ToolsPath
+    UnzipLocation   = $toolsDir
 }
 
 Get-ChocolateyUnzip @UnzipArgs
