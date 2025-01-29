bac5bf868769b7e9708babfad281553aac8d6d25
tunisiano187 <helpdesk.choc@gmail.com>
2025-01-29 00:27:16 +0000

    AU: 1 updated - wavpack [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/d09c695c2975545dfe85d65146afbfd99f39cb86

--- automatic/wavpack/tools/chocolateyInstall.ps1
+++ automatic/wavpack/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName    = 'wavpack'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/dbry/WavPack/releases/download/5.8.0/wavpack-5.8.0-x86.zip'
+$url            = 'https://github.com/dbry/WavPack/releases/download/5.8.1/wavpack-5.8.0-x86.zip'
 $checksum       = 'e78588e3f49a7b619bf1bc6cb86efd58094398614c707f82102b76c5c21d8cb6'
 $checksumType   = 'sha256'
-$url64          = 'https://github.com/dbry/WavPack/releases/download/5.8.0/wavpack-5.8.0-x64.zip'
+$url64          = 'https://github.com/dbry/WavPack/releases/download/5.8.1/wavpack-5.8.0-x64.zip'
 $checksum64     = 'bbb29d1478dfaab80a6ea868deb575a82414c5d79cbec91cdc1044c358945168'
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
