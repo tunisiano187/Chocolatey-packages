8136695bcf278489562dbe5d9165a3590bf28879
tunisiano187 <helpdesk.choc@gmail.com>
2025-02-06 08:16:22 +0000

    ps1 vars

--- automatic/exchange-edb-viewer/tools/chocolateyinstall.ps1
+++ automatic/exchange-edb-viewer/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = 'exchange-edb-viewer' 
+$packageName = 'exchange-edb-viewer'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'http://www.nucleustechnologies.com/downloads/exchange-edb-viewer.exe'
 $checksum    = '465BC1041EE2C5082F2E26A38B1B61C587423B4D2F032B5BE38CE7D574ADE3ED'
@@ -6,13 +6,13 @@ $checksum    = '465BC1041EE2C5082F2E26A38B1B61C587423B4D2F032B5BE38CE7D574ADE3ED
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' 
+  fileType      = 'exe'
   url           = $url
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-  softwareName  = 'Kernel Exchange EDB Viewer*' 
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName  = 'Kernel Exchange EDB Viewer*'
   checksum      = $checksum
   checksumType  = 'sha256'
   }
-  
+
 Install-ChocolateyPackage @packageArgs
 
