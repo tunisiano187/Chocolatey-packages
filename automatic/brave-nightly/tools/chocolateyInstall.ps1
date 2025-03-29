a169691fc5043ec8fe97ccd36f6d61ae017bd92d
tunisiano187 <helpdesk.choc@gmail.com>
2025-03-29 00:27:14 +0000

    AU: 1 updated - brave-nightly [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/b72b4b74400bccd3c250a9bc6253898954201bbd

--- automatic/brave-nightly/tools/chocolateyInstall.ps1
+++ automatic/brave-nightly/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $packageName      = $env:ChocolateyPackageName
-$url32            = 'https://github.com/brave/brave-browser/releases/download/v1.79.5/BraveBrowserStandaloneSilentNightlySetup.exe'
-$checksum32       = '49022bb52d54fc11d0b6f64326378472a91e6af28c0184e0b5b5792e0d1cc1ea'
+$url32            = 'https://github.com/brave/brave-browser/releases/download/v1.79.8/BraveBrowserStandaloneSilentNightlySetup.exe'
+$checksum32       = 'eac2786488609096f9f5376a7d6865ca3945bef303741dc26eea4182fa352011'
 $checksumType32   = 'sha256'
 
 $packageArgs = @{
