2b8106e12a68a85e15c6cf2ad4e30ab80e1657b5
tunisiano187 <helpdesk.choc@gmail.com>
2025-04-10 00:26:52 +0000

    AU: 1 updated - electron [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/288356c88e684157bab26fe3cf2ecb79241fe22c

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v35.1.4/electron-v35.1.4-win32-ia32.zip'
-$checksum = '06cfe544591e49831ceab93be5dcb61c2b78e9e2e8afc4599ce8a3bac118d469'
+$url = 'https://github.com/electron/electron/releases/download/v35.1.5/electron-v35.1.5-win32-ia32.zip'
+$checksum = '267e1ef8a806db811e3f89ac483be847757b53d2ea9ce57ca0aa1dd3cf781d4f'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v35.1.4/electron-v35.1.4-win32-x64.zip'
-$checksum64 = 'b7f2fd102b7fd5e5b2c226e83253ad0cb34d50d858d6a471baa69e24f817b03f'
+$url64 = 'https://github.com/electron/electron/releases/download/v35.1.5/electron-v35.1.5-win32-x64.zip'
+$checksum64 = 'd96947bb5111db344d5acedd9ce7bc2eb0c7d28992bf6307d0efeee2589c4ba2'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
