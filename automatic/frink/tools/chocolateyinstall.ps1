c5897ef870d9f80c188263aea1d100ee138cd530
tunisiano187 <helpdesk.choc@gmail.com>
2025-04-13 08:25:19 +0000

    AU: 1 updated - frink [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/4de895bc75688f71993087d786f7cb5a7ab7ea61

--- automatic/frink/tools/chocolateyinstall.ps1
+++ automatic/frink/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName  = 'frink'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://frinklang.org/frinkjar/frink.jar'
-$checksum     = '71488D7198199B938EA198981CECB999017F87995E89A6C60B1C9A18F542F597'
+$checksum     = '70A6BBDBF31E9DDF9247D2980ECF9F5EBF2A7EC97B4F763066A210FABAAF6C48'
 $checksumType = 'sha256'
 $ExeFile      = 'frink.jar'
 $ShortcutName = 'Frink.lnk'
