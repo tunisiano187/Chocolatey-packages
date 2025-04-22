0d7a2cfbbb52537c1cfd5a7a62a0ea4bd265a304
tunisiano187 <helpdesk.choc@gmail.com>
2025-04-22 00:29:02 +0000

    AU: 1 updated - odrive [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/903d19b71e1a9760946bd70d6121b567ee26f0ef

--- automatic/odrive/tools/chocolateyinstall.ps1
+++ automatic/odrive/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url			= 'https://dba0rg1h0v09q.cloudfront.net/odrivesync.7353.exe'
-$checksum		= '8fa8d83408859dae27cb5645f35d12f3d69835c50dadc8faf8bbf2c7f0cfc790'
+$url			= 'https://dba0rg1h0v09q.cloudfront.net/odrivesync.7364.exe'
+$checksum		= 'f5d20752b18be867dee0347e2352247a76e320766ce0c7d4ac9f278bb2570ad0'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
