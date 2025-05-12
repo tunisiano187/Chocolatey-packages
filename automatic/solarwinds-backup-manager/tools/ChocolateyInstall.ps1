f744187091b5b83a492e2b9d749879e3fce8368f
tunisiano187 <helpdesk.choc@gmail.com>
2025-05-12 16:28:15 +0000

    AU: 1 updated - solarwinds-backup-manager [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/15769d994f17938494693c3c4e120e7ca588596d

--- automatic/solarwinds-backup-manager/tools/ChocolateyInstall.ps1
+++ automatic/solarwinds-backup-manager/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $packageName    = 'solarwinds-backup-manager'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://cdn.cloudbackup.management/maxdownloads/mxb-windows-x86_x64.exe'
-$checksum       = '15241071193B053B349AFEA02A3C323AE6C809C7ACE98B1F195A894020696F72'
+$checksum       = '2A73787CAD71901BAE3C48275A32566EE78B60928274E7E1CE22E44D71471341'
 $checksumType   = 'sha256'
 
 Write-Host "  ** Backup Manager will open its web console in your default browser after installing." -ForeGround Magenta
