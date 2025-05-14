4f22415be1f93f6545ded591b0a18beb7e20dbc8
tunisiano187 <helpdesk.choc@gmail.com>
2025-05-14 08:31:21 +0000

    AU: 1 updated - sdio [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/22eb790cfd90708d6c313bec8d9f6bade678d269

--- automatic/sdio/tools/chocolateyinstall.ps1
+++ automatic/sdio/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $checksumType   = 'sha256'
-$checksum       = '0cc9f0419cd59b7671950e78b202952b260a305f7ec9eea5c228c338f6a257c9'
-$url            = 'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.15.3.814.zip'
+$checksum       = '88a3652f21569cc860d1073de3ce901419eeff1acc6410d965369846345b6e5e'
+$url            = 'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.15.4.815.zip'
 
 $packageArgs = @{
     packageName   = 'sdio'
