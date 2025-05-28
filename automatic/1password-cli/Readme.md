b605a06c0054ba9f28578e9ad1ae7568313fc8ec
Tunisiano187 <helpdesk.choc@gmail.com>
2025-05-28 10:38:49 +0200

    1password-cli

---
+++ automatic/1password-cli/Readme.md
@@ -0,0 +1,6 @@
+# 1Password-CLI chocolatey package
+
+https://support.1password.com/command-line-getting-started/
+
+# Original package Maintainer
+https://github.com/AnthonyMastrean/chocolateypackages/tree/master/packages/op
---
+++ automatic/1password-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = '1password-cli'
+$version     = 'v1.10.0'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "https://cache.agilebits.com/dist/1P/op/pkg/$version/op_windows_386_$version.zip"
+$checksum    = '7432FB0282F66929746DAB531143F0CC4D6A0F67328DDCA6AF6E3C8B92C9561F'
+$url64       = "https://cache.agilebits.com/dist/1P/op/pkg/$version/op_windows_amd64_$version.zip"
+$checksum64  = 'CD6A7D24E63DF4F8B3933F74B86740520DDBAD716E9847CDF161AE40AF101DDC'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'SHA256'
+  url64          = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'SHA256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
