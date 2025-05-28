790c594dd6be0e2210a5c77ad3f5acda54667e92
Tunisiano187 <helpdesk.choc@gmail.com>
2025-05-28 11:01:12 +0200

    automate

---
+++ automatic/1password-cli/update.ps1
@@ -0,0 +1,43 @@
+$ErrorActionPreference = 'Stop'
+import-module AU
+Import-Module ..\..\scripts\au_extensions.psm1
+
+$releases = 'https://app-updates.agilebits.com/product_history/CLI2'
+
+function global:au_SearchReplace {
+	@{
+		'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      		= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType32)'"
+			"(^[$]url64\s*=\s*)('.*')"      		= "`$1'$($Latest.URL64)'"
+			"(^[$]checksum64\s*=\s*)('.*')" 		= "`$1'$($Latest.Checksum64)'"
+			"(^[$]checksumType64\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType64)'"
+		}
+	}
+}
+
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+	$FileVersion = Get-FileVersion $Latest.URL64
+	$Latest.Checksum64 = $FileVersion.Checksum
+	$Latest.ChecksumType64 = $FileVersion.checksumType
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$url32 = (Invoke-WebRequest -Uri $releases).Links | Where-Object {$_.href -match '_386_'} | Select-Object -First 1
+	$url64 = (Invoke-WebRequest -Uri $releases).Links | Where-Object {$_.href -match 'amd64'} | Select-Object -First 1
+	$version = (Get-Version $url32).Version
+
+	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+	return $Latest
+}
+
+update -ChecksumFor none
