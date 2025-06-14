89f0900b7d20af3e1110f0d3475bf7b14c3655ec
tunisiano187 <helpdesk.choc@gmail.com>
2025-06-14 16:27:20 +0000

    AU: 1 updated - ntlite-free [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/dc10cc837bbeb7427a6798051a7f9d5ba5a077c8

--- automatic/ntlite-free/tools/chocolateyinstall.ps1
+++ automatic/ntlite-free/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://downloads.ntlite.com/files/NTLite_setup_x86.exe'
-$checksum       = 'a3170b861474d0551124dcc9004552679c1b5db514385d6e561809fa6e798a8c'
+$checksum       = '30b608bd46efe75e3d33a0b700ebd1875e17eb2a061651be29302d7c150f17ad'
 $checksumType   = 'sha256'
 $url64          = 'https://downloads.ntlite.com/files/NTLite_setup_x64.exe'
-$checksum64     = 'efb954027c8fa0ac0ccc7012311a9d45cd29e1191e781a87846d53a2e0c1bf6c'
+$checksum64     = '1dab4ad576a40b3fc400fc84774379bb8b5e0218ba6f29a5ccba3b815d1939d4'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
