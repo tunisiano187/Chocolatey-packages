ced16479fdd88166f8437eed8373ac94a5fbe804
tunisiano187 <helpdesk.choc@gmail.com>
2025-06-16 00:26:53 +0000

    AU: 1 updated - rufus.portable [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/0e2e144aeb90782552c475156bc1cf41f69b3bd0

--- automatic/rufus.portable/tools/chocolateyInstall.ps1
+++ automatic/rufus.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 
-$url            = 'https://github.com/pbatard/rufus/releases/download/v4.8/rufus-4.8p.exe'
-$checksum       = '092b1c10c0240e4ae16f30989c3f06a8e784067a944e4aa3e8a11a75f803d71b'
+$url            = 'https://github.com/pbatard/rufus/releases/download/v4.9/rufus-4.9p.exe'
+$checksum       = '497f796e6d076d4855d697965c04626e6d3624658fce3eca82ab14f7414eede2'
 $checksumType   = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile    = Join-Path $toolsDir "rufusp.exe"
