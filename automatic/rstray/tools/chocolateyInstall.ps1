bb57585024d1b84cff3ba5b2685bc6e7c3a500a1
tunisiano187 <helpdesk.choc@gmail.com>
2025-06-19 00:31:45 +0000

    AU: 2 updated - openhab rstray [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/a12bb606f1750ee182fc41ca91b44fc9e855c6e0

--- automatic/rstray/tools/chocolateyInstall.ps1
+++ automatic/rstray/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installDir = $toolsdir
-$version    = '2.2.3'
+$version    = '3.0.0'
 
 $packageArgs = @{
     packageName     = $env:ChocolateyPackageName
