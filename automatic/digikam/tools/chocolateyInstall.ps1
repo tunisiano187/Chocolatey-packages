30656e4099ed7f0fb33456b29984c417efeeb7cc
tunisiano187 <helpdesk.choc@gmail.com>
2025-06-30 16:36:46 +0000

    AU: 1 updated - digikam [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/ff7e7b834d3d4a7562eb840acf70497b9cf10b58

--- automatic/digikam/tools/chocolateyInstall.ps1
+++ automatic/digikam/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '/S'
-$url64          = 'https://download.kde.org/stable/digikam/8.6.0/digiKam-8.6.0-Qt6-Win64.exe'
-$checksum64     = '6be996efd54015d52fac4c5682d01ff080e4dcb496aafa434fdd8500be9e1c0c'
+$url64          = 'https://download.kde.org/stable/digikam/8.7.0/digiKam-8.7.0-Qt6-Win64.exe'
+$checksum64     = '0166b78006ac269a167457280d0d47b868e2f681c94952a659a22865dbb4bbae'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
