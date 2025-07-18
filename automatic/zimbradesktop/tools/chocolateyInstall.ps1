baf2fd67a6d95a36d851d290fed7fd412e2923d9
tunisiano187 <helpdesk.choc@gmail.com>
2025-07-18 08:33:00 +0000

    AU: 2 updated - electrum zimbradesktop [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/7a2bed2a0b9417b79738b828c7be73b033f916a1

--- automatic/zimbradesktop/tools/chocolateyInstall.ps1
+++ automatic/zimbradesktop/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName        = 'zimbra-desktop'
 $installerType      = 'exe'
 $silentArgs         = '/S'
-$url                = 'https://files.zimbra.com/downloads/zxui/4.45.0/Zimbra_Desktop_4.45.0_8e5331bdc_20250429110645.exe'
-$checksum           = '715a2a5dcb355435c653e1c5df0ec64448f5b6e1ae2203976a867400c8268646'
+$url                = 'https://files.zimbra.com/downloads/zxui/4.46.0/Zimbra_Desktop_4.46.0_c975480bb_20250701133104.exe'
+$checksum           = 'd34e29afc8173049c86736825436313ba2c1d3dea1b6c6a619eefcb55b8422d4'
 $checksumType       = 'sha256'
 $validExitCodes     = @(0)
 
