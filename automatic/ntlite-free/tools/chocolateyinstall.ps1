3b74de840b9640e46ae8909d024b025888e5c82c
tunisiano187 <helpdesk.choc@gmail.com>
2025-07-23 16:33:13 +0000

    AU: 2 updated - ntlite-free solarwinds-backup-manager [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/ff0e926f511663e2da4665ad3d501f44b95bfef4

--- automatic/ntlite-free/tools/chocolateyinstall.ps1
+++ automatic/ntlite-free/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://downloads.ntlite.com/files/NTLite_setup_x86.exe'
-$checksum       = '393804e99c92a6959b88dcf399ae9cf34a1e084c48443f16393e7ccab530bff8'
+$checksum       = 'ca0b3c6d2b57575277db87e2262cebfd38e7932cc33d481beb8f9e42cde3e0a4'
 $checksumType   = 'sha256'
 $url64          = 'https://downloads.ntlite.com/files/NTLite_setup_x64.exe'
-$checksum64     = '943a6ba5f98aaa4dd86d4b3f95d7c489103b8b1288fef334f91a9f3202825b9a'
+$checksum64     = 'd985c6c1263366e9cfab4fda04d2592949a68a143e2d2163012b1a1ef1d51ac6'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
--- automatic/solarwinds-backup-manager/tools/ChocolateyInstall.ps1
+++ automatic/solarwinds-backup-manager/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $packageName    = 'solarwinds-backup-manager'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://cdn.cloudbackup.management/maxdownloads/mxb-windows-x86_x64.exe'
-$checksum       = '6369E0F0A27F20BE36238DB3CC454798CA13192C6B7B197E307241B2BDE1F88A'
+$checksum       = 'D6EDB9AD95302184FAE0411D4A63F48A30C6D790C29B73B9EB9DD63B9A433F75'
 $checksumType   = 'sha256'
 
 Write-Host "  ** Backup Manager will open its web console in your default browser after installing." -ForeGround Magenta
