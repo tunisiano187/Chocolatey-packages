c715e80a4b2cb0f526bdcceab421cb8cba510c5c
tunisiano187 <helpdesk.choc@gmail.com>
2025-08-22 00:33:11 +0000

    AU: 1 updated - warp [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/9a8a110bf108e474e15a5f1275f4467ced30e6ce

--- automatic/warp/tools/chocolateyinstall.ps1
+++ automatic/warp/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $pp             = Get-PackageParameters
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url64          = 'https://1111-releases.cloudflareclient.com/windows/Cloudflare_WARP_Release-x64.msi'
-$checksum64     = '3D639B829381A4A006E8368ED800DD6B3A8F3FE36E39FFC346DEF029335CF8C0'
+$checksum64     = '011EB387AB6064AD3FC18321A377FCF6D8A9BD3B03B22208D05F3BB9965C0633'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
