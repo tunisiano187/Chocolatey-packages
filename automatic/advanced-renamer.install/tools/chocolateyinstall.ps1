b0eb10bffd7fb65436400a35375e1392d3f2e954
tunisiano187 <helpdesk.choc@gmail.com>
2025-09-02 00:33:43 +0000

    AU: 1 updated - advanced-renamer.install [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/00442ab44408dbb3fc0301e1596b72678f6037fe

--- automatic/advanced-renamer.install/tools/chocolateyinstall.ps1
+++ automatic/advanced-renamer.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://www.advancedrenamer.com/down/win/advanced_renamer_setup_4_15.exe'
+$url           = 'https://www.advancedrenamer.com/down/win/advanced_renamer_setup_4_16.exe'
 $checksumType  = 'sha256'
-$checksum      = '3127A699FB05A890362E5DC719E9F1850074B3EBCB5B972B41BDCD190EFE72C0'
+$checksum      = '5D4A863FF2FCBA653067DBF344C1252B66F565BC6C311B03E920E272726D65A2'
 
 
 $packageArgs = @{
