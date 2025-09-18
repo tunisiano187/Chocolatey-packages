9b500c06aec01bb3132024e89dd0498a3d0a4c38
Tunisiano <helpdesk.choc@gmail.com>
2025-09-18 17:08:49 +0000

    ps1 vars [skip ci]

--- automatic/crucial-storage-executive/update.ps1
+++ automatic/crucial-storage-executive/update.ps1
@@ -1,4 +1,4 @@
-Import-Module au
+import-module chocolatey-AU
 
 $releases = 'https://www.crucial.com/support/storage-executive'
 
@@ -6,7 +6,7 @@ function global:au_GetLatest {
 	$package = [AUPackage]::new( $pwd )
 	$download_page	= Invoke-WebRequest -UseBasicParsing -Uri $releases
 	$filere			= '\.zip$'
-	
+
 	$verre			= '(?<=Version\s)[\d.-]+(?=\s)'
 	$download_page.Content -match $verre | Out-Null
 	$version		= $Matches[0].ToString()
@@ -19,10 +19,10 @@ function global:au_GetLatest {
 	if ($url64.StartsWith("/")) {
 		# url is NOT fully qualified
 		$url64 = ([System.Uri]$releases).scheme + '://' + ([System.Uri]$releases).authority + $url64
-	}	
+	}
 	$checksumType	= 'sha256'
 	$checksum64		= Get-RemoteChecksum -Algorithm $checksumType -Url $url64
-	
+
 	return @{
 		Url64             = $url64
 		Version           = $version
