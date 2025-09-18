49af0a578ff228f9a967b6587b5f258fa5caf01a
Tunisiano187 <helpdesk.choc@gmail.com>
2025-09-18 19:08:23 +0200

    CSE

---
+++ automatic/crucial-storage-executive/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference  = 'Stop';
+
+$toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64                  = 'https://www.crucial.com/content/dam/crucial/support/storage-executive-win-64.zip'
+$checksum64             = '297a00cb8dd434876ca58998bcbf50f0d79d5736f63f633eaf57a5496903b6b1'
+$checksumType64         = 'sha256'
+$version				        = '8.07'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  unzipLocation         = $toolsDir
+  fileType              = 'EXE'
+  softwareName          = "Crucial Storage Executive*"
+  url64                 = $url64
+  checksum64            = $checksum64
+  checksumType64        = $checksumType64
+  validExitCodes        = @(0)
+  silentArgs			      = '--unattendedmodeui none --mode unattended'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+pushd $toolsDir
+$fileName = Get-ChildItem -Filter "StorageExecutive-$version*"
+$packageArgs.file = Join-Path -Path $toolsDir -ChildPath $fileName
+Install-ChocolateyInstallPackage @packageArgs
+del $fileName
+popd
---
+++ automatic/crucial-storage-executive/update.ps1
@@ -0,0 +1,45 @@
+Import-Module au
+
+$releases = 'https://www.crucial.com/support/storage-executive'
+
+function global:au_GetLatest {
+	$package = [AUPackage]::new( $pwd )
+	$download_page	= Invoke-WebRequest -UseBasicParsing -Uri $releases
+	$filere			= '\.zip$'
+	
+	$verre			= '(?<=Version\s)[\d.-]+(?=\s)'
+	$download_page.Content -match $verre | Out-Null
+	$version		= $Matches[0].ToString()
+	#short-circuit if there is no new update to the webpage
+	if ($version -eq $package.NuspecVersion) {
+		return @{version = $version}
+	}
+
+	$url64			= $download_page.links | ? href -match $filere | select -First 1 -expand href
+	if ($url64.StartsWith("/")) {
+		# url is NOT fully qualified
+		$url64 = ([System.Uri]$releases).scheme + '://' + ([System.Uri]$releases).authority + $url64
+	}	
+	$checksumType	= 'sha256'
+	$checksum64		= Get-RemoteChecksum -Algorithm $checksumType -Url $url64
+	
+	return @{
+		Url64             = $url64
+		Version           = $version
+		Checksum64        = $checksum64
+		ChecksumType64    = $checksumType
+	}
+}
+
+function global:au_SearchReplace {
+  @{
+      'tools\chocolateyInstall.ps1' = @{
+          "(^[$]url64\s*=\s*)('.*')"       	    = "`$1'$($Latest.Url64)'"
+		  "(^[$]version\s*=\s*)('.*')"			= "`$1'$($Latest.Version)'"
+          "(^[$]checksum64\s*=\s*)('.*')"     	= "`$1'$($Latest.Checksum64)'"
+          "(^[$]checksumType64\s*=\s*)('.*')" 	= "`$1'$($Latest.ChecksumType64)'"
+      }
+  }
+}
+
+Update-Package -ChecksumFor 64
