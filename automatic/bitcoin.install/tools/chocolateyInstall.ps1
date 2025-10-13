c1370c580b7151055c82358b041c0f8053bb3e9e
tunisiano187 <helpdesk.choc@gmail.com>
2025-10-13 16:51:32 +0000

    AU: 1 updated - bitcoin.install [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/df4dbcbdcb9ec6bb4b89296bb877d009183ec786

--- automatic/bitcoin.install/tools/chocolateyInstall.ps1
+++ automatic/bitcoin.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = $env:ChocolateyPackageName
 $installerType  = 'exe'
 $silentArgs     = '/S'
-$url64          = 'https://bitcoincore.org/bin/bitcoin-core-27.1/bitcoin-27.1-win64-setup.exe'
-$checksum64     = 'e8ab7a8161c5a0846c81275809c9f90302874cc71b4378dee540f0b380a6d049'
+$url64          = 'https://bitcoincore.org/bin/bitcoin-core-30.0/bitcoin-30.0-win64-setup.exe'
+$checksum64     = '3065d43b57f967687399f9c00d424556d16d33997e1653fdb5bf1934b95168e6'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
