0b397255c5ce85dd8fea61ec2f772bec3023b91d
Tunisiano <helpdesk.choc@gmail.com>
2025-10-19 09:21:16 +0000

    ps1 vars [skip ci]

--- automatic/scratch/update.ps1
+++ automatic/scratch/update.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
-import-module AU
+import-module chocolatey-AU
 $releases = 'https://api.github.com/repos/scratchfoundation/scratch-desktop/releases/latest'
 $Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
 $repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
