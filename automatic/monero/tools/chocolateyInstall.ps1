54dcd06d4f65757880e0f9855144f6a0db44d928
tunisiano187 <helpdesk.choc@gmail.com>
2025-11-19 16:46:04 +0000

    AU: 2 updated - axcrypt monero [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/23c81467d84e3b34483cf44ebd7c47acb87d7720

--- automatic/monero/tools/chocolateyInstall.ps1
+++ automatic/monero/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://downloads.getmonero.org/cli/monero-win-x86-v0.18.4.3.zip'
-$checksum = 'e642ed7bbfa34c30b185387fa553aa9c3ea608db1f3fc0e9332afa9b522c9c1a'
+$url = 'https://downloads.getmonero.org/cli/monero-win-x86-v0.18.4.4.zip'
+$checksum = 'a148a2bd2b14183fb36e2cf917fce6f33fb687564db2ed53193b8432097ab398'
 $checksumType = 'sha256'
-$url64 = 'https://downloads.getmonero.org/cli/monero-win-x64-v0.18.4.3.zip'
-$checksum64 = 'bd9f615657c35d2d7dd9a5168ad54f1547dbf9a335dee7f12fab115f6f394e36'
+$url64 = 'https://downloads.getmonero.org/cli/monero-win-x64-v0.18.4.4.zip'
+$checksum64 = '7eb3b87a105b3711361dd2b3e492ad14219d21ed8fd3dd726573a6cbd96e83a6'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
