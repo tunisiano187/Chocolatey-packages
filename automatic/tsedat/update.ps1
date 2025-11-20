7df9262725d908f8c6f28bf19e8e23dd34d5b4e6
Tunisiano18 <tunisiano187@users.noreply.github.com>
2025-11-20 19:55:43 +0000

    VirusScan

--- automatic/tsedat/update.ps1
+++ automatic/tsedat/update.ps1
@@ -13,7 +13,8 @@ function global:au_SearchReplace {
 }
 
 function global:au_AfterUpdate($Package) {
-  Invoke-VirusTotalScan $Package
+  Import-Module ..\..\scripts\au_extensions.psm1
+	Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
