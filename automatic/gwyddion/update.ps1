78353ebeb0a9259de64f4d911202dc0dcf951f44
Tunisiano187 <helpdesk.choc@gmail.com>
2025-11-28 19:12:09 +0100

    update

--- automatic/gwyddion/update.ps1
+++ automatic/gwyddion/update.ps1
@@ -14,6 +14,16 @@ function global:au_SearchReplace {
 	}
 }
 
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+	$FileVersion = Get-FileVersion $Latest.URL64
+	$Latest.Checksum64 = $FileVersion.Checksum
+	$Latest.ChecksumType64 = $FileVersion.checksumType
+}
+
 function global:au_AfterUpdate($Package) {
 	Invoke-VirusTotalScan $Package
 }
@@ -37,4 +47,4 @@ function global:au_GetLatest {
 	return $Latest
 }
 
-update -NoCheckChocoVersion
+update -NoCheckChocoVersion -ChecksumFor none
