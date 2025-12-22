3b902099f5ef54e817a1356b22a888668908e35b
Tunisiano187 <helpdesk.choc@gmail.com>
2025-12-22 13:55:12 +0100

    ALLUSERS

--- automatic/warzone2100/tools/chocolateyinstall.ps1
+++ automatic/warzone2100/tools/chocolateyinstall.ps1
@@ -1,6 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'warzone2100'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://github.com/Warzone2100/warzone2100/releases/download/4.6.2/warzone2100_win_installer.exe'
 $checksum     = '8d09ec0448b642a9e54f2ee912b04a669127f0ee8fcf3d6aab5aea3e3cc20a0a'
 $checksumType = 'sha256'
@@ -10,12 +9,10 @@ $packageArgs = @{
   fileType       = 'EXE'
   url            = $url
   validExitCodes = @(0)
-  silentArgs     = '/S'
+  silentArgs     = '/S /ALLUSERS=1'
   softwareName   = 'Warzone 2100*'
   checksum       = $checksum
   checksumType   = $checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
-
-Remove-Item $toolsDir\*.exe -EA SilentlyContinue | Out-Null
