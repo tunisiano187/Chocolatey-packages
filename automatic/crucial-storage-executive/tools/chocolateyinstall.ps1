9dbe0b23cc9c18e596ed2a74132f704b9940a74b
tunisiano187 <helpdesk.choc@gmail.com>
2025-12-26 00:33:56 +0000

    AU: 1 updated - crucial-storage-executive [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/ac5b03f3d29c00918d69680e3ab7c67a0784d31e

--- automatic/crucial-storage-executive/tools/chocolateyinstall.ps1
+++ automatic/crucial-storage-executive/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url64                  = 'https://www.crucial.com/content/dam/crucial/support/storage-executive-win-64.zip'
-$checksum64             = ''
-$checksumType64         = ''
-$version				        = '11.05.052025.00'
+$checksum64             = '672BB1ADEE81A02D4791149F97CADBAA59B12A74B0168425ABA7E455C4894355'
+$checksumType64         = 'sha256'
+$version				        = '11.11'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
