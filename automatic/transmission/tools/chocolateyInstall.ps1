7a69c45b4f649f67b5ab87df279426fe162eecf1
tunisiano187 <helpdesk.choc@gmail.com>
2026-01-06 08:35:52 +0000

    AU: 1 updated - transmission [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/e0586fbf5a7e7d8e50c94e4a76bc14450c33556e

--- automatic/transmission/tools/chocolateyInstall.ps1
+++ automatic/transmission/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference  = 'Stop'
-$url                    = 'https://github.com/transmission/transmission/releases/download/4.1.0-beta.4/transmission-4.1.0-beta.4+rc92b42de9c-x86.msi'
-$checksum               = '0e11707e8488126b7850ec4c0f4cd4cd041b3e8b8919154325cf12079eeac4cd'
+$url                    = 'https://github.com/transmission/transmission/releases/download/4.1.0-beta.5/transmission-4.1.0-beta.5+r11d3fc6f5a-x86.msi'
+$checksum               = '5484dd673c1db1bd95a4a51ea5d4dc1c135d9867178107eae722286fad0be995'
 $checksumType           = 'sha256'
-$url64                  = 'https://github.com/transmission/transmission/releases/download/4.1.0-beta.4/transmission-4.1.0-beta.4+rc92b42de9c-x64.msi'
-$checksum64             = 'b52105733c355957ba2b0a865b5decf60ae19f5e3a900bc66e0a63c65de95601'
+$url64                  = 'https://github.com/transmission/transmission/releases/download/4.1.0-beta.5/transmission-4.1.0-beta.5+r11d3fc6f5a-x64.msi'
+$checksum64             = '65be8bc603a54e4352768508ffa62b794a9415b56869daad565599cab4897bf3'
 $checksumType64         = 'sha256'
 
 $packageArgs = @{
