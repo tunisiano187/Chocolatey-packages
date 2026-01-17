084dbe8e7e9fda8f6e2229268f66187172729e72
tunisiano187 <helpdesk.choc@gmail.com>
2026-01-17 08:31:57 +0000

    AU: 1 updated - slade [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/fec3a955963d7f9aebb159bde67374dbc2446149

--- automatic/slade/tools/chocolateyinstall.ps1
+++ automatic/slade/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
 $checksumType = 'sha256'
-$url          = 'https://slade.mancubus.net/files/3.2.10/slade_3.2.10_x64.7z'
-$checksum     = '208a3d27ce2ed03499f68847cb6bf2de2fdf80bd7249c67b55a43dbd90950d70'
+$url          = 'https://slade.mancubus.net/files/3.2.11/slade_3.2.11_x64.7z'
+$checksum     = '69cf7c641cd2f587c58c5835773a4ee5d31137e04fb2a74256682f99c93ee538'
 
 
 $unzipLocation = $installLocation
