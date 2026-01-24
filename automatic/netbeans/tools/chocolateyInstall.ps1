f02349ea27bedb77285c0d0316d7d9a18a665f63
tunisiano187 <helpdesk.choc@gmail.com>
2026-01-24 16:36:01 +0000

    AU: 1 updated - netbeans [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/accf44cca979639d1678c33dc61729684f96eb2a

--- automatic/netbeans/tools/chocolateyInstall.ps1
+++ automatic/netbeans/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url			= 'https://github.com/Friends-of-Apache-NetBeans/netbeans-installers/releases/download/v28-build1/Apache-NetBeans-28.exe'
-$checksum		= 'C85C7376C39B144332969A45037D4DD9424DC508C8F9B71E466DBC5ED9D136F6'
+$url			= 'https://github.com/Friends-of-Apache-NetBeans/netbeans-installers/releases/download/v29-rc1/Apache-NetBeans-29-rc1.exe'
+$checksum		= '9E9DA43CA06F8B00ACE9A241D087AB13E46D714E6E1B0823D4A48011F2397B94'
 $checksumType	= 'sha256'
 $fileType		= 'exe'
 $softwareName	= 'NetBeans*'
