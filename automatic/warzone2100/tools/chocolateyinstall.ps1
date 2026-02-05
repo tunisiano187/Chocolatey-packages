8fe8c52426d5dfae2b4881296c59455b69e5aa00
tunisiano187 <helpdesk.choc@gmail.com>
2026-02-05 00:34:50 +0000

    AU: 1 updated - warzone2100 [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/22fb1f43599c5b78f3855eac86e0c6db751d8fab

--- automatic/warzone2100/tools/chocolateyinstall.ps1
+++ automatic/warzone2100/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'warzone2100'
-$url          = 'https://github.com/Warzone2100/warzone2100/releases/download/4.6.2/warzone2100_win_installer.exe'
-$checksum     = '8d09ec0448b642a9e54f2ee912b04a669127f0ee8fcf3d6aab5aea3e3cc20a0a'
+$url          = 'https://github.com/Warzone2100/warzone2100/releases/download/4.6.3/warzone2100_win_installer.exe'
+$checksum     = '9d64331fceac1d64ca480aaf51c703955b71d2d55f577157625353a56a20d261'
 $checksumType = 'sha256'
 
 $packageArgs = @{
