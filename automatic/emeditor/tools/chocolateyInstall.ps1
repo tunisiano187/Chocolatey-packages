2ec98edaa64fa08b4c0191dad0163a8869aafc47
Tunisiano187 <tunisiano187@gmail.com>
2026-03-01 17:05:54 +0000

    fix(emeditor): add update.ps1 (AU), use placeholder pattern in chocolateyInstall.ps1

--- automatic/emeditor/tools/chocolateyInstall.ps1
+++ automatic/emeditor/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$packageName = 'emeditor'
-$version     = '26.0.3'
-$fileType    = 'msi'
-$installArgs = '/quiet /norestart'
-$url64       = 'https://emeditor.blob.core.windows.net/emed64_26.0.3.msi'
-$checksum64  = '2762ee07156cbc2ad274aa62fe82e701cd6736182eac44c529eae98eb6bc5221'
+$packageName    = 'emeditor'
+$fileType       = 'msi'
+$installArgs    = '/quiet /norestart'
+$url64          = 'https://emeditor.blob.core.windows.net/emed64_26.0.3.msi'
+$checksum64     = '2762ee07156cbc2ad274aa62fe82e701cd6736182eac44c529eae98eb6bc5221'
+$checksumType64 = 'sha256'
 
 Install-ChocolateyPackage $packageName $fileType $installArgs `
-    -url64bit $url64 `
-    -checksum64 $checksum64 `
-    -checksumType64 'sha256'
+    -url64bit       $url64 `
+    -checksum64     $checksum64 `
+    -checksumType64 $checksumType64
