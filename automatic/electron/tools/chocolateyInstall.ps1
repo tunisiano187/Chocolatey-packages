ebb605544ad12b31910350175390ee4a8cf2bb0f
tunisiano187 <helpdesk.choc@gmail.com>
2026-03-04 00:53:33 +0000

    AU: 2 updated - electron ntlite-free [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/e64bb8b3d407a0b5031a3601240d17b0883a5df5

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$url = 'https://github.com/electron/electron/releases/download/v40.6.1/electron-v40.6.1-win32-ia32.zip'
-$checksum = 'a37a6041977b43d095eed202fd8be98732713154904de3fd532ab4789df579ac'
+$url = 'https://github.com/electron/electron/releases/download/v40.7.0/electron-v40.7.0-win32-ia32.zip'
+$checksum = 'a4394e1148f61234245aa04ce8aee94dc9739f5d51f7338ac35d01c39f50c56b'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/electron/electron/releases/download/v40.6.1/electron-v40.6.1-win32-x64.zip'
-$checksum64 = '236d75251b430ee7a56e8d85ffd6c343e6c7f3db217c3fbd86e6a773e24e78fa'
+$url64 = 'https://github.com/electron/electron/releases/download/v40.7.0/electron-v40.7.0-win32-x64.zip'
+$checksum64 = '4639e33056d65ab4cd205032455e4efd6f9ef0d12b6b2a11f149ddb2584bb7ec'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
--- automatic/ntlite-free/tools/chocolateyinstall.ps1
+++ automatic/ntlite-free/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://downloads.ntlite.com/files/NTLite_setup_x86_legacy.exe'
-$checksum       = 'a23a709463b68ec610712b97830a07981756721c32ba31b77ca16dde41947415'
+$checksum       = '6b3e89ac792dca30f205aebbd6b76a4ab26cfe07481b24b8bfc767070039f252'
 $checksumType   = 'sha256'
 $url64          = 'https://downloads.ntlite.com/files/NTLite_setup_x64.exe'
-$checksum64     = '4b1cc17c4834a4c5794751e2a73a23b2680547910415d4605c2bb697286c39a0'
+$checksum64     = '28364a45bbfecf8174908588c9fe751b8d3fa633bbfcf88a79ddc90596716e6f'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
