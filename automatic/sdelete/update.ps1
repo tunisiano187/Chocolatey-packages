a7f022e0d9b11b24d4bb7aa9d21ee1af80b22948
Tunisiano187 <helpdesk.choc@gmail.com>
2026-03-07 16:46:13 +0100

    VT

--- automatic/sdelete/update.ps1
+++ automatic/sdelete/update.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Stop'
 import-module chocolatey-AU
+Import-Module ..\..\scripts\au_extensions.psm1
 
 $releases = 'https://docs.microsoft.com/en-us/sysinternals/downloads/sdelete'
 
