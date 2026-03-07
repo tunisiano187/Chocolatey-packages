6c8fecf3cd1a91a803986a559eb741ac56368ee1
Tunisiano187 <helpdesk.choc@gmail.com>
2026-03-07 17:02:48 +0100

    First commit

--- automatic/belarc-advisor/tools/ChocolateyInstall.ps1
+++ automatic/belarc-advisor/tools/ChocolateyInstall.ps1
@@ -1,8 +1,19 @@
-$packageName = 'belarc-advisor'
-$fileType = 'exe'
-$url = 'http://www.belarc.com/Programs/advisorinstaller.exe'
-$silentArgs = '/S'
+$packageName    = 'belarc-advisor'
+$url            = 'https://www2.aomeisoftware.com/download/adb/ABServerTrial.exe'
+$checksum       = 'FCA81EC6930922351FB98C429D76164A0D82152F9BBE5D2E92F500CA83C43352'
+$checksumType   = 'sha256'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = "$packageName*"
+  checksum      = $checksum
+  checksumType  = $checksumType
+  silentArgs    = '/S'
+  validExitCodes= @(0,1)
+}
+
+Install-ChocolateyPackage @packageArgs
 
 
---
+++ automatic/belarc-advisor/update.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+import-module chocolatey-AU
+Import-Module ..\..\scripts\au_extensions.psm1
+
+$url32 = "http://www.belarc.com/Programs/advisorinstaller.exe"
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+        }
+     }
+}
+
+function global:au_AfterUpdate($Package) {
+	Invoke-VirusTotalScan $Package
+}
+
+function global:au_GetLatest {
+	$FileVersion = Get-FileVersion $url32
+    $version = $FileVersion.Version
+    $Checksum32 = $FileVersion.Checksum
+
+    $Latest = @{ URL32 = $url32; Version = $version; Checksum32 = $Checksum32; ChecksumType32 = $FileVersion.ChecksumType }
+    return $Latest
+}
+
+update -ChecksumFor none
