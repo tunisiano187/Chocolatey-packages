2bac5421261cab2e1caf16b27968c483d0d01326
tunisiano187 <helpdesk.choc@gmail.com>
2026-03-09 00:31:53 +0000

    AU: 2 updated - ntlite-free ultracopier [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/df7e1931a9be390084e493b56c934bcee26f9830

--- automatic/ntlite-free/tools/chocolateyinstall.ps1
+++ automatic/ntlite-free/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://downloads.ntlite.com/files/NTLite_setup_x86_legacy.exe'
-$checksum       = 'd93526b50f9ccb61cac58db3255fb2c1f747265143f474f725e95cd581c036f1'
+$checksum       = 'c5be525b0482834f3630fbc63732072638aaa5a4420b1c6cada9dd5bbaa2135f'
 $checksumType   = 'sha256'
 $url64          = 'https://downloads.ntlite.com/files/NTLite_setup_x64.exe'
-$checksum64     = '4f4ebbafb685d22a6156907e1e454243724fa52d1c9f2a5b24466ac3b9f94259'
+$checksum64     = 'f7c4dc456c3c3edefc951f30cc30f84171e4684ecf2e09f89832845e9e8d82e9'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
