26a16cf8a77ad5d82d8b11748e8259618fe6cf77
Tunisiano <helpdesk.choc@gmail.com>
2026-03-15 15:41:35 +0000

    ps1 vars [skip ci]

--- automatic/tonido-sync/tools/chocolateyinstall.ps1
+++ automatic/tonido-sync/tools/chocolateyinstall.ps1
@@ -1,20 +1,20 @@
-﻿$packageName    = 'tonido-sync'
+$packageName    = 'tonido-sync'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'http://patch.codelathe.com/tonido/live/installer/x86-win32/TonidoSync2Setup.exe' 
+$url            = 'http://patch.codelathe.com/tonido/live/installer/x86-win32/TonidoSync2Setup.exe'
 $checksum       = 'EE0981B6ED53BB809323AABEF3DBF77D1668D5727004341212D2033685117373'
-$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-$validExitCodes = @(0) 
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE' 
+  fileType      = 'EXE'
   url           = $url
-  silentArgs    = $silentArgs  
+  silentArgs    = $silentArgs
   validExitCodes= $validExitCodes
   softwareName  = 'TonidoSync2 *'
   checksum      = $checksum
-  checksumType  = 'sha256'   
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
