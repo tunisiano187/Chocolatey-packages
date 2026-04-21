b91d993e4f3112c06b0bfe92415ac61a9f3f2108
Tunisiano187 <helpdesk.choc@gmail.com>
2026-04-21 19:04:31 +0200

    VT

--- automatic/jbs/update.ps1
+++ automatic/jbs/update.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Stop'
 import-module chocolatey-AU
+Import-Module ..\..\scripts\au_extensions.psm1
 
 $releases = 'https://johnsad.ventures/software/backgroundswitcher/windows/'
 $padVersionUnder = '5.4.0.3'
