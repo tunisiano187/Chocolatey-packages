36ca2c6389851143b50df548ccbacb39f192c5c2
Tunisiano187 <helpdesk.choc@gmail.com>
2026-05-03 20:03:46 +0200

    2.71

--- automatic/gwyddion/tools/ChocolateyInstall.ps1
+++ automatic/gwyddion/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://faceit-client.faceit-cdn.net/release/FACEIT-setup-latest.exe'
-$checksum       = '7ef489ca4fc9aa00f627d6712b6d63576624546cc05d007fd053415e33aa2f43'
-$checksumType   = 'sha256'
-$url64          = 'https://faceit-client.faceit-cdn.net/release/FACEIT-setup-latest.exe'
-$checksum64     = '7ef489ca4fc9aa00f627d6712b6d63576624546cc05d007fd053415e33aa2f43'
+$url            = 'https://sourceforge.net/projects/gwyddion/files/gwyddion/2.71/Gwyddion-2.71.win32.exe/download'
+$checksum       = 'DE0D6185930B8B64A5550E9CCBFB70F40E60B043B5A59475D5CF6FFC5BFFE4EA4603685EA96CB9BFCD89299EC7A4C5BEB6A6BCF9DC2B83364578CFE5FC8EAD91'
+$checksumType   = 'SHA512'
+$url64          = 'https://sourceforge.net/projects/gwyddion/files/gwyddion/2.71/Gwyddion-2.71.win64.exe/download'
+$checksum64     = '0488D5BAD96683CDEABBAAABD0FC9FE8B0263B2A3FCCF5A47ED485FE6171949A3A9F429C67D38A12D281E366F39F4F3F70A57A14D18F5375BC89BB508F73D730'
 
 $packageArgs = @{
   packageName    = $packageName
