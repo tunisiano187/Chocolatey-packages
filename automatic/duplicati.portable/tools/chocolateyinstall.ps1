94fcb3f6d211349116eeb9a12c870de9a9c456db
tunisiano187 <helpdesk.choc@gmail.com>
2026-05-05 00:43:26 +0000

    AU: 1 updated - duplicati.portable [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/461869b733ab6b81a77c11ffe2fbea869becbd8d

--- automatic/duplicati.portable/tools/chocolateyinstall.ps1
+++ automatic/duplicati.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.3.0.100_canary_2026-04-23/duplicati-2.3.0.100_canary_2026-04-23-win-x64-gui.zip'
-$checksum       = 'ed35c1d7db648f24699a8c4dcbe326046a3dfd5618a1202d0458277e740b146f'
+$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.3.0.101_canary_2026-05-04/duplicati-2.3.0.101_canary_2026-05-04-win-x64-gui.zip'
+$checksum       = '39e20b89fa9da04789ce825b2837bb95fef7e51780a61c7f53f75170afa31bd7'
 $checksumType   = 'sha256'
 
 
