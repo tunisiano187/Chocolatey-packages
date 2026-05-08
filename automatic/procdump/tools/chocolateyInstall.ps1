7cff5e691545f5a59511702b10851f23ac993206
tunisiano187 <helpdesk.choc@gmail.com>
2026-05-08 00:42:44 +0000

    AU: 1 updated - procdump [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/b70cacec207c460189f8a6c969c21d95178828d6

--- automatic/procdump/tools/chocolateyInstall.ps1
+++ automatic/procdump/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $url = 'https://download.sysinternals.com/files/Procdump.zip'
-$checksum = '863e7f078b35327cedc5a1101c60e26c2a9e7f76388d0d0ffe44018b381784b4'
+$checksum = '28588d9cfd3354a3a71204a570b9b60129761962f505b3cfe26daa9a5175c14a'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage -PackageName "$packageName" `
