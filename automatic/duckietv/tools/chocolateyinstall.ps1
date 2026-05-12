a207983a962e6033d5699545f27c48ca470157a0
tunisiano187 <helpdesk.choc@gmail.com>
2026-05-12 09:14:18 +0000

    AU: 1 updated - duckietv [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/1d7a5e6fb07f13dd909eff7e97622538a2ff705e

--- automatic/duckietv/tools/chocolateyinstall.ps1
+++ automatic/duckietv/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName = 'duckietv'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-202603251838/DuckieTV-202603251838-windows-x32.zip'
-$url64 = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-202603251838/DuckieTV-202603251838-windows-x64.zip'
-$checksum = '9c1239be05eac696eee878083c4264b10256cf2a87f11b70c88fde70efb44fb7'
-$checksum64 = '5fdb9797bec565d0a8ac1794eeb29188b094f698402da8a67f2d29363a3c42e8'
+$url = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-202605121341/DuckieTV-202605121341-windows-x32.zip'
+$url64 = 'https://github.com/DuckieTV/Nightlies/releases/download/nightly-202605121341/DuckieTV-202605121341-windows-x64.zip'
+$checksum = '69d3e3baae13a70e28c0389ded846e62ccda445d1f795febad2fccc9b303d978'
+$checksum64 = '65dd883b78515699924ced7834e2b06c4b105fae0e57fe9407037530ec28e5fb'
 $checksumType = 'sha256'
 $silentArgs = '/S'
 $validExitCodes = @(0)
