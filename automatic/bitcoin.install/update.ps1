da8ee218df016988c69fed855514d20bee75e418
GitHub Action <action@github.com>
2026-05-17 21:14:16 +0200

    Fix bitcoin.install and bitcoin.portable to use GitHub releases API (CHO-77)
    Replace fragile bitcoincore.org HTML scraping with direct GitHub releases API
    calls, consistent with the fix already applied to bitcoin/update.ps1.
    
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- automatic/bitcoin.install/update.ps1
+++ automatic/bitcoin.install/update.ps1
@@ -2,38 +2,34 @@ $ErrorActionPreference = 'Stop'
 import-module Chocolatey-AU
 Import-Module ..\..\scripts\au_extensions.psm1
 
-$releases = 'https://github.com/bitcoin/bitcoin/releases/latest'
-$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
-$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
-
 if ($MyInvocation.InvocationName -ne '.') {
     function global:au_SearchReplace {
         @{
             'tools/chocolateyInstall.ps1' = @{
                 "(^[$]url64\s*=\s*)('.*')"            = "`$1'$($Latest.URL64)'"
                 "(^[$]checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
-                "(^[$]checksumType64\s*=\s*)('.*')"       = "`$1'$($Latest.ChecksumType64)'"
+                "(^[$]checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
             }
         }
     }
 }
 
 function global:au_AfterUpdate($Package) {
-	Invoke-VirusTotalScan $Package
+    Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
-    Write-Verbose 'Get files'
-	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
-    Update-Metadata -key "releaseNotes" -value $tags.html_url
+    $apiUrl = 'https://api.github.com/repos/bitcoin/bitcoin/releases/latest'
+    $release = Invoke-RestMethod -Uri $apiUrl -UseBasicParsing
+    $version = $release.tag_name -replace '^v', ''
+    Update-Metadata -key "releaseNotes" -value $release.html_url
 
-    Write-Verbose 'Get version'
-    [version]$version = $tags.name.Split(' ')[-1]
-    $folder = "https://bitcoincore.org/bin/$(((Invoke-WebRequest -Uri 'https://bitcoincore.org/bin/' -UseBasicParsing).Links | Where-Object {$_.href -match $version} | Select-Object -Last 1).href)"
+    $url64 = $release.assets.browser_download_url |
+        Where-Object { $_ -match 'win64.*\.exe$' } |
+        Where-Object { $_ -notmatch 'unsigned' } |
+        Select-Object -First 1
 
-	Write-Verbose 'Get files'
-	$file = $(((Invoke-WebRequest -Uri $folder -UseBasicParsing).Links | Where-Object {$_.href -match ".exe"} | Where-Object {$_.href -notmatch "unsigned" }).href)
-    $url64 = "$folder$file"
+    if (-not $url64) { throw "Could not find win64 installer asset in bitcoin release $version" }
 
     return @{ URL64 = $url64; Version = $version }
 }
--- automatic/bitcoin.portable/update.ps1
+++ automatic/bitcoin.portable/update.ps1
@@ -2,10 +2,6 @@ $ErrorActionPreference = 'Stop'
 import-module Chocolatey-AU
 Import-Module ..\..\scripts\au_extensions.psm1
 
-$releases = 'https://github.com/bitcoin/bitcoin/releases/latest'
-$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
-$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
-
 if ($MyInvocation.InvocationName -ne '.') {
     function global:au_SearchReplace {
         @{
@@ -19,23 +15,22 @@ if ($MyInvocation.InvocationName -ne '.') {
 }
 
 function global:au_AfterUpdate($Package) {
-	. ..\..\scripts\Invoke-VirusTotalScan.ps1
-	Invoke-VirusTotalScan $Package
+    Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
-    Write-Verbose 'Get files'
-	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
-    Update-Metadata -key "releaseNotes" -value $tags.html_url
+    $apiUrl = 'https://api.github.com/repos/bitcoin/bitcoin/releases/latest'
+    $release = Invoke-RestMethod -Uri $apiUrl -UseBasicParsing
+    $version = $release.tag_name -replace '^v', ''
+    Update-Metadata -key "releaseNotes" -value $release.html_url
 
-    Write-Verbose 'Get version'
-    $version = $tags.name.Split(' ')[-1]
-    $folder = "https://bitcoincore.org/bin/$(((Invoke-WebRequest -Uri 'https://bitcoincore.org/bin/' -UseBasicParsing).Links | Where-Object {$_.href -match $version} | Select-Object -Last 1).href)"
+    $url64 = $release.assets.browser_download_url |
+        Where-Object { $_ -match 'win64.*\.zip$' } |
+        Where-Object { $_ -notmatch 'debug' } |
+        Where-Object { $_ -notmatch 'unsigned' } |
+        Select-Object -First 1
 
-	Write-Verbose 'Get files'
-	$file = $(((Invoke-WebRequest -Uri $folder -UseBasicParsing).Links | Where-Object {$_.href -match ".zip"} | Where-Object {$_.href -match "win64"} | Where-Object {$_.href -notmatch "debug" } | Where-Object {$_.href -notmatch "unsigned" }).href)
-    $url64 = "$folder$file"
-	#$url64 = "https://github.com/$(((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links | Where-Object {$_ -match ".zip"} | Select-Object -First 1).href)"
+    if (-not $url64) { throw "Could not find win64 zip asset in bitcoin release $version" }
 
     return @{ URL64 = $url64; Version = $version }
 }
