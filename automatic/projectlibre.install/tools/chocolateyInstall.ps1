71c6438432f88bb6919c564eb2e6e19f03a8cf9a
GitHub Action <action@github.com>
2026-05-19 21:44:36 +0200

    Fix projectlibre.install: fill empty checksum (SHA256 verified for v1.9.8 MSI)
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- automatic/projectlibre.install/tools/chocolateyInstall.ps1
+++ automatic/projectlibre.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $url          = 'https://downloads.sourceforge.net/project/projectlibre/ProjectLibre/1.9.8/ProjectLibre-1.9.8.msi'
-$checksum     = ''
+$checksum     = '32e46ba3ce7b3a81dc8ec648f6baf8f6f6056c8cf9203f75f8187e9e4637833f'
 $checksumType = 'sha256'
 
 $packageArgs = @{
