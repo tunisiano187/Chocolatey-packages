62a0615a6b53ec11acdeb8e990fb713b352b8d53
tunisiano187 <helpdesk.choc@gmail.com>
2026-05-22 15:20:59 +0000

    AU: 2 updated - advanced-renamer duplicati.portable [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/51fd8d2891f3c98add8eca21dbe4f1ca35f821fe

--- automatic/duplicati.portable/tools/chocolateyinstall.ps1
+++ automatic/duplicati.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.3.0.102_canary_2026-05-09/duplicati-2.3.0.102_canary_2026-05-09-win-x64-gui.zip'
-$checksum       = '947f908189679de1f5c514446c4ce0b54605e2daafeadde6c6e8227c6a85176e'
+$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.3.0.103_canary_2026-05-22/duplicati-2.3.0.103_canary_2026-05-22-win-x64-gui.zip'
+$checksum       = '463c259360977c7bba91aec99063b31c9f6cc47985cceb6694450b8aea334ae5'
 $checksumType   = 'sha256'
 
 
