845dd04db59665a6ff4e754fb85c93aabe9057be
tunisiano187 <helpdesk.choc@gmail.com>
2026-05-24 16:33:15 +0000

    AU: 2 updated - Executor realsense-sdk2 [skip ci]

--- automatic/realsense-sdk2/tools/chocolateyInstall.ps1
+++ automatic/realsense-sdk2/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $pp = Get-PackageParameters
 
 $packageName    = $env:ChocolateyPackageName
-$url            = 'https://github.com/realsenseai/librealsense/releases/download/v2.57.7/RealSense.SDK-WIN10-2.57.7.10378.exe'
-$checksum       = '886e9f7879594a0dcd3b2e239de7fd9a74faba1b1118b4445b3a3cc22a9e371f'
+$url            = 'https://github.com/realsenseai/librealsense/releases/download/v2.58.1/RealSense.SDK-WIN10-2.58.1.10581.exe'
+$checksum       = '70397b7165e057c9e94c0398da97d7592e05e4793aaa23c1c3a277be984ccc70'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
