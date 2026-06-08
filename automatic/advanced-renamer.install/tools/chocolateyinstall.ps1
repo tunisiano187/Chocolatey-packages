5f07325304c1cd897691acd211f7a878b16613cb
tunisiano187 <helpdesk.choc@gmail.com>
2026-06-08 08:42:31 +0000

    AU: 1 updated - advanced-renamer.install [skip ci]

--- automatic/advanced-renamer.install/tools/chocolateyinstall.ps1
+++ automatic/advanced-renamer.install/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://www.advancedrenamer.com/down/win/advanced_renamer_setup_4_21.exe'
+$url           = 'https://www.advancedrenamer.com/down/win/advanced_renamer_setup_4_23.exe'
 $checksumType  = 'sha256'
-$checksum      = '036E038DF7987A2C658B672E7E5D001507ACB52B9A1CFF83E338CA0774B9FA89'
+$checksum      = '839723E3CC9971A9208CD38D1CF551B780802989669130A41687F090DCE2B4F3'
 
 
 $packageArgs = @{
