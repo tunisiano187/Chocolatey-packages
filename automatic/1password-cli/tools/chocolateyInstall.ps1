6c42b2be6bbed3bb8602c2bd58028d6ee3fcfade
tunisiano187 <helpdesk.choc@gmail.com>
2026-06-12 00:41:32 +0000

    AU: 1 updated - 1password-cli [skip ci]

--- automatic/1password-cli/tools/chocolateyInstall.ps1
+++ automatic/1password-cli/tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@
 
 $packageName      = '1password-cli'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url              = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.34.0/op_windows_386_v2.34.0.zip'
-$checksum         = 'DB46E206C3316EFCD0B26F03F78F57C20AA1C763CF99301802E84621B41A80CF'
+$url              = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.34.1/op_windows_386_v2.34.1.zip'
+$checksum         = 'ED9708FCE28D2313078FEBEC90D90748A89A5B3F4555799CE28C2789527DD086'
 $checksumType     = 'sha256'
-$url64            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.34.0/op_windows_amd64_v2.34.0.zip'
-$checksum64       = 'EA1FDEA5BC52A8CFF3F6B502C03EC5A567CD6E97D38FAC35C511841E7DFD67FC'
+$url64            = 'https://cache.agilebits.com/dist/1P/op2/pkg/v2.34.1/op_windows_amd64_v2.34.1.zip'
+$checksum64       = '25B25226AE1B5E1D6AA42EE8B9C6D1B956797E495A989B458F1DCC23D17611BF'
 $checksumType64   = 'sha256'
 
 $packageArgs = @{
