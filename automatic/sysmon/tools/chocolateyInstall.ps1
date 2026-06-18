af4b3a0a0b2e74fedc897ae1e5688e7ef389bf6a
tunisiano187 <helpdesk.choc@gmail.com>
2026-06-18 00:44:03 +0000

    AU: 1 updated - sysmon [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/4f4a5f974f886a12d3227d6b7e39285588fb8dd6

--- automatic/sysmon/tools/chocolateyInstall.ps1
+++ automatic/sysmon/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
 $url = 'https://download.sysinternals.com/files/Sysmon.zip'
-$checksum = '9c7bba9c88023abd33188cff4d3562278e0b7ddccb7935b6a6530887f9e054b1'
+$checksum = '6d48089c7fae14944c82b06767b79ccba3cc26d13218a4227ed28c90f80d0f0e'
 $checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
