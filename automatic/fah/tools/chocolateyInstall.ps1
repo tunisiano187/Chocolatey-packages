e09a09cb59d512657bb07bae7247771d8cf442d9
tunisiano187 <helpdesk.choc@gmail.com>
2026-07-13 16:51:47 +0000

    AU: 2 updated - fah nmm [skip ci]

--- automatic/fah/tools/chocolateyInstall.ps1
+++ automatic/fah/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $packageName = $env:ChocolateyPackageName
 $installerType = 'exe'
 $silentArgs = '/S'
-$url32       = 'https://download.foldingathome.org/releases/public/fah-client/windows-10-64bit/release/fah-client_8.5.5_AMD64.exe'
-$checksum32 = '718840240414eb065968a7a1a64b636f06857ea9ac284fbe4828cf21cd227ec9'
+$url32       = 'https://download.foldingathome.org/releases/public/fah-client/windows-10-64bit/debug/fah-client_8.5.6_AMD64.exe'
+$checksum32 = '8342f64f6f3280dbec18cde724db755d915c003cffd393d09f0877ac96c8c3ed'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
 
