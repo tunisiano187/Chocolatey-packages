58f1028eb664aa552b2167b31c9289eb208f49ee
tunisiano187 <helpdesk.choc@gmail.com>
2026-07-26 00:43:33 +0000

    AU: 1 updated - ollama [skip ci]

--- automatic/ollama/tools/chocolateyInstall.ps1
+++ automatic/ollama/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = 'https://github.com/ollama/ollama/releases/download/v0.32.3/OllamaSetup.exe'
-$checksum       = '78bc7e1f750882fcee9948561700bf881eb7b30bec95fd37934e6285a368b8b2'
+$url            = 'https://github.com/ollama/ollama/releases/download/v0.32.4/OllamaSetup.exe'
+$checksum       = 'cc1458d8af9a7ae0bb75ffa3b20d6c84afed98851f814384f99eab605b8b3299'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
