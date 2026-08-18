d3bf8aa9b00b4922822d6db863084d669f32f587
tunisiano187 <helpdesk.choc@gmail.com>
2026-08-18 00:36:07 +0000

    AU: 2 updated - nteract.install xmedia-recode [skip ci]

--- automatic/nteract.install/tools/chocolateyInstall.ps1
+++ automatic/nteract.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName  = $env:ChocolateyPackageName
-$url          = 'https://github.com/nteract/nteract/releases/download/v2.6.2-stable.202606300322/nteract-stable-windows-x64.exe'
-$checksum     = '62b4a24d65a21a8a5ec3077d9fa2a4bdd326c2a5ebd901b883bc436ca106d65d'
+$url          = 'https://github.com/nteract/nteract/releases/download/v2.7.0-stable.202608172336/nteract-stable-windows-x64.exe'
+$checksum     = 'd6eb760da4b01553ee597fa1da3fe00f16115bf6d1ee5cab759afb73be15187d'
 $checksumType = 'sha256'
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
