58482c47744c697ad54b02e61648ffa3470594d6
tunisiano187 <helpdesk.choc@gmail.com>
2026-08-23 07:32:22 +0000

    AU: 2 updated - nmm nteract.install [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/4acdcf2bf85fb2125b92841a5f438e5ecc54d53c

--- automatic/nteract.install/tools/chocolateyInstall.ps1
+++ automatic/nteract.install/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName  = $env:ChocolateyPackageName
-$url          = 'https://github.com/nteract/nteract/releases/download/v2.7.1-stable.202608192148/nteract-stable-windows-x64.exe'
-$checksum     = 'a8dfc8cb118baa5ebbc00d794f337676c2aa648e1e8c0a89a7885efd87d6aba4'
+$url          = 'https://github.com/nteract/nteract/releases/download/v2.7.3-stable.202608230344/nteract-stable-windows-x64.exe'
+$checksum     = '9be13e049bf8e6430c4d9b9a0862ebc1ac04ccbcedb6bae6d3553a59dfd1cdf5'
 $checksumType = 'sha256'
 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
