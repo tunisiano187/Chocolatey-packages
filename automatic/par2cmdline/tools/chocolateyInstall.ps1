2cb99c2f1cf5db4930eb0a722685b2ef165fc8a7
tunisiano187 <helpdesk.choc@gmail.com>
2026-09-09 12:17:43 +0000

    AU: 1 updated - par2cmdline [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/2dd491b2013407ee6f837e5bb065075d85fe9de9

--- automatic/par2cmdline/tools/chocolateyInstall.ps1
+++ automatic/par2cmdline/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$url            = 'https://github.com/Parchive/par2cmdline/releases/download/v1.3.0/par2cmdline-1.3.0-win-x64.zip'
-$checksum       = '4164d2e689b6bca6a14c4d4a303582ff40307b5739737f1d29695b65002b13ea'
+﻿$url            = 'https://github.com/Parchive/par2cmdline/releases/download/v1.4.0/par2cmdline-1.4.0-win-x64.zip'
+$checksum       = '200130143dba1e12f9d1be567d5a446e785d2093e5d735225d8ad0539125b71c'
 $checksumType   = 'sha256'
-$url64          = 'https://github.com/Parchive/par2cmdline/releases/download/v1.3.0/par2cmdline-1.3.0-win-x64.zip'
-$checksum64     = '4164d2e689b6bca6a14c4d4a303582ff40307b5739737f1d29695b65002b13ea'
+$url64          = 'https://github.com/Parchive/par2cmdline/releases/download/v1.4.0/par2cmdline-1.4.0-win-x64.zip'
+$checksum64     = '200130143dba1e12f9d1be567d5a446e785d2093e5d735225d8ad0539125b71c'
 $checksumType64 = 'sha256'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
