1b6135ea5e19c60846b1325c4cc7d5cb492a6ab0
tunisiano187 <helpdesk.choc@gmail.com>
2026-09-10 16:36:10 +0000

    AU: 2 updated - drivermax protonpass [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/0c461469ccbdc00c3e1930640ba59f0893d27c02

--- automatic/protonpass/tools/chocolateyinstall.ps1
+++ automatic/protonpass/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://proton.me/download/pass/windows/ProtonPass_1.40.0.msix'
-$checksum64     = 'F8D57BBFEB1A3D9D39CC29769E46BA078885505E2F6360D426E192A911C26BA6'
+$url64          = 'https://proton.me/download/pass/windows/ProtonPass_1.40.2.msix'
+$checksum64     = '7B3FFC1A35E043DAC5768067D3E03F07C3BB8C4A234FA77094311CB7EA299888'
 $checksumType64 = 'sha256'
 
 if ((Get-IsWinServer)) {
