22a12c4e468990481e3de2ec5df89546917dc03d
tunisiano187 <helpdesk.choc@gmail.com>
2026-09-14 17:01:20 +0000

    AU: 1 updated - ultracopier [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/63eedab754dc81c8b13e5c7e8b2d645c2f3aceaf

--- automatic/ultracopier/tools/chocolateyInstall.ps1
+++ automatic/ultracopier/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:ChocolateyPackageName
-$url32='https://cdn.confiared.com/ultracopier.herman-brule.com/files/3.1.0.4/ultracopier-windows-x86-3.1.0.4-setup.exe'
-$url64='https://cdn.confiared.com/ultracopier.herman-brule.com/files/3.1.0.4/ultracopier-windows-x86_64-3.1.0.4-setup.exe'
-$checksum32='2e1b6c5ceb02026ca4aee6a7a167209f48f246a4d24c826cd70f8a061bfd6b5b'
-$checksum64='4e1aca2d534635948ae40245308af29ed4895ffbef3f8dabf860b9635c24a93c'
+$url32='https://cdn.confiared.com/ultracopier.herman-brule.com/files/3.1.0.5/ultracopier-windows-x86-3.1.0.5-setup.exe'
+$url64='https://cdn.confiared.com/ultracopier.herman-brule.com/files/3.1.0.5/ultracopier-windows-x86_64-3.1.0.5-setup.exe'
+$checksum32='bed02a05f1ce5a1323c4d17f816c69e9f2748b69f26c919b308731d167ad3909'
+$checksum64='f1437e440180de288c1ae4be2c9efd4f1a74a95803d47943be77e41fa5b7fe57'
 $checksumType='sha256'
 
 $packageArgs = @{
