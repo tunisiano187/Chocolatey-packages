146567b9c152a138b2717ccca8b692e50856c0b1
tunisiano187 <helpdesk.choc@gmail.com>
2026-09-19 00:44:17 +0000

    AU: 1 updated - electron [skip ci] https://gist.github.com/tunisiano187/7032dea203873812b435f152ee3794bf/c0eb9d6cbcf22e56a7f4c2aaf0d58ce894a41adc

--- automatic/electron/tools/chocolateyInstall.ps1
+++ automatic/electron/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 # Electron stopped publishing a win32-ia32 (32-bit Windows) build as of v44 -- 64-bit only now.
-$url = 'https://github.com/electron/electron/releases/download/v44.4.2/electron-v44.4.2-win32-x64.zip'
-$checksum = '6aae435b6cd5c0eedf9fd38824bae4045ffdaecd029f0b8c8328bac3f5b71f03'
+$url = 'https://github.com/electron/electron/releases/download/v44.4.3/electron-v44.4.3-win32-x64.zip'
+$checksum = '790a355b684d5c7cc8dc3cdd8c4cca7c4b2d054685427c7554a956879a82e70b'
 $checksumType = 'sha256'
 
 $packageArgs = @{
